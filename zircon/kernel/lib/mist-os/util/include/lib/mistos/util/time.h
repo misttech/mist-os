f4fe4f7f9110947a34c74e60d101589e24f7d3a7
Bruno Herrera <bruno.herrera@mist-os.com>
2024-04-22 18:59:27 -0300

    [mistos][util] Fix include guard for time.h

--- zircon/kernel/lib/mist-os/util/include/lib/mistos/util/time.h
+++ zircon/kernel/lib/mist-os/util/include/lib/mistos/util/time.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
-#define ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
+#ifndef ZIRCON_KERNEL_LIB_MIST_OS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
+#define ZIRCON_KERNEL_LIB_MIST_OS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
 
 #include <zircon/types.h>
 
@@ -17,4 +17,4 @@ zx_time_t zx_clock_get_monotonic(void);
 }
 #endif
 
-#endif  // ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
+#endif  // ZIRCON_KERNEL_LIB_MIST_OS_UTIL_INCLUDE_LIB_MISTOS_UTIL_TIME_H_
