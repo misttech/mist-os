776772719dedc3b94238977f3bc2deb9cf1c2c82
Bruno Herrera <bruno.herrera@mist-os.com>
2024-04-24 15:51:29 -0300

    [mistos][zx] Add more vmo clone tests

---
+++ zircon/kernel/lib/mistos/zx/tests/helpers.cc
@@ -0,0 +1,78 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "helpers.h"
+
+#include <lib/boot-options/boot-options.h>
+#include <lib/zx/result.h>
+#include <lib/zx/vmo.h>
+#include <unistd.h>
+
+#include <zxtest/zxtest.h>
+
+namespace vmo_test {
+
+zx::result<PhysVmo> GetTestPhysVmo(size_t size) {
+  // We cannot create any physical VMOs without the mmio resource.
+  zx::unowned_resource mmio_resource = maybe_standalone::GetMmioResource();
+  if (!mmio_resource->is_valid()) {
+    return zx::error_result(ZX_ERR_NOT_SUPPORTED);
+  }
+
+  // Fetch the address of the test reserved RAM region.  Even with the mmio
+  // resource, we cannot use zx_vmo_create_physical to create a VMO which
+  // points to RAM unless someone passed a kernel command line argument telling
+  // the kernel to reserve a chunk of RAM for this purpose.
+  //
+  // If a chunk of RAM was reserved, the kernel will publish its size and
+  // physical location in the boot options.  If we have access to the mmio
+  // resource, it is because we are running in the core-tests.zbi.  The boot
+  // options command line arguments should be available to us as a VMO.
+  //
+  // This is an all-or-nothing thing.  If we have the mmio resource, then we
+  // should also have some RAM reserved for running these tests.  If we have
+  // the mmio resource, but _don't_ have any reserved RAM, it should be
+  // considered a test error.
+
+  RamReservation ram;
+  const BootOptions* boot_options = maybe_standalone::GetBootOptions();
+  EXPECT_TRUE(boot_options->test_ram_reserve);
+  ram = *boot_options->test_ram_reserve;
+  EXPECT_TRUE(ram.paddr.has_value());
+  if (!ram.paddr) {
+    return zx::error_result(ZX_ERR_NO_RESOURCES);
+  }
+
+  PhysVmo ret = {.addr = *ram.paddr, .size = ram.size};
+  if (size > 0) {
+    if (size > ret.size) {
+      return zx::error_result(ZX_ERR_INVALID_ARGS);
+    }
+    ret.size = size;
+  }
+
+  // Go ahead and create the VMO itself.
+  zx_status_t res = zx::vmo::create_physical(*mmio_resource, ret.addr, ret.size, &ret.vmo);
+  EXPECT_OK(res);
+  if (res != ZX_OK) {
+    return zx::error_result(res);
+  }
+
+  return zx::ok(std::move(ret));
+}
+
+zx::bti CreateNamedBti(const zx::iommu& fake_iommu, uint32_t options, uint64_t bti_id,
+                       const char* name) {
+  zx::bti ret;
+  EXPECT_OK(zx::bti::create(fake_iommu, options, bti_id, &ret));
+
+  if (ret.is_valid()) {
+    EXPECT_OK(ret.set_property(ZX_PROP_NAME, name, strlen(name)));
+  }
+
+  return ret;
+}
+
+}  // namespace vmo_test
---
+++ zircon/kernel/lib/mistos/zx/tests/helpers.h
@@ -0,0 +1,104 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ZIRCON_KERNEL_LIB_MISTOS_ZX_TESTS_HELPERS_H_
+#define ZIRCON_KERNEL_LIB_MISTOS_ZX_TESTS_HELPERS_H_
+
+#include <lib/fit/defer.h>
+#include <lib/mistos/util/system.h>
+#include <lib/mistos/zx/vmar.h>
+#include <lib/mistos/zx/vmo.h>
+#include <lib/zx/result.h>
+
+#include <zxtest/zxtest.h>
+
+namespace vmo_test {
+
+static inline void VmoWrite(const zx::vmo& vmo, uint32_t data, uint64_t offset = 0) {
+  zx_status_t status = vmo.write(static_cast<void*>(&data), offset, sizeof(data));
+  ASSERT_OK(status, "write failed");
+}
+
+static inline uint32_t VmoRead(const zx::vmo& vmo, uint64_t offset = 0) {
+  uint32_t val = 0;
+  zx_status_t status = vmo.read(&val, offset, sizeof(val));
+  EXPECT_OK(status, "read failed");
+  return val;
+}
+
+static inline void VmoCheck(const zx::vmo& vmo, uint32_t expected, uint64_t offset = 0) {
+  uint32_t data;
+  zx_status_t status = vmo.read(static_cast<void*>(&data), offset, sizeof(data));
+  ASSERT_OK(status, "read failed");
+  ASSERT_EQ(expected, data);
+}
+
+// Creates a vmo with |page_count| pages and writes (page_index + 1) to each page.
+static inline void InitPageTaggedVmo(uint32_t page_count, zx::vmo* vmo) {
+  zx_status_t status;
+  status = zx::vmo::create(page_count * zx_system_get_page_size(), ZX_VMO_RESIZABLE, vmo);
+  ASSERT_OK(status, "create failed");
+  for (unsigned i = 0; i < page_count; i++) {
+    ASSERT_NO_FATAL_FAILURE(VmoWrite(*vmo, i + 1, i * zx_system_get_page_size()));
+  }
+}
+
+static inline size_t VmoNumChildren(const zx::vmo& vmo) {
+  zx_info_vmo_t info;
+  if (vmo.get_info(ZX_INFO_VMO, &info, sizeof(info), nullptr, nullptr) != ZX_OK) {
+    return UINT64_MAX;
+  }
+  return info.num_children;
+}
+
+static inline size_t VmoPopulatedBytes(const zx::vmo& vmo) {
+  zx_info_vmo_t info;
+  if (vmo.get_info(ZX_INFO_VMO, &info, sizeof(info), nullptr, nullptr) != ZX_OK) {
+    return UINT64_MAX;
+  }
+  return info.populated_bytes;
+}
+
+static inline bool PollVmoPopulatedBytes(const zx::vmo& vmo, size_t expected_bytes) {
+  zx_info_vmo_t info;
+  while (true) {
+    if (vmo.get_info(ZX_INFO_VMO, &info, sizeof(info), nullptr, nullptr) != ZX_OK) {
+      return false;
+    }
+    if (info.populated_bytes == expected_bytes) {
+      return true;
+    }
+    printf("polling again. page count %zu\n", info.populated_bytes / zx_system_get_page_size());
+    zx::nanosleep(zx::deadline_after(zx::msec(50)));
+  }
+}
+
+// Simple class for managing vmo mappings w/o any external dependencies.
+class Mapping {
+ public:
+  ~Mapping() {
+    if (addr_) {
+      ZX_ASSERT(zx::vmar::root_self()->unmap(addr_, len_) == ZX_OK);
+    }
+  }
+
+  zx_status_t Init(const zx::vmo& vmo, size_t len) {
+    zx_status_t status =
+        zx::vmar::root_self()->map(ZX_VM_PERM_READ | ZX_VM_PERM_WRITE, 0, vmo, 0, len, &addr_);
+    len_ = len;
+    return status;
+  }
+
+  uint32_t* ptr() { return reinterpret_cast<uint32_t*>(addr_); }
+  uint8_t* bytes() { return reinterpret_cast<uint8_t*>(addr_); }
+
+ private:
+  uint64_t addr_ = 0;
+  size_t len_ = 0;
+};
+
+}  // namespace vmo_test
+
+#endif  // ZIRCON_KERNEL_LIB_MISTOS_ZX_TESTS_HELPERS_H_
