888cd55a7c13b1c7efba522c4ed715733212c768
Bruno Herrera <bruno.herrera@mist-os.com>
2024-04-26 15:16:57 -0300

    [mistos][kernel][vdso|syscalls] Add @no_protocol_prefix to syscall.test.fidl

--- zircon/kernel/lib/mistos/syscalls/test.cc
+++ zircon/kernel/lib/mistos/syscalls/test.cc
@@ -7,17 +7,10 @@
 
 #include <lib/syscalls/forward.h>
 
-// long zx_syscall_test_0
-long sys_syscall_test_0(void) { return 0; }
-// long zx_syscall_test_1
-long sys_syscall_test_1(int a) { return a; }
-// long zx_syscall_test_2
-long sys_syscall_test_2(int a, int b) { return a + b; }
-// long zx_syscall_test_3
-long sys_syscall_test_3(int a, int b, int c) { return a + b + c; }
-// long zx_syscall_test_4
-long sys_syscall_test_4(int a, int b, int c, int d) { return a + b + c + d; }
-// long zx_syscall_test_5
-long sys_syscall_test_5(int a, int b, int c, int d, int e) { return a + b + c + d + e; }
-// long zx_syscall_test_6
-long sys_syscall_test_6(int a, int b, int c, int d, int e, int f) { return a + b + c + d + e + f; }
+long sys_test_0() { return 0; }
+long sys_test_1(int a) { return a; }
+long sys_test_2(int a, int b) { return a + b; }
+long sys_test_3(int a, int b, int c) { return a + b + c; }
+long sys_test_4(int a, int b, int c, int d) { return a + b + c + d; }
+long sys_test_5(int a, int b, int c, int d, int e) { return a + b + c + d + e; }
+long sys_test_6(int a, int b, int c, int d, int e, int f) { return a + b + c + d + e + f; }
