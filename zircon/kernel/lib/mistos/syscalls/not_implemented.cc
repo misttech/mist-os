83d7b5a71205731b02c559e3fd227d3afded809f
Bruno Herrera <bruno.herrera@mist-os.com>
2024-05-02 16:25:20 -0300

    [mistos][syscalls|vdso] Add initial skeleton will all linux syscalls for x86_64

---
+++ zircon/kernel/lib/mistos/syscalls/not_implemented.cc
@@ -0,0 +1,18 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <debug.h>
+#include <lib/user_copy/user_ptr.h>
+#include <zircon/syscalls/types.h>
+#include <zircon/types.h>
+
+// Make the function a weak symbol so real impl can override it.
+#define KERNEL_SYSCALL(name, type, attrs, nargs, arglist, prototype) \
+  attrs type sys_##name prototype __attribute__((weak));             \
+  attrs type sys_##name prototype { PANIC_UNIMPLEMENTED; }
+
+#define INTERNAL_SYSCALL(...) KERNEL_SYSCALL(__VA_ARGS__)
+#define BLOCKING_SYSCALL(...) KERNEL_SYSCALL(__VA_ARGS__)
+
+#include <lib/syscalls/kernel.inc>
