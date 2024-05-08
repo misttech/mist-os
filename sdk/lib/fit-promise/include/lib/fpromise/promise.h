vsrinivas/fuchsia

commit 6066b9d8c79a16a7c4a6b9d124048e1b10f32d0f

[fit] Migrate content to fit-promise from fit
Now that everything in-tree and out- that uses the promise-related
content explicitly depends on fit-promise, we can complete the
migration.

Fixed: 57348
Change-Id: I2522ff949de1816a9910996dbbcd2fa89c39f930
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/507120
Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
Reviewed-by: Roland McGrath <mcgrathr@google.com>
API-Review: Roland McGrath <mcgrathr@google.com>

sdk/lib/fit-promise/fit-promise.api
@@ -1,3 +1,13 @@
 {
-  "pkg/fit-promise/include/lib/fit/do-not-include.h": "27bfbbb009ef1a7bb13244b4deb242d9"
+  "pkg/fit-promise/include/lib/fit/barrier.h": "7a98c961fb72439e238fb7de745a1808",
+  "pkg/fit-promise/include/lib/fit/bridge.h": "56e952d952015528f1c3fee2101732f0",
+  "pkg/fit-promise/include/lib/fit/bridge_internal.h": "b88722a4e7a6cf9764a392b6a37e1899",
+  "pkg/fit-promise/include/lib/fit/in_place_internal.h": "750bf38d2cae0c6728e93e2e0ab8f58c",
+  "pkg/fit-promise/include/lib/fit/promise.h": "ba3e152351c4661ca6c3ff085fbcc0db",
+  "pkg/fit-promise/include/lib/fit/promise_internal.h": "e110d2ebe1cd6b3cf524cdc040986142",
+  "pkg/fit-promise/include/lib/fit/result.h": "4fb676a322cd39bedac54188200d1864",
+  "pkg/fit-promise/include/lib/fit/scheduler.h": "b31aa1289a4bc33d226b0efeb55abdbb",
+  "pkg/fit-promise/include/lib/fit/scope.h": "a398f3121ad52790123a52562265f42a",
+  "pkg/fit-promise/include/lib/fit/sequencer.h": "4bcfe190a01338718434b979ceb1cbe0",
+  "pkg/fit-promise/include/lib/fit/single_threaded_executor.h": "c74cb4b8a8d0b90e592e1130a24d118b"
 }

sdk/lib/fit-promise/include/lib/fit/promise.h
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_FIT_PROMISE_H_
-#define LIB_FIT_PROMISE_H_
+#ifndef LIB_FIT_PROMISE_INCLUDE_LIB_FIT_PROMISE_H_
+#define LIB_FIT_PROMISE_INCLUDE_LIB_FIT_PROMISE_H_
 
 #include <assert.h>
+#include <lib/fit/function.h>
 #include <lib/stdcompat/variant.h>
 
 #include <tuple>
 #include <type_traits>
 #include <utility>
 
-#include "function.h"
 #include "promise_internal.h"
 #include "result.h"
 
@@ -1584,4 +1584,4 @@ inline void swap(suspended_task& a, suspended_task& b) { a.swap(b); }
 
 }  // namespace fit
 
-#endif  // LIB_FIT_PROMISE_H_
+#endif  // LIB_FIT_PROMISE_INCLUDE_LIB_FIT_PROMISE_H_

build/config/sanitizers/BUILD.gn
@@ -85,6 +85,7 @@ sanitizer_extra_options("suppress-lsan.DO-NOT-USE-THIS") {
     "//garnet/bin/setui/*",
     "//garnet/bin/trace/*",
     "//garnet/bin/trace/tests/*",
+    "//sdk/lib/fit-promise/*",
     "//src/cobalt/bin/system-metrics/*",
     "//src/connectivity/bluetooth/profiles/bt-a2dp-sink/*",
     "//src/connectivity/bluetooth/profiles/bt-a2dp/*",

sdk/lib/BUILD.gn
@@ -8,6 +8,7 @@ group("tests") {
   deps = [
     "fdio:tests",
     "file-lock:tests",
+    "fit-promise:tests",
     "procfs:tests",
     "stdcompat:tests",
     "syslog:tests",

sdk/lib/fit-promise/BUILD.gn
@@ -14,7 +14,36 @@ zx_library("fit-promise") {
   sdk_publishable = true
   sdk_migrated = true
 
-  sdk_headers = [ "lib/fit/do-not-include.h" ]
+  sdk_headers = [
+    "lib/fit/barrier.h",
+    "lib/fit/bridge.h",
+    "lib/fit/bridge_internal.h",
+    "lib/fit/in_place_internal.h",
+    "lib/fit/promise.h",
+    "lib/fit/promise_internal.h",
+    "lib/fit/result.h",
+    "lib/fit/scheduler.h",
+    "lib/fit/scope.h",
+    "lib/fit/sequencer.h",
+    "lib/fit/single_threaded_executor.h",
+  ]
 
-  public_deps = [ "//zircon/system/ulib/fit" ]
+  sources = [
+    "barrier.cc",
+    "promise.cc",
+    "scheduler.cc",
+    "scope.cc",
+    "sequencer.cc",
+    "single_threaded_executor.cc",
+  ]
+
+  public_deps = [
+    "//sdk/lib/stdcompat",
+    "//zircon/system/ulib/fit",
+  ]
+}
+
+group("tests") {
+  testonly = true
+  deps = [ "tests" ]
 }

sdk/lib/fit-promise/include/lib/fit/barrier.h
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_FIT_BARRIER_H_
-#define LIB_FIT_BARRIER_H_
+#ifndef LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BARRIER_H_
+#define LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BARRIER_H_
 
 #include <assert.h>
+#include <lib/fit/thread_safety.h>
 
 #include <atomic>
 #include <mutex>
 
 #include "bridge.h"
 #include "promise.h"
-#include "thread_safety.h"
 
 namespace fit {
 
@@ -103,4 +103,4 @@ class barrier final {
 
 }  // namespace fit
 
-#endif  // LIB_FIT_BARRIER_H_
+#endif  // LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BARRIER_H_

sdk/lib/fit-promise/include/lib/fit/bridge.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_FIT_BRIDGE_H_
-#define LIB_FIT_BRIDGE_H_
+#ifndef LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BRIDGE_H_
+#define LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BRIDGE_H_
 
 #include "bridge_internal.h"
 
@@ -459,4 +459,4 @@ inline consumer<typename Promise::value_type, typename Promise::error_type> sche
 
 }  // namespace fit
 
-#endif  // LIB_FIT_BRIDGE_H_
+#endif  // LIB_FIT_PROMISE_INCLUDE_LIB_FIT_BRIDGE_H_

misttech/mist-os

commit 377b2120fffbd067ec37a703fdcb15ddc69b32d3

[sdk][fit-promise] Fix broken hyperlinks
The files in the broken links were moved in commit
6066b9d8c79a16a7c4a6b9d124048e1b10f32d0f.

Change-Id: Ie85f0b66a44d075b567a4a07683c23c3736a96ce
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1044150
API-Review: Adam Barth <abarth@google.com>
Reviewed-by: Adam Barth <abarth@google.com>
Commit-Queue: Adam Barth <abarth@google.com>

sdk/lib/fit-promise/fit-promise.api
@@ -2,7 +2,7 @@
   "pkg/fit-promise/include/lib/fpromise/barrier.h": "165eaf8820b9055c94adc5199c9a496d",
   "pkg/fit-promise/include/lib/fpromise/bridge.h": "092502f25421c313acba8fd7aeda3f14",
   "pkg/fit-promise/include/lib/fpromise/bridge_internal.h": "00fadbe222fc29d3d3b5bb63697c611c",
-  "pkg/fit-promise/include/lib/fpromise/promise.h": "e78fc1bb957af4cc6b850d3e110cdf99",
+  "pkg/fit-promise/include/lib/fpromise/promise.h": "d376509bf1005b52916f131857f45aa7",
   "pkg/fit-promise/include/lib/fpromise/promise_internal.h": "ebb8a10a0da343651d1903a2c99954c3",
   "pkg/fit-promise/include/lib/fpromise/result.h": "25a239df59de0e20652dd961db57fc0c",
   "pkg/fit-promise/include/lib/fpromise/scheduler.h": "9dbe6a9c3546b7ca9cd0d4ec276f5ff1",

sdk/lib/fit-promise/include/lib/fpromise/promise.h
@@ -285,9 +285,9 @@ namespace fpromise {
 // EXAMPLE
 //
 // -
-// https://fuchsia.googlesource.com/fuchsia/+/HEAD/zircon/system/utest/fit/examples/promise_example1.cc
+// https://fuchsia.googlesource.com/fuchsia/+/HEAD/sdk/lib/fit-promise/tests/examples/promise_example1.cc
 // -
-// https://fuchsia.googlesource.com/fuchsia/+/HEAD/zircon/system/utest/fit/examples/promise_example2.cc
+// https://fuchsia.googlesource.com/fuchsia/+/HEAD/sdk/lib/fit-promise/tests/examples/promise_example2.cc
 //
 template <typename V = void, typename E = void>
 using promise = promise_impl<::fit::function<result<V, E>(fpromise::context&)>>;
@@ -1066,7 +1066,7 @@ enum class future_state {
 // EXAMPLE
 //
 // -
-// https://fuchsia.googlesource.com/fuchsia/+/HEAD/zircon/system/utest/fit/examples/promise_example2.cc
+// https://fuchsia.googlesource.com/fuchsia/+/HEAD/sdk/lib/fit-promise/tests/examples/promise_example2.cc
 template <typename V = void, typename E = void>
 using future = future_impl<promise<V, E>>;
 