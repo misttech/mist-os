fcdfb07e8f8fa16989d1f4cbd4978e094e5f456c
Bruno Herrera <bruno.herrera@mist-os.com>
2024-05-18 11:47:46 -0300

    [mistos][syscalls] Add getpid support

---
+++ zircon/kernel/lib/mistos/syscalls/kernel/sys.cc
@@ -0,0 +1,28 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/mistos/starnix/kernel/arch/x64/syscalls.h>
+#include <lib/mistos/starnix/kernel/task/current_task.h>
+#include <lib/mistos/starnix/kernel/task/process_group.h>
+#include <lib/mistos/starnix/kernel/task/syscalls.h>
+#include <lib/mistos/starnix/kernel/task/task.h>
+#include <lib/mistos/starnix/kernel/task/thread_group.h>
+#include <lib/mistos/starnix/kernel/zircon/task_dispatcher.h>
+#include <lib/syscalls/forward.h>
+#include <trace.h>
+
+#include "../priv.h"
+
+#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
+
+long sys_getpid() {
+  LTRACE;
+  auto ut = ThreadDispatcher::GetCurrent();
+  auto syscall_ret =
+      sys_getpid(starnix::CurrentTask::From(starnix::TaskBuilder(ut->task()->task())));
+  if (syscall_ret.is_error()) {
+    return syscall_ret.error_value().return_value();
+  }
+  return syscall_ret.value();
+}
