4ac748c3d20775181aebd19e5796494ff895b45f
Bruno Herrera <bruno.herrera@mist-os.com>
2024-05-18 11:46:32 -0300

    [mistos][syscalls] Add brk support

---
+++ zircon/kernel/lib/mistos/syscalls/mm/mmap.cc
@@ -0,0 +1,27 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/mistos/starnix/kernel/arch/x64/syscalls.h>
+#include <lib/mistos/starnix/kernel/mm/syscalls.h>
+#include <lib/mistos/starnix/kernel/task/process_group.h>
+#include <lib/mistos/starnix/kernel/task/task.h>
+#include <lib/mistos/starnix/kernel/task/thread_group.h>
+#include <lib/mistos/starnix/kernel/zircon/task_dispatcher.h>
+#include <lib/syscalls/forward.h>
+#include <trace.h>
+
+#include "../priv.h"
+
+#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
+
+long sys_brk(unsigned long brk) {
+  LTRACEF_LEVEL(2, "brk=0x%lx\n", brk);
+  auto ut = ThreadDispatcher::GetCurrent();
+  auto syscall_ret =
+      sys_brk(starnix::CurrentTask::From(starnix::TaskBuilder(ut->task()->task())), brk);
+  if (syscall_ret.is_error()) {
+    return syscall_ret.error_value().return_value();
+  }
+  return syscall_ret.value().ptr();
+}
