20a018d151332edf6d54f7a712cd0b850a17b60f
Bruno Herrera <bruno.herrera@mist-os.com>
2024-05-18 10:29:55 -0300

    [mistos][syscalls] Fix unimplemented syscall to not panic

--- zircon/kernel/lib/mistos/syscalls/not_implemented.cc
+++ zircon/kernel/lib/mistos/syscalls/not_implemented.cc
@@ -2,15 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang format off
+#include <linux/errno.h>
+// clang format on
+
 #include <debug.h>
 #include <lib/user_copy/user_ptr.h>
+#include <trace.h>
 #include <zircon/syscalls/types.h>
 #include <zircon/types.h>
+
+#include "priv.h"
+
+#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
 
 // Make the function a weak symbol so real impl can override it.
 #define KERNEL_SYSCALL(name, type, attrs, nargs, arglist, prototype) \
   attrs type sys_##name prototype __attribute__((weak));             \
-  attrs type sys_##name prototype { PANIC_UNIMPLEMENTED; }
+  attrs type sys_##name prototype {                                  \
+    LTRACEF("not implemented (%s)\n", __PRETTY_FUNCTION__);          \
+    return -ENOSYS;                                                  \
+  }
 
 #define INTERNAL_SYSCALL(...) KERNEL_SYSCALL(__VA_ARGS__)
 #define BLOCKING_SYSCALL(...) KERNEL_SYSCALL(__VA_ARGS__)
