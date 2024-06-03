ee9c633e1fd7f8365471e5f65d4612237bb741d0
Bruno Herrera <bruno.herrera@mist-os.com>
2024-06-03 16:11:20 -0300

    [mistos][starnix_lite] Initial build structure

---
+++ src/starnix_lite/kernel/main.rs
@@ -0,0 +1,35 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    fuchsia_runtime::{job_default, process_self},
+    fuchsia_zircon::JobCriticalOptions,
+};
+
+extern "C" {
+    fn dl_set_loader_service(
+        handle: fuchsia_zircon::sys::zx_handle_t,
+    ) -> fuchsia_zircon::sys::zx_handle_t;
+}
+
+fn main() {
+    // Set ourselves as critical to our job. If we do not fail gracefully, our
+    // job will be killed.
+    if let Err(err) =
+        job_default().set_critical(JobCriticalOptions::RETCODE_NONZERO, &process_self())
+    {
+        panic!("Component manager failed to set itself as critical: {}", err);
+    }
+
+    // Close any loader service passed to component manager so that the service session can be
+    // freed, as component manager won't make use of a loader service such as by calling dlopen.
+    // If userboot invoked component manager directly, this service was the only reason userboot
+    // continued to run and closing it will let userboot terminate.
+    let ldsvc = unsafe {
+        fuchsia_zircon::Handle::from_raw(dl_set_loader_service(
+            fuchsia_zircon::sys::ZX_HANDLE_INVALID,
+        ))
+    };
+    drop(ldsvc);
+}
