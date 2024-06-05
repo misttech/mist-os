f5f7aab99cf17f975ff2e4c7e6641f2c5a50c4a1
Casey Dahlin <sadmac@google.com>
2024-06-05 20:46:56 +0000

    [ffx][playground] Run the tests for the playground tool
    The tool is experimental but we should still run the tests even though we
    haven't enabled it.
    
    We also fix a circular reference issue in cf_fs that was making the
    tests fail lsan. We do that in this CL so that the change is verified by
    CQ.
    
    Change-Id: Ibd12314c699ef1a465dd6fb6084808d32d174977
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1058493
    Reviewed-by: Steven Grady <slgrady@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/tools/playground/src/cf_fs.rs
+++ src/developer/ffx/tools/playground/src/cf_fs.rs
@@ -11,7 +11,7 @@ use futures::channel::oneshot;
 use std::collections::BTreeMap;
 use std::hash::{Hash, Hasher};
 use std::ops::Bound;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, Weak};
 use vfs::attributes;
 use vfs::common::rights_to_posix_mode_bits;
 use vfs::directory::dirents_sink::{self, AppendResult};
@@ -30,7 +30,7 @@ use vfs::ToObjectRequest;
 /// (https://fxbug.dev/331829928)
 enum CacheState {
     /// We have a fresh cache ready for use.
-    Here(BTreeMap<String, (Instance, Option<Arc<CFDirectory>>)>),
+    Here(BTreeMap<String, (Instance, Option<Weak<CFDirectory>>)>),
     /// There is no cache ready but someone is refreshing it now. The senders in
     /// the vec will all be sent to when it's time to check again.
     Refreshing(Vec<oneshot::Sender<()>>),
@@ -38,7 +38,7 @@ enum CacheState {
     /// state should change it to `Refreshing` and start refreshing the cache.
     /// The stale cache state, if available, is included as the argument to the
     /// variant.
-    NeedsRefresh(Option<BTreeMap<String, (Instance, Option<Arc<CFDirectory>>)>>),
+    NeedsRefresh(Option<BTreeMap<String, (Instance, Option<Weak<CFDirectory>>)>>),
 }
 
 /// Lock-protected portion of [`CFDirectory`]
@@ -178,13 +178,13 @@ impl CFDirectory {
     /// footguns with locking semantics.
     async fn with_cache<T>(
         &self,
-        f: impl FnOnce(&mut BTreeMap<String, (Instance, Option<Arc<CFDirectory>>)>) -> T,
+        f: impl FnOnce(&mut BTreeMap<String, (Instance, Option<Weak<CFDirectory>>)>) -> T,
     ) -> Result<T, GetAllInstancesError> {
         enum NextAction {
             Wait(oneshot::Receiver<()>),
             Refresh(
                 sys2::RealmQueryProxy,
-                Option<BTreeMap<String, (Instance, Option<Arc<CFDirectory>>)>>,
+                Option<BTreeMap<String, (Instance, Option<Weak<CFDirectory>>)>>,
             ),
         }
 
@@ -346,13 +346,16 @@ impl CFDirectory {
                 match self
                     .with_cache(|cache| {
                         if let Some((_, node)) = cache.get_mut(&path_to_here) {
-                            let node = node.get_or_insert_with(|| {
-                                Arc::new(CFDirectory {
+                            if let Some(node) = node.as_ref().and_then(Weak::upgrade) {
+                                Ok(node)
+                            } else {
+                                let new_node = Arc::new(CFDirectory {
                                     inner: Arc::clone(&self.inner),
                                     path: path_to_here,
-                                })
-                            });
-                            Ok(Arc::clone(node))
+                                });
+                                *node = Some(Arc::downgrade(&new_node));
+                                Ok(new_node)
+                            }
                         } else {
                             Err(Status::NOT_FOUND)
                         }
