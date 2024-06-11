a796c259801f90be27972d5471d66f88b276b596
Naomi McCracken <nmccracken@google.com>
2024-06-11 00:28:54 +0000

    [wlan][policy] prevent hw-sim autoconnect flake
    Before this change, when a manual connect request comes in (or saving a
    network when there is no active connection), IfaceManager will cancel
    ongoing bss selection for auto connect only after the bss selection for
    the manual connect request is finished.
    
    The new behavior will cancel ongoing bss selection before bss selection
    in the manual connect path.
    
    In hw-sim-tests the current behavior can cause a flake if the auto
    connect requests bss selection before the manual connect request does,
    since the manual connect request will fail at the bss selection step.
    Then the test fails since it expects a bad password but gets a general
    failure.
    
    Multiply: connect_with_bad_password
    Bug: 334153524
    Change-Id: I59bb434c50480cbff49dd1ed2c42d1b1202ed2d0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1060972
    Commit-Queue: Naomi Mccracken <nmccracken@google.com>
    Reviewed-by: Hayden Nix <haydennix@google.com>

--- src/connectivity/wlan/wlancfg/src/mode_management/iface_manager.rs
+++ src/connectivity/wlan/wlancfg/src/mode_management/iface_manager.rs
@@ -506,9 +506,6 @@ impl IfaceManagerService {
             }
         }
 
-        // Cancel any ongoing attempt to auto connect the previously idle iface.
-        self.connection_selection_futures.clear();
-
         client_iface.last_roam_time = fasync::Time::now();
         self.clients.push(client_iface);
         Ok(())
@@ -951,6 +948,9 @@ async fn initiate_connection_selection_for_connect_request(
             .await
             .map(|candidate| ConnectionSelectionResponse::ConnectRequest { candidate, request })
     };
+
+    // Cancel any ongoing attempt to auto connect the previously idle iface.
+    iface_manager.connection_selection_futures.clear();
 
     iface_manager.connection_selection_futures.push(fut.boxed());
     Ok(())
@@ -2071,18 +2071,29 @@ mod tests {
     }
 
     #[fuchsia::test]
-    fn test_connect_cancels_auto_reconnect_future() {
+    fn test_connect_request_cancels_auto_reconnect_future() {
         let mut exec = fuchsia_async::TestExecutor::new();
 
         let mut test_values = test_setup(&mut exec);
-        let (mut iface_manager, mut _sme_stream) =
-            create_iface_manager_with_client(&test_values, false);
         let (saved_networks, mut stash_server) =
             exec.run_singlethreaded(SavedNetworksManager::new_and_stash_server());
         test_values.saved_networks = Arc::new(saved_networks);
 
+        let (mut iface_manager, mut _sme_stream) =
+            create_iface_manager_with_client(&test_values, false);
+
+        let scanned_candidate = generate_random_scanned_candidate();
+        let connect_selection = client_types::ConnectSelection {
+            target: scanned_candidate.clone(),
+            reason: client_types::ConnectReason::NewSavedNetworkAutoconnect,
+        };
+        let connect_request = ConnectAttemptRequest::new(
+            scanned_candidate.network.clone(),
+            scanned_candidate.credential.clone(),
+            connect_selection.reason,
+        );
+
         // Add credentials for the test network to the saved networks.
-        let connect_selection = generate_connect_selection();
         let save_network_fut = test_values.saved_networks.store(
             connect_selection.target.network.clone(),
             connect_selection.target.credential.clone(),
@@ -2091,42 +2102,67 @@ mod tests {
         assert_variant!(exec.run_until_stalled(&mut save_network_fut), Poll::Pending);
 
         process_stash_write(&mut exec, &mut stash_server);
+        assert_variant!(exec.run_until_stalled(&mut save_network_fut), Poll::Ready(_));
 
-        // Add a network selection future which won't complete that should be canceled by a
-        // connect request.
-        async fn blocking_fn() -> Result<ConnectionSelectionResponse, anyhow::Error> {
-            loop {
-                fasync::Timer::new(zx::Duration::from_millis(1).after_now()).await
-            }
+        // Initiate automatic connection selection
+        {
+            let mut sel_fut = pin!(initiate_automatic_connection_selection(&mut iface_manager));
+            assert_variant!(exec.run_until_stalled(&mut sel_fut), Poll::Ready(()));
         }
-        iface_manager.connection_selection_futures.push(blocking_fn().boxed());
 
         // Request a connect through IfaceManager and respond to requests needed to complete it.
         {
-            let connect_fut = iface_manager.connect(connect_selection);
+            assert_eq!(iface_manager.connection_selection_futures.len(), 1);
+
+            let connect_fut = iface_manager.handle_connect_request(connect_request);
             let mut connect_fut = pin!(connect_fut);
 
-            // Expect that we have requested a client SME proxy.
-            assert_variant!(exec.run_until_stalled(&mut connect_fut), Poll::Pending);
-            let mut monitor_service_fut = test_values.monitor_service_stream.into_future();
-            assert_variant!(
-                poll_service_req(&mut exec, &mut monitor_service_fut),
-                Poll::Ready(fidl_fuchsia_wlan_device_service::DeviceMonitorRequest::GetClientSme {
-                    iface_id: TEST_CLIENT_IFACE_ID, sme_server: _, responder
-                }) => {
-                    // Send back a positive acknowledgement.
-                    assert!(responder.send(Ok(())).is_ok());
-                }
-            );
-
-            let mut connect_fut = pin!(connect_fut);
-            assert_variant!(exec.run_until_stalled(&mut connect_fut), Poll::Ready(connect_result) => {
-                assert!(connect_result.is_ok());
-            });
+            // Run the future for the connect request
+            assert_variant!(exec.run_until_stalled(&mut connect_fut), Poll::Ready(Ok(())));
         }
 
-        // Verify that the network selection future was dropped from the list.
-        assert!(iface_manager.connection_selection_futures.is_empty());
+        // There should be a new connection selection future for the manual connect request,
+        // and the previous one should have been removed.
+        assert_eq!(iface_manager.connection_selection_futures.len(), 1);
+
+        // Progress the connection selection future, which should make a scan request if it's
+        // the correct future and wouldn't if it's the future from the beginning of the test.
+        let (_sender, receiver) = mpsc::channel(1);
+        let serve_fut = serve_iface_manager_requests(
+            iface_manager,
+            receiver,
+            test_values.defect_receiver,
+            test_values.recovery_receiver,
+        );
+        let mut serve_fut = pin!(serve_fut);
+        assert_variant!(exec.run_until_stalled(&mut serve_fut), Poll::Pending);
+
+        // Respond to the connection selection request
+        assert_variant!(test_values.connection_selection_request_receiver.try_next(), Ok(Some(request)) => {
+            assert_variant!(request, ConnectionSelectionRequest::NewConnectionSelection {network_id, reason, responder} => {
+                assert!(network_id.is_some());
+                assert_eq!(reason, client_types::ConnectReason::NewSavedNetworkAutoconnect);
+                responder.send(Some(scanned_candidate)).expect("failed to send selection");
+            });
+        });
+        assert_variant!(exec.run_until_stalled(&mut serve_fut), Poll::Pending);
+
+        // Since an AP was selected, the iface manager should request an SME handle to
+        // initialize a state machine.
+        let mut monitor_service_fut = test_values.monitor_service_stream.into_future();
+        assert_variant!(
+            poll_service_req(&mut exec, &mut monitor_service_fut),
+            Poll::Ready(fidl_fuchsia_wlan_device_service::DeviceMonitorRequest::GetClientSme {
+                iface_id: TEST_CLIENT_IFACE_ID, sme_server: _, responder
+            }) => {
+                // Send back a positive acknowledgement.
+                assert!(responder.send(Ok(())).is_ok());
+            }
+        );
+
+        // Check the connection selection futures receiver to see that connection selection
+        // wasn't initiated again.
+        assert_variant!(exec.run_until_stalled(&mut serve_fut), Poll::Pending);
     }
 
     #[fuchsia::test]
