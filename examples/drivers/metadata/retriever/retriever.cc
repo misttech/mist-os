802e9c8ed326caf4132d2821b813131e5661ade9
Nick Eaton <nickeaton@google.com>
2024-06-12 20:30:10 +0000

    [fdf][metadata] Clean up metadata examples
    Add example test for passing metadata between drivers.
    
    Bug: 315834403
    Change-Id: I252867a9f9fe9b9cbd8888a927183c47458b3236
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1057715
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Commit-Queue: Nick Eaton <nickeaton@google.com>
    API-Review: Jocelyn Dang <jocelyndang@google.com>

---
+++ examples/drivers/metadata/retriever/retriever.cc
@@ -0,0 +1,86 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/driver/component/cpp/driver_export.h>
+#include <lib/driver/devfs/cpp/connector.h>
+#include <lib/driver/logging/cpp/structured_logger.h>
+
+#include "examples/drivers/metadata/fuchsia.examples.metadata/metadata.h"
+
+namespace examples::drivers::metadata {
+
+// This driver demonstrates how to retrieve the metadata from its parent driver,
+// Forwarder. It implements the `fuchsia_examples_metadata::Retriever` protocol
+// for testing purposes.
+class Retriever final : public fdf::DriverBase,
+                        public fidl::Server<fuchsia_examples_metadata::Retriever> {
+ public:
+  Retriever(fdf::DriverStartArgs start_args, fdf::UnownedSynchronizedDispatcher driver_dispatcher)
+      : DriverBase("child", std::move(start_args), std::move(driver_dispatcher)) {}
+
+  zx::result<> Start() override {
+    zx_status_t status = AddDevfsChild();
+    if (status != ZX_OK) {
+      FDF_SLOG(ERROR, "Failed to add child.", KV("status", zx_status_get_string(status)));
+      return zx::error(status);
+    }
+
+    return zx::ok();
+  }
+
+  // fuchsia.hardware.test/Child implementation.
+  void GetMetadata(GetMetadataCompleter::Sync& completer) override {
+    zx::result metadata =
+        fdf_metadata::GetMetadata<fuchsia_examples_metadata::Metadata>(incoming());
+    if (metadata.is_error()) {
+      FDF_SLOG(ERROR, "Failed to get metadata.", KV("status", metadata.status_string()));
+      completer.Reply(fit::error(metadata.status_value()));
+      return;
+    }
+
+    completer.Reply(fit::ok(std::move(metadata.value())));
+  }
+
+ private:
+  void Serve(fidl::ServerEnd<fuchsia_examples_metadata::Retriever> request) {
+    bindings_.AddBinding(dispatcher(), std::move(request), this, fidl::kIgnoreBindingClosure);
+  }
+
+  // Add child that has a devfs connection in order for tests to communicate with this driver.
+  zx_status_t AddDevfsChild() {
+    if (child_node_.has_value()) {
+      FDF_LOG(ERROR, "Child node already created.");
+      return ZX_ERR_BAD_STATE;
+    }
+
+    zx::result connector = devfs_connector_.Bind(dispatcher());
+    if (connector.is_error()) {
+      FDF_SLOG(ERROR, "Failed to bind devfs connector.", KV("status", connector.status_string()));
+      return connector.status_value();
+    }
+
+    fuchsia_driver_framework::DevfsAddArgs devfs_args{{.connector = std::move(connector.value())}};
+
+    zx::result owned_child = AddOwnedChild("retriever", devfs_args);
+    if (owned_child.is_error()) {
+      FDF_SLOG(ERROR, "Failed to add owned child.", KV("status", owned_child.status_string()));
+      return owned_child.error_value();
+    }
+
+    child_node_.emplace(std::move(owned_child.value()));
+
+    return ZX_OK;
+  }
+
+  // Used by tests in order to communicate with the driver.
+  driver_devfs::Connector<fuchsia_examples_metadata::Retriever> devfs_connector_{
+      fit::bind_member<&Retriever::Serve>(this)};
+
+  fidl::ServerBindingGroup<fuchsia_examples_metadata::Retriever> bindings_;
+  std::optional<fdf::OwnedChildNode> child_node_;
+};
+
+}  // namespace examples::drivers::metadata
+
+FUCHSIA_DRIVER_EXPORT(examples::drivers::metadata::Retriever);
---
+++ examples/drivers/metadata/test/metadata_example_test.cc
@@ -0,0 +1,89 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fuchsia.driver.test/cpp/fidl.h>
+#include <fidl/fuchsia.examples.metadata/cpp/fidl.h>
+#include <lib/device-watcher/cpp/device-watcher.h>
+#include <lib/driver_test_realm/realm_builder/cpp/lib.h>
+#include <lib/fdio/fd.h>
+
+#include "src/lib/testing/loop_fixture/test_loop_fixture.h"
+
+namespace metadata_test {
+
+class MetadataTest : public gtest::TestLoopFixture {
+ public:
+  void SetUp() override {
+    // Create and build the realm.
+    auto realm_builder = component_testing::RealmBuilder::Create();
+    driver_test_realm::Setup(realm_builder);
+    realm_.emplace(realm_builder.Build(dispatcher()));
+
+    // Start DriverTestRealm.
+    zx::result result = realm_->component().Connect<fuchsia_driver_test::Realm>();
+    ASSERT_EQ(result.status_value(), ZX_OK);
+    fidl::SyncClient<fuchsia_driver_test::Realm> driver_test_realm{std::move(result.value())};
+    fidl::Result start_result = driver_test_realm->Start(
+        fuchsia_driver_test::RealmArgs{{.root_driver = "fuchsia-boot:///dtr#meta/sender.cm"}});
+    ASSERT_TRUE(start_result.is_ok()) << start_result.error_value();
+
+    // Connect to /dev directory.
+    zx::result endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+    ASSERT_EQ(endpoints.status_value(), ZX_OK);
+    ASSERT_EQ(realm_->component().Connect("dev-topological", endpoints->server.TakeChannel()),
+              ZX_OK);
+
+    ASSERT_EQ(fdio_fd_create(endpoints->client.TakeChannel().release(), &dev_fd_), ZX_OK);
+  }
+
+ protected:
+  template <typename FidlProtocol>
+  fidl::SyncClient<FidlProtocol> ConnectToNode(std::string_view path) const {
+    zx::result channel = device_watcher::RecursiveWaitForFile(dev_fd_, std::string{path}.c_str());
+    EXPECT_EQ(channel.status_value(), ZX_OK) << path;
+    return fidl::SyncClient{fidl::ClientEnd<FidlProtocol>{std::move(channel.value())}};
+  }
+
+ private:
+  std::optional<component_testing::RealmRoot> realm_;
+  int dev_fd_;
+};
+
+TEST_F(MetadataTest, TransferMetadata) {
+  const char* kMetadataPropertyValue = "test property value";
+
+  auto sender = ConnectToNode<fuchsia_examples_metadata::Sender>("sender");
+  auto forwarder = ConnectToNode<fuchsia_examples_metadata::Forwarder>("sender/forwarder");
+  auto retriever =
+      ConnectToNode<fuchsia_examples_metadata::Retriever>("sender/forwarder/retriever");
+
+  // Set the metadata of the `sender` driver and offer it to its child driver
+  // (the `forwarder` driver).
+  {
+    fuchsia_examples_metadata::Metadata metadata{{.test_property = kMetadataPropertyValue}};
+    fidl::Result result = sender->SetMetadata(std::move(metadata));
+    ASSERT_TRUE(result.is_ok()) << result.error_value();
+  }
+
+  // Make the `forwarder` driver retrieve metadata from its parent driver (the
+  // `sender` driver) and offer it to its child driver (the `retriever` driver).
+  {
+    fidl::Result result = forwarder->ForwardMetadata();
+    ASSERT_TRUE(result.is_ok()) << result.error_value();
+  }
+
+  // Retrieve the metadata from `retriever`'s parent driver, `forwarder`.
+  // This verifies that:
+  //   * The `sender` driver sent the correct metadata.
+  //   * The `forwarder` driver forwarded the correct metadata.
+  //   * The `retriever` driver retrieved the correct metadata.
+  {
+    fidl::Result result = retriever->GetMetadata();
+    ASSERT_TRUE(result.is_ok()) << result.error_value();
+    auto metadata = std::move(result.value().metadata());
+    ASSERT_EQ(metadata.test_property(), kMetadataPropertyValue);
+  }
+}
+
+}  // namespace metadata_test
