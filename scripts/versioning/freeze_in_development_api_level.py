57fe5cfab69e1cf17c972920330e9079240d2acc
David Dorwin <ddorwin@google.com>
2024-06-18 22:08:22 +0000

    [versioning][idk] Simplify freeze/bump scripts
    Remove unnecessary exception handling and use of absolute paths. This
    addresses suggestions in https://fxrev.dev/1067432.
    
    Also update a function comment to match the implementation in that CL.
    
    Bug: 346644923
    Change-Id: I4dfb9012aea232ad0c514c9ec027d39f8ce6d87b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1068155
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Commit-Queue: David Dorwin <ddorwin@google.com>

--- scripts/versioning/freeze_in_development_api_level.py
+++ scripts/versioning/freeze_in_development_api_level.py
@@ -40,25 +40,16 @@ def freeze_version_history(
     raise Exception("No in-development API level found.")
 
 
-def update_owners_file(root_source_dir: str, fuchsia_api_level: str) -> bool:
+def update_owners_file(root_source_dir: str, fuchsia_api_level: str) -> None:
     """Updates the OWNERS file with more limited access for frozen API levels."""
-    root = _join_path(root_source_dir, "sdk", "history")
-    level_dir_path = _join_path(root, fuchsia_api_level)
-    owners_path = _join_path(level_dir_path, "OWNERS")
+    level_dir_path = os.path.join(
+        root_source_dir, "sdk", "history", fuchsia_api_level
+    )
+    owners_path = os.path.join(level_dir_path, "OWNERS")
 
-    try:
-        print(f"Updating {owners_path}")
-        with open(owners_path, "w") as f:
-            f.write("include /sdk/history/FROZEN_API_LEVEL_OWNERS\n")
-    except Exception as e:
-        print(f"Failed to update {owners_path}: {e}")
-        return False
-    return True
-
-
-def _join_path(root_dir: str, *paths: str) -> str:
-    """Returns absolute path"""
-    return os.path.abspath(os.path.join(root_dir, *paths))
+    print(f"Updating {owners_path}")
+    with open(owners_path, "w") as f:
+        f.write("include /sdk/history/FROZEN_API_LEVEL_OWNERS\n")
 
 
 def main() -> int:
@@ -71,8 +62,7 @@ def main() -> int:
 
     level_frozen = freeze_in_development_api_level(args.sdk_version_history)
 
-    if not update_owners_file(args.root_source_dir, level_frozen):
-        return 1
+    update_owners_file(args.root_source_dir, level_frozen)
 
     return 0
 
