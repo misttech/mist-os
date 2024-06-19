4e5056e2124a556d406f17381b9a727dc3c5e9ad
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2024-06-19 00:29:27 +0000

    [connectivity] Remove dead code
    A new roll of the Rust toolchain will treat this as dead code.
    
    Bug: 347752627
    Change-Id: I7aa58cb04e54d6a8d22650886050581644ce0a42
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1066814
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>

--- src/connectivity/network/netstack3/src/bindings/socket.rs
+++ src/connectivity/network/netstack3/src/bindings/socket.rs
@@ -289,8 +289,11 @@ fn flags_for_device(info: &DeviceSpecificInfo<'_>) -> psocket::InterfaceFlags {
 pub(crate) trait SockAddr: std::fmt::Debug + Sized + Send {
     /// The concrete address type for this `SockAddr`.
     type AddrType: IpAddress + ScopeableAddress;
+
     /// The socket's domain.
+    #[cfg(test)]
     const DOMAIN: psocket::Domain;
+
     /// The unspecified instance of this `SockAddr`.
     const UNSPECIFIED: Self;
 
@@ -324,6 +327,7 @@ pub(crate) trait SockAddr: std::fmt::Debug + Sized + Send {
 
 impl SockAddr for fnet::Ipv6SocketAddress {
     type AddrType = Ipv6Addr;
+    #[cfg(test)]
     const DOMAIN: psocket::Domain = psocket::Domain::Ipv6;
     const UNSPECIFIED: Self = fnet::Ipv6SocketAddress {
         address: fnet::Ipv6Address { addr: [0; 16] },
@@ -366,6 +370,7 @@ impl SockAddr for fnet::Ipv6SocketAddress {
 
 impl SockAddr for fnet::Ipv4SocketAddress {
     type AddrType = Ipv4Addr;
+    #[cfg(test)]
     const DOMAIN: psocket::Domain = psocket::Domain::Ipv4;
     const UNSPECIFIED: Self =
         fnet::Ipv4SocketAddress { address: fnet::Ipv4Address { addr: [0; 4] }, port: 0 };
