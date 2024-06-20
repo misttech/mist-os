8b759ee730cfb2c8a6bfca263f2455721fef6562
Bruno Herrera <bruno.herrera@mist-os.com>
2024-06-20 12:31:30 -0300

    [mistos][revert] Revert fbl code to fuchsia-main
    Change-Id: I6677a5693263f599049f592a76ac9510a7dc0e1b

--- zircon/system/ulib/fbl/include/fbl/vector.h
+++ zircon/system/ulib/fbl/include/fbl/vector.h
@@ -1,4 +1,3 @@
-// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
 // Copyright 2017 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -6,17 +5,15 @@
 #ifndef FBL_VECTOR_H_
 #define FBL_VECTOR_H_
 
-#include <string.h>
-#include <zircon/assert.h>
-
-#include <initializer_list>
-#include <iterator>
 #include <new>
-#include <type_traits>
-#include <utility>
+#include <string.h>
 
 #include <fbl/alloc_checker.h>
 #include <fbl/macros.h>
+#include <initializer_list>
+#include <type_traits>
+#include <utility>
+#include <zircon/assert.h>
 
 namespace fbl {
 
@@ -100,20 +97,6 @@ struct AlignedAllocatorTraits {
 template <typename T, typename AllocatorTraits = AlignedAllocatorTraits<T>>
 class __OWNER(T) Vector {
  public:
-#if _KERNEL_MISTOS
-  using value_type = T;
-  using pointer = T*;
-  using const_pointer = const T*;
-  using reference = value_type&;
-  using const_reference = const value_type&;
-  using size_type = size_t;
-  using difference_type = ptrdiff_t;
-  using iterator = T*;
-  using const_iterator = const T*;
-  using reverse_iterator = std::reverse_iterator<iterator>;
-  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-#endif
-
   // move semantics only
   DISALLOW_COPY_AND_ASSIGN_ALLOW_MOVE(Vector);
 
@@ -256,26 +239,6 @@ class __OWNER(T) Vector {
 
   T* end() { return &ptr_[size_]; }
   const T* end() const { return &ptr_[size_]; }
-
-#if _KERNEL_MISTOS
-  //
-  // Reverse Iterators
-  //
-
-  // clang-format off
-
-  constexpr reverse_iterator       rbegin()        noexcept { return reverse_iterator(end()); }
-  constexpr const_reverse_iterator rbegin()  const noexcept { return const_reverse_iterator(end()); }
-  constexpr reverse_iterator       rend()          noexcept { return reverse_iterator(begin()); }
-  constexpr const_reverse_iterator rend()    const noexcept { return const_reverse_iterator(begin()); }
-
-  // clang-format on
-
-  void set_size(size_t size) {
-    ZX_DEBUG_ASSERT(size <= capacity_);
-    size_ = size;
-  }
-#endif
 
  private:
   // TODO(smklein): In the future, if we want to be able to push back
--- zircon/system/ulib/fbl/test/array_tests.cc
+++ zircon/system/ulib/fbl/test/array_tests.cc
@@ -1,4 +1,3 @@
-// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
 // Copyright 2017 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -42,9 +41,7 @@ TEST(ArrayTest, Destructor) {
 
 TEST(ArrayTest, MoveToConstCtor) {
   constexpr size_t kSize = 10;
-  fbl::AllocChecker ac;
-  fbl::Array<uint32_t> array(new (ac) uint32_t[kSize], kSize);
-  EXPECT_TRUE(ac.check());
+  fbl::Array<uint32_t> array(new uint32_t[kSize], kSize);
   for (uint32_t i = 0; i < kSize; ++i) {
     array[i] = i;
   }
@@ -62,9 +59,7 @@ TEST(ArrayTest, MoveToConstCtor) {
 
 TEST(ArrayTest, MoveToConstAssignment) {
   constexpr size_t kSize = 10;
-  fbl::AllocChecker ac;
-  fbl::Array<uint32_t> array(new (ac) uint32_t[kSize], kSize);
-  EXPECT_TRUE(ac.check());
+  fbl::Array<uint32_t> array(new uint32_t[kSize], kSize);
   for (uint32_t i = 0; i < kSize; ++i) {
     array[i] = i;
   }
@@ -81,7 +76,6 @@ TEST(ArrayTest, MoveToConstAssignment) {
   }
 }
 
-#if !_KERNEL
 TEST(ArrayTest, MakeArraySimple) {
   constexpr size_t kSize = 10;
   fbl::Array<uint32_t> array = fbl::MakeArray<uint32_t>(kSize);
@@ -95,7 +89,6 @@ TEST(ArrayTest, MakeArraySimple) {
     array[i] = i;
   }
 }
-#endif
 
 TEST(ArrayTest, MakeArrayAllocChecker) {
   constexpr size_t kSize = 10;
@@ -127,7 +120,6 @@ TEST(ArrayTest, MakeArrayFailingAllocChecker) {
   EXPECT_EQ(array.data(), nullptr);
 }
 
-#if !_KERNEL
 TEST(ArrayTest, MakeArrayEmpty) {
   fbl::Array<uint32_t> array = fbl::MakeArray<uint32_t>(0);
   EXPECT_EQ(array.size(), 0);
@@ -145,6 +137,5 @@ TEST(ArrayTest, MakeArrayDefaultConstructed) {
     EXPECT_EQ(array[i].value, 42);
   }
 }
-#endif
 
 }  // namespace
