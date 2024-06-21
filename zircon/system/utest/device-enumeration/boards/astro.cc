b2baf7032e6f3d8f0c0f78d54d3b0ae2d5675b0d
Sarah Chan <spqchan@google.com>
2024-06-21 21:42:50 +0000

    [fdf] Migrate astro device-enumeration-test
    Migrate the astro device-enumeration-test to verify
    the node topology from driver development service
    using monikers.
    
    Bug: 345811829
    Change-Id: I5169ee854ff0f3bb38e75ea9977b626464945402
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1062249
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Fuchsia-Auto-Submit: Sarah Chan <spqchan@google.com>

--- zircon/system/utest/device-enumeration/boards/astro.cc
+++ zircon/system/utest/device-enumeration/boards/astro.cc
@@ -7,100 +7,98 @@
 namespace {
 
 TEST_F(DeviceEnumerationTest, AstroTest) {
-  static const char* kDevicePaths[] = {
-      "sys/platform/pt/astro",
-      "sys/platform/pt/astro/post-init/post-init",
-      "sys/platform/gpio/aml-gpio/gpio",
-      "sys/platform/gpio/aml-gpio/gpio-init",
-      "sys/platform/astro-buttons/astro-buttons/buttons",
-      "sys/platform/i2c-0/i2c-0/aml-i2c",
-      "sys/platform/i2c-1/i2c-1/aml-i2c",
-      "sys/platform/i2c-2/i2c-2/aml-i2c",
-      "sys/platform/aml_gpu/aml-gpu-composite/aml-gpu",
-      "sys/platform/aml-usb-phy/aml_usb_phy",
-      "sys/platform/bt-uart/bluetooth-composite-spec/aml-uart/bt-transport-uart",
-      "sys/platform/bt-uart/bluetooth-composite-spec/aml-uart/bt-transport-uart/bt-hci-broadcom",
+  static const char* kNodeMonikers[] = {
+      "dev.sys.platform.pt.astro",
+      "dev.sys.platform.pt.astro.post-init.post-init",
+      "dev.sys.platform.gpio.aml-gpio.gpio",
+      "dev.sys.platform.gpio.aml-gpio.gpio-init",
+      "dev.sys.platform.astro-buttons.astro-buttons.buttons",
+      "dev.sys.platform.i2c-0.i2c-0.aml-i2c",
+      "dev.sys.platform.i2c-1.i2c-1.aml-i2c",
+      "dev.sys.platform.i2c-2.i2c-2.aml-i2c",
+      "dev.sys.platform.aml_gpu.aml-gpu-composite.aml-gpu",
+      "dev.sys.platform.aml-usb-phy.aml_usb_phy",
+      "dev.sys.platform.bt-uart.bluetooth-composite-spec.aml-uart.bt-transport-uart",
+      "dev.sys.platform.bt-uart.bluetooth-composite-spec.aml-uart.bt-transport-uart.bt-hci-broadcom",
 
       // XHCI driver will not be loaded if we are in USB peripheral mode.
-      // "xhci/xhci/usb-bus",
+      // "xhci.xhci.usb-bus",
 
-      "sys/platform/i2c-2/i2c-2/aml-i2c/i2c/i2c-2-44/backlight/ti-lp8556",
-      "sys/platform/display/display/amlogic-display/display-coordinator",
-      "sys/platform/canvas/aml-canvas",
-      "sys/platform/tee/tee/optee",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/bl2/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/tpl/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/fts/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/factory/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/zircon-b/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/zircon-a/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/zircon-r/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/sys-config/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/migration/skip-block",
-      "sys/platform/raw_nand/raw_nand/aml-raw_nand/nand/fvm/ftl/block",
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc",
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc/sdmmc-sdio",
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc/sdmmc-sdio/sdmmc-sdio-1",
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc/sdmmc-sdio/sdmmc-sdio-2",
+      "dev.sys.platform.i2c-2.i2c-2.aml-i2c.i2c.i2c-2-44.backlight.ti-lp8556",
+      "dev.sys.platform.display.display.amlogic-display.display-coordinator",
+      "dev.sys.platform.canvas.aml-canvas",
+      "dev.sys.platform.tee.tee.optee",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.bl2.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.tpl.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.fts.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.factory.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.zircon-b.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.zircon-a.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.zircon-r.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.sys-config.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.migration.skip-block",
+      "dev.sys.platform.raw_nand.raw_nand.aml-raw_nand.nand.fvm.ftl.block",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc.sdmmc-sdio",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc.sdmmc-sdio.sdmmc-sdio-1",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc.sdmmc-sdio.sdmmc-sdio-2",
 
-      "sys/platform/i2c-0/i2c-0/aml-i2c/i2c/i2c-0-57/tcs3400_light/tcs-3400",
-      "sys/platform/astro-clk/clocks",
-      "sys/platform/astro-clk/clocks/clock-init",
-      "sys/platform/astro-i2s-audio-out/aml_tdm/astro-audio-i2s-out",
-      "sys/platform/astro-audio-pdm-in/aml_pdm/astro-audio-pdm-in",
-      "sys/platform/aml-secure-mem/aml_securemem/aml-securemem",
-      "sys/platform/pwm/aml-pwm-device/pwm-4/pwm_init",
+      "dev.sys.platform.i2c-0.i2c-0.aml-i2c.i2c.i2c-0-57.tcs3400_light.tcs-3400",
+      "dev.sys.platform.astro-clk.clocks",
+      "dev.sys.platform.astro-clk.clocks.clock-init",
+      "dev.sys.platform.astro-i2s-audio-out.aml_tdm.astro-audio-i2s-out",
+      "dev.sys.platform.astro-audio-pdm-in.aml_pdm.astro-audio-pdm-in",
+      "dev.sys.platform.aml-secure-mem.aml_securemem.aml-securemem",
+      "dev.sys.platform.pwm.aml-pwm-device.pwm-4.pwm_init",
 
       // CPU Device.
-      "sys/platform/aml-cpu",
-      "class/cpu-ctrl/000",
-      "sys/platform/aml-power-impl-composite/aml-power-impl-composite/power-impl/power-core/power-0/aml_cpu/s905d2-arm-a53",
+      "dev.sys.platform.aml-cpu",
+      "dev.sys.platform.aml-power-impl-composite.aml-power-impl-composite.power-impl.power-core.power-0.aml_cpu.s905d2-arm-a53",
       // LED.
-      "sys/platform/gpio-light/aml_light",
+      "dev.sys.platform.gpio-light.aml_light",
       // RAM (DDR) control.
-      "sys/platform/aml-ram-ctl/ram",
+      "dev.sys.platform.aml-ram-ctl.ram",
 
       // Power Device.
-      "sys/platform/aml-power-impl-composite/aml-power-impl-composite",
-      "sys/platform/aml-power-impl-composite/aml-power-impl-composite/power-impl/power-core",
-      "sys/platform/aml-power-impl-composite/aml-power-impl-composite/power-impl/power-core/power-0",
+      "dev.sys.platform.aml-power-impl-composite.aml-power-impl-composite",
+      "dev.sys.platform.aml-power-impl-composite.aml-power-impl-composite.power-impl.power-core",
+      "dev.sys.platform.aml-power-impl-composite.aml-power-impl-composite.power-impl.power-core.power-0",
 
       // Thermal
-      "sys/platform/05:03:a/thermal",
-      "sys/platform/aml-thermal-ddr/thermal",
-      "class/thermal/000",
-      "class/thermal/001",
+      "dev.sys.platform.aml-thermal-ddr.thermal",
+      "dev.sys.platform.05_03_a.thermal",
+      "dev.sys.platform.aml-thermal-ddr.thermal",
 
-      // Thermistor/ADC
-      "class/adc/000",
-      "class/adc/001",
-      "class/adc/002",
-      "class/adc/003",
-      "class/temperature/000",
-      "class/temperature/001",
-      "class/temperature/002",
-      "class/temperature/003",
+      // Thermistor.ADC
+      "dev.sys.platform.adc.aml-saradc.ASTRO_THERMISTOR_SOC",
+      "dev.sys.platform.adc.aml-saradc.ASTRO_THERMISTOR_WIFI",
+      "dev.sys.platform.adc.aml-saradc.ASTRO_THERMISTOR_DSP",
+      "dev.sys.platform.adc.aml-saradc.ASTRO_THERMISTOR_AMBIENT",
+      "dev.sys.platform.03_03_27.thermistor.thermistor-device.therm-soc",
+      "dev.sys.platform.03_03_27.thermistor.thermistor-device.therm-wifi",
+      "dev.sys.platform.03_03_27.thermistor.thermistor-device.therm-dsp",
+      "dev.sys.platform.03_03_27.thermistor.thermistor-device.therm-ambient",
+      "dev.sys.platform.05_03_a.thermal",
+      "dev.sys.platform.aml-thermal-ddr.thermal",
 
       // Registers Device.
-      "sys/platform/registers",
+      "dev.sys.platform.registers",
 #ifdef include_packaged_drivers
-      "sys/platform/05:03:e/aml_video",
+      "dev.sys.platform.05:03:e.aml_video",
 
       // WLAN
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc/sdmmc-sdio/sdmmc-sdio-1/wifi/brcmfmac-wlanphyimpl",
-      "sys/platform/aml-sdio/aml_sdio/aml-sd-emmc/sdmmc/sdmmc-sdio/sdmmc-sdio-1/wifi/brcmfmac-wlanphyimpl/wlanphy",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc.sdmmc-sdio.sdmmc-sdio-1.wifi.brcmfmac-wlanphyimpl",
+      "dev.sys.platform.aml-sdio.aml_sdio.aml-sd-emmc.sdmmc.sdmmc-sdio.sdmmc-sdio-1.wifi.brcmfmac-wlanphyimpl.wlanphy",
 #endif
 
   };
+  VerifyNodes(kNodeMonikers);
 
-  ASSERT_NO_FATAL_FAILURE(TestRunner(kDevicePaths, std::size(kDevicePaths)));
-
-  static const char* kTouchscreenDevicePaths[] = {
-      "sys/platform/i2c-1/i2c-1/aml-i2c/i2c/i2c-1-56/focaltech_touch/focaltouch-HidDevice",
-      "sys/platform/i2c-1/i2c-1/aml-i2c/i2c/i2c-1-93/gt92xx_touch/gt92xx-HidDevice",
+  static const char* kTouchscreenNodeMonikers[] = {
+      "dev.sys.platform.i2c-1.i2c-1.aml-i2c.i2c.i2c-1-56.focaltech_touch.focaltouch-HidDevice",
+      "dev.sys.platform.i2c-1.i2c-1.aml-i2c.i2c.i2c-1-93.gt92xx_touch.gt92xx-HidDevice",
   };
-  ASSERT_NO_FATAL_FAILURE(device_enumeration::WaitForOne(
-      cpp20::span(kTouchscreenDevicePaths, std::size(kTouchscreenDevicePaths))));
+  VerifyOneOf(kTouchscreenNodeMonikers);
 }
 
 }  // namespace
