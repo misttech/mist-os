24e09edc08eace1f25407fe55115e6506ef812e4
Josh Conner <joshconner@google.com>
2024-06-24 19:09:55 +0000

    [bt][testing] Add connection role to fake controller
    Add support for setting the LE connection role in the fake
    controller, reported in LE Connection Complete events.
    
    Bug: b/311639432
    Test: fx test //src/connectivity/bluetooth/core/bt-host
    Change-Id: Idcf5fdbb7b18a1404f9c507291f3863c36334127
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1066752
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Joshua Conner <joshconner@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/iso/iso_stream.cc
@@ -0,0 +1,7 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/connectivity/bluetooth/core/bt-host/public/pw_bluetooth_sapphire/internal/host/iso/iso_stream.h"
+
+namespace bt::iso {}  // namespace bt::iso
