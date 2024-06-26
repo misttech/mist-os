812c60f2173f0300ed058162861faa514037b582
Bruno Herrera <bruno.herrera@mist-os.com>
2024-06-26 10:15:35 -0300

    [mistos][starnix_lite] Support running linux apps
    Change-Id: I56203734218214f7ad59f08ee23b75c18227a87f

---
+++ src/starnix_lite/kernel/builtin/time.rs
@@ -0,0 +1,1 @@
+../../../sys/component_manager/src/builtin/time.rs
--- src/starnix_lite/kernel/main.rs
+++ src/starnix_lite/kernel/main.rs
@@ -12,11 +12,23 @@
 // unconditional dependency for rustdoc generation.
 use tracing_mutex as _;
 
-use mistos_bootfs::bootfs::BootfsSvc;
+use crate::bootfs::BootfsSvc;
+use crate::builtin::svc_controller::SvcController;
+use crate::builtin::time::create_utc_clock;
+use anyhow::{Context, Error};
+use builtins::vmex_resource::VmexResource;
+use fuchsia_component::server::*;
+use fuchsia_runtime::swap_utc_clock_handle;
+use futures::prelude::*;
 use mistos_logger::klog;
 use starnix_core::mm::{init_usercopy, zxio_maybe_faultable_copy_impl};
-use starnix_logging::log_info;
-use {fuchsia_runtime as fruntime, fuchsia_zircon as zx};
+use starnix_kernel_config::Config;
+use starnix_kernel_runner::{create_container_from_config, Container, ContainerServiceConfig};
+use starnix_logging::{log_info, log_warn};
+use {fuchsia_async as fasync, fuchsia_runtime as fruntime, fuchsia_zircon as zx};
+
+mod bootfs;
+mod builtin;
 
 extern "C" {
     fn dl_set_loader_service(
@@ -44,7 +56,16 @@ extern "C" fn zxio_fault_catching_disabled() -> bool {
     false
 }
 
-fn main() {
+async fn build_container(
+    config: Config,
+    returned_config: &mut Option<ContainerServiceConfig>,
+) -> Result<Container, Error> {
+    let (container, config) = create_container_from_config(config).await?;
+    *returned_config = Some(config);
+    Ok(container)
+}
+
+fn main() -> Result<(), Error> {
     klog::KernelLogger::init();
 
     log_info!("Starnix Lite is starting up...");
@@ -67,19 +88,111 @@ fn main() {
         panic!("Starnix Lite failed to set itself as critical: {}", err);
     }
 
+    let config = Config {
+        features: Default::default(),
+        init: vec!["/bin/hello_starnix".to_owned()],
+        kernel_cmdline: Default::default(),
+        mounts: vec!["/:remotefs".to_owned()],
+        rlimits: Default::default(),
+        name: "starnix_lite".to_owned(),
+        startup_file_path: Default::default(),
+        remote_block_devices: Default::default(),
+    };
+
+    let mut executor = fasync::LocalExecutor::new();
+    executor.run_singlethreaded(async_main(config)).context("async main")?;
+
+    Ok(())
+}
+
+async fn async_main(config: Config) -> Result<(), Error> {
     let system_resource_handle =
         fruntime::take_startup_handle(fruntime::HandleType::SystemResource.into())
             .map(zx::Resource::from);
 
+    let svc_stash_channel =
+        fruntime::take_startup_handle(fruntime::HandleInfo::new(fruntime::HandleType::User0, 0))
+            .map(zx::Channel::from)
+            .expect("Failed to get svc server channel");
+
     let bootfs_svc = BootfsSvc::new().expect("Failed to create bootfs");
+    let bootfs_svc = Some(bootfs_svc);
+    let clock = create_utc_clock(&bootfs_svc).await.expect("failed to create UTC clock");
+
+    // We are affecting the process-wide clock here, but since Rust test cases are run in their
+    // own process, this won't interact with other running tests.
+    let _ = swap_utc_clock_handle(clock).expect("failed to swap clocks");
+
     let bootfs_svc = bootfs_svc
+        .unwrap()
         .ingest_bootfs_vmo_with_system_resource(&system_resource_handle)
         .expect("Failed to ingest bootfs");
-    let _ = bootfs_svc.create_and_bind_vfs();
 
-    //let mut executor = fasync::SendExecutor::new(1);
+    let _ = bootfs_svc.create_and_bind_vfs().expect("failed to bind");
+
+    let mut service_fs = ServiceFs::new();
+
+    // Set up the VmexResource service.
+    let vmex_resource = system_resource_handle
+        .as_ref()
+        .and_then(|handle| {
+            handle
+                .create_child(
+                    zx::ResourceKind::SYSTEM,
+                    None,
+                    zx::sys::ZX_RSRC_SYSTEM_VMEX_BASE,
+                    1,
+                    b"vmex",
+                )
+                .ok()
+        })
+        .map(VmexResource::new)
+        .and_then(Result::ok)
+        .unwrap();
+
+    service_fs.add_fidl_service(move |stream| {
+        let vmex = vmex_resource.clone();
+        fasync::Task::spawn(async move {
+            let result = vmex.serve(stream).await;
+            if let Err(error) = result {
+                log_warn!(%error, "vmex failed");
+            }
+        })
+        .detach();
+    });
+
+    let svc_stash = SvcController::new(svc_stash_channel);
+    svc_stash.wait_for_epitaph().await;
+
+    // Bind to the channel
+    service_fs
+        .serve_connection(svc_stash.svc_endpoint().lock().take().expect("No svc channel found"))?;
+
+    // Run the service with its own executor to avoid reentrancy issues.
+    std::thread::spawn(move || {
+        fasync::LocalExecutor::new().run_singlethreaded(fasync::Task::spawn(async move {
+            service_fs.collect::<()>().await;
+        }));
+    });
 
     // We call this early during Starnix boot to make sure the usercopy utilities
     // are ready for use before any restricted-mode/Linux processes are created.
     init_usercopy();
+
+    let container = async_lock::OnceCell::<Container>::new();
+
+    let mut config_service: Option<ContainerServiceConfig> = None;
+
+    let container = container
+        .get_or_try_init(|| build_container(config, &mut config_service))
+        .await
+        .expect("failed to start container");
+
+    if let Some(config) = config_service {
+        container
+            .run(config)
+            .await
+            .expect("failed to run the expected services from the container");
+    }
+    Ok(())
 }
