f49df64ec48f3077bc7e124579d1c24c5539680f
Bruno Herrera <bruno.herrera@mist-os.com>
2024-06-26 09:53:58 -0300

    [mistos][starnix_lite] Add support for kernel runner
    Change-Id: Ieca032cd220a1f399d4eac005a9c089c42c24ea1

--- src/starnix/kernel/runner/lib.rs
+++ src/starnix/kernel/runner/lib.rs
@@ -1,15 +1,21 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
 // Copyright 2023 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #![recursion_limit = "512"]
 
+
+#[cfg(not(feature = "starnix_lite"))]
 mod component_runner;
 mod container;
 mod features;
+#[cfg(not(feature = "starnix_lite"))]
 mod serve_protocols;
 
+#[cfg(not(feature = "starnix_lite"))]
 pub use component_runner::*;
 pub use container::*;
 pub use features::*;
+#[cfg(not(feature = "starnix_lite"))]
 pub use serve_protocols::*;
