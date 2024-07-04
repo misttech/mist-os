d91077fcf1e5240265fff0aadc55a642d3fde1ad
Mark Dittmer <markdittmer@google.com>
2024-07-04 20:04:55 +0000

    [starnix][selinux] Fix kernel/security module doc-comment
    Change-Id: Ife26e8626bf25a95c452978d63a1f09bb1320e93
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1076815
    Commit-Queue: Mark Dittmer <markdittmer@google.com>
    Reviewed-by: Wez <wez@google.com>
    Fuchsia-Auto-Submit: Mark Dittmer <markdittmer@google.com>

--- src/starnix/kernel/security/mod.rs
+++ src/starnix/kernel/security/mod.rs
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// This module provides types and hook APIs supporting Linux Security Modules
-/// functionality in Starnix.  LSM provides a generic set of hooks, and opaque
-/// types, used to decouple the rest of the kernel from the details of any
-/// specific security enforcement subsystem (e.g. SELinux, POSIX.1e, etc).
-///
-/// Although this module is hard-wired to the SELinux implementation, callers
-/// should treat the types as opaque; hook implementations necessarily have access
-/// to kernel structures, but not the other way around.
+//! This module provides types and hook APIs supporting Linux Security Modules
+//! functionality in Starnix.  LSM provides a generic set of hooks, and opaque
+//! types, used to decouple the rest of the kernel from the details of any
+//! specific security enforcement subsystem (e.g. SELinux, POSIX.1e, etc).
+//!
+//! Although this module is hard-wired to the SELinux implementation, callers
+//! should treat the types as opaque; hook implementations necessarily have access
+//! to kernel structures, but not the other way around.
 use selinux::SecurityId;
 
 /// SELinux implementations called by the LSM hooks.
