c2fabc583b9759b9f74bbabc7ea808c3456922d1
Christopher R. Johnson <crjohns@google.com>
2024-07-10 00:14:17 +0000

    [diagnostics] Create python inspect parsing library
    Change-Id: Ia06b978f549daa480e5758e81d05f36618fe95a4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1074461
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Fuchsia-Auto-Submit: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/lib/diagnostics/python/fuchsia_inspect/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from .lib import *
+
+__all__ = [
+    "InspectData",
+    "InspectDataError",
+    "InspectMetadata",
+    "InspectMetadataError",
+    "InvalidFieldError",
+    "MissingFieldError",
+    "Timestamp",
+    "VersionMismatchError",
+]
