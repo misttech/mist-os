88caf10cc80092fc7146b35309ffecc5e69442ce
Bruno Herrera <bruno.herrera@mist-os.com>
2024-07-16 18:09:18 -0300

    [mistos][image] Add minimal toybox/mistos container
    Change-Id: I9d0741d10bd89f975913e55674d4d4d46c37e4b7

---
+++ examples/toybox/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -x
+set -e
+
+wget http://landley.net/toybox/bin/toybox-x86_64 -O toybox-x86_64
