c300710d17ba3b5c496050e9544d333a99bbd631
Adam Perry <adamperry@google.com>
2024-07-16 23:14:55 +0000

    [starnix] getrandom() writes values page-by-page.
    Fixed: 349187897
    Change-Id: I23f5a573db5113ca81d6cf8d7d9fdbdfac88b058
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1072393
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Commit-Queue: Adam Perry <adamperry@google.com>
    Reviewed-by: James Robinson <jamesr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/starnix/tests/syscalls/cpp/getrandom_test.cc
@@ -0,0 +1,44 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <gtest/gtest.h>
+
+#include "test_helper.h"
+
+namespace {
+
+// Define our own wrapper so that libc doesn't do anything surprising.
+ssize_t sys_getrandom(void* buffer, size_t buffer_size, unsigned int flags) {
+  return syscall(__NR_getrandom, buffer, buffer_size, flags);
+}
+
+TEST(GetRandomTest, BasicSuccess) {
+  int64_t buffer_len = 0x1000000ul;
+  auto buffer = ASSERT_RESULT_SUCCESS_AND_RETURN(test_helper::ScopedMMap::MMap(
+      nullptr, buffer_len, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+  EXPECT_EQ(sys_getrandom(buffer.mapping(), buffer_len, 0), buffer_len);
+}
+
+TEST(GetRandomTest, OnlyWritesValidMappedBuffer) {
+  uint64_t page_size = sysconf(_SC_PAGESIZE);
+  int64_t buffer_len = 0x1000000ul;
+
+  // First, map the full buffer we expect plus an additional page.
+  auto buffer = ASSERT_RESULT_SUCCESS_AND_RETURN(test_helper::ScopedMMap::MMap(
+      nullptr, buffer_len + page_size, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+
+  // Then unmap the last page.
+  ASSERT_FALSE(munmap((void*)(static_cast<std::byte*>(buffer.mapping()) + buffer_len), page_size))
+      << strerror(errno);
+
+  // We should only see random bytes up to the end of the mapped region.
+  EXPECT_EQ(sys_getrandom(buffer.mapping(), buffer_len * 2, 0), buffer_len);
+}
+
+}  // anonymous namespace
