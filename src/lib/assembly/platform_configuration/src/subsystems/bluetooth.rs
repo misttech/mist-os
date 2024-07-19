760df92ad84151cb6879831af3da643047353f47
Ani Ramakrishnan <aniramakri@google.com>
2024-07-19 17:30:13 +0000

    [assembly][bt] Correctly add bt-transport-uart driver to assembly
    As is, the driver is only included on builds that require "Standard"
    bluetooth (e.g. all Bluetooth components). It is valid for the driver to
    be included on "Coreless" builds as well, so move up the platform bundle
    inclusion before the early return in the config.
    
    Test: Manual
    Bug: 354005030
    
    Change-Id: I954f720aa37fef8f7da7ba8de5cdf7b28c503830
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1084662
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Fuchsia-Auto-Submit: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: David Iserovich <diserovich@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/assembly/platform_configuration/src/subsystems/bluetooth.rs
+++ src/lib/assembly/platform_configuration/src/subsystems/bluetooth.rs
@@ -27,6 +27,15 @@ impl DefineSubsystemConfiguration<BluetoothConfig> for BluetoothSubsystemConfig 
             }
         }
 
+        // Include bt-transport-uart driver through a platform AIB.
+        if context.board_info.provides_feature("fuchsia::bt_transport_uart") {
+            if *context.feature_set_level == FeatureSupportLevel::Standard
+                || *context.feature_set_level == FeatureSupportLevel::Utility
+            {
+                builder.platform_bundle("bt_transport_uart_driver");
+            }
+        }
+
         let BluetoothConfig::Standard { profiles, .. } = config else {
             return Ok(());
         };
@@ -54,11 +63,6 @@ impl DefineSubsystemConfiguration<BluetoothConfig> for BluetoothSubsystemConfig 
             && *context.build_type == BuildType::Eng
         {
             builder.platform_bundle("bluetooth_pandora");
-        }
-
-        // Include bt-transport-uart driver through a platform AIB.
-        if context.board_info.provides_feature("fuchsia::bt_transport_uart") {
-            builder.platform_bundle("bt_transport_uart_driver");
         }
 
         Ok(())
