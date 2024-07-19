5c7178616dbf7317624a1a5dbea1c6223e57c3d4
Victor Costan <pwnall@chromium.org>
2024-07-19 04:39:52 +0000

    [platform-device] Fix bad optional access.
    Bug: 354051142
    Test: Local testing with new driver prototype.
    Change-Id: I5038225ca8d9ddf8b56937ccf149a6ac27564adc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1084720
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Commit-Queue: Victor Costan <costan@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

--- src/devices/bus/drivers/platform/node-util.cc
+++ src/devices/bus/drivers/platform/node-util.cc
@@ -4,66 +4,67 @@
 
 #include "src/devices/bus/drivers/platform/node-util.h"
 
+#include <optional>
+#include <string>
+
 namespace platform_bus {
+
+namespace {
 
 // Most drivers only have a few of each resource, so we have just kept these as
 // linear searches for simplicity. We can consider pre-constructing a map for
 // each resource and storing it with the platform device if performance is an issue.
 
-// Returns the index of the mmio that matches |mmio_name|.
-std::optional<uint32_t> GetMmioIndex(const fuchsia_hardware_platform_bus::Node& node,
-                                     std::string_view mmio_name) {
-  if (node.mmio() == std::nullopt) {
+template <typename ResourceType>
+std::optional<uint32_t> GetResourceIndex(
+    const std::optional<std::vector<ResourceType>>& node_resources,
+    std::string_view node_resource_name) {
+  if (!node_resources.has_value()) {
     return std::nullopt;
   }
-  for (size_t i = 0; i < node.mmio()->size(); i++) {
-    if (node.mmio().value()[i].name().value().compare(mmio_name) == 0) {
-      return i;
+
+  const std::vector<ResourceType>& resources = node_resources.value();
+  for (size_t resource_index = 0; resource_index < resources.size(); ++resource_index) {
+    const ResourceType& resource = resources[resource_index];
+
+    // It's valid for resources not to have names. Unnamed resources are
+    // inaccessible using Get*ByName(), and can only be retrieved by Get*ById().
+    const std::optional<std::string>& resource_name = resource.name();
+    if (!resource_name.has_value()) {
+      continue;
+    }
+
+    if (resource_name.value() == node_resource_name) {
+      return resource_index;
     }
   }
   return std::nullopt;
+}
+
+}  // namespace
+
+// Returns the index of the mmio that matches |mmio_name|.
+std::optional<uint32_t> GetMmioIndex(const fuchsia_hardware_platform_bus::Node& node,
+                                     std::string_view mmio_name) {
+  return GetResourceIndex(node.mmio(), mmio_name);
 }
 
 // Returns the index of the irq that matches |irq_name|.
 std::optional<uint32_t> GetIrqIndex(const fuchsia_hardware_platform_bus::Node& node,
                                     std::string_view irq_name) {
-  if (node.mmio() == std::nullopt) {
-    return std::nullopt;
-  }
-  for (size_t i = 0; i < node.irq()->size(); i++) {
-    if (node.irq().value()[i].name().value().compare(irq_name) == 0) {
-      return i;
-    }
-  }
-  return std::nullopt;
+  return GetResourceIndex(node.irq(), irq_name);
 }
 
 // Returns the index of the bti that matches |bti_name|.
 std::optional<uint32_t> GetBtiIndex(const fuchsia_hardware_platform_bus::Node& node,
                                     std::string_view bti_name) {
-  if (node.bti() == std::nullopt) {
-    return std::nullopt;
-  }
-  for (size_t i = 0; i < node.bti()->size(); i++) {
-    if (node.bti().value()[i].name().value().compare(bti_name) == 0) {
-      return i;
-    }
-  }
-  return std::nullopt;
+  return GetResourceIndex(node.bti(), bti_name);
 }
 
 // Returns the index of the smc that matches |smc_name|.
 std::optional<uint32_t> GetSmcIndex(const fuchsia_hardware_platform_bus::Node& node,
                                     std::string_view smc_name) {
-  if (node.smc() == std::nullopt) {
-    return std::nullopt;
-  }
-  for (size_t i = 0; i < node.smc()->size(); i++) {
-    if (node.smc().value()[i].name().value().compare(smc_name) == 0) {
-      return i;
-    }
-  }
-  return std::nullopt;
+  return GetResourceIndex(node.smc(), smc_name);
 }
 
 }  // namespace platform_bus
