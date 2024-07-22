af86896ab3e5562809216731d8c4b14542f18eaa
Joshua Seaton <joshuaseaton@google.com>
2024-07-22 21:04:26 +0000

    [kernel][phys] Truncate NVRAM when kernel.memory-limit-mb is set
    physboot currently forwards the NVRAM range to the kernel proper as it
    existed in the ZBI without taking into account the possible truncation
    of the physical address space that may have been performed in handling
    kernel.memory-limit-mb.
    
    Fixed: 350747248
    Change-Id: I5a41799171b8d0343aeeec8267e30eb59e4a43d4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1086919
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

--- zircon/kernel/phys/handoff-prep-zbi.cc
+++ zircon/kernel/phys/handoff-prep-zbi.cc
@@ -24,6 +24,7 @@
 #include <fbl/algorithm.h>
 #include <ktl/algorithm.h>
 #include <ktl/byte.h>
+#include <ktl/iterator.h>
 #include <ktl/limits.h>
 #include <ktl/span.h>
 #include <ktl/type_traits.h>
@@ -85,12 +86,26 @@ void HandoffPrep::SummarizeMiscZbiItems(ktl::span<ktl::byte> zbi) {
         handoff_->reboot_reason = *reinterpret_cast<const zbi_hw_reboot_reason_t*>(payload.data());
         break;
 
-      case ZBI_TYPE_NVRAM:
+      case ZBI_TYPE_NVRAM: {
         ZX_ASSERT(payload.size() >= sizeof(zbi_nvram_t));
-        handoff_->nvram = *reinterpret_cast<const zbi_nvram_t*>(payload.data());
+        const zbi_nvram_t& nvram = *reinterpret_cast<const zbi_nvram_t*>(payload.data());
+
+        // We may have truncated the address range (per kernel.memory-limit-mb),
+        // so be sure to truncate the NVRAM range accordingly.
+        auto back = ktl::prev(Allocation::GetPool().end());
+        while (!memalloc::IsRamType(back->type)) {
+          --back;
+        }
+        uint64_t addr_cutoff = back->end();
+        if (nvram.base >= addr_cutoff) {
+          break;
+        }
+        handoff_->nvram = nvram;
+        handoff_->nvram->length = ktl::min(nvram.length, addr_cutoff - nvram.base);
+
         SaveForMexec(*header, payload);
         break;
-
+      }
       case ZBI_TYPE_PLATFORM_ID:
         ZX_ASSERT(payload.size() >= sizeof(zbi_platform_id_t));
         handoff_->platform_id = *reinterpret_cast<const zbi_platform_id_t*>(payload.data());
