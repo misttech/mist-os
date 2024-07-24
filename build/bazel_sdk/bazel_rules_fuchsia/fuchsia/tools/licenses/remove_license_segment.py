0aacbd1f0f355e4e8032cbe772913e3e210462f5
Jerry Belton <jcecil@google.com>
2024-07-24 17:11:21 +0000

    [licenses] Support pruning of part of a license text.
    At times, an upstream dependency may include license texts for things
    that we don't care about (e.g. test files). We don't want to ship
    those licenses, but removing the text from the upstream spdx file
    can be challenging / impossible.
    
    This adds support for removing a part of the SPDX license text
    before collecting / classifying it. Right now it simply removes
    everything after a provided matching string, but this can easily
    be expanded to support more complicated / flexible regular
    expressions.
    
    Bug: b/354735218
    Change-Id: I8d4f2415c3d370e5583ea6fc5e4aea55d5b49963
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1087891
    Commit-Queue: Jerry Belton <jcecil@google.com>
    Reviewed-by: Darren Chan <chandarren@google.com>

---
+++ build/bazel_sdk/bazel_rules_fuchsia/fuchsia/tools/licenses/remove_license_segment.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import json
+import sys
+import re
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--license_id", required=True)
+    parser.add_argument("--cut_after", required=True)
+    parser.add_argument("--exit_on_failure", default=False)
+    args = parser.parse_args()
+
+    with open(args.input, "r") as spdx:
+        data = json.load(spdx)
+
+    found_segment = False
+    error_msg = "Did not find licenseID {} in spdx file {}.".format(
+        args.license_id, args.input
+    )
+    for d in data["hasExtractedLicensingInfos"]:
+        if d["licenseId"] == args.license_id:
+            error_msg = (
+                "Did not find string pattern {} in license text.".format(
+                    args.cut_after
+                )
+            )
+            text = d["extractedText"]
+            match = re.search(args.cut_after, text)
+            if match:
+                d["extractedText"] = text[: match.end()]
+                found_segment = True
+            break
+
+    if not found_segment:
+        if args.exit_on_failure:
+            raise ValueError(error_msg)
+        else:
+            print(error_msg)
+
+    with open(args.output, "w") as spdx:
+        json.dump(data, spdx, ensure_ascii=False, indent="    ")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
