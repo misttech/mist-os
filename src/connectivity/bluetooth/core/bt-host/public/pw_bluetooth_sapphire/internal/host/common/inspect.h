6c1fb8879937a16ab3cf4bfeb2c7a19d56ed181d
Eric Rahm <erahm@google.com>
2024-07-30 19:00:12 +0000

    [bt][host] Handle switch warning with pigweed
    This switches over to using `pw_preproccesor` to annotate and disable
    switch related warnings. Adding this to `inspect.h` allows us to
    explicitly enable the switch warning in order to avoid future
    regressions in downstream users.
    
    Bug: b/355511476
    Test: fx test //src/connectivity/bluetooth/core/bt-host
    
    Change-Id: I427da4a5af186560d328ba3ad96c86845cbb7b2d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1091334
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Eric Rahm <erahm@google.com>

--- src/connectivity/bluetooth/core/bt-host/public/pw_bluetooth_sapphire/internal/host/common/inspect.h
+++ src/connectivity/bluetooth/core/bt-host/public/pw_bluetooth_sapphire/internal/host/common/inspect.h
@@ -10,7 +10,11 @@
 #ifdef NINSPECT
 #include "src/connectivity/bluetooth/core/bt-host/public/pw_bluetooth_sapphire/internal/host/common/fake_inspect.h"
 #else
+#include "pw_preprocessor/compiler.h"
+PW_MODIFY_DIAGNOSTICS_PUSH();
+PW_MODIFY_DIAGNOSTIC(ignored, "-Wswitch-enum");
 #include <lib/inspect/cpp/inspect.h>
+PW_MODIFY_DIAGNOSTICS_POP();
 #endif  // NINSPECT
 
 #endif  // SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_PUBLIC_PW_BLUETOOTH_SAPPHIRE_INTERNAL_HOST_COMMON_INSPECT_H_
