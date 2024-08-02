1d864ed7e73cbf7580e6d281c8c44f5059f0e43f
Ida Isugi <isugiidap@google.com>
2024-08-02 20:40:58 +0000

    [system_monitor] Add a function to return CPU data Integration and unit test to verify that CPU data will never be empty. Test: fx test system_monitor_integration_test
    Change-Id: I167137c5417b242241fa2d5f1bf182c0b5d40f96
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1091553
    Reviewed-by: Caroline Liu <carolineliu@google.com>
    Reviewed-by: Emircan Uysaler <emircan@google.com>
    Commit-Queue: Ida Isugi <isugiidap@google.com>

---
+++ src/ui/bin/system_monitor/tests/system_monitor_unittest.cc
@@ -0,0 +1,11 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/ui/bin/system_monitor/system_monitor.h"
+
+#include <gtest/gtest.h>
+#include <src/lib/testing/loop_fixture/test_loop_fixture.h>
+
+class SystemMonitorUnitTest : public gtest::TestLoopFixture {};
+TEST_F(SystemMonitorUnitTest, RunTests) {}
