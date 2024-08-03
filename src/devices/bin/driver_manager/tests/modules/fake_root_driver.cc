49d83587e4e24c3dcc12037fb373bfab6a709255
Jocelyn Dang <jocelyndang@google.com>
2024-08-03 00:04:28 +0000

    [driver_manager] Node changes for loading a driver w/ dynamic linking.
    Also adds some new test modules for loading. These will be expanded on
    in a future CL once the loading code is also up for review. Currently
    all the test is checking is that the node can extract the correct driver
    module and libs dir.
    
    Bug: 341998660
    Change-Id: If261d092fa85624bebc7575ab1558bb7897df2e5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1093092
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Novin Changizi <novinc@google.com>

---
+++ src/devices/bin/driver_manager/tests/modules/fake_root_driver.cc
@@ -0,0 +1,8 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "deps.h"
+#include "driver_entry_point.h"
+
+int64_t DriverStart() { return 0; }
