3dfd5e8a643fa2093b522e8b6a006f2e597cfe07
Suraj Malhotra <surajmalhotra@google.com>
2024-08-04 06:03:05 +0000

    [camera-controller][sysmem] Connect to sysmem via incoming namespace
    Change-Id: I6fef4e237a06612a6abbadfdc1169f821c754734
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1034225
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Yilong Li <liyl@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/camera/drivers/controller/test/fake_sysmem.h
+++ src/camera/drivers/controller/test/fake_sysmem.h
@@ -5,37 +5,24 @@
 #ifndef SRC_CAMERA_DRIVERS_CONTROLLER_TEST_FAKE_SYSMEM_H_
 #define SRC_CAMERA_DRIVERS_CONTROLLER_TEST_FAKE_SYSMEM_H_
 
-#include <fidl/fuchsia.hardware.sysmem/cpp/wire_test_base.h>
+#include <fidl/fuchsia.sysmem2/cpp/wire_test_base.h>
 #include <fuchsia/sysmem/cpp/fidl.h>
 #include <lib/async/default.h>
 #include <lib/sys/cpp/component_context.h>
 
-#include "fidl/fuchsia.hardware.sysmem/cpp/markers.h"
-
-class FakeSysmem : public fidl::testing::WireTestBase<fuchsia_hardware_sysmem::Sysmem> {
+class FakeSysmem : public fidl::testing::WireTestBase<fuchsia_sysmem2::Allocator> {
  public:
   void NotImplemented_(const std::string& name, ::fidl::CompleterBase& completer) override {
     completer.Close(ZX_ERR_NOT_SUPPORTED);
   }
 
-  fuchsia_hardware_sysmem::Service::InstanceHandler CreateInstanceHandler() {
-    return fuchsia_hardware_sysmem::Service::InstanceHandler({
-        .sysmem = sysmem_bindings_.CreateHandler(this, async_get_default_dispatcher(),
-                                                 fidl::kIgnoreBindingClosure),
-        .allocator_v1 = [](fidl::ServerEnd<fuchsia_sysmem::Allocator> request) {},
-        .allocator_v2 = [](fidl::ServerEnd<fuchsia_sysmem2::Allocator> request) {},
-    });
-  }
-
-  fidl::ClientEnd<fuchsia_hardware_sysmem::Sysmem> Connect() {
-    auto endpoints = fidl::Endpoints<fuchsia_hardware_sysmem::Sysmem>::Create();
-    sysmem_bindings_.AddBinding(async_get_default_dispatcher(), std::move(endpoints.server), this,
+  void Connect(fidl::ServerEnd<fuchsia_sysmem2::Allocator> request) {
+    sysmem_bindings_.AddBinding(async_get_default_dispatcher(), std::move(request), this,
                                 fidl::kIgnoreBindingClosure);
-    return std::move(endpoints.client);
   }
 
  private:
-  fidl::ServerBindingGroup<fuchsia_hardware_sysmem::Sysmem> sysmem_bindings_;
+  fidl::ServerBindingGroup<fuchsia_sysmem2::Allocator> sysmem_bindings_;
 };
 
 #endif  // SRC_CAMERA_DRIVERS_CONTROLLER_TEST_FAKE_SYSMEM_H_
