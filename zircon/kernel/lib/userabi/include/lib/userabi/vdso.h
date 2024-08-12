c607e04b1675936e19c1a372ab33dc35c6aee518
Anirudh Mathukumilli <rudymathu@google.com>
2024-08-12 17:27:23 +0000

    [kernel] Teach IdlePowerThread how to pause the monotonic clock
    Adds the functionality needed to pause the monotonic clock to the
    IdlePowerThread. All of this code is checked in behind a feature flag
    that is disabled, so this CL should have no functional impacts. It will
    give us a chance, however, to review the code and get it checked in so
    that we can flip the flag as soon as the boot timeline RFC is accepted.
    
    Bug: 341785588
    Change-Id: Ia8c5b34c909672977c97ec183e2e5b5ff003fec8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1093967
    Fuchsia-Auto-Submit: Anirudh Mathukumilli <rudymathu@google.com>
    Commit-Queue: Anirudh Mathukumilli <rudymathu@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Reviewed-by: John Grossman <johngro@google.com>

--- zircon/kernel/lib/userabi/include/lib/userabi/vdso.h
+++ zircon/kernel/lib/userabi/include/lib/userabi/vdso.h
@@ -54,10 +54,9 @@ class VDso : public RoDso {
   // for entering the kernel with <syscall-name>'s syscall number.
   struct ValidSyscallPC;
 
-  // Adds the additional ticks value to the monotonic ticks offset in all of the vDSO variants'
-  // TimeValues structure. This will be called on system resume to update the amount of time the
-  // system spent in a suspended state.
-  static void AddMonotonicTicksOffset(zx_ticks_t additional);
+  // Sets the monotonic ticks offset in all of the vDSO variants' TimeValues structure. This will
+  // be called on system resume to update the amount of time the system spent in a suspended state.
+  static void SetMonotonicTicksOffset(zx_ticks_t new_offset);
 
  private:
   using Variant = userboot::VdsoVariant;
