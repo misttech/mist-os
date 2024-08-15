d6d1869682796d41eeae5e673576669c082ef1a6
Adrian Danis <adanis@google.com>
2024-08-15 03:25:56 +0000

    [dwmac] Implement NetworkDevice protocol
    Directly implements the NetworkDevice protocol in the dwmac driver
    instead of relying on the netdevice-migration adapter.
    
    This is almost universally an improvement in benchmarks, with the
    exception of udp/send having higher CPU usage. However, this higher
    CPU usage is corrolated with increases in throughput, so it's not
    clear this is the driver's fault.
    
    Bug: 42068759
    Change-Id: I97faa35e22a9e3bb786820d0a15a4a105001a3ba
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1089712
    Commit-Queue: Adrian Danis <adanis@google.com>
    Reviewed-by: Bjoern Johansson <bjoernj@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- zircon/system/utest/device-enumeration/boards/vim3.cc
+++ zircon/system/utest/device-enumeration/boards/vim3.cc
@@ -25,9 +25,7 @@ TEST_F(DeviceEnumerationTest, Vim3DeviceTreeTest) {
       "dev.sys.platform.clock-controller-ff63c000.clocks",
       "dev.sys.platform.clock-controller-ff63c000.clocks.clock-init",
       "dev.sys.platform.dsi-display-ff900000",
-      // TODO(https://fxbug.dev/42068759): Update topopath when dwmac is off
-      // netdevice migration.
-      "dev.sys.platform.ethernet-phy-ff634000.ethernet-phy-ff634000_group.aml-ethernet.dwmac-ff3f0000_group.dwmac.Designware-MAC.netdevice-migration.network-device",
+      "dev.sys.platform.ethernet-phy-ff634000.ethernet-phy-ff634000_group.aml-ethernet.dwmac-ff3f0000_group.dwmac.Designware-MAC.network-device",
       "dev.sys.platform.ethernet-phy-ff634000.ethernet-phy-ff634000_group.aml-ethernet.dwmac-ff3f0000_group.dwmac.eth_phy.phy_null_device",
       "dev.sys.platform.fuchsia-sysmem.sysmem",
       "dev.sys.platform.gpio-buttons.gpio-buttons_group.buttons",
