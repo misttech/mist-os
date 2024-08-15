cbd2f9b5af15e142276a33d72f3ee786299343db
Yilong Li <liyl@google.com>
2024-08-15 00:50:19 +0000

    [display-tweak] Migrate to CoordinatorListener for event handling.
    Test: "display-tweak panel --power on" on VIM3.
    Test: "display-tweak panel --power off" on VIM3.
    Bug: 355334166
    
    Change-Id: I101d34ad5e81c63be6c3fdddcecc2d04221f55ab
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1094374
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

--- src/graphics/display/bin/display-tweak/src/utils.rs
+++ src/graphics/display/bin/display-tweak/src/utils.rs
@@ -14,18 +14,6 @@ pub fn flatten_zx_error<T>(
         .map_err(|zx_status| anyhow!("Server response: {}", zx::Status::from_raw(zx_status)))
 }
 
-/// Unwraps the result of a FIDL call that returns a zx::result into a
-/// Result<T, E>. If the result is an error, it has `context` attached to it.
-pub fn flatten_zx_status(
-    fidl_result: Result<zx::sys::zx_status_t, fidl::Error>,
-) -> Result<(), Error> {
-    // Make the return type look like the FIDL call errors with zx.Status.
-    let zx_error_result =
-        fidl_result
-            .map(|zx_status| if zx_status == zx::sys::ZX_OK { Ok(()) } else { Err(zx_status) });
-    flatten_zx_error(zx_error_result)
-}
-
 /// Helper for accepting boolean values as "off" / "on" strings.
 pub fn on_off_to_bool(value: &str) -> Result<bool, String> {
     match value {
@@ -66,37 +54,6 @@ mod tests {
         let fidl_result: Result<Result<i32, zx::sys::zx_status_t>, fidl::Error> =
             Err(fidl_error.clone());
         let result: Result<i32, Error> = flatten_zx_error(fidl_result);
-
-        assert_matches!(result, Err(_));
-        let result_error: Error = result.unwrap_err();
-        assert_eq!(result_error.to_string(), format!("{}", fidl_error));
-    }
-
-    #[fuchsia::test]
-    fn flatten_zx_status_success() {
-        let fidl_result: Result<zx::sys::zx_status_t, fidl::Error> = Ok(zx::sys::ZX_OK);
-        assert_matches!(flatten_zx_status(fidl_result), Ok(()));
-    }
-
-    #[fuchsia::test]
-    fn flatten_zx_status_annotates_zx_error() {
-        let fidl_result: Result<zx::sys::zx_status_t, fidl::Error> =
-            Ok(zx::sys::ZX_ERR_NOT_SUPPORTED);
-        let result: Result<(), Error> = flatten_zx_status(fidl_result);
-
-        assert_matches!(result, Err(_));
-        let result_error: Error = result.unwrap_err();
-        assert_eq!(result_error.to_string(), "Server response: NOT_SUPPORTED");
-    }
-
-    #[fuchsia::test]
-    fn flatten_zx_status_annotates_fidl_error() {
-        let fidl_error = fidl::Error::ClientChannelClosed {
-            status: zx::Status::PEER_CLOSED,
-            protocol_name: "TestService",
-        };
-        let fidl_result: Result<zx::sys::zx_status_t, fidl::Error> = Err(fidl_error.clone());
-        let result: Result<(), Error> = flatten_zx_status(fidl_result);
 
         assert_matches!(result, Err(_));
         let result_error: Error = result.unwrap_err();
