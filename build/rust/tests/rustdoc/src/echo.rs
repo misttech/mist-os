4680fdf1031ce4c53d3fb9c2c9e0a9615a8ec292
EtomicBomb <ethanws@google.com>
2024-08-20 01:01:27 +0000

    [rust][build] .rustdoc subtarget (libs) + merging
    * This initial change adds .rustdoc subtargets to first and third party
      rust crates
    * It adds the `fx rustdoc-link` script to merge documentation generated
      by the .rustdoc subtargets
    * The subtargets can be opted out with a per-target disable_rustdoc
      flag
    * With the the global include_rustdoc (default false) is an opt-out
      mechanism for rustdoc subtargets. Rustdoc subtargets are only defined
      for include_rustdoc = true && !disable_rustdoc
    * Only includes .rustdoc subtargets for rust_library and rustc_library
      (addressed in subsequent CL)
    
    Bug: 360939846, 360939862
    Change-Id: I70f05d3f1c8fb050520d80687262a80ebc37c4ab
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1089251
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Reviewed-by: Julia Ryan <pineapple@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Ethan Williams <ethanws@google.com>

---
+++ build/rust/tests/rustdoc/src/echo.rs
@@ -0,0 +1,7 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#![allow(unused_crate_dependencies, unused_extern_crates)]
+extern crate foxtrot;
+pub enum Echo {}
+impl foxtrot::Foxtrot for Echo {}
