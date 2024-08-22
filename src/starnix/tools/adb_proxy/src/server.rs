a72e394e08b4e0f8d97f1991aeaed91a0d99b2dd
David Iserovich <diserovich@google.com>
2024-08-22 23:22:09 +0000

    [starnix] add adb_proxy utility
    Add an adb_proxy utility component which can open a Fuchsia vsock to proxy
    adb to starnix.
    
    Change-Id: Ie1660c3487b27e8ca4af9ad9b0e3e090345664e0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1106256
    Reviewed-by: James Robinson <jamesr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: David Iserovich <diserovich@google.com>

---
+++ src/starnix/tools/adb_proxy/src/server.rs
@@ -0,0 +1,79 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::{anyhow, Context, Result};
+use fidl::endpoints;
+use fidl_fuchsia_starnix_container::{ControllerMarker, ControllerVsockConnectRequest};
+use fidl_fuchsia_vsock::{
+    AcceptorMarker, AcceptorRequest, ConnectionMarker, ConnectionProxy, ConnectionTransport,
+    ConnectorMarker,
+};
+use fuchsia_component::client::connect_to_protocol;
+use futures::StreamExt;
+
+pub struct ProxyServer {
+    connections: Vec<ConnectionProxy>,
+}
+
+const ADB_DEFAULT_PORT: u32 = 5555;
+
+async fn handle_connection(host_socket: fidl::Socket) -> Result<()> {
+    let controller = connect_to_protocol::<ControllerMarker>()
+        .context("Failed to connect to starnix controller service")?;
+
+    controller
+        .vsock_connect(ControllerVsockConnectRequest {
+            port: Some(ADB_DEFAULT_PORT),
+            bridge_socket: Some(host_socket),
+            ..Default::default()
+        })
+        .map_err(|e| anyhow!("Error connecting to adbd: {:?}", e))?;
+
+    Ok(())
+}
+
+fn make_con() -> Result<(fidl::Socket, ConnectionProxy, ConnectionTransport), anyhow::Error> {
+    let (client_socket, server_socket) = fidl::Socket::create_stream();
+    let (client_end, server_end) = endpoints::create_endpoints::<ConnectionMarker>();
+    let client_end = client_end.into_proxy()?;
+    let con = ConnectionTransport { data: server_socket, con: server_end };
+    Ok((client_socket, client_end, con))
+}
+
+impl ProxyServer {
+    pub fn new() -> Self {
+        ProxyServer { connections: Vec::new() }
+    }
+
+    pub async fn start(&mut self, port: u32) -> Result<()> {
+        tracing::info!("Connecting to VSOCK protocol");
+        let app_client = connect_to_protocol::<ConnectorMarker>();
+
+        let (acceptor_remote, acceptor_client) = endpoints::create_endpoints::<AcceptorMarker>();
+        let mut acceptor_client = acceptor_client.into_stream()?;
+
+        let status = app_client?.listen(port, acceptor_remote).await?;
+
+        tracing::info!("Listening for VSOCK connections. status={}", status);
+
+        while let Some(Ok(AcceptorRequest::Accept { addr: _addr, responder })) =
+            acceptor_client.next().await
+        {
+            tracing::info!("Incoming VSOCK connection.");
+            let (data_socket, client_end, con) = make_con().context("Making connection")?;
+            self.connections.push(client_end);
+            responder.send(Some(con)).context("Sending con")?;
+
+            match handle_connection(data_socket).await {
+                Ok(_) => (),
+                Err(e) => {
+                    tracing::error!("Failed to handle connection: {:?}", e);
+                    continue;
+                }
+            };
+        }
+
+        Ok(())
+    }
+}
