c7e2e703f9eeb03e8b42365a15362fd4865ee076
Brian Bosak <bbosak@google.com>
2024-08-27 18:34:39 +0000

    [logs] API cleanup
    * Add additional comments
    * Convert comments for functions to doc comments
    * Remove old reference to VLOG
    
    Change-Id: I57164572bfe57e86f1728f9064745b53c7e660e0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1108016
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- sdk/lib/syslog/cpp/macros.h
+++ sdk/lib/syslog/cpp/macros.h
@@ -257,8 +257,8 @@ void WriteStructuredLog(fuchsia_logging::LogSeverity severity, const char* file,
 
 namespace fuchsia_logging {
 
-// Used to denote a key-value pair for use in structured logging API calls.
-// This macro exists solely to improve readability of calls to FX_LOG_KV
+/// Used to denote a key-value pair for use in structured logging API calls.
+/// This macro exists solely to improve readability of calls to FX_LOG_KV
 #define FX_KV(a, b) ::syslog_runtime::KeyValue(a, b)
 
 class LogMessageVoidify final {
@@ -300,25 +300,30 @@ class LogFirstNState final {
   std::atomic<uint32_t> counter_{0};
 };
 
-// Returns true if |severity| is at or above the current minimum log level.
-// LOG_FATAL and above is always true.
+/// Returns true if |severity| is at or above the current minimum log level.
+/// LOG_FATAL and above is always true.
 bool IsSeverityEnabled(LogSeverity severity);
 
 }  // namespace fuchsia_logging
 
+/// Used for stream-based logging with a custom tag.
 #define FX_LOG_STREAM(severity, tag)                                                            \
   ::fuchsia_logging::LogMessage(::fuchsia_logging::LOG_##severity, __FILE__, __LINE__, nullptr, \
                                 tag)                                                            \
       .stream()
 
+/// Used for stream-based logging of a zx_status_t combined
+/// with a log message.
 #define FX_LOG_STREAM_STATUS(severity, status, tag)                                             \
   ::fuchsia_logging::LogMessage(::fuchsia_logging::LOG_##severity, __FILE__, __LINE__, nullptr, \
                                 tag, status)                                                    \
       .stream()
 
+// Internal macro used by other macros
 #define FX_LAZY_STREAM(stream, condition) \
   !(condition) ? (void)0 : ::fuchsia_logging::LogMessageVoidify() & (stream)
 
+// Internal macro used by other macros
 #define FX_EAT_STREAM_PARAMETERS(ignored)                                                       \
   true || (ignored)                                                                             \
       ? (void)0                                                                                 \
@@ -326,11 +331,15 @@ bool IsSeverityEnabled(LogSeverity severity);
             ::fuchsia_logging::LogMessage(::fuchsia_logging::LOG_FATAL, 0, 0, nullptr, nullptr) \
                 .stream()
 
+// Checks if a given severity level is enabled.
+// Intended for use by other macros in this file.
 #define FX_LOG_IS_ON(severity) \
   (::fuchsia_logging::IsSeverityEnabled(::fuchsia_logging::LOG_##severity))
 
+/// Logs a message with a given severity level
 #define FX_LOGS(severity) FX_LOGST(severity, nullptr)
 
+/// Logs a message with a given severity level and tag.
 #define FX_LOGST(severity, tag) FX_LAZY_STREAM(FX_LOG_STREAM(severity, tag), FX_LOG_IS_ON(severity))
 
 #if defined(__Fuchsia__)
@@ -339,20 +348,20 @@ bool IsSeverityEnabled(LogSeverity severity);
 #define FX_PLOGS(severity, status) FX_PLOGST(severity, nullptr, status)
 #endif
 
-// Writes a message to the global logger, the first |n| times that any callsite
-// of this macro is invoked. |n| should be a positive integer literal.
-// |severity| is one of INFO, WARNING, ERROR, FATAL
-//
-// Implementation notes:
-// The outer for loop is a trick to allow us to introduce a new scope and
-// introduce the variable |do_log| into that scope. It executes exactly once.
-//
-// The inner for loop is a trick to allow us to introduce a new scope and
-// introduce the static variable |internal_state| into that new scope. It
-// executes either zero or one times.
-//
-// C++ does not allow us to introduce two new variables into a single for loop
-// scope and we need |do_log| so that the inner for loop doesn't execute twice.
+/// Writes a message to the global logger, the first |n| times that any callsite
+/// of this macro is invoked. |n| should be a positive integer literal.
+/// |severity| is one of INFO, WARNING, ERROR, FATAL
+///
+/// Implementation notes:
+/// The outer for loop is a trick to allow us to introduce a new scope and
+/// introduce the variable |do_log| into that scope. It executes exactly once.
+///
+/// The inner for loop is a trick to allow us to introduce a new scope and
+/// introduce the static variable |internal_state| into that new scope. It
+/// executes either zero or one times.
+///
+/// C++ does not allow us to introduce two new variables into a single for loop
+/// scope and we need |do_log| so that the inner for loop doesn't execute twice.
 #define FX_FIRST_N(n, log_statement)                              \
   for (bool do_log = true; do_log; do_log = false)                \
     for (static ::fuchsia_logging::LogFirstNState internal_state; \
@@ -369,8 +378,7 @@ bool IsSeverityEnabled(LogSeverity severity);
                      .stream(),                                                                  \
                  !(condition))
 
-// The VLOG macros log with translated verbosities
-
+// Macros used to log based on whether or not NDEBUG is defined
 #ifndef NDEBUG
 #define FX_DLOGS(severity) FX_LOGS(severity)
 #define FX_DCHECK(condition) FX_CHECK(condition)
@@ -379,10 +387,15 @@ bool IsSeverityEnabled(LogSeverity severity);
 #define FX_DCHECK(condition) FX_EAT_STREAM_PARAMETERS(condition)
 #endif
 
+/// Used to indicate unreachable code.
+/// Crashes if this is hit.
 #define FX_NOTREACHED() FX_DCHECK(false)
 
+/// Used to indicate unimplemented code.
+/// This doesn't crash, but prints an error.
 #define FX_NOTIMPLEMENTED() FX_LOGS(ERROR) << "Not implemented in: " << __PRETTY_FUNCTION__
 
+// Used internally by FX_LOG_KV
 #define FX_LOG_KV_ETC(severity, args...)                                                \
   do {                                                                                  \
     if (::fuchsia_logging::IsSeverityEnabled(severity)) {                               \
@@ -390,6 +403,10 @@ bool IsSeverityEnabled(LogSeverity severity);
     }                                                                                   \
   } while (0)
 
+/// Logs a structured message with a given severity,
+/// message, and optional key-value pairs.
+/// Example usage:
+/// FX_LOG_KV(INFO, "Test message", FX_KV("meaning_of_life", 42));
 #define FX_LOG_KV(severity, msg...) FX_LOG_KV_ETC(::fuchsia_logging::LOG_##severity, msg)
 
 #endif  // LIB_SYSLOG_CPP_MACROS_H_
