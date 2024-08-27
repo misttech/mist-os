84c978b6c77af0d94b722c96eddcfb5d4947e072
Gianfranco Valentino <gevalentino@google.com>
2024-08-27 22:23:29 +0000

    [kernel] Introduce energy model for kernel RPPM
    Basic data structures for storing and accessing the energy model.
    
    An energy model consist on a well defined set of power level and a
    transition matrix defining properties for transitioning form one level
    to another.
    
    A power domain consist on a collection of CPU's that are bound to the
    same energy model, and when a collective power level transition happens,
    they are all affected.
    
    A registry for enforcing invariants regarding existing power domains.
    
    While power domains and models may be updated, this is handled through
    versioning, that is, every instance of a power domain is immutable. An
    update is performed by creating a new instance, and replacing the
    reference to the old instances to the new one.
    
    Following CLs will introduce the versioning mechanism, the control
    interface handlers, etc.
    
    Change-Id: I498f51cc63ae61f0054a381a47424ca2fa8f6094
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1084118
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>

---
+++ zircon/kernel/lib/power-management/energy-model-test.cc
@@ -0,0 +1,484 @@
+// Copyright 2024 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include "lib/power-management/energy-model.h"
+
+#include <lib/stdcompat/array.h>
+#include <lib/stdcompat/utility.h>
+#include <zircon/syscalls-next.h>
+#include <zircon/types.h>
+
+#include <cstddef>
+#include <cstdint>
+
+#include <fbl/ref_ptr.h>
+#include <gtest/gtest.h>
+
+namespace {
+
+using power_management::PowerDomain;
+using power_management::PowerDomainRegistry;
+using power_management::PowerModel;
+
+template <typename... Cpus>
+zx_cpu_set_t MakeCpuSet(Cpus... cpus) {
+  zx_cpu_set_t set = {};
+  auto set_bit = [&set](size_t num_cpu) {
+    set.mask[num_cpu / ZX_CPU_SET_BITS_PER_WORD] |= uint64_t(1)
+                                                    << num_cpu % ZX_CPU_SET_BITS_PER_WORD;
+    return true;
+  };
+  (set_bit(cpus) && ...);
+  return set;
+}
+
+auto MakePowerDomain(uint32_t id, zx_cpu_set_t cpus) {
+  return fbl::MakeRefCounted<PowerDomain>(id, cpus, PowerModel{});
+}
+
+template <typename... Cpus>
+auto MakePowerDomainHelper(uint32_t id, Cpus... cpus) {
+  return MakePowerDomain(id, MakeCpuSet(cpus...));
+}
+
+template <typename CpuVisitor>
+void ForEachCpuIn(zx_cpu_set_t cpus, CpuVisitor&& visitor) {
+  for (size_t i = 0; i < ZX_CPU_SET_MAX_CPUS / ZX_CPU_SET_BITS_PER_WORD; ++i) {
+    if (cpus.mask[i] == 0) {
+      continue;
+    }
+    for (size_t j = 0; j < ZX_CPU_SET_BITS_PER_WORD; ++j) {
+      if ((cpus.mask[i] & (static_cast<uint64_t>(1) << j)) == 0) {
+        continue;
+      }
+      visitor(i * ZX_CPU_SET_BITS_PER_WORD + j);
+    }
+  }
+}
+
+TEST(PowerLevelTest, Ctor) {
+  constexpr zx_processor_power_level_t kLevel = {
+      .options = 0,
+      .processing_rate = 456,
+      .power_coefficient_nw = 789,
+      .control_interface = cpp23::to_underlying(power_management::ControlInterface::kArmPsci),
+      .control_argument = 12345,
+      .diagnostic_name = "foobar one two three",
+  };
+
+  power_management::PowerLevel level(0, kLevel);
+
+  EXPECT_EQ(level.level(), 0);
+  EXPECT_EQ(level.processing_rate(), kLevel.processing_rate);
+  EXPECT_EQ(level.power_coefficient_nw(), kLevel.power_coefficient_nw);
+  EXPECT_EQ(level.control(),
+            static_cast<power_management::ControlInterface>(kLevel.control_interface));
+  EXPECT_EQ(level.control_argument(), kLevel.control_argument);
+  EXPECT_EQ(level.type(), power_management::PowerLevel::Type::kActive);
+  EXPECT_EQ(level.name(), std::string_view(kLevel.diagnostic_name));
+  EXPECT_TRUE(level.TargetsPowerDomain());
+  EXPECT_FALSE(level.TargetsCpus());
+}
+
+TEST(PowerLevelTest, Ctor2) {
+  constexpr zx_processor_power_level_t kLevel = {
+      .options = ZX_PROCESSOR_POWER_LEVEL_OPTIONS_DOMAIN_INDEPENDENT,
+      .processing_rate = 123,
+      .power_coefficient_nw = 789,
+      .control_interface = cpp23::to_underlying(power_management::ControlInterface::kArmPsci),
+      .control_argument = 12345,
+      .diagnostic_name = "foobar one two three",
+  };
+
+  power_management::PowerLevel level(123, kLevel);
+
+  EXPECT_EQ(level.level(), 123);
+  EXPECT_EQ(level.processing_rate(), kLevel.processing_rate);
+  EXPECT_EQ(level.power_coefficient_nw(), kLevel.power_coefficient_nw);
+  EXPECT_EQ(level.control(),
+            static_cast<power_management::ControlInterface>(kLevel.control_interface));
+  EXPECT_EQ(level.control_argument(), kLevel.control_argument);
+  EXPECT_EQ(level.type(), power_management::PowerLevel::Type::kActive);
+  EXPECT_EQ(level.name(), std::string_view(kLevel.diagnostic_name));
+  EXPECT_FALSE(level.TargetsPowerDomain());
+  EXPECT_TRUE(level.TargetsCpus());
+}
+
+TEST(PowerLevelTransitionTest, Ctor) {
+  static constexpr zx_processor_power_level_transition_t kTransition = {
+      .latency = 456,
+      .energy_nj = 1234,
+      .from = 0,
+      .to = 1,
+  };
+
+  power_management::PowerLevelTransition transition(kTransition);
+
+  EXPECT_EQ(transition.latency(), zx::duration(kTransition.latency));
+  EXPECT_EQ(transition.energy_cost(), kTransition.energy_nj);
+}
+
+TEST(PowerModelTest, Create) {
+  static constexpr auto kPowerLevels = cpp20::to_array<zx_processor_power_level_t>({
+      {
+          .options = 0,
+          .processing_rate = 0,
+          .power_coefficient_nw = 1,
+          .control_interface = cpp23::to_underlying(power_management::ControlInterface::kArmPsci),
+          .control_argument = 1,
+          .diagnostic_name = "0",
+      },
+      {
+          .options = 0,
+          .processing_rate = 4,
+          .power_coefficient_nw = 8,
+          .control_interface = cpp23::to_underlying(power_management::ControlInterface::kCpuDriver),
+          .control_argument = 3,
+          .diagnostic_name = "1",
+      },
+      {
+          .options = 0,
+          .processing_rate = 0,
+          .power_coefficient_nw = 2,
+          .control_interface = cpp23::to_underlying(power_management::ControlInterface::kArmWfi),
+          .control_argument = 0,
+          .diagnostic_name = "2",
+      },
+      {
+          .options = 0,
+          .processing_rate = 4,
+          .power_coefficient_nw = 10,
+          .control_interface = cpp23::to_underlying(power_management::ControlInterface::kCpuDriver),
+          .control_argument = 1,
+          .diagnostic_name = "3",
+      },
+  });
+
+  static constexpr auto kTransitions = cpp20::to_array<zx_processor_power_level_transition_t>({
+      {
+          .latency = 1,
+          .energy_nj = 2,
+          .from = 1,
+          .to = 0,
+      },
+      {
+
+          .latency = 2,
+          .energy_nj = 3,
+          .from = 2,
+          .to = 0,
+
+      },
+      {
+
+          .latency = 3,
+          .energy_nj = 4,
+          .from = 3,
+          .to = 0,
+      },
+      {
+
+          .latency = 1,
+          .energy_nj = 2,
+          .from = 0,
+          .to = 1,
+      },
+      {
+
+          .latency = 3,
+          .energy_nj = 4,
+          .from = 2,
+          .to = 1,
+      },
+      {
+
+          .latency = 4,
+          .energy_nj = 5,
+          .from = 3,
+          .to = 1,
+      },
+      {
+
+          .latency = 2,
+          .energy_nj = 3,
+          .from = 0,
+          .to = 2,
+      },
+      {
+
+          .latency = 3,
+          .energy_nj = 4,
+          .from = 1,
+          .to = 2,
+      },
+      {
+          .latency = 5,
+          .energy_nj = 6,
+          .from = 3,
+          .to = 2,
+      },
+      {
+
+          .latency = 3,
+          .energy_nj = 4,
+          .from = 0,
+          .to = 3,
+
+      },
+      {
+
+          .latency = 4,
+          .energy_nj = 5,
+          .from = 1,
+          .to = 3,
+      },
+      {
+
+          .latency = 5,
+          .energy_nj = 6,
+          .from = 2,
+          .to = 3,
+      },
+  });
+
+  auto power_model = PowerModel::Create(kPowerLevels, kTransitions);
+  ASSERT_TRUE(power_model.is_ok());
+
+  // Proper transformation of the model and the transition table.
+  auto check_level = [](const power_management::PowerLevel& actual,
+                        const power_management::PowerLevel& expected) {
+    EXPECT_EQ(actual.level(), expected.level());
+    EXPECT_EQ(actual.control(), expected.control());
+    EXPECT_EQ(actual.control_argument(), expected.control_argument());
+    EXPECT_EQ(actual.name(), expected.name());
+    EXPECT_EQ(actual.power_coefficient_nw(), expected.power_coefficient_nw());
+    EXPECT_EQ(actual.processing_rate(), expected.processing_rate());
+    EXPECT_EQ(actual.type(), expected.type());
+    EXPECT_EQ(actual.TargetsCpus(), expected.TargetsCpus());
+    EXPECT_EQ(actual.TargetsPowerDomain(), expected.TargetsPowerDomain());
+  };
+
+  ASSERT_EQ(power_model->levels().size(), 4u);
+  auto levels = power_model->levels();
+  for (size_t i = 0; i < levels.size() - 1; ++i) {
+    size_t j = i + 1;
+    EXPECT_LE(levels[i].processing_rate(), levels[i].processing_rate());
+    if (levels[i].processing_rate() == levels[j].processing_rate()) {
+      EXPECT_LE(levels[i].power_coefficient_nw(), levels[j].power_coefficient_nw());
+    }
+    check_level(levels[i],
+                power_management::PowerLevel(levels[i].level(), kPowerLevels[levels[i].level()]));
+    check_level(levels[j],
+                power_management::PowerLevel(levels[j].level(), kPowerLevels[levels[j].level()]));
+  }
+
+  auto get_original_transition = [&levels](size_t i, size_t j) {
+    size_t og_i = levels[i].level();
+    size_t og_j = levels[j].level();
+    for (const auto& transition : kTransitions) {
+      if (transition.from == og_i && transition.to == og_j) {
+        return power_management::PowerLevelTransition(transition);
+      }
+    }
+    return power_management::PowerLevelTransition::Invalid();
+  };
+
+  auto transitions = power_model->transitions();
+  for (size_t i = 0; i < levels.size(); ++i) {
+    for (size_t j = 0; j < levels.size(); ++j) {
+      auto transition = transitions[i][j];
+      auto og_transition = get_original_transition(i, j);
+      EXPECT_EQ(transition.latency(), og_transition.latency());
+      EXPECT_EQ(transition.energy_cost(), og_transition.energy_cost());
+    }
+  }
+
+  // Properly partitioned.
+  ASSERT_EQ(power_model->idle_levels().size(), 2u);
+  EXPECT_EQ(power_model->idle_levels()[0].level(), 0u);
+  EXPECT_EQ(power_model->idle_levels()[1].level(), 2u);
+
+  ASSERT_EQ(power_model->active_levels().size(), 2u);
+  EXPECT_EQ(power_model->active_levels()[0].level(), 1u);
+  EXPECT_EQ(power_model->active_levels()[1].level(), 3u);
+
+  // Sorter by tuple <Control Interface, Control Argument>
+  for (size_t i = 0; i < levels.size(); ++i) {
+    auto& level = levels[i];
+    EXPECT_EQ(power_model->FindPowerLevel(level.control(), level.control_argument()), i);
+  }
+  EXPECT_FALSE(power_model->FindPowerLevel(power_management::ControlInterface::kArmPsci, 495));
+  EXPECT_FALSE(
+      power_model->FindPowerLevel(static_cast<power_management::ControlInterface>(495), 0));
+}
+
+TEST(PowerDomainRegistryTest, RegisterUniquePowerDomains) {
+  PowerDomainRegistry registry;
+  std::map<size_t, fbl::RefPtr<PowerDomain>> registed_domains;
+  registed_domains[0] = MakePowerDomainHelper(0, 1, 2, 3);
+  registed_domains[1] = MakePowerDomainHelper(1, 4, 5, 6);
+  registed_domains[2] = MakePowerDomainHelper(2, 7, 8, 9);
+
+  std::map<size_t, fbl::RefPtr<PowerDomain>> cpu_domains;
+  auto domain_update = [&cpu_domains](size_t num, auto domain) {
+    cpu_domains[num] = std::move(domain);
+  };
+
+  for (auto [domain_id, domain] : registed_domains) {
+    ASSERT_TRUE(registry.Register(domain, domain_update).is_ok());
+  }
+
+  // Validate the domains currently registered.
+  size_t domains = 0;
+  int cpus = 0;
+  registry.Visit([&cpus, &domains, &registed_domains, &cpu_domains](const PowerDomain& domain) {
+    domains++;
+    auto& expected = registed_domains[domain.id()];
+    EXPECT_EQ(expected.get(), &domain);
+    ForEachCpuIn(domain.cpus(), [&domain, &cpu_domains, &cpus](size_t num_cpu) {
+      EXPECT_EQ(&domain, cpu_domains[num_cpu].get());
+      cpus++;
+    });
+  });
+  EXPECT_EQ(domains, registed_domains.size());
+  EXPECT_EQ(cpu_domains.size(), 9u);
+  EXPECT_EQ(cpus, 9);
+}
+
+// The purpose of this test is to demonstrate the process for updating immutable characteristics of
+// an already registered domain.
+TEST(PowerDomainRegistryTest, UpdatingOverlapping) {
+  PowerDomainRegistry registry;
+  std::map<size_t, fbl::RefPtr<PowerDomain>> registed_domains;
+  registed_domains[0] = MakePowerDomainHelper(0, 1, 2, 3);
+  registed_domains[1] = MakePowerDomainHelper(1, 4, 5, 6);
+  registed_domains[2] = MakePowerDomainHelper(2, 7, 8, 9);
+  registed_domains[3] = MakePowerDomainHelper(3, 1, 4, 7, 10);
+
+  std::map<size_t, fbl::RefPtr<PowerDomain>> cpu_domains;
+  auto domain_update = [&cpu_domains](size_t num, auto domain) {
+    if (!domain) {
+      cpu_domains.erase(num);
+    } else {
+      cpu_domains[num] = std::move(domain);
+    }
+  };
+
+  // Succeeds, but as seen below domain 3 will fail due to overlapping with existing domain.
+  for (auto [domain_id, domain] : registed_domains) {
+    if (domain_id != 3) {
+      ASSERT_TRUE(registry.Register(domain, domain_update).is_ok());
+    } else {
+      ASSERT_FALSE(registry.Register(domain, domain_update).is_ok());
+    }
+  }
+
+  // First we will introduce an updated version of 0-2 that exclude the overlapping cpus.
+  registed_domains[0] = MakePowerDomainHelper(0, 2, 3);
+  registed_domains[1] = MakePowerDomainHelper(1, 5, 6);
+  registed_domains[2] = MakePowerDomainHelper(2, 8, 9);
+  for (size_t domain_id = 0; domain_id <= 3; ++domain_id) {
+    auto& domain = registed_domains[domain_id];
+    ASSERT_TRUE(registry.Register(domain, domain_update).is_ok());
+  }
+
+  // Validate the domains currently registered.
+  size_t domains = 0;
+  int cpus = 0;
+  registry.Visit([&cpus, &domains, &registed_domains, &cpu_domains](const PowerDomain& domain) {
+    domains++;
+    auto& expected = registed_domains[domain.id()];
+    EXPECT_EQ(expected.get(), &domain);
+    ForEachCpuIn(domain.cpus(), [&domain, &cpu_domains, &cpus](size_t num_cpu) {
+      EXPECT_EQ(&domain, cpu_domains[num_cpu].get());
+      cpus++;
+    });
+  });
+  EXPECT_EQ(domains, registed_domains.size());
+  EXPECT_EQ(cpu_domains.size(), 10u);
+  EXPECT_EQ(cpus, 10);
+}
+
+TEST(PowerDomainRegistryTest, OverlappingIsError) {
+  PowerDomainRegistry registry;
+  std::map<size_t, fbl::RefPtr<PowerDomain>> registed_domains;
+  registed_domains[0] = MakePowerDomainHelper(0, 1, 2, 3);
+  registed_domains[1] = MakePowerDomainHelper(1, 4, 5, 6);
+  registed_domains[2] = MakePowerDomainHelper(2, 7, 8, 9);
+  registed_domains[3] = MakePowerDomainHelper(3, 1, 4, 7, 10);
+
+  std::map<size_t, fbl::RefPtr<PowerDomain>> cpu_domains;
+  auto domain_update = [&cpu_domains](size_t num, auto domain) {
+    cpu_domains[num] = std::move(domain);
+  };
+
+  for (auto [domain_id, domain] : registed_domains) {
+    auto res = registry.Register(domain, domain_update);
+    ASSERT_TRUE((domain_id != 3 && res.is_ok()) || (domain_id == 3 && !res.is_ok()));
+  }
+
+  // Validate the domains currently registered.
+  size_t domains = 0;
+  int cpus = 0;
+  registry.Visit([&cpus, &domains, &registed_domains, &cpu_domains](const PowerDomain& domain) {
+    domains++;
+    auto& expected = registed_domains[domain.id()];
+    EXPECT_EQ(expected.get(), &domain);
+    ForEachCpuIn(domain.cpus(), [&domain, &cpu_domains, &cpus](size_t num_cpu) {
+      EXPECT_EQ(&domain, cpu_domains[num_cpu].get());
+      cpus++;
+    });
+  });
+  EXPECT_EQ(domains, registed_domains.size() - 1);
+  EXPECT_EQ(cpu_domains.size(), 9u);
+  EXPECT_EQ(cpus, 9);
+}
+
+TEST(PowerDomainRegistryTest, RegisterWithSameIdIsUpdate) {
+  PowerDomainRegistry registry;
+  std::map<size_t, fbl::RefPtr<PowerDomain>> registed_domains;
+  registed_domains[0] = MakePowerDomainHelper(0, 1, 2, 3);
+  registed_domains[1] = MakePowerDomainHelper(1, 4, 5, 6);
+  registed_domains[2] = MakePowerDomainHelper(2, 7, 8, 9);
+
+  std::map<size_t, fbl::RefPtr<PowerDomain>> cpu_domains;
+  auto domain_update = [&cpu_domains](size_t num, auto domain) {
+    if (!domain) {
+      cpu_domains.erase(num);
+    } else {
+      cpu_domains[num] = std::move(domain);
+    }
+  };
+
+  for (auto [domain_id, domain] : registed_domains) {
+    ASSERT_TRUE(registry.Register(domain, domain_update).is_ok());
+  }
+
+  registed_domains[1] = MakePowerDomainHelper(1, 4, 10);
+  ASSERT_TRUE(registry.Register(registed_domains[1], domain_update).is_ok());
+
+  // Validate the domains currently registered.
+  size_t domains = 0;
+  int cpus = 0;
+  registry.Visit([&cpus, &domains, &registed_domains, &cpu_domains](const PowerDomain& domain) {
+    domains++;
+    auto& expected = registed_domains[domain.id()];
+    EXPECT_EQ(expected.get(), &domain);
+    ForEachCpuIn(domain.cpus(), [&domain, &cpu_domains, &cpus](size_t num_cpu) {
+      EXPECT_EQ(&domain, cpu_domains[num_cpu].get());
+      cpus++;
+    });
+  });
+
+  EXPECT_EQ(domains, registed_domains.size());
+  EXPECT_EQ(cpu_domains.size(), 8u);
+  EXPECT_EQ(cpus, 8);
+
+  EXPECT_EQ(cpu_domains.count(5), 0u);
+  EXPECT_EQ(cpu_domains.count(6), 0u);
+}
+
+}  // namespace
---
+++ zircon/kernel/lib/power-management/energy-model.cc
@@ -0,0 +1,220 @@
+// Copyright 2024 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include "lib/power-management/energy-model.h"
+
+#include <lib/stdcompat/utility.h>
+#include <zircon/errors.h>
+#include <zircon/syscalls-next.h>
+#include <zircon/types.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+
+#include <fbl/alloc_checker.h>
+#include <fbl/ref_ptr.h>
+#include <fbl/vector.h>
+
+namespace power_management {
+
+namespace {
+
+template <size_t N>
+static constexpr bool HasOverlappingCpu(const uint64_t (&a)[N], const uint64_t (&b)[N]) {
+  for (size_t i = 0; i < N; ++i) {
+    if ((a[i] & b[i]) != 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+}  // namespace
+
+zx::result<PowerModel> PowerModel::Create(
+    cpp20::span<const zx_processor_power_level_t> levels,
+    cpp20::span<const zx_processor_power_level_transition_t> transitions) {
+  // Validate that transitions are to and from valid levels.
+  for (const auto& transition : transitions) {
+    if (transition.from >= levels.size()) {
+      return zx::error(ZX_ERR_INVALID_ARGS);
+    }
+
+    if (transition.to >= levels.size()) {
+      return zx::error(ZX_ERR_INVALID_ARGS);
+    }
+  }
+
+  // Validate that power level interfaces are supported values.
+  for (const auto& level : levels) {
+    if (!IsSupportedControlInterface(level.control_interface)) {
+      return zx::error(ZX_ERR_INVALID_ARGS);
+    }
+  }
+
+  fbl::AllocChecker ac;
+  fbl::Vector<PowerLevel> power_levels;
+  power_levels.reserve(levels.size(), &ac);
+  if (!ac.check()) {
+    return zx::error(ZX_ERR_NO_MEMORY);
+  }
+
+  fbl::Vector<size_t> power_levels_lookup;
+  power_levels_lookup.reserve(levels.size(), &ac);
+  if (!ac.check()) {
+    return zx::error(ZX_ERR_NO_MEMORY);
+  }
+
+  // If we assume symmetry in the matrix, we could reduce the amount of space required to support
+  // transitions by more than half, since any element below the main diagonal would be equivalent to
+  // its mirror, and the diagonal itself would be 0.
+  fbl::Vector<PowerLevelTransition> power_level_transitions;
+  power_level_transitions.resize(levels.size() * levels.size(), PowerLevelTransition::Invalid(),
+                                 &ac);
+  if (!ac.check()) {
+    return zx::error(ZX_ERR_NO_MEMORY);
+  }
+
+  size_t idle_levels = 0;
+  for (size_t i = 0; i < levels.size(); ++i) {
+    power_levels.push_back(PowerLevel(static_cast<uint8_t>(i), levels[i]));
+    power_levels_lookup.push_back(i);
+    if (power_levels[i].type() == PowerLevel::kIdle) {
+      idle_levels++;
+    }
+  }
+
+  // Generate lookup table based on original indexes.
+  std::sort(power_levels_lookup.begin(), power_levels_lookup.end(),
+            [&power_levels](const size_t& a, const size_t& b) constexpr {
+              if (power_levels[a].processing_rate() == power_levels[b].processing_rate()) {
+                return power_levels[a].power_coefficient_nw() <
+                       power_levels[b].power_coefficient_nw();
+              }
+              return power_levels[a].processing_rate() < power_levels[b].processing_rate();
+            });
+
+  // This will naturally partition idle and active states, having all idle states at the beginning.
+  std::sort(power_levels.begin(), power_levels.end(),
+            [](const PowerLevel& a, const PowerLevel& b) constexpr {
+              if (a.processing_rate() == b.processing_rate()) {
+                return a.power_coefficient_nw() < b.power_coefficient_nw();
+              }
+              return a.processing_rate() < b.processing_rate();
+            });
+
+  // Fill up the square matrix from level i to level j, where i and j, are indexes into the
+  // power_level array.
+  for (const auto& transition : transitions) {
+    size_t i = power_levels_lookup[transition.from];
+    size_t j = power_levels_lookup[transition.to];
+
+    // Double check that translation is correct.
+    ZX_ASSERT(power_levels[i].level() == transition.from);
+    ZX_ASSERT(power_levels[j].level() == transition.to);
+
+    size_t transition_offset = i * power_levels.size() + j;
+    power_level_transitions[transition_offset] = PowerLevelTransition(transition);
+  }
+
+  // Reset the power level lookup, and turn it into a lookup by control interface, such that the set
+  // path doesnt have to traverse every level.
+  for (size_t i = 0; i < power_levels_lookup.size(); ++i) {
+    power_levels_lookup[i] = i;
+  }
+
+  // Generate lookup table based on control interface and control argument tuple..
+  std::sort(power_levels_lookup.begin(), power_levels_lookup.end(),
+            [&power_levels](size_t a, size_t b) constexpr {
+              if (cpp23::to_underlying(power_levels[a].control()) ==
+                  cpp23::to_underlying(power_levels[b].control())) {
+                return power_levels[a].control_argument() < power_levels[b].control_argument();
+              }
+
+              return cpp23::to_underlying(power_levels[a].control()) <
+                     cpp23::to_underlying(power_levels[b].control());
+            });
+
+  return zx::ok(PowerModel{std::move(power_levels), std::move(power_level_transitions),
+                           std::move(power_levels_lookup), idle_levels});
+}
+
+zx::result<> PowerDomainRegistry::UpdateRegistry(
+    fbl::RefPtr<PowerDomain> power_domain,
+    fit::inline_function<void(size_t, fbl::RefPtr<PowerDomain>)> update_cpu_power_domain) {
+  std::optional<decltype(domains_)::iterator> old_domain_prev;
+  std::optional<decltype(domains_)::iterator> prev_it = std::nullopt;
+  bool existing_id = false;
+  for (auto it = domains_.begin(); it != domains_.end(); ++it) {
+    auto& entry = *it;
+    if (entry.id() == power_domain->id()) {
+      old_domain_prev = prev_it;
+      existing_id = true;
+    } else if (HasOverlappingCpu(entry.cpus().mask, power_domain->cpus().mask)) {
+      return zx::error(ZX_ERR_INVALID_ARGS);
+    }
+
+    prev_it = it;
+  }
+
+  fbl::RefPtr<PowerDomain> old_domain = nullptr;
+  // Now remove old_domain from the list, and update the domain's generation number.
+  if (existing_id) {
+    if (!old_domain_prev) {
+      old_domain = domains_.pop_front();
+    } else {
+      old_domain = domains_.erase_next(*old_domain_prev);
+    }
+  }
+
+  // Update every CPU reference from previous power domain to `power_domain`.
+  for (size_t i = 0; i < kBuckets; ++i) {
+    const size_t bucket_offset = i * kBitsPerBucket;
+    if (power_domain->cpus().mask[i] == 0 && (old_domain && old_domain->cpus().mask[i] == 0)) {
+      continue;
+    }
+
+    for (size_t j = 0; j < kBitsPerBucket; ++j) {
+      const uint64_t bit_mask = 1ull << j;
+      const size_t num_cpu = bucket_offset + j;
+      if ((power_domain->cpus().mask[i] & bit_mask) != 0) {
+        // This would be done, for example under the scheduler`s `queue_lock_`, and we want to
+        // keep it as short as possible.
+        update_cpu_power_domain(num_cpu, power_domain);
+      } else if (old_domain && (old_domain->cpus().mask[i] & bit_mask) != 0) {
+        update_cpu_power_domain(num_cpu, nullptr);
+      }
+    }
+  }
+
+  domains_.push_front(std::move(power_domain));
+  return zx::ok();
+}
+
+std::optional<size_t> PowerModel::FindPowerLevel(ControlInterface interface_id,
+                                                 uint64_t control_argument) const {
+  auto it = std::lower_bound(control_lookup_.begin(), control_lookup_.end(),
+                             zx_processor_power_level_t{
+                                 .control_interface = cpp23::to_underlying(interface_id),
+                                 .control_argument = control_argument,
+                             },
+                             [this](size_t i, const zx_processor_power_level_t& val) {
+                               const auto& a = power_levels_[i];
+                               return cpp23::to_underlying(a.control()) < val.control_interface ||
+                                      (cpp23::to_underlying(a.control()) == val.control_interface &&
+                                       a.control_argument() < val.control_argument);
+                             });
+  if (it != control_lookup_.end() && power_levels_[*it].control() == interface_id &&
+      power_levels_[*it].control_argument() == control_argument) {
+    return *it;
+  }
+
+  return std::nullopt;
+}
+
+}  // namespace power_management
