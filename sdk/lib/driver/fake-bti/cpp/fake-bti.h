e4a42f04b5d72fac4e0917ecde8b0d7f73fc9007
Nick Eaton <nickeaton@google.com>
2024-08-28 20:30:42 +0000

    [sdk][fake-bti] Create SDK library for fake bti
    Create the //sdk/lib/driver/fake-bti/cpp library. It is a
    nearly identical copy of //src/devices/testing/fake-bti. The end goal is
    for the new library to replace the old library.
    
    Bug: 326252240
    Change-Id: Id652e8dda7e2b6f69f316ad8c499c28a6130f529
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1107815
    Commit-Queue: Nick Eaton <nickeaton@google.com>
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>

---
+++ sdk/lib/driver/fake-bti/cpp/fake-bti.h
@@ -0,0 +1,61 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_DRIVER_FAKE_BTI_CPP_FAKE_BTI_H_
+#define LIB_DRIVER_FAKE_BTI_CPP_FAKE_BTI_H_
+
+#include <limits.h>
+#include <zircon/compiler.h>
+#include <zircon/types.h>
+
+__BEGIN_CDECLS
+
+// All physical addresses returned by zx_bti_pin with a fake BTI will be set to this value.
+// PAGE_SIZE is chosen so that so superficial validity checks like "is the address correctly
+// aligned" and "is the address non-zero" in the code under test will pass.
+#define FAKE_BTI_PHYS_ADDR PAGE_SIZE
+
+zx_status_t fake_bti_create(zx_handle_t* out);
+
+// Like fake_bti_create, except zx_bti_pin will return the fake physical addresses in |paddrs|, or
+// ZX_ERR_OUT_OF_RANGE if not enough address were specified. If |paddrs| is NULL or paddr_count is
+// zero, each address is set to FAKE_BTI_PHYS_ADDR, and no range check is performed. |paddrs| must
+// remain valid until the last call to zx_bti_pin is made.
+zx_status_t fake_bti_create_with_paddrs(const zx_paddr_t* paddrs, size_t paddr_count,
+                                        zx_handle_t* out);
+
+// This struct stores state of a VMO pinned to a BTI. |size| and |offset| are
+// the actual size and offset used to pin pages when calling |zx_bti_pin()|;
+// |vmo| is a duplicate of the original pinned VMO.
+typedef struct {
+  zx_handle_t vmo;
+  uint64_t size;
+  uint64_t offset;
+} fake_bti_pinned_vmo_info_t;
+
+// Fake BTI stores all pinned VMOs for testing purposes. Tests can call this
+// method to get duplicates of all pinned VMO handles, as well as the pinned
+// pages' size and offset for each VMO.
+//
+// |out_vmo_info| points to a buffer containing |out_num_vmos| vmo info
+// elements. The method writes no more than |out_num_vmos| elements to the
+// buffer, and will write the actual number of pinned vmos to |actual_num_vmos|
+// if the argument is not null.
+//
+// It's the caller's repsonsibility to close all the returned VMO handles.
+zx_status_t fake_bti_get_pinned_vmos(zx_handle_t bti, fake_bti_pinned_vmo_info_t* out_vmo_info,
+                                     size_t out_num_vmos, size_t* actual_num_vmos);
+
+// Fake BTI stores all the fake physical addresses that is returned by |zx_bti_pin|.
+// Tests can call this method to get the fake physical addresses corresponding to |vmo_info|.
+// |out_paddrs| points to a buffer containing |out_num_paddrs| physical address elements.
+// The method writes no more than |out_num_paddrs| elements to the buffer, and will write the actual
+// number of physical addresses to |actual_num_paddrs| if the argument is not null.
+zx_status_t fake_bti_get_phys_from_pinned_vmo(zx_handle_t bti, fake_bti_pinned_vmo_info_t vmo_info,
+                                              zx_paddr_t* out_paddrs, size_t out_num_paddrs,
+                                              size_t* actual_num_paddrs);
+
+__END_CDECLS
+
+#endif  // LIB_DRIVER_FAKE_BTI_CPP_FAKE_BTI_H_
