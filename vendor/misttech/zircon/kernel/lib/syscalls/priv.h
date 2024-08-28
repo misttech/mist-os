8a906d0ed23a05c9a245c9e7290ff12fc20859e3
Bruno Herrera <bruno.herrera@mist-os.com>
2024-08-28 15:18:36 -0300

    [mistos][syscalls] Initial linux syscall bind (all calling not implemented version)
    Change-Id: Ia4364297fae330a4298604c13b9ffea8c208dfd6

--- vendor/misttech/zircon/kernel/lib/syscalls/priv.h
+++ vendor/misttech/zircon/kernel/lib/syscalls/priv.h
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ZIRCON_KERNEL_LIB_MISTOS_SYSCALLS_PRIV_H_
-#define ZIRCON_KERNEL_LIB_MISTOS_SYSCALLS_PRIV_H_
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_SYSCALLS_PRIV_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_SYSCALLS_PRIV_H_
 
-// Individual files do `#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)`.
+// Individual files do `#define LOCAL_TRACE MISTOS_SYSCALLS_GLOBAL_TRACE(0)`.
 // This lets one edit just that one file to switch that `0` to `1`,
 // or edit this file to replace `local` with `1` and get all those
 // files at once.
-#define SYSCALLS_GLOBAL_TRACE(local) (local | 0)
+#define MISTOS_SYSCALLS_GLOBAL_TRACE(local) (local | 0)
 
-#endif  // ZIRCON_KERNEL_LIB_MISTOS_SYSCALLS_PRIV_H_
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_SYSCALLS_PRIV_H_
