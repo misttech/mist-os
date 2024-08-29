0375f4f6cfbd72e5849dd18d23e0ca8be93fa5a7
Bruno Herrera <bruno.herrera@mist-os.com>
2024-08-29 09:51:56 -0300

    [mistos][syscalls] Add mock sys_brk impl
    Change-Id: I0d7b23cf19218946fc0096a5a3a0f745241fc930

--- vendor/misttech/zircon/kernel/lib/syscalls/mm/mmap.cc
+++ vendor/misttech/zircon/kernel/lib/syscalls/mm/mmap.cc
@@ -2,30 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/mistos/starnix/kernel/arch/x64/syscalls.h>
-#include <lib/mistos/starnix/kernel/mm/syscalls.h>
-#include <lib/mistos/starnix/kernel/task/process_group.h>
-#include <lib/mistos/starnix/kernel/task/task.h>
-#include <lib/mistos/starnix/kernel/task/thread_group.h>
-#include <lib/mistos/starnix/kernel/zircon/task_dispatcher.h>
-#include <lib/mistos/starnix_syscalls/syscall_result.h>
 #include <lib/syscalls/forward.h>
 #include <trace.h>
 
 #include "../priv.h"
 
-#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
+#include <linux/errno.h>
 
-using namespace starnix_uapi;
-using namespace starnix;
-using namespace starnix_syscalls;
+#define LOCAL_TRACE MISTOS_SYSCALLS_GLOBAL_TRACE(2)
 
-long sys_brk(unsigned long brk) {
+long sys_a0012_brk(unsigned long brk) {
   LTRACEF_LEVEL(2, "brk=0x%lx\n", brk);
-  auto ut = ThreadDispatcher::GetCurrent();
-  auto syscall_ret = sys_brk(CurrentTask::From(TaskBuilder(ut->task()->task())), brk);
-  if (syscall_ret.is_error()) {
-    return syscall_ret.error_value().return_value();
-  }
-  return SyscallResult::From(*syscall_ret).value();
+  return -ENOSYS;
 }
