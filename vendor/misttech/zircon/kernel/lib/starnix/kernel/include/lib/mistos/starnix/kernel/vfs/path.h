4a7719fea625c767082fe199c518364ed35428e6
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-02 15:02:32 -0300

    [mistos][starnix] Refactor PathBuilder and add BString/Bstr impl
    Change-Id: I4e0f0104b1e9f3a3bf78ded2962c285fc3b4e165

--- vendor/misttech/zircon/kernel/lib/starnix/kernel/include/lib/mistos/starnix/kernel/vfs/path.h
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/include/lib/mistos/starnix/kernel/vfs/path.h
@@ -3,76 +3,101 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ZIRCON_KERNEL_LIB_MISTOS_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
-#define ZIRCON_KERNEL_LIB_MISTOS_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
 
+#include <lib/mistos/util/back_insert_iterator.h>
 #include <stdio.h>
 
-#include <algorithm>
-#include <functional>
-#include <vector>
-
+#include <fbl/alloc_checker.h>
 #include <fbl/intrusive_single_list.h>
-#include <fbl/string.h>
+#include <fbl/vector.h>
+#include <ktl/algorithm.h>
+#include <ktl/string_view.h>
 
 namespace starnix {
 
-using FsString = fbl::String;
-using FsStr = fbl::String;
+class BString {
+ public:
+  // Constructors
+  BString() = default;
+
+  // Creates a copy of another string.
+  BString(const BString& other) {
+    ktl::copy(other.begin(), other.end(), util::back_inserter(data_));
+  }
+
+  // Move constructs from another string.
+  // The other string is set to empty.
+  // Does not allocate heap memory.
+  BString(BString&& other) { data_.swap(other.data_); }
+
+  BString(const ktl::string_view& str) {
+    ktl::copy(str.begin(), str.end(), util::back_inserter(data_));
+  }
+
+  BString(const char* data, size_t size) {
+    ktl::copy(data, data + size, util::back_inserter(data_));
+  }
+
+  BString(const char* data) {
+    const ktl::string_view tmp(data);
+    ktl::copy(tmp.begin(), tmp.end(), util::back_inserter(data_));
+  }
+
+  // Assigns this string to a copy of another string.
+  BString& operator=(BString& other) {
+    data_.reset();
+    ktl::copy(other.begin(), other.end(), util::back_inserter(data_));
+    return *this;
+  }
+
+  // Move assigns from another string.
+  // The other string is set to empty.
+  // Does not allocate heap memory.
+  BString& operator=(BString&& other) {
+    data_.reset();
+    data_.swap(other.data_);
+    return *this;
+  }
+
+  // Accessors
+  const char* data() const { return data_.data(); }
+  size_t size() const { return data_.size(); }
+
+  // Iterators
+  const char* begin() const { return data_.begin(); }
+  const char* end() const { return data_.end(); }
+
+ private:
+  fbl::Vector<char> data_;
+};
+
+using FsString = BString;
+using FsStr = ktl::string_view;
 
 constexpr char SEPARATOR = '/';
 
 // Helper that can be used to build paths backwards, from the tail to head.
 class PathBuilder {
  public:
-  PathBuilder() : pos_(0) {}
+  void prepend_element(const FsStr& element);
 
-  void prepend_element(const FsStr& element) {
-    ensure_capacity(element.size() + 1);
-    pos_ -= element.size() + 1;
-    std::copy(element.begin(), element.end(), data_.begin() + pos_ + 1);
-    data_[pos_] = '/';
-  }
+  FsString build_absolute();
 
-  FsString build_absolute() {
-    if (pos_ == data_.size()) {
-      return "/";
-    }
-    data_.erase(data_.begin(), data_.begin() + pos_);
-    return FsString(data_.data(), data_.size());
-  }
-
-  FsString build_relative() {
-    FsString absolute = build_absolute();
-    std::vector<char> vec(absolute.begin(), absolute.end());
-    vec.erase(vec.begin());
-    return FsString(vec.data(), vec.size());
-  }
+  FsString build_relative();
 
  private:
   static constexpr size_t INITIAL_CAPACITY = 32;
 
-  void ensure_capacity(size_t capacity_needed) {
-    if (capacity_needed > pos_) {
-      auto current_size = data_.size();
-      auto len = current_size - pos_;
-      auto min_size = len + capacity_needed;
-      auto new_size = std::max(current_size * 2, INITIAL_CAPACITY);
-      while (new_size < min_size) {
-        new_size *= 2;
-      }
-      data_.reserve(new_size - current_size);
-      data_.resize(new_size - len, 0);
-      std::copy(data_.data() + pos_, data_.data() + pos_ + len, std::back_inserter(data_));
-      pos_ = new_size - len;
-    }
-  }
+  void ensure_capacity(size_t capacity_needed);
 
   // The path is kept in `data[pos..]`.
-  std::vector<char> data_;
-  size_t pos_;
+  fbl::Vector<char> data_;
+
+  size_t pos_ = 0;
 };
 
 }  // namespace starnix
 
-#endif  // ZIRCON_KERNEL_LIB_MISTOS_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
