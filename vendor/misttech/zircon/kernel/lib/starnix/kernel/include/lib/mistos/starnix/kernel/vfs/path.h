9bb55b7c2226f5ba2aa256be832e233ccc3a7dd4
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-03 17:13:05 -0300

    [mistos][util] Add support for 'rust' BString
    Change-Id: I96b4f5b64938dce402f8800b5f1f39982b0254c9

--- vendor/misttech/zircon/kernel/lib/starnix/kernel/include/lib/mistos/starnix/kernel/vfs/path.h
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/include/lib/mistos/starnix/kernel/vfs/path.h
@@ -7,6 +7,7 @@
 #define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_PATH_H_
 
 #include <lib/mistos/util/back_insert_iterator.h>
+#include <lib/mistos/util/bstring.h>
 #include <stdio.h>
 
 #include <fbl/alloc_checker.h>
@@ -16,67 +17,6 @@
 #include <ktl/string_view.h>
 
 namespace starnix {
-
-class BString {
- public:
-  // Constructors
-  BString() = default;
-
-  // Creates a copy of another string.
-  BString(const BString& other) {
-    ktl::copy(other.begin(), other.end(), util::back_inserter(data_));
-  }
-
-  // Move constructs from another string.
-  // The other string is set to empty.
-  // Does not allocate heap memory.
-  BString(BString&& other) { data_.swap(other.data_); }
-
-  BString(const ktl::string_view& str) {
-    ktl::copy(str.begin(), str.end(), util::back_inserter(data_));
-  }
-
-  BString(const char* data, size_t size) {
-    ktl::copy(data, data + size, util::back_inserter(data_));
-  }
-
-  BString(const char* data) {
-    const ktl::string_view tmp(data);
-    ktl::copy(tmp.begin(), tmp.end(), util::back_inserter(data_));
-  }
-
-  // Assigns this string to a copy of another string.
-  BString& operator=(const BString& other) {
-    data_.reset();
-    ktl::copy(other.begin(), other.end(), util::back_inserter(data_));
-    return *this;
-  }
-
-  // Move assigns from another string.
-  // The other string is set to empty.
-  // Does not allocate heap memory.
-  BString& operator=(BString&& other) {
-    data_.reset();
-    data_.swap(other.data_);
-    return *this;
-  }
-
-  // Create a std::string_view backed by the string.
-  // The view does not take ownership of the data so the string
-  // must outlast the std::string_view.
-  operator ktl::string_view() const { return {data(), size()}; }
-
-  // Accessors
-  const char* data() const { return data_.data(); }
-  size_t size() const { return data_.size(); }
-
-  // Iterators
-  const char* begin() const { return data_.begin(); }
-  const char* end() const { return data_.end(); }
-
- private:
-  fbl::Vector<char> data_;
-};
 
 using FsString = BString;
 using FsStr = ktl::string_view;
