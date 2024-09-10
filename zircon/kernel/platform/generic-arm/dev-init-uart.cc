4950eeb99fd3c38a4665de7328dd5479920f4b24
Gianfranco Valentino <gevalentino@google.com>
2024-09-10 01:07:53 +0000

    [kernel] Remove unused legacy amlogic uart.
    Remove legacy code.
    
    Bug: 364713557
    Change-Id: I2170fb74b38194958897949c3ef36a2e2317fff7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1115666
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>

--- zircon/kernel/platform/generic-arm/dev-init-uart.cc
+++ zircon/kernel/platform/generic-arm/dev-init-uart.cc
@@ -10,7 +10,6 @@
 #include <lib/zbi-format/driver-config.h>
 
 #include <dev/init.h>
-#include <dev/uart/amlogic_s905/init.h>
 #include <dev/uart/dw8250/init.h>
 #include <dev/uart/motmot/init.h>
 #include <dev/uart/pl011/init.h>
@@ -25,9 +24,6 @@ void UartInitEarly(uint32_t extra, const uart::null::Driver::config_type& config
 
 void UartInitEarly(uint32_t extra, const zbi_dcfg_simple_t& config) {
   switch (extra) {
-    case ZBI_KERNEL_DRIVER_AMLOGIC_UART:
-      AmlogicS905UartInitEarly(config);
-      break;
     case ZBI_KERNEL_DRIVER_I8250_MMIO8_UART:
       Dw8250UartInitEarly(config, 1);
       break;
@@ -46,9 +42,6 @@ void UartInitEarly(uint32_t extra, const zbi_dcfg_simple_t& config) {
 
 void UartInitLate(uint32_t extra) {
   switch (extra) {
-    case ZBI_KERNEL_DRIVER_AMLOGIC_UART:
-      AmlogicS905UartInitLate();
-      break;
     case ZBI_KERNEL_DRIVER_I8250_MMIO8_UART:
     case ZBI_KERNEL_DRIVER_I8250_MMIO32_UART:
     case ZBI_KERNEL_DRIVER_DW8250_UART:
