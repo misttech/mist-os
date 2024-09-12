b67d84afc820d55dbaf3ff24b0ab67eab9cc28ee
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-12 10:04:15 -0300

    [mistos][starnix][bootfs] Initial bootfs/zbi impl
    Change-Id: I976c96e598fd9bdc5e2238748cad8b0c2e15b351

---
+++ vendor/misttech/zircon/kernel/lib/starnix/modules/bootfs/include/lib/modules/bootfs/bootfs.h
@@ -0,0 +1,85 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_FS_BOOTFS_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_FS_BOOTFS_H_
+
+#include <lib/fit/result.h>
+#include <lib/mistos/starnix/kernel/vfs/file_system.h>
+#include <lib/mistos/starnix/kernel/vfs/file_system_ops.h>
+#include <lib/mistos/starnix/kernel/vfs/fs_node_ops.h>
+#include <lib/mistos/starnix/kernel/vfs/xattr.h>
+#include <lib/mistos/starnix_uapi/device_type.h>
+#include <lib/mistos/starnix_uapi/errors.h>
+#include <lib/starnix_sync/locks.h>
+#include <zircon/compiler.h>
+#include <zircon/types.h>
+
+#include <fbl/ref_counted.h>
+#include <fbl/ref_ptr.h>
+#include <kernel/mutex.h>
+
+namespace bootfs {
+
+using namespace starnix;
+using namespace starnix_sync;
+
+class BootFs : public FileSystemOps {
+ public:
+  static FileSystemHandle new_fs(const fbl::RefPtr<Kernel>& kernel);
+
+  static fit::result<Errno, FileSystemHandle> new_fs_with_options(const fbl::RefPtr<Kernel>& kernel,
+                                                                  FileSystemOptions options);
+
+  fit::result<Errno, struct statfs> statfs(const FileSystem& fs,
+                                           const CurrentTask& current_task) final;
+
+  const FsStr& name() final;
+
+ public:
+  // C++
+  ~BootFs();
+
+ private:
+  const FsStr name_ = "bootfs";
+};
+
+class BootfsDirectory : public FsNodeOps {
+ private:
+  MemoryXattrStorage xattrs_;
+
+  mutable StarnixMutex<uint32_t> child_count_;
+
+ public:
+  static BootfsDirectory* New();
+
+  fit::result<Errno, ktl::unique_ptr<FileOps>> create_file_ops(
+      /*FileOpsCore& locked,*/ const FsNode& node, const CurrentTask& current_task,
+      OpenFlags flags) final;
+
+  fit::result<Errno, FsNodeHandle> mkdir(const FsNode& node, const CurrentTask& current_task,
+                                         const FsStr& name, FileMode mode, FsCred owner) final;
+
+  fit::result<Errno, FsNodeHandle> mknod(const FsNode& node, const CurrentTask& current_task,
+                                         const FsStr& name, FileMode mode, DeviceType dev,
+                                         FsCred owner) final;
+
+  fit::result<Errno, FsNodeHandle> create_tmpfile(const FsNode& node,
+                                                  const CurrentTask& current_task, FileMode mode,
+                                                  FsCred owner) final;
+
+  fit::result<Errno> link(const FsNode& node, const CurrentTask& current_task, const FsStr& name,
+                          const FsNodeHandle& child) final;
+
+  fit::result<Errno> unlink(const FsNode& node, const CurrentTask& current_task, const FsStr& name,
+                            const FsNodeHandle& child) final;
+};
+
+fit::result<Errno, FsNodeHandle> create_child_node(const CurrentTask& current_task,
+                                                   const FsNode& parent, FileMode mode,
+                                                   DeviceType dev, FsCred owner);
+
+}  // namespace bootfs
+
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_FS_BOOTFS_H_
