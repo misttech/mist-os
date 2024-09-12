ec5f17a06273ef0dd13d48fd2bd1d062490d04da
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-12 10:04:15 -0300

    [mistos][starnix][bootfs] Initial bootfs/zbi impl
    Change-Id: I976c96e598fd9bdc5e2238748cad8b0c2e15b351

---
+++ vendor/misttech/zircon/kernel/lib/starnix/modules/bootfs/tests/bootfs-test.cc
@@ -0,0 +1,27 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/mistos/starnix/kernel/task/kernel.h>
+#include <lib/mistos/starnix/kernel/task/task.h>
+#include <lib/mistos/starnix/kernel/vfs/fs_node.h>
+#include <lib/mistos/starnix/testing/testing.h>
+#include <lib/modules/bootfs/bootfs.h>
+#include <lib/unittest/unittest.h>
+
+namespace unit_testing {
+
+bool test_bootfs() {
+  BEGIN_TEST;
+
+  auto [kernel, current_task] = starnix::testing::create_kernel_and_task();
+
+  END_TEST;
+}
+
+}  // namespace unit_testing
+
+UNITTEST_START_TESTCASE(starnix_fs_bootfs)
+UNITTEST("test bootfs", unit_testing::test_bootfs)
+UNITTEST_END_TESTCASE(starnix_fs_bootfs, "starnix_fs_bootfs", "Tests for starnix bootfs")
