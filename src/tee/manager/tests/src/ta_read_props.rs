f464f8806e448336bbfbb363a115a98f26174aaf
Eric Stone <ecstone@google.com>
2024-09-17 23:05:51 +0000

    [tee] Add TA properties template
    This template properties file will be included with each TA, with the
    option to override it. A future change will add validation, which the
    template will fail and therefore require TAs to provide a valid property
    file.
    
    This change also adds a read_props test TA which currently checks for the
    existence of the properties file. This TA can be extended to exercise
    each internal core GetProperty API.
    
    Bug: b/361406102
    Test: fx test //src/tee/manager
    Change-Id: I856149ff3b6526af56b585d2f9c57c90b0bd666c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1118496
    Reviewed-by: James Robinson <jamesr@google.com>
    Commit-Queue: Eric Stone <ecstone@google.com>

---
+++ src/tee/manager/tests/src/ta_read_props.rs
@@ -0,0 +1,32 @@
+// Copyright 2024 The Fuchsia Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// These tests use a rust test component to talk to the test_props TA component.
+// The TA component itself performs the interesting test via internal core APIs.
+use anyhow::Error;
+use fuchsia_component::client::connect_to_protocol_at;
+use tee_internal::binding::TEE_SUCCESS;
+use {fidl_fuchsia_io as _, fidl_fuchsia_tee, fuchsia_fs as _};
+
+// TODO: single source of truth for TA CMD IDs, which should be defined by the TA.
+const CMD_TEST_PROPS_EXIST: u32 = 0;
+
+#[fuchsia::test]
+async fn test_props_file_exists() -> Result<(), Error> {
+    const TA_READ_PROPS_UUID: &str = "932517f3-e807-48f8-b584-fbeec848c2b6";
+
+    let noop_ta = connect_to_protocol_at::<fidl_fuchsia_tee::ApplicationMarker>(
+        "/ta/".to_owned() + TA_READ_PROPS_UUID,
+    )?;
+    let (session_id, op_result) = noop_ta.open_session2(vec![]).await?;
+    assert_eq!(op_result.return_code, Some(TEE_SUCCESS as u64));
+    assert_eq!(op_result.return_origin, Some(fidl_fuchsia_tee::ReturnOrigin::TrustedApplication));
+
+    let op_result = noop_ta.invoke_command(session_id, CMD_TEST_PROPS_EXIST, vec![]).await?;
+    assert_eq!(op_result.return_code, Some(TEE_SUCCESS as u64));
+    assert_eq!(op_result.return_origin, Some(fidl_fuchsia_tee::ReturnOrigin::TrustedApplication));
+    noop_ta.close_session(session_id).await?;
+
+    Ok(())
+}
