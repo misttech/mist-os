dc9d574110119b0ba7b1c81b7d28f336f163f2ce
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-19 09:57:03 -0300

    [mistos][starnix][fs] Add support for TreeBuilder
    Change-Id: I188ad5620ffe8d37035f5bd0f437f69c9030186f

---
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/include/lib/mistos/starnix/kernel/vfs/simple_directory.h
@@ -0,0 +1,55 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_SIMPLE_DIRECTORY_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_SIMPLE_DIRECTORY_H_
+
+#include <lib/fit/result.h>
+#include <lib/mistos/starnix/kernel/vfs/fs_node_ops.h>
+#include <lib/mistos/starnix/kernel/vfs/path.h>
+#include <lib/mistos/starnix_uapi/errors.h>
+#include <lib/mistos/util/allocator.h>
+
+#include <map>
+
+#include <fbl/ref_ptr.h>
+#include <ktl/unique_ptr.h>
+
+namespace starnix {
+
+using FileMode = starnix_uapi::FileMode;
+using FsCred = starnix_uapi::FsCred;
+
+class FileSystem;
+class CurrentTask;
+class FsNode;
+using FsNodeHandle = fbl::RefPtr<FsNode>;
+using BTreeMap = std::map<const FsStr, FsNodeHandle, std::less<const FsStr>,
+                          util::Allocator<ktl::pair<const FsStr, FsNodeHandle>>>;
+
+class SimpleDirectory : public FsNodeOps {
+ private:
+  BTreeMap entries_;
+
+ public:
+  /// Adds a child entry to this directory.
+  fit::result<Errno> add_entry(const FsStr name, FsNodeHandle, bool overwrite = false);
+
+ public:
+  SimpleDirectory() = default;
+
+  /// impl FsNodeOps
+  fs_node_impl_dir_readonly();
+
+  fit::result<Errno, ktl::unique_ptr<FileOps>> create_file_ops(
+      /*FileOpsCore& locked,*/ const FsNode& node, const CurrentTask& current_task,
+      OpenFlags flags) final;
+
+  fit::result<Errno, FsNodeHandle> lookup(const FsNode& node, const CurrentTask& current_task,
+                                          const FsStr& name) final;
+};
+
+}  // namespace starnix
+
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_KERNEL_INCLUDE_LIB_MISTOS_STARNIX_KERNEL_VFS_SIMPLE_DIRECTORY_H_
