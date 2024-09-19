c9ef8d2495f182354d36a14fbef331c87e0499d7
Thomas Zander <thomasalpinus@google.com>
2024-09-19 17:27:11 +0000

    [make-fuchsia-vol] Refactor in lib and main
    Refactor the make-fuchsia-vol tool into a library and main. This allows
    the functionality to be used by interfacing with the lib, rather than
    launching the binary.
    
    Fixed: b/367856215
    Change-Id: I996b430c24c3a39ae48ceab907b633bbb58aa035
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1121254
    Reviewed-by: David Pursell <dpursell@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>

---
+++ tools/make-fuchsia-vol/src/args.rs
@@ -0,0 +1,188 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use argh::FromArgs;
+use camino::Utf8PathBuf;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Arch {
+    X64,
+    Arm64,
+}
+
+impl FromStr for Arch {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "x64" => Ok(Arch::X64),
+            "arm64" => Ok(Arch::Arm64),
+            _ => Err("Expected x64 or arm64".to_string()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BootPart {
+    BootA,
+    BootB,
+    BootR,
+}
+
+impl FromStr for BootPart {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "a" => Ok(BootPart::BootA),
+            "b" => Ok(BootPart::BootB),
+            "r" => Ok(BootPart::BootR),
+            _ => Err("Expected a, b or r".to_string()),
+        }
+    }
+}
+
+/// make-fuchsia-vol command line arguments
+#[derive(FromArgs, Debug)]
+pub struct TopLevel {
+    /// disk-path
+    #[argh(positional)]
+    pub disk_path: Utf8PathBuf,
+
+    /// enable verbose logging
+    #[argh(switch)]
+    pub verbose: bool,
+
+    /// fuchsia build dir
+    #[argh(option)]
+    pub fuchsia_build_dir: Option<Utf8PathBuf>,
+
+    /// use named product information from Product Bundle Metadata (PBM). If no
+    /// product bundle is specified and there is an obvious choice, that will be
+    /// used (e.g. if there is only one PBM available).
+    #[argh(option)]
+    pub product_bundle: Option<Utf8PathBuf>,
+
+    /// the name of a particular product bundle to use, consulted if
+    /// --product-bundle is unset.
+    #[argh(option)]
+    pub product_bundle_name: Option<String>,
+
+    /// the architecture of the target CPU (x64|arm64)
+    #[argh(option, default = "Arch::X64")]
+    pub arch: Arch,
+
+    /// the architecture of the host CPU (x64|arm64)
+    #[argh(option, default = "Arch::X64")]
+    pub host_arch: Arch,
+
+    /// path to fuchsia-efi.efi
+    #[argh(option)]
+    pub bootloader: Option<Utf8PathBuf>,
+
+    /// path to zbi (default: zircon-a from image manifests)
+    #[argh(option)]
+    pub zbi: Option<Utf8PathBuf>,
+
+    /// path to command line file (if exists)
+    #[argh(option)]
+    pub cmdline: Option<Utf8PathBuf>,
+
+    /// path to zedboot.zbi (default: zircon-r from image manifests)
+    #[argh(option)]
+    pub zedboot: Option<Utf8PathBuf>,
+
+    /// ramdisk-only mode - only write an ESP partition
+    #[argh(switch)]
+    pub ramdisk_only: bool,
+
+    /// path to blob partition image (not used with ramdisk)
+    #[argh(option)]
+    pub blob: Option<Utf8PathBuf>,
+
+    /// if true, use sparse fvm instead of full fvm
+    #[argh(switch)]
+    pub use_sparse_fvm: bool,
+
+    /// path to sparse FVM image (default: storage-sparse from image manifests)
+    #[argh(option)]
+    pub sparse_fvm: Option<Utf8PathBuf>,
+
+    /// don't add Zircon-{{A,B,R}} partitions
+    #[argh(switch)]
+    pub no_abr: bool,
+
+    /// path to partition image for Zircon-A (default: from --zbi)
+    #[argh(option)]
+    pub zircon_a: Option<Utf8PathBuf>,
+
+    /// path to partition image for Vbmeta-A
+    #[argh(option)]
+    pub vbmeta_a: Option<Utf8PathBuf>,
+
+    /// path to partition image for Zircon-B (default: from --zbi)
+    #[argh(option)]
+    pub zircon_b: Option<Utf8PathBuf>,
+
+    /// path to partition image for Vbmeta-B
+    #[argh(option)]
+    pub vbmeta_b: Option<Utf8PathBuf>,
+
+    /// path to partition image for Zircon-R (default: zircon-r from image manifests)
+    #[argh(option)]
+    pub zircon_r: Option<Utf8PathBuf>,
+
+    /// path to partition image for Vbmeta-R
+    #[argh(option)]
+    pub vbmeta_r: Option<Utf8PathBuf>,
+
+    /// kernel partition size for A/B/R
+    #[argh(option, default = "256 * 1024 * 1024")]
+    pub abr_size: u64,
+
+    /// partition size for vbmeta A/B/R
+    #[argh(option, default = "64 * 1024")]
+    pub vbmeta_size: u64,
+
+    /// A/B/R partition to boot by default
+    #[argh(option, default = "BootPart::BootA")]
+    pub abr_boot: BootPart,
+
+    /// the block size of the target disk
+    #[argh(option)]
+    pub block_size: Option<u64>,
+
+    /// efi partition size in bytes
+    #[argh(option, default = "63 * 1024 * 1024")]
+    pub efi_size: u64,
+
+    /// system (i.e. FVM or Fxfs) disk partition size in bytes (unspecified means `fill`)
+    #[argh(option)]
+    pub system_disk_size: Option<u64>,
+
+    /// create or resize the image to this size in bytes
+    #[argh(option)]
+    pub resize: Option<u64>,
+
+    /// a seed from which the UUIDs are derived; this will also set any timestamps used to fixed
+    /// values, so the resulting image should be reproducible (only suitable for TESTING).
+    #[argh(option)]
+    pub seed: Option<String>,
+
+    /// if true, write a dependency file which will be the same as the output path but with a '.d'
+    /// extension.
+    #[argh(switch)]
+    pub depfile: bool,
+
+    /// whether to use Fxfs instead of FVM to store the base system
+    #[argh(switch)]
+    pub use_fxfs: bool,
+
+    /// path to Fxfs partition image (if --use_fxfs is set)
+    #[argh(option)]
+    pub fxfs: Option<Utf8PathBuf>,
+
+    /// contents to put in a "qemu-commandline" partition
+    #[argh(option)]
+    pub qemu_commandline: Option<String>,
+}
