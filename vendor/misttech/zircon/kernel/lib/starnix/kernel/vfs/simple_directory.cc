38c08d66411b624170190b5e1fb95df0e9c970a0
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-19 09:57:03 -0300

    [mistos][starnix][fs] Add support for TreeBuilder
    Change-Id: I188ad5620ffe8d37035f5bd0f437f69c9030186f

---
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/vfs/simple_directory.cc
@@ -0,0 +1,50 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "lib/mistos/starnix/kernel/vfs/simple_directory.h"
+
+#include <lib/fit/result.h>
+#include <lib/mistos/starnix/kernel/vfs/file_ops.h>
+#include <lib/mistos/starnix/kernel/vfs/file_system.h>
+#include <lib/mistos/starnix/kernel/vfs/fs_node.h>
+#include <lib/mistos/starnix/kernel/vfs/path.h>
+#include <trace.h>
+
+#include <fbl/alloc_checker.h>
+
+#include "../kernel_priv.h"
+
+#define LOCAL_TRACE STARNIX_KERNEL_GLOBAL_TRACE(0)
+
+namespace starnix {
+
+fit::result<Errno> SimpleDirectory::add_entry(const FsStr name, FsNodeHandle entry,
+                                              bool overwrite) {
+  LTRACEF("this(%p) name=[%.*s]\n", this, static_cast<int>(name.length()), name.data());
+  if (!overwrite && entries_.contains(name)) {
+    return fit::error(errno(EROFS));
+  }
+  auto [it, inserted] = entries_.emplace(name, entry);
+  if (!inserted) {
+    return fit::error(errno(EROFS));
+  }
+  return fit::ok();
+}
+
+fit::result<Errno, ktl::unique_ptr<FileOps>> SimpleDirectory::create_file_ops(
+    /*FileOpsCore& locked,*/ const FsNode& node, const CurrentTask& current_task, OpenFlags flags) {
+  return fit::error(errno(ENOMEM));
+}
+
+fit::result<Errno, FsNodeHandle> SimpleDirectory::lookup(const FsNode& node,
+                                                         const CurrentTask& current_task,
+                                                         const FsStr& name) {
+  LTRACEF("this(%p) name=[%.*s]\n", this, static_cast<int>(name.length()), name.data());
+  auto it = entries_.find(name);
+  if (it != entries_.end())
+    return fit::ok(it->second);
+  return fit::error(errno(ENOENT));
+}
+
+}  // namespace starnix
