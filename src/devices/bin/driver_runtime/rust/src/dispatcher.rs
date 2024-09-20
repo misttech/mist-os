ebfac54255e28226c529bc3179a8cf8548795cfb
Megan Batty <mgnb@google.com>
2024-09-20 01:29:04 +0000

    [driver_runtime][rust] Async channel receiving
    Implements future-based functions for async receiving of data from
    a driver channel handle, in parallel with the existing synchronous
    `try_` read functions.
    
    Fixed: 359228344
    Change-Id: I112fad62d8fa73f37eb0bb3918cbe202e532a984
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1099333
    Reviewed-by: David Koloski <dkoloski@google.com>
    Reviewed-by: Novin Changizi <novinc@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>

--- src/devices/bin/driver_runtime/rust/src/dispatcher.rs
+++ src/devices/bin/driver_runtime/rust/src/dispatcher.rs
@@ -149,7 +149,7 @@ impl DispatcherBuilder {
 }
 
 #[derive(Debug)]
-pub struct Dispatcher(NonNull<fdf_dispatcher_t>);
+pub struct Dispatcher(pub(crate) NonNull<fdf_dispatcher_t>);
 
 // SAFETY: The api of fdf_dispatcher_t is thread safe.
 unsafe impl Send for Dispatcher {}
@@ -167,7 +167,22 @@ impl Dispatcher {
         Self(handle)
     }
 
-    pub fn post_task_sync(&self, p: impl TaskCallback) -> Result<(), Status> {
+    fn get_raw_flags(&self) -> u32 {
+        // SAFETY: the inner fdf_dispatcher_t is valid by construction
+        unsafe { fdf_dispatcher_get_options(self.0.as_ptr()) }
+    }
+
+    /// Whether this dispatcher's tasks and futures can run on multiple threads at the same time.
+    pub fn is_unsynchronized(&self) -> bool {
+        (self.get_raw_flags() & DispatcherBuilder::UNSYNCHRONIZED) != 0
+    }
+
+    /// Whether this dispatcher is allowed to call blocking functions or not
+    pub fn allows_thread_blocking(&self) -> bool {
+        (self.get_raw_flags() & DispatcherBuilder::ALLOW_THREAD_BLOCKING) != 0
+    }
+
+    pub fn post_task_sync<'a>(&self, p: impl TaskCallback<'a>) -> Result<(), Status> {
         // SAFETY: the fdf dispatcher is valid by construction and can provide an async dispatcher.
         let async_dispatcher = unsafe { fdf_dispatcher_get_async_dispatcher(self.0.as_ptr()) };
         let task_arc = Arc::new(UnsafeCell::new(TaskFunc {
@@ -197,11 +212,11 @@ impl Dispatcher {
         }
     }
 
-    pub fn spawn_task(
-        &self,
-        future: impl Future<Output = ()> + 'static + Send,
+    pub fn spawn_task<'a>(
+        &'a self,
+        future: impl Future<Output = ()> + 'a + Send,
     ) -> Result<(), Status> {
-        let task = Arc::new(TaskFuture {
+        let task = Arc::new(Task {
             future: Mutex::new(Some(future.boxed())),
             dispatcher: ManuallyDrop::new(Dispatcher(self.0)),
         });
@@ -269,15 +284,15 @@ impl<'a> core::ops::DerefMut for DispatcherRef<'a> {
     }
 }
 
-pub trait TaskCallback: FnOnce(Status) + 'static + Send + Sync {}
-impl<T> TaskCallback for T where T: FnOnce(Status) + 'static + Send + Sync {}
+pub trait TaskCallback<'a>: FnOnce(Status) + 'a + Send + Sync {}
+impl<'a, T> TaskCallback<'a> for T where T: FnOnce(Status) + 'a + Send + Sync {}
 
-struct TaskFuture {
-    future: Mutex<Option<BoxFuture<'static, ()>>>,
+struct Task<'a> {
+    future: Mutex<Option<BoxFuture<'a, ()>>>,
     dispatcher: ManuallyDrop<Dispatcher>,
 }
 
-impl ArcWake for TaskFuture {
+impl<'a> ArcWake for Task<'a> {
     fn wake_by_ref(arc_self: &Arc<Self>) {
         match arc_self.queue() {
             Err(e) if e == Status::from_raw(ZX_ERR_BAD_STATE) => {
@@ -291,7 +306,7 @@ impl ArcWake for TaskFuture {
     }
 }
 
-impl TaskFuture {
+impl<'a> Task<'a> {
     /// Posts a task to progress the currently stored future. The task will
     /// consume the future if the future is ready after the next poll.
     /// Otherwise, the future is kept to be polled again after being woken.
@@ -320,17 +335,17 @@ impl TaskFuture {
 }
 
 #[repr(C)]
-struct TaskFunc {
+struct TaskFunc<'a> {
     task: async_task,
     dispatcher: *mut async_dispatcher,
-    func: Box<dyn TaskCallback>,
+    func: Box<dyn TaskCallback<'a>>,
 }
 
-impl TaskFunc {
+impl<'a> TaskFunc<'a> {
     extern "C" fn call(_dispatcher: *mut async_dispatcher, task: *mut async_task, status: i32) {
         // SAFETY: the async api promises that this function will only be called
         // up to once, so we can reconstitute the `Arc` and let it get dropped.
-        let task = unsafe { Arc::from_raw(task as *const UnsafeCell<TaskFunc>) };
+        let task = unsafe { Arc::from_raw(task as *const UnsafeCell<Self>) };
         // SAFETY: if we can't get a mut ref from the arc, then the task is already
         // being cancelled, so we don't want to call it.
         if let Some(task) = Arc::try_unwrap(task).ok() {
@@ -424,7 +439,7 @@ pub(crate) mod test {
         });
     }
 
-    pub fn with_raw_dispatcher<T>(name: &str, p: impl FnOnce(&Dispatcher) -> T) -> T {
+    pub fn with_raw_dispatcher<T>(name: &str, p: impl for<'a> FnOnce(&'a Dispatcher) -> T) -> T {
         ensure_driver_env();
 
         let (shutdown_tx, shutdown_rx) = mpsc::channel();
