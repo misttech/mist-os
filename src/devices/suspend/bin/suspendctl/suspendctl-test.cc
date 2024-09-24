d7ed3a6c7b53609a15416f6c0b81ff101d693649
Gurjant Kalsi <gkalsi@google.com>
2024-09-24 19:40:02 +0000

    [fsh][suspendctl] Introduce a suspendctl to invoke FSH directly
    Test: invoked `suspendctl suspend` on vim3, also `fx test suspendctl-test`
    
    Change-Id: I18f1ecde5072b6d17b3d42532bb28504a1885df6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1113256
    Reviewed-by: Novin Changizi <novinc@google.com>
    Commit-Queue: Gurjant Kalsi <gkalsi@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>

---
+++ src/devices/suspend/bin/suspendctl/suspendctl-test.cc
@@ -0,0 +1,128 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "suspendctl.h"
+
+#include <fidl/fuchsia.device/cpp/wire_test_base.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/markers.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/wire_test_base.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/wire_types.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/fidl/cpp/wire/channel.h>
+#include <lib/zx/time.h>
+#include <zircon/errors.h>
+
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+class FakeSuspendDevice : public fidl::testing::WireTestBase<suspendctl::fh_suspend::Suspender> {
+ public:
+  int SuspendCallCount() const { return suspend_call_count_; }
+  void SetSuspendCallCount(int count) { suspend_call_count_ = count; }
+
+  void NotImplemented_(const std::string& name, fidl::CompleterBase& completer) override {
+    // ADD_FAILURE("unexpected call to %s", name.c_str());
+    completer.Close(ZX_ERR_NOT_SUPPORTED);
+  }
+
+ private:
+  static constexpr zx::duration kDefaultSuspendDuration = zx::sec(5);
+  static constexpr zx::duration kDefaultSuspendOverhead = zx::sec(1);
+  static constexpr zx::duration kDefaultResumeLatency = zx::usec(100);
+
+  void Suspend(SuspendRequestView request, SuspendCompleter::Sync& completer) override;
+  void GetSuspendStates(GetSuspendStatesCompleter::Sync& completer) override;
+
+  int suspend_call_count_ = 0;
+};
+
+void FakeSuspendDevice::Suspend(SuspendRequestView request, SuspendCompleter::Sync& completer) {
+  suspend_call_count_++;
+
+  if (!request->has_state_index() || request->state_index() != 0) {
+    completer.ReplyError(ZX_ERR_INVALID_ARGS);
+    return;
+  }
+
+  fidl::Arena arena;
+  auto result = suspendctl::fh_suspend::wire::SuspenderSuspendResponse::Builder(arena)
+                    .suspend_duration(kDefaultSuspendDuration.to_nsecs())
+                    .suspend_overhead(kDefaultSuspendOverhead.to_nsecs())
+                    .Build();
+
+  completer.ReplySuccess(result);
+}
+
+void FakeSuspendDevice::GetSuspendStates(GetSuspendStatesCompleter::Sync& completer) {
+  fidl::Arena arena;
+  auto state = suspendctl::fh_suspend::wire::SuspendState::Builder(arena)
+                   .resume_latency(kDefaultResumeLatency.to_nsecs())
+                   .Build();
+  std::vector<suspendctl::fh_suspend::wire::SuspendState> states = {state};
+  auto result = suspendctl::fh_suspend::wire::SuspenderGetSuspendStatesResponse::Builder(arena)
+                    .suspend_states(states)
+                    .Build();
+  completer.ReplySuccess(result);
+}
+
+class SuspendctlTest : public testing::Test {
+ public:
+  void SetUp() override;
+
+ protected:
+  fidl::SyncClient<suspendctl::fh_suspend::Suspender> client_;
+
+ private:
+  FakeSuspendDevice suspend_;
+  async::Loop loop_{&kAsyncLoopConfigNeverAttachToThread};
+};
+
+void SuspendctlTest::SetUp() {
+  auto suspend_endpoints = fidl::Endpoints<suspendctl::fh_suspend::Suspender>::Create();
+  fidl::BindServer(loop_.dispatcher(), std::move(suspend_endpoints.server),
+                   static_cast<fidl::WireServer<suspendctl::fh_suspend::Suspender>*>(&suspend_));
+
+  client_ =
+      fidl::SyncClient<suspendctl::fh_suspend::Suspender>(std::move(suspend_endpoints.client));
+  ASSERT_EQ(loop_.StartThread("suspender-test-fidl-thread"), ZX_OK);
+}
+
+TEST_F(SuspendctlTest, TestCallSuspend) {
+  std::stringstream discard_cout, discard_cerr;
+  int result = suspendctl::DoSuspend(std::move(client_), discard_cout, discard_cerr);
+  EXPECT_EQ(result, 0);
+}
+
+TEST(SuspendCtlStandaloneTest, TestParseArgs) {
+  {
+    const std::vector<std::string> kTestArgs = {"suspendctl", "world"};
+    auto result = suspendctl::ParseArgs(kTestArgs);
+    EXPECT_EQ(result, suspendctl::Action::Error);
+  }
+  {
+    const std::vector<std::string> kTestArgs = {"suspendctl", "help"};
+    auto result = suspendctl::ParseArgs(kTestArgs);
+    EXPECT_EQ(result, suspendctl::Action::PrintHelp);
+  }
+  {
+    const std::vector<std::string> kTestArgs = {"suspendctl", "suspend"};
+    auto result = suspendctl::ParseArgs(kTestArgs);
+    EXPECT_EQ(result, suspendctl::Action::Suspend);
+  }
+  {
+    const std::vector<std::string> kTestArgs = {"suspendctl"};
+    auto result = suspendctl::ParseArgs(kTestArgs);
+    EXPECT_EQ(result, suspendctl::Action::Error);
+  }
+  {
+    const std::vector<std::string> kTestArgs = {"suspendctl", "suspend", "extra"};
+    auto result = suspendctl::ParseArgs(kTestArgs);
+    EXPECT_EQ(result, suspendctl::Action::Suspend);
+  }
+}
+
+}  // namespace
---
+++ src/devices/suspend/bin/suspendctl/suspendctl.h
@@ -0,0 +1,35 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVICES_SUSPEND_BIN_SUSPENDCTL_SUSPENDCTL_H_
+#define SRC_DEVICES_SUSPEND_BIN_SUSPENDCTL_SUSPENDCTL_H_
+
+#include <fidl/fuchsia.device/cpp/wire.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/fidl.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/natural_ostream.h>
+#include <fidl/fuchsia.hardware.suspend/cpp/natural_types.h>
+#include <lib/zx/result.h>
+#include <lib/zx/time.h>
+
+#include <ostream>
+#include <string>
+#include <vector>
+
+namespace suspendctl {
+
+namespace fh_suspend = fuchsia_hardware_suspend;
+
+enum class Action {
+  Suspend,
+  PrintHelp,
+  Error,
+};
+
+Action ParseArgs(const std::vector<std::string>& argv);
+int DoSuspend(fidl::SyncClient<fh_suspend::Suspender> client, std::ostream& out, std::ostream& err);
+int run(int argc, char* argv[]);
+
+}  // namespace suspendctl
+
+#endif  // SRC_DEVICES_SUSPEND_BIN_SUSPENDCTL_SUSPENDCTL_H_
