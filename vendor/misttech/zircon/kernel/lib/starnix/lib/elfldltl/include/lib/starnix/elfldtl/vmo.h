0109d71e1e115690f53e5220561a92350aa47062
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-24 09:15:56 -0300

    [mistos][starnix] Add elfldltl lib (vmo file suppport)
    Change-Id: I687698d9fe8c8376c4fbb3ae4706b906e6856133

---
+++ vendor/misttech/zircon/kernel/lib/starnix/lib/elfldltl/include/lib/starnix/elfldtl/vmo.h
@@ -0,0 +1,60 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_ELFLDLTL_INCLUDE_LIB_STARNIX_ELFLDTL_VMO_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_ELFLDLTL_INCLUDE_LIB_STARNIX_ELFLDTL_VMO_H_
+
+#include <lib/elfldltl/file.h>
+#include <lib/elfldltl/zircon.h>
+#include <lib/fit/result.h>
+#include <lib/stdcompat/span.h>
+#include <zircon/errors.h>
+
+#include <fbl/ref_ptr.h>
+#include <vm/vm_object.h>
+
+namespace elfldltl {
+
+// elfldltl::VmoFile is constructible from fbl::RefPtr<VmObject> and meets the File API
+// (see <lib/elfldltl/memory.h>) by calling read.
+
+namespace internal {
+
+inline fit::result<ZirconError> ReadVmo(const fbl::RefPtr<VmObject>& vmo, uint64_t offset,
+                                        cpp20::span<std::byte> buffer) {
+  zx_status_t status = vmo->Read(buffer.data(), offset, buffer.size());
+  if (status == ZX_ERR_OUT_OF_RANGE) {
+    return fit::error{ZirconError{}};  // This indicates EOF.
+  }
+  if (status != ZX_OK) {
+    return fit::error{ZirconError{status}};
+  }
+  return fit::ok();
+}
+
+}  // namespace internal
+
+template <class Diagnostics>
+using VmoFileBase = File<Diagnostics, fbl::RefPtr<VmObject>, uint64_t, internal::ReadVmo>;
+
+template <class Diagnostics>
+class VmoFile : public VmoFileBase<Diagnostics> {
+ public:
+  using VmoFileBase<Diagnostics>::VmoFileBase;
+
+  VmoFile(VmoFile&&) noexcept = default;
+
+  VmoFile& operator=(VmoFile&&) noexcept = default;
+
+  fbl::RefPtr<VmObject> borrow() const { return this->get(); }
+};
+
+// Deduction guide.
+template <class Diagnostics>
+VmoFile(fbl::RefPtr<VmObject>, Diagnostics&& diagnostics) -> VmoFile<Diagnostics>;
+
+}  // namespace elfldltl
+
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_ELFLDLTL_INCLUDE_LIB_STARNIX_ELFLDTL_VMO_H_
