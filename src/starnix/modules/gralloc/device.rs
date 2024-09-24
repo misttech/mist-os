3e73b0bac2ee643cedab2f6a3d65e8e55445ddf4
Adam Barth <abarth@google.com>
2024-09-24 20:41:50 +0000

    [starnix][gralloc] Switch to synchronous proxy
    There's no need to use an async FIDL proxy and then run a local
    executor. We can just use a synchronous FIDL proxy, which already blocks
    the current thread.
    
    Change-Id: I40c51daddfa15ba8c5c64969cc0211ac33ee088b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1123923
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/modules/gralloc/device.rs
+++ src/starnix/modules/gralloc/device.rs
@@ -3,39 +3,28 @@
 // found in the LICENSE file.
 
 use super::file::GrallocFile;
-use fidl::endpoints::DiscoverableProtocolMarker;
-use fidl_fuchsia_starnix_gralloc as fgralloc;
 use starnix_core::device::DeviceOps;
 use starnix_core::fs::sysfs::DeviceDirectory;
 use starnix_core::task::CurrentTask;
 use starnix_core::vfs::{FileOps, FsNode};
-use starnix_sync::{DeviceOpen, FileOpsCore, LockBefore, Locked, Mutex};
+use starnix_sync::{DeviceOpen, FileOpsCore, LockBefore, Locked};
 use starnix_uapi::device_type::DeviceType;
 use starnix_uapi::errors::Errno;
 use starnix_uapi::open_flags::OpenFlags;
-use std::sync::Arc;
 
 #[derive(Clone)]
-struct GrallocDevice {
-    mode_setter: Arc<Mutex<fgralloc::VulkanModeSetterProxy>>,
-}
-
-impl GrallocDevice {
-    fn new(mode_setter: Arc<Mutex<fgralloc::VulkanModeSetterProxy>>) -> GrallocDevice {
-        GrallocDevice { mode_setter }
-    }
-}
+struct GrallocDevice;
 
 impl DeviceOps for GrallocDevice {
     fn open(
         &self,
         _locked: &mut Locked<'_, DeviceOpen>,
-        _current_task: &CurrentTask,
+        current_task: &CurrentTask,
         _id: DeviceType,
         _node: &FsNode,
         _flags: OpenFlags,
     ) -> Result<Box<dyn FileOps>, Errno> {
-        GrallocFile::new_file(self.mode_setter.clone())
+        GrallocFile::new_file(current_task)
     }
 }
 
@@ -43,12 +32,8 @@ pub fn gralloc_device_init<L>(locked: &mut Locked<'_, L>, current_task: &Current
 where
     L: LockBefore<FileOpsCore>,
 {
-    let mode_setter = current_task.kernel().connect_to_named_protocol_at_container_svc::<fgralloc::VulkanModeSetterMarker>(fgralloc::VulkanModeSetterMarker::PROTOCOL_NAME).expect("gralloc feature requires fuchsia.starnix.gralloc.VulkanModeSetter protocol in container /svc dir, and a corresponding server").into_proxy().expect("into_proxy failed");
-    let mode_setter = Arc::new(Mutex::new(mode_setter));
-
     let kernel = current_task.kernel();
     let registry = &kernel.device_registry;
-
     registry
         .register_dyn_device(
             locked,
@@ -56,7 +41,7 @@ where
             "virtgralloc0".into(),
             registry.objects.starnix_class(),
             DeviceDirectory::new,
-            GrallocDevice::new(mode_setter),
+            GrallocDevice,
         )
         .expect("can register virtgralloc0");
 }
