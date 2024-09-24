fda175003cdeed2e1bdce25187de9e90d4ce1caa
Joshua Seaton <joshuaseaton@google.com>
2024-09-24 01:13:50 +0000

    [tee] Reorder functions as they appear in the spec
    Makes for easier cross-referencing (and maybe one day extension).
    
    This change also colocates the deprecated functions with their
    successors, keeping like code with like.
    
    Change-Id: I6fead382b5f02ac646f2aa27890b8d387fcd1e69
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1123922
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: James Robinson <jamesr@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>

--- src/tee/runtime/api_impl/src/binding_stubs.rs
+++ src/tee/runtime/api_impl/src/binding_stubs.rs
@@ -22,15 +22,16 @@ use crate::mem;
 // This function returns a list of the C entry point that we want to expose from
 // this program. They need to be referenced from main to ensure that the linker
 // thinks that they are referenced and need to be included in the final binary.
+//
+// Keep in the order as they appear in the spec.
+#[rustfmt::skip]
 pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
     &[
-        TEE_Panic as *const extern "C" fn(),
-        TEE_Malloc as *const extern "C" fn(),
-        TEE_Realloc as *const extern "C" fn(),
-        TEE_Free as *const extern "C" fn(),
-        TEE_MemMove as *const extern "C" fn(),
-        TEE_MemCompare as *const extern "C" fn(),
-        TEE_MemFill as *const extern "C" fn(),
+        //
+        // Trusted Core Framework API
+        //
+
+        // Property Access Functions
         TEE_GetPropertyAsString as *const extern "C" fn(),
         TEE_GetPropertyAsBool as *const extern "C" fn(),
         TEE_GetPropertyAsU32 as *const extern "C" fn(),
@@ -44,20 +45,45 @@ pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
         TEE_ResetPropertyEnumerator as *const extern "C" fn(),
         TEE_GetPropertyName as *const extern "C" fn(),
         TEE_GetNextProperty as *const extern "C" fn(),
+
+        // Panics
+        TEE_Panic as *const extern "C" fn(),
+
+        // Internal Client API
         TEE_OpenTASession as *const extern "C" fn(),
         TEE_CloseTASession as *const extern "C" fn(),
         TEE_InvokeTACommand as *const extern "C" fn(),
+
+        // Cancellation Functions
         TEE_GetCancellationFlag as *const extern "C" fn(),
         TEE_UnmaskCancellation as *const extern "C" fn(),
         TEE_MaskCancellation as *const extern "C" fn(),
+
+        // Memory Management Functions
         TEE_CheckMemoryAccessRights as *const extern "C" fn(),
         TEE_SetInstanceData as *const extern "C" fn(),
         TEE_GetInstanceData as *const extern "C" fn(),
+        TEE_Malloc as *const extern "C" fn(),
+        TEE_Realloc as *const extern "C" fn(),
+        TEE_Free as *const extern "C" fn(),
+        TEE_MemMove as *const extern "C" fn(),
+        TEE_MemCompare as *const extern "C" fn(),
+        TEE_MemFill as *const extern "C" fn(),
+
+        //
+        // Trusted Storage API for Data and Keys
+        //
+
+        // Generic Object Functions
         TEE_GetObjectInfo1 as *const extern "C" fn(),
+        TEE_GetObjectInfo as *const extern "C" fn(),
         TEE_RestrictObjectUsage1 as *const extern "C" fn(),
+        TEE_RestrictObjectUsage as *const extern "C" fn(),
         TEE_GetObjectBufferAttribute as *const extern "C" fn(),
         TEE_GetObjectValueAttribute as *const extern "C" fn(),
         TEE_CloseObject as *const extern "C" fn(),
+
+        // Transient Object Functions
         TEE_AllocateTransientObject as *const extern "C" fn(),
         TEE_FreeTransientObject as *const extern "C" fn(),
         TEE_ResetTransientObject as *const extern "C" fn(),
@@ -65,20 +91,34 @@ pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
         TEE_InitRefAttribute as *const extern "C" fn(),
         TEE_InitValueAttribute as *const extern "C" fn(),
         TEE_CopyObjectAttributes1 as *const extern "C" fn(),
+        TEE_CopyObjectAttributes as *const extern "C" fn(),
         TEE_GenerateKey as *const extern "C" fn(),
+
+        // Persistent Object Functions
         TEE_OpenPersistentObject as *const extern "C" fn(),
         TEE_CreatePersistentObject as *const extern "C" fn(),
         TEE_CloseAndDeletePersistentObject1 as *const extern "C" fn(),
+        TEE_CloseAndDeletePersistentObject as *const extern "C" fn(),
         TEE_RenamePersistentObject as *const extern "C" fn(),
+
+        // Persistent Object Enumeration Functions
         TEE_AllocatePersistentObjectEnumerator as *const extern "C" fn(),
         TEE_FreePersistentObjectEnumerator as *const extern "C" fn(),
         TEE_ResetPersistentObjectEnumerator as *const extern "C" fn(),
         TEE_StartPersistentObjectEnumerator as *const extern "C" fn(),
         TEE_GetNextPersistentObject as *const extern "C" fn(),
+
+        // Data Stream Access Functions
         TEE_ReadObjectData as *const extern "C" fn(),
         TEE_WriteObjectData as *const extern "C" fn(),
         TEE_TruncateObjectData as *const extern "C" fn(),
         TEE_SeekObjectData as *const extern "C" fn(),
+
+        //
+        // Cryptographic Operations API
+        //
+
+        // Generic Options Functions
         TEE_AllocateOperation as *const extern "C" fn(),
         TEE_FreeOperation as *const extern "C" fn(),
         TEE_GetOperationInfo as *const extern "C" fn(),
@@ -88,41 +128,73 @@ pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
         TEE_SetOperationKey2 as *const extern "C" fn(),
         TEE_CopyOperation as *const extern "C" fn(),
         TEE_IsAlgorithmSupported as *const extern "C" fn(),
+
+        // Message Digest Functions
         TEE_DigestUpdate as *const extern "C" fn(),
         TEE_DigestDoFinal as *const extern "C" fn(),
         TEE_DigestExtract as *const extern "C" fn(),
+
+        // Symmetric Cipher Functions
         TEE_CipherInit as *const extern "C" fn(),
         TEE_CipherUpdate as *const extern "C" fn(),
         TEE_CipherDoFinal as *const extern "C" fn(),
+
+        // MAC Functions
         TEE_MACInit as *const extern "C" fn(),
         TEE_MACUpdate as *const extern "C" fn(),
         TEE_MACComputeFinal as *const extern "C" fn(),
         TEE_MACCompareFinal as *const extern "C" fn(),
+
+        // Authenticated Encryption Functions
         TEE_AEInit as *const extern "C" fn(),
         TEE_AEUpdateAAD as *const extern "C" fn(),
         TEE_AEUpdate as *const extern "C" fn(),
         TEE_AEEncryptFinal as *const extern "C" fn(),
         TEE_AEDecryptFinal as *const extern "C" fn(),
+
+        // Asymmmetric Functions
         TEE_AsymmetricEncrypt as *const extern "C" fn(),
         TEE_AsymmetricDecrypt as *const extern "C" fn(),
         TEE_AsymmetricSignDigest as *const extern "C" fn(),
         TEE_AsymmetricVerifyDigest as *const extern "C" fn(),
+
+        // Key Derivation Functions
         TEE_DeriveKey as *const extern "C" fn(),
+
+        // Random Data Generation Functions
         TEE_GenerateRandom as *const extern "C" fn(),
+
+        //
+        // Time API
+        //
+
+        // Time Functions
         TEE_GetSystemTime as *const extern "C" fn(),
         TEE_Wait as *const extern "C" fn(),
         TEE_GetTAPersistentTime as *const extern "C" fn(),
         TEE_SetTAPersistentTime as *const extern "C" fn(),
         TEE_GetREETime as *const extern "C" fn(),
+
+        //
+        // TEE Arithmetical API
+        //
+
+        // Memory Allocation and Size of Objects
         TEE_BigIntFMMContextSizeInU32 as *const extern "C" fn(),
         TEE_BigIntFMMSizeInU32 as *const extern "C" fn(),
+
+        // Initialization Functions
         TEE_BigIntInit as *const extern "C" fn(),
         TEE_BigIntInitFMMContext1 as *const extern "C" fn(),
         TEE_BigIntInitFMM as *const extern "C" fn(),
+
+        // Converter Functions
         TEE_BigIntConvertFromOctetString as *const extern "C" fn(),
         TEE_BigIntConvertToOctetString as *const extern "C" fn(),
         TEE_BigIntConvertFromS32 as *const extern "C" fn(),
         TEE_BigIntConvertToS32 as *const extern "C" fn(),
+
+        // Logical Operations
         TEE_BigIntCmp as *const extern "C" fn(),
         TEE_BigIntCmpS32 as *const extern "C" fn(),
         TEE_BigIntShiftRight as *const extern "C" fn(),
@@ -131,12 +203,16 @@ pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
         TEE_BigIntSetBit as *const extern "C" fn(),
         TEE_BigIntAssign as *const extern "C" fn(),
         TEE_BigIntAbs as *const extern "C" fn(),
+
+        // Basic Arithmetic Operations
         TEE_BigIntAdd as *const extern "C" fn(),
         TEE_BigIntSub as *const extern "C" fn(),
         TEE_BigIntNeg as *const extern "C" fn(),
         TEE_BigIntMul as *const extern "C" fn(),
         TEE_BigIntSquare as *const extern "C" fn(),
         TEE_BigIntDiv as *const extern "C" fn(),
+
+        // Modular Arithmetic Operations
         TEE_BigIntMod as *const extern "C" fn(),
         TEE_BigIntAddMod as *const extern "C" fn(),
         TEE_BigIntSubMod as *const extern "C" fn(),
@@ -144,66 +220,24 @@ pub fn exposed_c_entry_points() -> &'static [*const extern "C" fn()] {
         TEE_BigIntSquareMod as *const extern "C" fn(),
         TEE_BigIntInvMod as *const extern "C" fn(),
         TEE_BigIntExpMod as *const extern "C" fn(),
+
+        // Other Arithmetic Operations
         TEE_BigIntRelativePrime as *const extern "C" fn(),
         TEE_BigIntComputeExtendedGcd as *const extern "C" fn(),
         TEE_BigIntIsProbablePrime as *const extern "C" fn(),
+
+        // Fast Modular Multiplication Operations
         TEE_BigIntConvertToFMM as *const extern "C" fn(),
         TEE_BigIntConvertFromFMM as *const extern "C" fn(),
         TEE_BigIntComputeFMM as *const extern "C" fn(),
-        TEE_GetObjectInfo as *const extern "C" fn(),
-        TEE_RestrictObjectUsage as *const extern "C" fn(),
-        TEE_CopyObjectAttributes as *const extern "C" fn(),
-        TEE_CloseAndDeletePersistentObject as *const extern "C" fn(),
-        TEE_BigIntInitFMMContext as *const extern "C" fn(),
+
+        //
+        // Additional
+        //
+
         // This function is exposed to configure our default heap allocator.
         mem::__scudo_default_options as *const extern "C" fn(),
     ]
-}
-
-#[no_mangle]
-pub extern "C" fn TEE_Panic(code: u32) {
-    crate::panic(code)
-}
-
-#[no_mangle]
-extern "C" fn TEE_Malloc(size: usize, hint: u32) -> *mut ::std::os::raw::c_void {
-    mem::malloc(size, hint)
-}
-
-#[no_mangle]
-extern "C" fn TEE_Realloc(
-    buffer: *mut ::std::os::raw::c_void,
-    newSize: usize,
-) -> *mut ::std::os::raw::c_void {
-    unsafe { mem::realloc(buffer, newSize) }
-}
-
-#[no_mangle]
-extern "C" fn TEE_Free(buffer: *mut ::std::os::raw::c_void) {
-    unsafe { mem::free(buffer) }
-}
-
-#[no_mangle]
-extern "C" fn TEE_MemMove(
-    dest: *mut ::std::os::raw::c_void,
-    src: *mut ::std::os::raw::c_void,
-    size: usize,
-) {
-    mem::mem_move(dest, src, size)
-}
-
-#[no_mangle]
-extern "C" fn TEE_MemCompare(
-    buffer1: *mut ::std::os::raw::c_void,
-    buffer2: *mut ::std::os::raw::c_void,
-    size: usize,
-) -> i32 {
-    mem::mem_compare(buffer1, buffer2, size)
-}
-
-#[no_mangle]
-extern "C" fn TEE_MemFill(buffer: *mut ::std::os::raw::c_void, x: u8, size: usize) {
-    mem::mem_fill(buffer, x, size)
 }
 
 #[no_mangle]
@@ -309,6 +343,11 @@ extern "C" fn TEE_GetNextProperty(enumerator: TEE_PropSetHandle) -> TEE_Result {
 }
 
 #[no_mangle]
+pub extern "C" fn TEE_Panic(code: u32) {
+    crate::panic(code)
+}
+
+#[no_mangle]
 extern "C" fn TEE_OpenTASession(
     destination: *mut TEE_UUID,
     cancellationRequestTimeout: u32,
@@ -362,6 +401,47 @@ extern "C" fn TEE_CheckMemoryAccessRights(
 }
 
 #[no_mangle]
+extern "C" fn TEE_Malloc(size: usize, hint: u32) -> *mut ::std::os::raw::c_void {
+    mem::malloc(size, hint)
+}
+
+#[no_mangle]
+extern "C" fn TEE_Realloc(
+    buffer: *mut ::std::os::raw::c_void,
+    newSize: usize,
+) -> *mut ::std::os::raw::c_void {
+    unsafe { mem::realloc(buffer, newSize) }
+}
+
+#[no_mangle]
+extern "C" fn TEE_Free(buffer: *mut ::std::os::raw::c_void) {
+    unsafe { mem::free(buffer) }
+}
+
+#[no_mangle]
+extern "C" fn TEE_MemMove(
+    dest: *mut ::std::os::raw::c_void,
+    src: *mut ::std::os::raw::c_void,
+    size: usize,
+) {
+    mem::mem_move(dest, src, size)
+}
+
+#[no_mangle]
+extern "C" fn TEE_MemCompare(
+    buffer1: *mut ::std::os::raw::c_void,
+    buffer2: *mut ::std::os::raw::c_void,
+    size: usize,
+) -> i32 {
+    mem::mem_compare(buffer1, buffer2, size)
+}
+
+#[no_mangle]
+extern "C" fn TEE_MemFill(buffer: *mut ::std::os::raw::c_void, x: u8, size: usize) {
+    mem::mem_fill(buffer, x, size)
+}
+
+#[no_mangle]
 extern "C" fn TEE_SetInstanceData(instanceData: *mut ::std::os::raw::c_void) {
     unimplemented!()
 }
@@ -380,7 +460,17 @@ extern "C" fn TEE_GetObjectInfo1(
 }
 
 #[no_mangle]
+extern "C" fn TEE_GetObjectInfo(object: TEE_ObjectHandle, objectInfo: *mut TEE_ObjectInfo) {
+    unimplemented!()
+}
+
+#[no_mangle]
 extern "C" fn TEE_RestrictObjectUsage1(object: TEE_ObjectHandle, objectUsage: u32) -> TEE_Result {
+    unimplemented!()
+}
+
+#[no_mangle]
+extern "C" fn TEE_RestrictObjectUsage(object: TEE_ObjectHandle, objectUsage: u32) {
     unimplemented!()
 }
 
@@ -461,6 +551,11 @@ extern "C" fn TEE_CopyObjectAttributes1(
 }
 
 #[no_mangle]
+extern "C" fn TEE_CopyObjectAttributes(destObject: TEE_ObjectHandle, srcObject: TEE_ObjectHandle) {
+    unimplemented!()
+}
+
+#[no_mangle]
 extern "C" fn TEE_GenerateKey(
     object: TEE_ObjectHandle,
     keySize: u32,
@@ -497,6 +592,11 @@ extern "C" fn TEE_CreatePersistentObject(
 
 #[no_mangle]
 extern "C" fn TEE_CloseAndDeletePersistentObject1(object: TEE_ObjectHandle) -> TEE_Result {
+    unimplemented!()
+}
+
+#[no_mangle]
+extern "C" fn TEE_CloseAndDeletePersistentObject(object: TEE_ObjectHandle) {
     unimplemented!()
 }
 
@@ -922,6 +1022,15 @@ extern "C" fn TEE_BigIntInitFMMContext1(
 }
 
 #[no_mangle]
+extern "C" fn TEE_BigIntInitFMMContext(
+    context: *mut TEE_BigIntFMMContext,
+    len: usize,
+    modulus: *mut TEE_BigInt,
+) {
+    unimplemented!()
+}
+
+#[no_mangle]
 extern "C" fn TEE_BigIntInitFMM(bigIntFMM: *mut TEE_BigIntFMM, len: usize) {
     unimplemented!()
 }
@@ -1134,35 +1243,6 @@ extern "C" fn TEE_BigIntComputeFMM(
     op2: *mut TEE_BigIntFMM,
     n: *mut TEE_BigInt,
     context: *mut TEE_BigIntFMMContext,
-) {
-    unimplemented!()
-}
-
-#[no_mangle]
-extern "C" fn TEE_GetObjectInfo(object: TEE_ObjectHandle, objectInfo: *mut TEE_ObjectInfo) {
-    unimplemented!()
-}
-
-#[no_mangle]
-extern "C" fn TEE_RestrictObjectUsage(object: TEE_ObjectHandle, objectUsage: u32) {
-    unimplemented!()
-}
-
-#[no_mangle]
-extern "C" fn TEE_CopyObjectAttributes(destObject: TEE_ObjectHandle, srcObject: TEE_ObjectHandle) {
-    unimplemented!()
-}
-
-#[no_mangle]
-extern "C" fn TEE_CloseAndDeletePersistentObject(object: TEE_ObjectHandle) {
-    unimplemented!()
-}
-
-#[no_mangle]
-extern "C" fn TEE_BigIntInitFMMContext(
-    context: *mut TEE_BigIntFMMContext,
-    len: usize,
-    modulus: *const TEE_BigInt,
 ) {
     unimplemented!()
 }
