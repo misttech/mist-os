misttech/mist-os

commit 44d76f1cbfd701f60359ecb23ac28b0b38af25fc

[cobalt][fidl] Delete removed FIDL elements
This FIDL library is not in the SDK, so it only needs to support HEAD.
This also correctly marking the entire library `@available(added=HEAD)`.

`@available(added=7)` was added in https://fxrev.dev/664072 and not
removed when the category was changed in https://fxrev.dev/792622.

Fixed: 315496857
Bug: 364628320
Change-Id: If0ad0cb20f25dab9cd4583ae857e68c298c51d46
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1114041
Commit-Queue: David Dorwin <ddorwin@google.com>
API-Review: Francois Rousseau <frousseau@google.com>
Reviewed-by: Francois Rousseau <frousseau@google.com>

src/cobalt/bin/app/cobalt_app.cc
@@ -17,7 +17,6 @@
 #include "src/cobalt/bin/app/current_channel_provider.h"
 #include "src/cobalt/bin/app/diagnostics_impl.h"
 #include "src/cobalt/bin/app/metric_event_logger_factory_impl.h"
-#include "src/cobalt/bin/app/system_data_updater_impl.h"
 #include "src/cobalt/bin/app/utils.h"
 #include "src/cobalt/bin/utils/fuchsia_http_client.h"
 #include "src/public/lib/statusor/statusor.h"
@@ -214,8 +213,7 @@ CobaltApp::CobaltApp(
       validated_clock_(std::move(validated_clock)),
       current_channel_provider_(
           CreateCurrentChannelProvider(dispatcher, inspect_node_.CreateChild("system_data"),
-                                       cobalt_service_->system_data()->channel())),
-      system_data_updater_impl_(std::make_unique<SystemDataUpdaterImpl>()) {
+                                       cobalt_service_->system_data()->channel())) {
   validated_clock_->AwaitExternalSource([this, start_event_aggregator_worker]() {
     // Now that the clock is accurate, notify CobaltService.
     cobalt_service_->SystemClockIsAccurate(std::make_unique<util::SystemClock>(),
@@ -235,19 +233,6 @@ CobaltApp::CobaltApp(
         cobalt_service_.get(), metric_event_logger_factory_impl_.get(), std::move(shutdown),
         std::move(lifecycle_handle), dispatcher);
   }
-
-  if (current_channel_provider_ != nullptr) {
-    current_channel_provider_->GetCurrentChannel([this](const std::string& current_channel) {
-      const system_data::SoftwareDistributionInfo info{
-          .channel = current_channel,
-      };
-      cobalt_service_->system_data()->SetSoftwareDistributionInfo(info);
-    });
-  }
-
-  // Start serving SystemDataUpdater protocol.
-  context_->outgoing()->AddPublicService(
-      system_data_updater_bindings_.GetHandler(system_data_updater_impl_.get()));
 
   if (watch_for_user_consent) {
     user_consent_watcher_ = std::make_unique<UserConsentWatcher>(

src/cobalt/bin/testapp/BUILD.gn
@@ -62,7 +62,6 @@ executable("cobalt_testapp") {
     "//sdk/lib/syslog/cpp",
     "//src/cobalt/bin/utils:base64",
     "//src/cobalt/bin/utils:error_utils",
-    "//src/cobalt/bin/utils:status_utils",
     "//src/lib/fsl",
     "//src/lib/fxl:fxl_cli",
     "//third_party/cobalt/src/lib/util:datetime_util",

src/cobalt/bin/testapp/tests.cc
@@ -22,7 +22,6 @@
 namespace cobalt::testapp {
 
 using fidl::VectorPtr;
-using fuchsia::cobalt::Status;
 using util::SystemClockInterface;
 using util::TimeToDayIndex;
 using LoggerMethod = cobalt_internal_registry::LoggerCallsMadeMigratedMetricDimensionLoggerMethod;

src/cobalt/bin/BUILD.gn
@@ -221,15 +221,9 @@ fuchsia_fuzzer_component("logger-fuzzer-component") {
   deps = [ "app/fuzzer:logger_fuzzer" ]
 }
 
-fuchsia_fuzzer_component("system-data-updater-fuzzer-component") {
-  manifest = "app/fuzzer/meta/system_data_updater_fuzzer.cml"
-  deps = [ "app/fuzzer:system_data_updater_fuzzer" ]
-}
-
 fuchsia_fuzzer_package("cobalt-fuzzers") {
   cpp_fuzzer_components = [
     ":logger-factory-fuzzer-component",
     ":logger-fuzzer-component",
-    ":system-data-updater-fuzzer-component",
   ]
 }

src/cobalt/bin/app/BUILD.gn
@@ -59,8 +59,6 @@ source_set("lib") {
     "metric_event_logger_impl.cc",
     "metric_event_logger_impl.h",
     "process_lifecycle_impl.h",
-    "system_data_updater_impl.cc",
-    "system_data_updater_impl.h",
     "user_consent_watcher.cc",
     "user_consent_watcher.h",
     "utils.cc",
@@ -216,7 +214,6 @@ executable("cobalt_app_unittests") {
     "current_channel_provider_test.cc",
     "diagnostics_impl_test.cc",
     "metric_event_logger_impl_test.cc",
-    "system_data_updater_impl_test.cc",
     "user_consent_watcher_test.cc",
   ]
   deps = [

src/cobalt/bin/app/fuzzer/BUILD.gn
@@ -34,21 +34,6 @@ source_set("logger_server_provider") {
   ]
 }
 
-source_set("system_data_updater_server_provider") {
-  testonly = true
-
-  sources = [ "system_data_updater_server_provider.cc" ]
-
-  public_deps = [
-    "//sdk/fidl/fuchsia.cobalt:fuchsia.cobalt_hlcpp",
-    "//sdk/lib/fidl/hlcpp",
-    "//sdk/lib/fidl/hlcpp/fuzzing",
-    "//sdk/lib/sys/cpp",
-    "//src/cobalt/bin/app:lib",
-    "//zircon/system/ulib/inspect",
-  ]
-}
-
 fidl_protocol_fuzzer("logger_factory_fuzzer") {
   fidl = "//sdk/fidl/fuchsia.metrics:fuchsia.metrics"
   protocol = "fuchsia.metrics.MetricEventLoggerFactory"
@@ -59,10 +44,4 @@ fidl_protocol_fuzzer("logger_fuzzer") {
   fidl = "//sdk/fidl/fuchsia.metrics:fuchsia.metrics"
   protocol = "fuchsia.metrics.MetricEventLogger"
   deps = [ ":logger_server_provider" ]
-}
-
-fidl_protocol_fuzzer("system_data_updater_fuzzer") {
-  fidl = "//sdk/fidl/fuchsia.cobalt:fuchsia.cobalt"
-  protocol = "fuchsia.cobalt.SystemDataUpdater"
-  deps = [ ":system_data_updater_server_provider" ]
 }

src/cobalt/bin/utils/BUILD.gn
@@ -60,12 +60,6 @@ source_set("error_utils") {
   public_deps = [ "//sdk/fidl/fuchsia.metrics:fuchsia.metrics_hlcpp" ]
 }
 
-source_set("status_utils") {
-  sources = [ "status_utils.h" ]
-
-  public_deps = [ "//sdk/fidl/fuchsia.cobalt:fuchsia.cobalt_hlcpp" ]
-}
-
 executable("cobalt_utils_unittests") {
   testonly = true
 

sdk/fidl/fuchsia.cobalt/cobalt.fidl
@@ -3,86 +3,6 @@
 // found in the LICENSE file.
 library fuchsia.cobalt;
 
-/// Channels should not be longer than this.
-///
-/// # Deprecation
-///
-/// The SystemDataUpdater protocol is deprecated so this type is no longer needed.
-@available(deprecated=18, removed=22, note="SystemDataUpdater protocol is deprecated")
-const MAX_CHANNEL_NAME_LENGTH uint32 = 256;
-
-/// Response codes for Logger operations.
-///
-/// # Deprecation
-///
-/// The SystemDataUpdater protocol is deprecated so this type is no longer needed.
-@available(deprecated=18, removed=22, note="SystemDataUpdater protocol is deprecated")
-type Status = strict enum : int32 {
-    OK = 0;
-
-    /// For example the supplied metric id is invalid.
-    INVALID_ARGUMENTS = 1;
-
-    /// An attempt was made to log an Event whose serialized size exceeds
-    /// MAX_BYTES_PER_EVENT.
-    EVENT_TOO_BIG = 2;
-
-    /// Cobalt's local buffer is temporarily full and cannot handle any more
-    /// Events at this time. Try again later. This condition should be rare
-    BUFFER_FULL = 3;
-
-    // Cobalt has received a ShutDown signal and will not accept any more
-    // events.
-    SHUT_DOWN = 4;
-
-    /// Catch-all for unexpected errors.
-    INTERNAL_ERROR = -1;
-};
-
-/////////////////////////////////////////////////////////////////////
-// SystemProfileUpdater Interface
-/////////////////////////////////////////////////////////////////////
-
-/// A collection of fields describing a system's software distribution.
-///
-/// # Deprecation
-///
-/// The SystemDataUpdater protocol is deprecated so this type is no longer needed.
-@available(deprecated=18, removed=22, note="SystemDataUpdater protocol is deprecated")
-type SoftwareDistributionInfo = table {
-    /// The channel that the device last used as an update source. This value
-    /// may be empty to indicate that the device is not currently associated
-    /// with any channel.
-    1: current_channel string:MAX_CHANNEL_NAME_LENGTH;
-};
-
-/// The SystemDataUpdater interface allows callers to update the state of
-/// the SystemProfile in Cobalt. The changes are global and affect all loggers
-/// running on the device.
-///
-/// # Deprecation
-///
-/// This protocol does not have any special properties compared to fuchsia.update.channel/Provider
-/// other than requiring another component to push the current channel to Cobalt, rather than
-/// having Cobalt pull the value. This protocol is deprecated and Cobalt is now retrieving the
-/// channel via the standard fuchsia.update.channel/Provider::GetCurrent method.
-@discoverable
-@available(
-        deprecated=18,
-        removed=22,
-        note="channel retrieved through fuchsia.update.channel/Provider")
-closed protocol SystemDataUpdater {
-    /// Sets Cobalt's view of the system-wide distribution information replacing the
-    /// existing values.
-    ///
-    /// `info` The specifications of the current system's software distribution.
-    strict SetSoftwareDistributionInfo(struct {
-        info SoftwareDistributionInfo;
-    }) -> (struct {
-        status Status;
-    });
-};
-
 /////////////////////////////////////////////////////////////////////
 // AggregateAndUpload Interface
 /////////////////////////////////////////////////////////////////////

commit 6298f1d3c8a25a609a9f862ea2ea4368f6d6429c

[cobalt] Fix current channel retrieval
Commit 44d76f1cbfd701f60359ecb23ac28b0b38af25fc accidentally removed
logic that causes Cobalt to retrieve the current channel. This CL
restores that logic and adds a test case to the prober/integration test
to make sure it doesn't get removed again.

Fixed: 369150442
Tested: fx test cobalt_testapp
Tested: `fx shell crasher` @ eb7decbc798f249a to check that channel
isn't '<unset>'. The channel is '' because this was tested on an eng
device.

Change-Id: I4fca1bb52708dce4b709ce4d908d36832f74f550
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1123916
Reviewed-by: Alex Pankhurst <pankhurst@google.com>
Commit-Queue: Thomas Gales <tgales@google.com>
Reviewed-by: Sen Jiang <senj@google.com>

src/cobalt/bin/app/cobalt_app.cc
@@ -234,6 +234,17 @@ CobaltApp::CobaltApp(
         std::move(lifecycle_handle), dispatcher);
   }
 
+  if (current_channel_provider_ != nullptr) {
+    current_channel_provider_->GetCurrentChannel([this](const std::string& current_channel) {
+      // TODO(https://fxbug.dev/369213457): Call SetChannel instead.
+      // TODO(https://fxbug.dev/341104129): Verify this is called in a unit test.
+      const system_data::SoftwareDistributionInfo info{
+          .channel = current_channel,
+      };
+      cobalt_service_->system_data()->SetSoftwareDistributionInfo(info);
+    });
+  }
+
   if (watch_for_user_consent) {
     user_consent_watcher_ = std::make_unique<UserConsentWatcher>(
         dispatcher, inspect_node_.CreateChild("user_consent_watcher"), context_->svc(),

src/cobalt/bin/meta/base/cobalt_testapp.shard.cml
@@ -15,6 +15,10 @@
             name: "sysinfo",
             url: "fuchsia-boot:///#meta/sysinfo.cm",
         },
+        {
+            name: "fake_channel_control",
+            url: "#meta/fake-channel-control.cm",
+        },
     ],
     use: [
         { protocol: "fuchsia.diagnostics.ArchiveAccessor" },
@@ -71,6 +75,11 @@
             protocol: "fuchsia.inspect.InspectSink",
             from: "parent",
             to: [ "#sysinfo" ],
+        },
+        {
+            protocol: "fuchsia.update.channel.Provider",
+            from: "#fake_channel_control",
+            to: [ "#realm_builder" ],
         },
     ],
 }

src/cobalt/bin/testapp/BUILD.gn
@@ -65,6 +65,7 @@ cc_test_executable("cobalt_testapp") {
     "//src/cobalt/bin/utils:error_utils",
     "//src/lib/fsl",
     "//src/lib/fxl:fxl_cli",
+    "//src/sys/pkg/bin/fake-channel-control",
     "//third_party/cobalt/src/lib/util:datetime_util",
     "//third_party/cobalt/src/public/lib:clock_interfaces",
     "//third_party/cobalt/src/registry:cobalt_registry_proto",

src/cobalt/bin/testapp/tests.cc
@@ -131,6 +131,14 @@ bool CheckInspectData(CobaltTestAppLogger* logger, uint32_t project_id, LoggerMe
     }
   }
 
+  pointer = rapidjson::Pointer("/payload/root/cobalt_app/system_data/channel");
+  value = rapidjson::GetValueByPointerWithDefault(inspect, pointer, "");
+  if (value.GetString() != std::string("fake-current-channel")) {
+    FX_LOGS(ERROR) << "Expected 'fake-current-channel', found '" << value.GetString() << "'."
+                   << "\nFull inspect data: " << inspect_json;
+    return false;
+  }
+
   return true;
 }
 

src/sys/pkg/bin/fake-channel-control/BUILD.gn
@@ -9,6 +9,7 @@ rustc_binary("bin") {
   output_name = "fake-channel-control"
   edition = "2021"
   deps = [
+    "//sdk/fidl/fuchsia.update.channel:fuchsia.update.channel_rust",
     "//sdk/fidl/fuchsia.update.channelcontrol:fuchsia.update.channelcontrol_rust",
     "//src/lib/fuchsia-async",
     "//src/lib/fuchsia-component",

src/sys/pkg/bin/fake-channel-control/meta/fake-channel-control.cml
@@ -9,12 +9,18 @@
     },
     capabilities: [
         {
-            protocol: [ "fuchsia.update.channelcontrol.ChannelControl" ],
+            protocol: [
+                "fuchsia.update.channel.Provider",
+                "fuchsia.update.channelcontrol.ChannelControl",
+            ],
         },
     ],
     expose: [
         {
-            protocol: [ "fuchsia.update.channelcontrol.ChannelControl" ],
+            protocol: [
+                "fuchsia.update.channel.Provider",
+                "fuchsia.update.channelcontrol.ChannelControl",
+            ],
             from: "self",
         },
     ],

src/sys/pkg/bin/fake-channel-control/src/main.rs
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 use anyhow::{Context as _, Error};
+use fidl_fuchsia_update_channel::{ProviderRequest, ProviderRequestStream};
 use fidl_fuchsia_update_channelcontrol::{ChannelControlRequest, ChannelControlRequestStream};
 use fuchsia_async as fasync;
 use fuchsia_component::server::ServiceFs;
@@ -10,12 +11,15 @@ use futures::prelude::*;
 
 enum IncomingServices {
     ChannelControl(ChannelControlRequestStream),
+    Provider(ProviderRequestStream),
 }
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     let mut fs = ServiceFs::new_local();
-    fs.dir("svc").add_fidl_service(IncomingServices::ChannelControl);
+    fs.dir("svc")
+        .add_fidl_service(IncomingServices::ChannelControl)
+        .add_fidl_service(IncomingServices::Provider);
     fs.take_and_serve_directory_handle().context("while serving directory handle")?;
 
     let mut target_channel = String::new();
@@ -41,6 +45,17 @@ async fn main() -> Result<(), Error> {
                                 "fake-current-channel".to_owned(),
                                 "other-channel".to_owned(),
                             ])?;
+                        }
+                    }
+                }
+            }
+            IncomingServices::Provider(mut stream) => {
+                while let Some(request) =
+                    stream.try_next().await.context("error receiving Provider request")?
+                {
+                    match request {
+                        ProviderRequest::GetCurrent { responder } => {
+                            responder.send("fake-current-channel")?;
                         }
                     }
                 }