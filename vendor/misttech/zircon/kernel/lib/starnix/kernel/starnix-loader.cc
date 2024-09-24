eeba6ad2e1d7468aee2bfd02f534a8aeaa2da2f2
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-24 11:11:58 -0300

    [mistos][starnix] Refactor StarnixLoader
    Change-Id: I4bd2241cf8e8c8beb31d43ac43b51ca12cca53da

--- vendor/misttech/zircon/kernel/lib/starnix/kernel/starnix-loader.cc
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/starnix-loader.cc
@@ -5,18 +5,22 @@
 
 #include "starnix-loader.h"
 
+#include <lib/mistos/starnix/kernel/mm/memory.h>
+
 namespace {
 
-constexpr std::string_view kVmoNameUnknown = "<unknown ELF file>";
+using namespace starnix;
 
-constexpr std::string_view kVmoNamePrefixData = "data";
-constexpr std::string_view kVmoNamePrefixBss = "bss";
+constexpr ktl::string_view kVmoNameUnknown = "<unknown ELF file>";
+
+constexpr ktl::string_view kVmoNamePrefixData = "data";
+constexpr ktl::string_view kVmoNamePrefixBss = "bss";
 
 constexpr char kHexDigits[] = "0132456789ABCDEF";
 
-template <const std::string_view& Prefix>
-void SetVmoName(zx::unowned_vmo vmo, std::string_view base_name, size_t n) {
-  std::array<char, ZX_MAX_NAME_LEN> buffer{};
+template <const ktl::string_view& Prefix>
+void SetVmoName(fbl::RefPtr<MemoryObject> memory, ktl::string_view base_name, size_t n) {
+  ktl::array<char, ZX_MAX_NAME_LEN> buffer{};
   cpp20::span vmo_name(buffer);
 
   // First, "data" or "bss".
@@ -41,85 +45,81 @@ void SetVmoName(zx::unowned_vmo vmo, std::string_view base_name, size_t n) {
   name_idx += base_name.copy(avail.data(), avail.size());
   ZX_DEBUG_ASSERT(name_idx <= vmo_name.size());
 
-  vmo->set_property(ZX_PROP_NAME, vmo_name.data(), vmo_name.size());
+  memory->set_zx_name(vmo_name.data());
 }
 
 }  // namespace
 
 namespace starnix {
 
-StarnixLoader::VmoName StarnixLoader::GetVmoName(zx::unowned_vmo vmo) {
+StarnixLoader::VmoName StarnixLoader::GetVmoName(fbl::RefPtr<MemoryObject> memory) {
   StarnixLoader::VmoName base_vmo_name{};
-  zx_status_t status = vmo->get_property(ZX_PROP_NAME, base_vmo_name.data(), base_vmo_name.size());
-
-  if (status != ZX_OK || base_vmo_name.front() == '\0') {
+  memory->as_vmo()->get().dispatcher()->vmo()->get_name(base_vmo_name.data(), base_vmo_name.size());
+  if (base_vmo_name.front() == '\0') {
     kVmoNameUnknown.copy(base_vmo_name.data(), base_vmo_name.size());
   }
-
   return base_vmo_name;
 }
 
 // This has both explicit instantiations below.
 template <bool ZeroInVmo>
-zx_status_t StarnixLoader::MapWritable(uintptr_t vmar_offset, zx::unowned_vmo vmo, bool copy_vmo,
-                                       std::string_view base_name, uint64_t vmo_offset, size_t size,
+zx_status_t StarnixLoader::MapWritable(uintptr_t vmar_offset, fbl::RefPtr<MemoryObject> memory,
+                                       bool copy_vmo, ktl::string_view base_name,
+                                       uint64_t vmo_offset, size_t size,
                                        size_t& num_data_segments) {
   if constexpr (!ZeroInVmo) {
     ZX_DEBUG_ASSERT((size & (page_size() - 1)) == 0);
   }
 
-  zx::vmo writable_vmo;
-  zx::unowned_vmo map_vmo;
+  fbl::RefPtr<MemoryObject> map_vmo;
   if (copy_vmo) {
-    zx_status_t status =
-        vmo->create_child(ZX_VMO_CHILD_SNAPSHOT_AT_LEAST_ON_WRITE, vmo_offset, size, &writable_vmo);
-    if (status != ZX_OK) [[unlikely]] {
-      return status;
+    auto result = memory->create_child(ZX_VMO_CHILD_SNAPSHOT_AT_LEAST_ON_WRITE, vmo_offset, size);
+    if (result.is_error()) [[unlikely]] {
+      return result.error_value();
     }
-    map_vmo = writable_vmo.borrow();
+    map_vmo = result.value();
   } else {
-    map_vmo = vmo->borrow();
+    map_vmo = memory;
   }
 
   // If the size is not page-aligned, zero the last page beyond the size.
   if constexpr (ZeroInVmo) {
     const size_t subpage_size = size & (page_size() - 1);
     if (subpage_size > 0) {
-      const size_t zero_offset = size;
-      const size_t zero_size = page_size() - subpage_size;
-      zx_status_t status = map_vmo->op_range(ZX_VMO_OP_ZERO, zero_offset, zero_size, nullptr, 0);
-      if (status != ZX_OK) [[unlikely]] {
-        return status;
+      uint64_t zero_offset = size;
+      uint64_t zero_size = page_size() - subpage_size;
+      auto result = map_vmo->op_range(ZX_VMO_OP_ZERO, &zero_offset, &zero_size);
+      if (result.is_error()) [[unlikely]] {
+        return result.error_value();
       }
     }
   }
 
-  SetVmoName<kVmoNamePrefixData>(map_vmo->borrow(), base_name, num_data_segments++);
+  SetVmoName<kVmoNamePrefixData>(map_vmo, base_name, num_data_segments++);
 
-  return Map(vmar_offset, kMapWritable, map_vmo->borrow(), 0, size);
+  return Map(vmar_offset, kMapWritable, map_vmo, 0, size);
 }
 
 // Explicitly instantiate both flavors.
 
 template zx_status_t StarnixLoader::MapWritable<false>(  //
-    uintptr_t vmar_offset, zx::unowned_vmo vmo, bool copy_vmo, std::string_view base_name,
-    uint64_t vmo_offset, size_t size, size_t& num_data_segments);
+    uintptr_t vmar_offset, fbl::RefPtr<MemoryObject> memory, bool copy_vmo,
+    ktl::string_view base_name, uint64_t vmo_offset, size_t size, size_t& num_data_segments);
 
 template zx_status_t StarnixLoader::MapWritable<true>(  //
-    uintptr_t vmar_offset, zx::unowned_vmo vmo, bool copy_vmo, std::string_view base_name,
-    uint64_t vmo_offset, size_t size, size_t& num_data_segments);
+    uintptr_t vmar_offset, fbl::RefPtr<MemoryObject> memory, bool copy_vmo,
+    ktl::string_view base_name, uint64_t vmo_offset, size_t size, size_t& num_data_segments);
 
-zx_status_t StarnixLoader::MapZeroFill(uintptr_t vmar_offset, std::string_view base_name,
+zx_status_t StarnixLoader::MapZeroFill(uintptr_t vmar_offset, ktl::string_view base_name,
                                        size_t size, size_t& num_zero_segments) {
-  zx::vmo vmo;
-  zx_status_t status = zx::vmo::create(size, 0, &vmo);
-  if (status != ZX_OK) [[unlikely]] {
-    return status;
+  auto status = create_vmo(size, 0);
+  if (status.is_error()) {
+    return status.error_value();
   }
+  fbl::RefPtr<MemoryObject> memory = status.value();
+  SetVmoName<kVmoNamePrefixBss>(memory, base_name, num_zero_segments++);
 
-  SetVmoName<kVmoNamePrefixBss>(vmo.borrow(), base_name, num_zero_segments++);
-
-  return Map(vmar_offset, kMapWritable, vmo.borrow(), 0, size);
+  return Map(vmar_offset, kMapWritable, memory, 0, size);
 }
 
 }  // namespace starnix
