b0dfcf41e646e1a8a9b2a9091728e1ffcb6b3b89
Joshua Seaton <joshuaseaton@google.com>
2024-09-26 23:50:14 +0000

    [build][bindgen] Generate with #![allow(clippy::missing_safety_doc)]
    Bindgen can generate types with unsafe methods (e.g., with
    `__IncompleteArrayField` for flexible array members).
    
    Change-Id: Ib8e0cbdef0ab30596bb151f23abc5d4c12675864
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1126683
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Yegor Pomortsev <ypomortsev@google.com>

--- build/rust/bindgen.py
+++ build/rust/bindgen.py
@@ -28,6 +28,7 @@ GENERATED_FILE_HEADER = """#![allow(dead_code)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::missing_safety_doc)]
 """
 
 # Replacements to add to these defined by the user.
