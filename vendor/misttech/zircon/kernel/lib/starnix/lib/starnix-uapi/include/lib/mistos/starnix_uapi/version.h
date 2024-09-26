12897d406484478f46b91668322cc1f5c9541360
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-26 11:26:07 -0300

    [mistos][starnix_uapi] Add version file
    Change-Id: I3ef2bd3e33440511189fa7aa7d995de73d96bd14

---
+++ vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-uapi/include/lib/mistos/starnix_uapi/version.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_STARNIX_UAPI_INCLUDE_LIB_MISTOS_STARNIX_UAPI_VERSION_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_STARNIX_UAPI_INCLUDE_LIB_MISTOS_STARNIX_UAPI_VERSION_H_
+
+namespace starnix_uapi {
+
+const char* KERNEL_RELEASE = "5.10.199-starnix";
+const char* KERNEL_VERSION = "#1 SMP PREEMPT";
+
+}  // namespace starnix_uapi
+
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_STARNIX_LIB_STARNIX_UAPI_INCLUDE_LIB_MISTOS_STARNIX_UAPI_VERSION_H_
