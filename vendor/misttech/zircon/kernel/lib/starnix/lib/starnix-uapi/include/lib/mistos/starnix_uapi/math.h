fa8509236b21c8973f2aeb238e61968804442c29
Bruno Herrera <bruno.herrera@mist-os.com>
2024-09-26 16:48:48 -0300

    [mistos][util] Add mtl::checked_add
    Change-Id: I17494f05d99fc245d577b84517e1a93884310f3f

--- vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-uapi/include/lib/mistos/starnix_uapi/math.h
+++ vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-uapi/include/lib/mistos/starnix_uapi/math.h
@@ -16,15 +16,6 @@ namespace starnix_uapi {
 fit::result<Errno, size_t> round_up_to_increment(size_t size, size_t increment);
 fit::result<Errno, size_t> round_up_to_system_page_size(size_t size);
 
-template <typename T>
-inline ktl::optional<T> checked_add(const T& lhs, const T& rhs) {
-  T result;
-  if (add_overflow(lhs, rhs, &result)) {
-    return ktl::nullopt;
-  }
-  return result;
-}
-
 }  // namespace starnix_uapi
 
 #endif  // ZIRCON_KERNEL_LIB_MISTOS_STARNIX_UAPI_INCLUDE_LIB_MISTOS_STARNIX_UAPI_MATH_H_
