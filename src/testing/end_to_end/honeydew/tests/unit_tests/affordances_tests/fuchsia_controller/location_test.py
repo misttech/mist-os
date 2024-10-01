277443f73e7730af45e51a96224df624f6a26ddf
Sam Balana <sbalana@google.com>
2024-10-01 23:34:47 +0000

    [honeydew] Remove location from starnix builds
    The location functional test was failing in CI due to starnix builds not
    implementing the required FIDL for this affordance.
    
    Change-Id: I94043ca9afbd7939942c95eff125f73575d861d2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1128202
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Fuchsia-Auto-Submit: Sam Balana <sbalana@google.com>

--- src/testing/end_to_end/honeydew/tests/unit_tests/affordances_tests/fuchsia_controller/location_test.py
+++ src/testing/end_to_end/honeydew/tests/unit_tests/affordances_tests/fuchsia_controller/location_test.py
@@ -1,7 +1,7 @@
 # Copyright 2024 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Unit tests for honeydew.affordances.fuchsia_controller.netstack."""
+"""Unit tests for honeydew.affordances.fuchsia_controller.location."""
 
 import unittest
 from typing import TypeVar
