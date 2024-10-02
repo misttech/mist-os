485af07906f30dc9e3c46ef4b537e5105bfbde75
Braden Kell <bradenkell@google.com>
2024-10-02 19:38:32 +0000

    [spmi] Switch to debug service in spmi-ctl and update routing
    The --target option is now mandatory and accepts an integer ID of the
    SPMI target device to connect to. An optional --controller option is
    added to specify the name of the controller to connect to: if
    unspecified, the first entry in the service directory is used. The
    ability to connect using a hexadecimal service name has been removed.
    
    All routes that previously referred to TargetService and
    SubTargetService have been changed to DebugService to reflect the
    changes to spmi-ctl.
    
    Test: spmi-ctl-test
    Test: Used spmi-ctl to read registers on Kola
    Fixed: 368117439
    Bug: 361853516
    Change-Id: I4de90d86684a973c3a8d971e1e64647db64e6376
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1128274
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Ruby Zhuang <rdzhuang@google.com>
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Fuchsia-Auto-Submit: Braden Kell <bradenkell@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/spmi/bin/spmi-ctl/spmi-ctl-impl.h
+++ src/devices/spmi/bin/spmi-ctl/spmi-ctl-impl.h
@@ -3,19 +3,21 @@
 // found in the LICENSE file.
 #include <fidl/fuchsia.hardware.spmi/cpp/fidl.h>
 
+#include <optional>
+
 class SpmiCtl {
  public:
-  explicit SpmiCtl(fidl::ClientEnd<fuchsia_hardware_spmi::Device> device) {
-    test_client_.emplace(std::move(device));  // Allows injecting the device from tests.
+  explicit SpmiCtl(fidl::ClientEnd<fuchsia_hardware_spmi::Debug> debug) {
+    test_client_.emplace(std::move(debug));  // Allows injecting the device from tests.
   }
   SpmiCtl() = default;
 
   int Execute(int argc, char** argv);
 
  private:
-  fidl::SyncClient<fuchsia_hardware_spmi::Device> GetSpmiClient(std::string target,
-                                                                std::string sub_target);
+  fidl::SyncClient<fuchsia_hardware_spmi::Device> GetSpmiClient(std::string controller,
+                                                                uint8_t target);
   void ListDevices();
 
-  std::optional<fidl::SyncClient<fuchsia_hardware_spmi::Device>> test_client_;
+  std::optional<fidl::SyncClient<fuchsia_hardware_spmi::Debug>> test_client_;
 };
--- src/devices/spmi/bin/spmi-ctl/spmi-ctl-test.cc
+++ src/devices/spmi/bin/spmi-ctl/spmi-ctl-test.cc
@@ -13,8 +13,11 @@
 #include "lib/zx/result.h"
 #include "spmi-ctl-impl.h"
 
-class FakeSpmi : public fidl::Server<fuchsia_hardware_spmi::Device> {
+class FakeSpmi : public fidl::Server<fuchsia_hardware_spmi::Device>,
+                 public fidl::Server<fuchsia_hardware_spmi::Debug> {
  public:
+  explicit FakeSpmi(async_dispatcher_t* dispatcher) : dispatcher_(dispatcher) {}
+
   // FIDL natural C++ methods for fuchsia.hardware.spmi.
   void GetProperties(GetPropertiesCompleter::Sync& completer) override {
     fuchsia_hardware_spmi::DeviceGetPropertiesResponse response;
@@ -40,14 +43,35 @@ class FakeSpmi : public fidl::Server<fuchsia_hardware_spmi::Device> {
   void handle_unknown_method(fidl::UnknownMethodMetadata<fuchsia_hardware_spmi::Device> metadata,
                              fidl::UnknownMethodCompleter::Sync& completer) override {}
 
+  void ConnectTarget(ConnectTargetRequest& request,
+                     ConnectTargetCompleter::Sync& completer) override {
+    if (request.target_id() >= fuchsia_hardware_spmi::kMaxTargets) {
+      completer.Reply(fit::error(fuchsia_hardware_spmi::DriverError::kInvalidArgs));
+      return;
+    }
+    target_id_ = request.target_id();
+    device_bindings_.AddBinding(dispatcher_, std::move(request.server()), this,
+                                fidl::kIgnoreBindingClosure);
+    completer.Reply(zx::ok());
+  }
+  void GetControllerProperties(GetControllerPropertiesCompleter::Sync& completer) override {
+    completer.Reply({{"spmi-controller"}});
+  }
+  void handle_unknown_method(fidl::UnknownMethodMetadata<fuchsia_hardware_spmi::Debug> metadata,
+                             fidl::UnknownMethodCompleter::Sync& completer) override {}
+
+  uint8_t target_id() const { return target_id_; }
   std::vector<uint8_t>& data() { return data_; }
   uint16_t address() { return *address_; }
   size_t read_size() { return read_size_; }
 
  private:
+  async_dispatcher_t* const dispatcher_;
+  uint8_t target_id_{fuchsia_hardware_spmi::kMaxTargets};
   std::optional<uint16_t> address_;
   std::vector<uint8_t> data_;
   size_t read_size_;
+  fidl::ServerBindingGroup<fuchsia_hardware_spmi::Device> device_bindings_;
 };
 
 class SpmiCtlTest : public zxtest::Test {
@@ -55,7 +79,7 @@ class SpmiCtlTest : public zxtest::Test {
   void SetUp() override {
     loop_ = std::make_unique<async::Loop>(&kAsyncLoopConfigAttachToCurrentThread);
     ASSERT_OK(loop_->StartThread("spmi-ctl-test-loop"));
-    spmi_ = std::make_unique<FakeSpmi>();
+    spmi_ = std::make_unique<FakeSpmi>(loop_->dispatcher());
   }
 
   void TearDown() override { loop_->Shutdown(); }
@@ -67,7 +91,7 @@ class SpmiCtlTest : public zxtest::Test {
     for (size_t i = 0; i < args.size(); ++i) {
       argv[i] = const_cast<char*>(args[i].c_str());
     }
-    fidl::ClientEnd<fuchsia_hardware_spmi::Device> client;
+    fidl::ClientEnd<fuchsia_hardware_spmi::Debug> client;
     zx::result server = fidl::CreateEndpoints(&client);
     ZX_ASSERT(server.status_value() == ZX_OK);
     fidl::BindServer(loop_->dispatcher(), std::move(server.value()), spmi_.get());
@@ -82,27 +106,40 @@ class SpmiCtlTest : public zxtest::Test {
 };
 
 TEST_F(SpmiCtlTest, UnknownCommands) {
-  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-b"}), -1);
-  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "--bad"}), -1);
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-b"}), -1);
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "--bad"}), -1);
+}
+
+TEST_F(SpmiCtlTest, InvalidTarget) {
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-r", "4"}), -1);
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "-a", "0x1234", "-r", "4"}), -1);
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "16", "-a", "0x1234", "-r", "4"}), -1);
+  EXPECT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "100", "-a", "0x1234", "-r", "4"}), -1);
 }
 
 TEST_F(SpmiCtlTest, ReadWrite) {
   // Successful.
   std::vector<uint8_t> kCannedData = {0x12, 0x34, 0x56};
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-w", "0x12", "0x34", "0x56"}), 0);
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x1234", "-w", "0x12", "0x34", "0x56"}), 0);
+  EXPECT_EQ(spmi_->target_id(), 0);
   EXPECT_TRUE(spmi_->data() == kCannedData);
   EXPECT_EQ(spmi_->address(), 0x1234);
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-r", "4"}), 0);
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "11", "-a", "0x1234", "-r", "4"}), 0);
+  EXPECT_EQ(spmi_->target_id(), 11);
   EXPECT_EQ(spmi_->read_size(), 4);
   EXPECT_EQ(spmi_->address(), 0x1234);
 
   // Errors.
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-w", "0x12", "0x34", "0x56"}), -1);  // No address.
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x5678", "-r", "4"}), -1);     // Unknown address.
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x10000", "-r", "4"}), -1);    // Address too big.
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-r", "9"}), -1);     // Read too big.
-  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-w"}), -1);          // Write no data.
-  ASSERT_EQ(
-      CallSpmiCtl({"spmi-ctl", "-a", "0x1234", "-w", "1", "2", "3", "4", "5", "6", "7", "8", "9"}),
-      -1);  // Write too big.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-w", "0x12", "0x34", "0x56"}),
+            -1);  // No address.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x5678", "-r", "4"}),
+            -1);  // Unknown address.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x10000", "-r", "4"}),
+            -1);  // Address too big.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x1234", "-r", "9"}),
+            -1);                                                              // Read too big.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x1234", "-w"}), -1);  // Write no data.
+  ASSERT_EQ(CallSpmiCtl({"spmi-ctl", "-t", "0", "-a", "0x1234", "-w", "1", "2", "3", "4", "5", "6",
+                         "7", "8", "9"}),
+            -1);  // Write too big.
 }
