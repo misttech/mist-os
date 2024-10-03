100b8b98a7998292e396a900366025cef53771ce
Kevin Lindkvist <lindkvist@google.com>
2024-10-03 12:23:34 +0000

    [starnix] Add power to suspend_inspect test
    Bug: 370526509
    Change-Id: I06a3c40062294160a658074b12870ad188829e4e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1129956
    Reviewed-by: Guocheng Wei <guochengwei@google.com>
    Fuchsia-Auto-Submit: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/starnix/tests/diagnostics/suspend_inspect/src/main.rs
+++ src/starnix/tests/diagnostics/suspend_inspect/src/main.rs
@@ -6,61 +6,11 @@ use anyhow::{Context, Result};
 use component_events::events::{EventStream, Started, Stopped};
 use component_events::matcher::EventMatcher;
 use diagnostics_reader::{ArchiveReader, Inspect};
-use fasync::{DurationExt, TimeoutExt};
-use futures::{TryFutureExt, TryStreamExt};
 use tracing::info;
 use {
-    fidl_fuchsia_test_suspend as fftsu, fidl_fuchsia_test_syscalls as ffts,
-    fuchsia_async as fasync, fuchsia_component as fxc, fuchsia_component_test as fxct,
-    fuchsia_driver_test as _, zx,
+    fidl_fuchsia_test_suspend as fftsu, fuchsia_async as fasync, fuchsia_component as fxc,
+    fuchsia_component_test as fxct, fuchsia_driver_test as _,
 };
-
-// Wait at most this long for a suspender device to appear in the service
-// dir.
-const SUSPEND_DEVICE_TIMEOUT: zx::Duration = zx::Duration::from_seconds(5);
-
-// The connection method is borrowed from system-activity-governor code.
-async fn connect_to_control() -> Result<ffts::ControlProxy> {
-    let service_dir = fxc::client::open_service::<ffts::ControlServiceMarker>()
-        .expect("failed to open service dir");
-
-    let mut watcher = fuchsia_fs::directory::Watcher::new(&service_dir)
-        .await
-        .map_err(|e| anyhow::anyhow!("Failed to create watcher: {:?}", e))?;
-
-    // Connect to the first suspend service instance that is discovered.
-    let filename = loop {
-        let next = watcher
-            .try_next()
-            .map_err(|e| anyhow::anyhow!("Failed to get next watch message: {e:?}"))
-            .on_timeout(SUSPEND_DEVICE_TIMEOUT.after_now(), || {
-                Err(anyhow::anyhow!(
-                    "Timeout waiting for next watcher message on ffs::directory::Watcher."
-                ))
-            })
-            .await?;
-
-        if let Some(watch_msg) = next {
-            let filename = watch_msg.filename.as_path().to_str().unwrap().to_owned();
-            if filename != "." {
-                if watch_msg.event == fuchsia_fs::directory::WatchEvent::ADD_FILE
-                    || watch_msg.event == fuchsia_fs::directory::WatchEvent::EXISTING
-                {
-                    break Ok(filename);
-                }
-            }
-        } else {
-            break Err(anyhow::anyhow!("Suspend service watcher returned None entry."));
-        }
-    }?;
-
-    let svc_inst =
-        fxc::client::connect_to_service_instance::<ffts::ControlServiceMarker>(filename.as_str())?;
-
-    svc_inst
-        .connect_to_control()
-        .map_err(|e| anyhow::anyhow!("Failed to connect to control: {:?}", e))
-}
 
 #[fasync::run_singlethreaded(test)]
 async fn test_suspend() -> Result<()> {
@@ -84,15 +34,6 @@ async fn test_suspend() -> Result<()> {
 
         // Required for the test realm to be created.
         realm_control_proxy.create().await.context("while creating test realm")?;
-
-        let control_proxy = connect_to_control().await.context("while connecting to control")?;
-
-        // If we want OK to be returned, we must set it explicitly. Otherwise an
-        // error will be returned by default.
-        control_proxy.set_suspend_enter_result(zx::Status::OK.into_raw()).await?;
-
-        let state = control_proxy.get_state().await.context("while calling get_state")?;
-        assert_eq!(0, state);
 
         {
             let instance = fxct::ScopedInstance::new_with_name(
@@ -120,9 +61,6 @@ async fn test_suspend() -> Result<()> {
             .await?;
         print!("{:?}", kernel_inspect);
         assert_eq!(kernel_inspect.len(), 1);
-
-        let state = control_proxy.get_state().await.context("while calling get_state")?;
-        assert_eq!(1, state);
 
         info!("tearing down the test realm now");
     }
--- src/starnix/tests/diagnostics/suspend_inspect/src/suspend_linux.cc
+++ src/starnix/tests/diagnostics/suspend_inspect/src/suspend_linux.cc
@@ -2,17 +2,45 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstdio>
+#include <signal.h>
+#include <time.h>
 
 #include "src/lib/files/file.h"
 
+timer_t start_interval_timer() {
+  timer_t timer_id;
+  struct sigevent sev = {};
+  sev.sigev_notify = SIGEV_NONE;
+  timer_create(CLOCK_REALTIME_ALARM, &sev, &timer_id);
+
+  // Set a test timer that will trigger every 1 second to wake
+  // up all the subsequent suspends.
+  struct itimerspec its = {};
+  its.it_value.tv_sec = 2;
+  its.it_interval.tv_sec = 2;
+  timer_settime(timer_id, 0, &its, nullptr);
+
+  return timer_id;
+}
+
+void stop_interval_timer(timer_t timer_id) {
+  struct itimerspec its = {};
+  its.it_value.tv_sec = 0;
+  its.it_interval.tv_sec = 0;
+  timer_settime(timer_id, 0, &its, nullptr);
+}
+
 int main(int argc, char** argv) {
   printf("suspend_linux, suspending ...\n");
+  timer_t timer = start_interval_timer();
   bool ok = files::WriteFile("/sys/power/state", "mem");
+
   if (!ok) {
     printf("suspend_linux, error. Could not open /sys/power/state.\n");
     return 1;
   }
   printf("suspend_linux, resumed, and exiting.\n");
+
+  stop_interval_timer(timer);
   return 0;
 }
