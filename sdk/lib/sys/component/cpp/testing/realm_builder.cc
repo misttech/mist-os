5e77dfe88df8859a5f1b837b36bf24c407bb11ae
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2024-10-04 16:27:12 +0000

    [fidl][component] Remove stuff from unsupported API levels
    Change-Id: I9585348bf8c7facd303d9aa61127b097ce301c24
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1129953
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    API-Review: Gary Bressler <geb@google.com>
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>

--- sdk/lib/sys/component/cpp/testing/realm_builder.cc
+++ sdk/lib/sys/component/cpp/testing/realm_builder.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuchsia/component/config/cpp/fidl.h>
 #include <fuchsia/component/cpp/fidl.h>
 #include <fuchsia/component/decl/cpp/fidl.h>
 #include <fuchsia/component/runner/cpp/fidl.h>
--- sdk/lib/sys/component/cpp/testing/realm_builder_types.cc
+++ sdk/lib/sys/component/cpp/testing/realm_builder_types.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuchsia/component/config/cpp/fidl.h>
 #include <fuchsia/component/test/cpp/fidl.h>
 #include <fuchsia/io/cpp/fidl.h>
 #include <fuchsia/mem/cpp/fidl.h>
