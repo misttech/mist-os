d0e8ce81c587d8bf88c8e4496092259d16a695d6
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-08 15:53:17 -0300

    [mistos][process_builder] Unused lib (so far)
    Change-Id: I123c632683eb78fc94d2054c8ae0fa51d117012d

---
+++ vendor/misttech/zircon/kernel/lib/process_builder/diag.h
@@ -0,0 +1,15 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_PROCESS_BUILDER_DIAG_H_
+#define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_PROCESS_BUILDER_DIAG_H_
+
+#include <lib/elfldltl/diagnostics.h>
+#include <trace.h>
+
+namespace process_builder {
+
+}  // namespace process_builder
+
+#endif  // VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_PROCESS_BUILDER_DIAG_H_
