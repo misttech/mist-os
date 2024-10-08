82fb9a7a8dafce22e82f62b42ba63edb0e9459c8
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-08 12:23:19 -0300

    [mistos][syscalls] Re-enabled minimal syscall to be able to load a dyn binary
    Change-Id: Ic2d19bbe0e98783719c0373340dccf41e76f0335

--- vendor/misttech/zircon/kernel/lib/syscalls/mm/mprotect.cc
+++ vendor/misttech/zircon/kernel/lib/syscalls/mm/mprotect.cc
@@ -2,34 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/mistos/starnix/kernel/arch/x64/syscalls.h>
+#include <lib/mistos/starnix/kernel/execution/executor.h>
 #include <lib/mistos/starnix/kernel/mm/syscalls.h>
-#include <lib/mistos/starnix/kernel/task/process_group.h>
-#include <lib/mistos/starnix/kernel/task/task.h>
-#include <lib/mistos/starnix/kernel/task/thread_group.h>
-#include <lib/mistos/starnix/kernel/zircon/task_dispatcher.h>
+#include <lib/mistos/starnix/kernel/task/current_task.h>
 #include <lib/mistos/starnix_syscalls/syscall_result.h>
+#include <lib/mistos/starnix_uapi/user_address.h>
+#include <lib/starnix_zircon/task_wrapper.h>
 #include <lib/syscalls/forward.h>
 #include <trace.h>
 
-#include <cstdint>
-
 #include "../priv.h"
-#include "lib/mistos/starnix_uapi/user_address.h"
 
-#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
+#define LOCAL_TRACE MISTOS_SYSCALLS_GLOBAL_TRACE(0)
 
-using namespace starnix_uapi;
-using namespace starnix;
-using namespace starnix_syscalls;
-
-long sys_mprotect(unsigned long start, size_t len, unsigned long prot) {
+long sys_a0010_mprotect(uint64_t start, size_t len, uint64_t prot) {
   LTRACEF_LEVEL(2, "start=0x%lx len=%ld prot=0x%lx\n", start, len, prot);
-  auto ut = ThreadDispatcher::GetCurrent();
-  auto syscall_ret = sys_mprotect(CurrentTask::From(TaskBuilder(ut->task()->task())),
-                                  UserAddress(start), len, static_cast<uint32_t>(prot));
-  if (syscall_ret.is_error()) {
-    return syscall_ret.error_value().return_value();
-  }
-  return SyscallResult::From(0).value();
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(starnix::sys_mprotect, current_task, UserAddress::const_from(start), len,
+                         static_cast<uint32_t>(prot));
 }
