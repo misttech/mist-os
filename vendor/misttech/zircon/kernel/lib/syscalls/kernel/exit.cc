5d6aa9675cb59c76a667b419c8a1c972c8ae8024
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-09 16:24:54 -0300

    [mistos][syscall] sys_exit/sys_exit_group glue
    Change-Id: I95bd95706b8d20e7fe73f2714b546aaa82e94a5c

--- vendor/misttech/zircon/kernel/lib/syscalls/kernel/exit.cc
+++ vendor/misttech/zircon/kernel/lib/syscalls/kernel/exit.cc
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/mistos/starnix/kernel/execution/executor.h>
+#include <lib/mistos/starnix/kernel/task/current_task.h>
+#include <lib/mistos/starnix/kernel/task/syscalls.h>
+#include <lib/mistos/starnix/kernel/task/task.h>
+#include <lib/mistos/starnix_syscalls/syscall_result.h>
+#include <lib/starnix_zircon/task_wrapper.h>
 #include <lib/syscalls/forward.h>
 #include <trace.h>
 
@@ -9,13 +15,14 @@
 
 #define LOCAL_TRACE MISTOS_SYSCALLS_GLOBAL_TRACE(0)
 
-void __NO_RETURN do_exit(long code) {
-  LTRACEF_LEVEL(2, "code=0x%lx\n", code);
-  ProcessDispatcher::ExitCurrent(code);
+long sys_a0060_exit(int32_t error_code) {
+  LTRACEF_LEVEL(2, "error_code=%d\n", error_code);
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(starnix::sys_exit, current_task, error_code);
 }
 
-long sys_a0060_exit(int32_t retcode) {
-  do_exit((retcode & 0xff) << 8);
-  /* NOTREACHED */
-  return 0;
+long sys_a0231_exit_group(int32_t error_code) {
+  LTRACEF_LEVEL(2, "error_code=%d\n", error_code);
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(starnix::sys_exit_group, current_task, error_code);
 }
