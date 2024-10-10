27759270929c6206348046f35a6bf04667a8380e
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-10 14:29:20 -0300

    [mistos][util] Add as_mut_ptr for MaybeUninit
    Change-Id: Ic5ee6597a4c750b9a904ae79936306f408e1100b

--- vendor/misttech/zircon/kernel/lib/mistos/util/include/lib/mistos/util/maybe_uninit.h
+++ vendor/misttech/zircon/kernel/lib/mistos/util/include/lib/mistos/util/maybe_uninit.h
@@ -5,6 +5,8 @@
 #ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_MAYBE_UNINIT_H_
 #define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_MAYBE_UNINIT_H_
 
+// https://chatgpt.com/share/6707c679-b748-8001-8751-c9f02f9de762
+
 #include <cstdint>
 #include <optional>
 #include <type_traits>
@@ -14,9 +16,6 @@ namespace mtl {
 template <typename T>
 class MaybeUninit {
  public:
-  // Constexpr constructor to create uninitialized memory
-  constexpr MaybeUninit() : storage_() {}
-
   // Static method to create uninitialized MaybeUninit<T>
   static constexpr MaybeUninit<T> uninit() { return MaybeUninit<T>(); }
 
@@ -25,10 +24,17 @@ class MaybeUninit {
     return std::move(*get_ptr());
   }
 
+  // Returns a mutable pointer to the underlying uninitialized memory
+  T* as_mut_ptr() { return get_ptr(); }
+
  private:
+  // Constexpr constructor to create uninitialized memory
+  constexpr MaybeUninit() : storage_() {}
+
   // Returns a pointer to the memory
   T* get_ptr() { return reinterpret_cast<T*>(&storage_); }
 
+  // Storage to hold an uninitialized object of type T
   std::aligned_storage_t<sizeof(T), alignof(T)> storage_;
 };
 
