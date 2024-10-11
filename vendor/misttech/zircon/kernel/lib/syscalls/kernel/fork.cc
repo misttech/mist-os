d5b949e746115bb485adc05da03c8c07bb3ab11a
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-11 08:51:46 -0300

    [mistos][syscalls] Glue fork/exec functions
    Change-Id: I88288e8287ecf2df6c0bad8eb9f3ef0683be8568

--- vendor/misttech/zircon/kernel/lib/syscalls/kernel/fork.cc
+++ vendor/misttech/zircon/kernel/lib/syscalls/kernel/fork.cc
@@ -3,43 +3,39 @@
 // found in the LICENSE file.
 
 #include <lib/mistos/starnix/kernel/arch/x64/syscalls.h>
-#include <lib/mistos/starnix/kernel/mm/syscalls.h>
-#include <lib/mistos/starnix/kernel/task/process_group.h>
+#include <lib/mistos/starnix/kernel/execution/executor.h>
+#include <lib/mistos/starnix/kernel/task/current_task.h>
 #include <lib/mistos/starnix/kernel/task/task.h>
-#include <lib/mistos/starnix/kernel/task/thread_group.h>
-#include <lib/mistos/starnix/kernel/zircon/task_dispatcher.h>
 #include <lib/mistos/starnix_syscalls/syscall_result.h>
-#include <lib/mistos/starnix_uapi/user_address.h>
+#include <lib/starnix_zircon/task_wrapper.h>
 #include <lib/syscalls/forward.h>
 #include <trace.h>
 
 #include "../priv.h"
 
-#define LOCAL_TRACE SYSCALLS_GLOBAL_TRACE(0)
+#define LOCAL_TRACE MISTOS_SYSCALLS_GLOBAL_TRACE(0)
 
-using namespace starnix_uapi;
-using namespace starnix;
-using namespace starnix_syscalls;
+using starnix_uapi::UserAddress;
 
-long sys_clone(uint64_t clone_flags, uint64_t newsp, user_out_ptr<int32_t> parent_tidptr,
-               user_out_ptr<int32_t> child_tidptr, uint64_t tls) {
-  auto ut = ThreadDispatcher::GetCurrent();
-  auto syscall_ret =
-      sys_clone(CurrentTask::From(TaskBuilder(ut->task()->task())), clone_flags, newsp,
-                UserRef<pid_t>(UserAddress()), UserRef<pid_t>(UserAddress()), tls);
-  if (syscall_ret.is_error()) {
-    return syscall_ret.error_value().return_value();
-  }
-  return SyscallResult::From(*syscall_ret).value();
+long sys_a0056_clone(uint64_t clone_flags, uint64_t newsp, user_out_ptr<int32_t> parent_tidptr,
+                     user_out_ptr<int32_t> child_tidptr, uint64_t tls) {
+  LTRACEF_LEVEL(2, "clone_flags=0x%lx, newsp=0x%lx, parent_tidptr=%p, child_tidptr=%p, tls=%lu\n",
+                clone_flags, newsp, parent_tidptr.get(), child_tidptr.get(), tls);
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(
+      starnix::sys_clone, current_task, clone_flags, UserAddress::const_from(newsp),
+      UserRef<pid_t>::From(
+          UserAddress::from_ptr(reinterpret_cast<zx_vaddr_t>(parent_tidptr.get()))),
+      UserRef<pid_t>::From(UserAddress::from_ptr(reinterpret_cast<zx_vaddr_t>(child_tidptr.get()))),
+      UserAddress::from_ptr(tls));
 }
 
-long sys_vfork() { return -ENOSYS; }
+long sys_a0057_fork() {
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(starnix::sys_fork, current_task);
+}
 
-long sys_fork() {
-  auto ut = ThreadDispatcher::GetCurrent();
-  auto syscall_ret = sys_fork(CurrentTask::From(TaskBuilder(ut->task()->task())));
-  if (syscall_ret.is_error()) {
-    return syscall_ret.error_value().return_value();
-  }
-  return SyscallResult::From(*syscall_ret).value();
+long sys_a0058_vfork() {
+  auto current_task = ThreadDispatcher::GetCurrent()->task()->into();
+  return execute_syscall(starnix::sys_vfork, current_task);
 }
