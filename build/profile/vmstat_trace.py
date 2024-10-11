2959b7047c62719e25f3cf2661a71665429622e1
David Fang <fangism@google.com>
2024-10-11 19:24:22 +0000

    [profile] Convert vmstat to chrome-trace format
    Produces a simple Chrome trace event array JSON
    from vmstat output.
    Loadable in perfetto.
    
    Bug: 370569880
    Change-Id: I83854d5681a8ab2bff14b1a48243d8b3f5cb6d90
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1135718
    Fuchsia-Auto-Submit: David Fang <fangism@google.com>
    Reviewed-by: David Turner <digit@google.com>
    Commit-Queue: David Fang <fangism@google.com>

---
+++ build/profile/vmstat_trace.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Converts vmstat output to various trace formats.
+
+Usage:
+  vmstat.py [options] INPUT > OUTPUT
+"""
+
+import argparse
+import dataclasses
+import datetime
+import sys
+from pathlib import Path
+from typing import Any, Iterable, Iterator, Sequence
+
+_SCRIPT_BASENAME = Path(__file__).name
+
+
+@dataclasses.dataclass
+class ProcessCounts:
+    running: int
+    blocked: int
+
+
+@dataclasses.dataclass
+class MemoryUsage:
+    swap: int
+    free: int
+    buffers: int
+    cache: int
+    # inactive: int
+    # active: int
+
+
+@dataclasses.dataclass
+class SwapRates:
+    bytes_in_per_second: int
+    bytes_out_per_second: int
+
+
+@dataclasses.dataclass
+class BlockIORates:
+    received_per_second: int
+    sent_per_second: int
+
+
+@dataclasses.dataclass
+class SystemEventRates:
+    interrupts_per_second: int
+    context_switches_per_second: int
+
+
+@dataclasses.dataclass
+class CPUUsage:
+    user: int
+    system: int
+    idle: int
+    wait_io: int
+    stolen: int
+    kvm_guest: int
+
+
+def event_json(
+    name: str, category: str, time: int, value_type: str, value: Any
+) -> str:
+    """Formats JSON for a single event's value."""
+    return f"""{{"name": "{name}", "cat": "{category}", "ph": "C", "pid": 1, "tid": 1, "ts": {time}, "args": {{"{value_type}": "{value}"}} }},"""
+
+
+@dataclasses.dataclass
+class VmstatEntry:
+    """Represents one line of output from 'vmstat -t'"""
+
+    processes: ProcessCounts
+    memory: MemoryUsage
+    swap: SwapRates
+    block_io: BlockIORates
+    system: SystemEventRates
+    cpu: CPUUsage
+    timestamp: datetime.datetime
+
+    def chrome_trace_events_json(
+        self, start_time: datetime.datetime
+    ) -> Iterable[str]:
+        """Yields a set of trace events at a single time."""
+        tdelta_us = int(
+            (self.timestamp - start_time) / datetime.timedelta(microseconds=1)
+        )
+
+        def event(name: str, value_type: str, value: Any) -> str:
+            return event_json(name, "system", tdelta_us, value_type, value)
+
+        yield event("processes.running", "count", self.processes.running)
+        yield event("processes.blocked", "count", self.processes.blocked)
+
+        yield event("memory.swap", "bytes", self.memory.swap)
+        yield event("memory.free", "bytes", self.memory.free)
+        yield event("memory.buffers", "bytes", self.memory.buffers)
+        yield event("memory.cache", "bytes", self.memory.cache)
+
+        yield event(
+            "swap.in", "bytes_per_second", self.swap.bytes_in_per_second
+        )
+        yield event(
+            "swap.out", "bytes_per_second", self.swap.bytes_out_per_second
+        )
+
+        yield event(
+            "block.in", "bytes_per_second", self.block_io.received_per_second
+        )
+        yield event(
+            "block.out", "bytes_per_second", self.block_io.sent_per_second
+        )
+
+        yield event(
+            "system.interrupts",
+            "count_per_second",
+            self.system.interrupts_per_second,
+        )
+        yield event(
+            "system.context_switches",
+            "count_per_second",
+            self.system.context_switches_per_second,
+        )
+
+        yield event("cpu.user", "percent", self.cpu.user)
+        yield event("cpu.system", "percent", self.cpu.system)
+        yield event("cpu.idle", "percent", self.cpu.idle)
+        yield event("cpu.wait_io", "percent", self.cpu.wait_io)
+        yield event("cpu.stolen", "percent", self.cpu.stolen)
+        yield event("cpu.kvm_guest", "percent", self.cpu.kvm_guest)
+
+
+def _parse_data_row(line: str, max_fields: int) -> VmstatEntry:
+    d = line.split(maxsplit=max_fields)
+    timestamp_text = " ".join(d[18:])
+    return VmstatEntry(
+        processes=ProcessCounts(
+            running=int(d[0]),  # "r"
+            blocked=int(d[1]),  # "b"
+        ),
+        memory=MemoryUsage(
+            swap=int(d[2]),  # "swpd"
+            free=int(d[3]),  # "free"
+            buffers=int(d[4]),  # "buff"
+            cache=int(d[5]),  # "cache"
+        ),
+        swap=SwapRates(
+            bytes_in_per_second=int(d[6]),  # "si"
+            bytes_out_per_second=int(d[7]),  # "so"
+        ),
+        block_io=BlockIORates(
+            received_per_second=int(d[8]),  # "bi"
+            sent_per_second=int(d[9]),  # "bo"
+        ),
+        system=SystemEventRates(
+            interrupts_per_second=int(d[10]),  # "in"
+            context_switches_per_second=int(d[11]),  # "cs"
+        ),
+        cpu=CPUUsage(
+            user=int(d[12]),  # "us"
+            system=int(d[13]),  # "sy"
+            idle=int(d[14]),  # "id"
+            wait_io=int(d[15]),  # "wa"
+            stolen=int(d[16]),  # "st"
+            kvm_guest=int(d[17]),  # "gu"
+        ),
+        timestamp=datetime.datetime.strptime(
+            timestamp_text, "%Y-%m-%d %H:%M:%S"
+        ),  # "UTC"
+    )
+
+
+def _parse_vmstat_output(lines: Iterable[str]) -> Iterator[VmstatEntry]:
+    num_fields = 18
+    for line in lines:
+        if line.startswith("#"):  # comment
+            continue
+        if line.startswith("procs"):  # header categories
+            continue
+        if line.startswith(
+            " r  b"
+        ):  # header fields, starting with running/blocked processes
+            # treat consecutive whitespace as single separator
+            fields = line.split()
+            num_fields = len(fields)
+            continue
+        # else is a line of trace data
+        yield _parse_data_row(line, num_fields)
+
+
+def print_chrome_trace_json(trace: Iterator[VmstatEntry]) -> Iterable[str]:
+    yield "["
+
+    try:
+        first: VmstatEntry = next(trace)
+    except StopIteration:
+        # if trace is empty, abort
+        yield "]"
+        return
+
+    start_time = first.timestamp
+    for line in first.chrome_trace_events_json(start_time):
+        yield f"  {line}"
+
+    # The remainder
+    for t in trace:
+        for line in t.chrome_trace_events_json(start_time):
+            yield f"  {line}"
+
+    yield "]"
+
+
+def _main_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        # positional argument
+        "input",
+        type=Path,
+        help="text file or pipe that represents vmstat output",
+    )
+    return parser
+
+
+_MAIN_ARG_PARSER = _main_arg_parser()
+
+
+def main(argv: Sequence[str]) -> int:
+    args = _MAIN_ARG_PARSER.parse_args(argv)
+    vmstat_text = args.input.read_text()
+
+    trace = _parse_vmstat_output(vmstat_text.splitlines())
+
+    for line in print_chrome_trace_json(trace):
+        print(line)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
---
+++ build/profile/vmstat_trace_test.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import datetime
+import unittest
+from pathlib import Path
+from unittest import mock
+
+import vmstat_trace
+
+
+class EventJsonTests(unittest.TestCase):
+    def test_basic(self) -> None:
+        text = vmstat_trace.event_json("marbles", "fun", 5000, "count", 12)
+        self.assertEqual(
+            text,
+            """{"name": "marbles", "cat": "fun", "ph": "C", "pid": 1, "tid": 1, "ts": 5000, "args": {"count": "12"} },""",
+        )
+
+
+_TEST_START_TIME = datetime.datetime(
+    year=1984,
+    month=11,
+    day=5,
+    hour=9,
+    minute=30,
+    second=0,
+)
+
+# this value matches _SAMPLE_VMSTAT_ENTRY
+_SAMPLE_VMSTAT_DATA_LINE = (
+    " 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 1984-11-05 9:30:00"
+)
+
+# this value matches _SAMPLE_VMSTAT_DATA_LINE
+_SAMPLE_VMSTAT_ENTRY = vmstat_trace.VmstatEntry(
+    processes=vmstat_trace.ProcessCounts(
+        running=1,
+        blocked=2,
+    ),
+    memory=vmstat_trace.MemoryUsage(
+        swap=3,
+        free=4,
+        buffers=5,
+        cache=6,
+    ),
+    swap=vmstat_trace.SwapRates(
+        bytes_in_per_second=7,
+        bytes_out_per_second=8,
+    ),
+    block_io=vmstat_trace.BlockIORates(
+        received_per_second=9,
+        sent_per_second=10,
+    ),
+    system=vmstat_trace.SystemEventRates(
+        interrupts_per_second=11,
+        context_switches_per_second=12,
+    ),
+    cpu=vmstat_trace.CPUUsage(
+        user=13,
+        system=14,
+        idle=15,
+        wait_io=16,
+        stolen=17,
+        kvm_guest=18,
+    ),
+    timestamp=_TEST_START_TIME,
+)
+
+
+class VmstatEntryTests(unittest.TestCase):
+    def test_chrome_trace_events_json(self) -> None:
+        events = list(
+            _SAMPLE_VMSTAT_ENTRY.chrome_trace_events_json(_TEST_START_TIME)
+        )
+        # There are 18 fields of vmstat output (not counting timestamp).
+        self.assertEqual(len(events), 18)
+
+    def test_parse_data_row(self) -> None:
+        entry = vmstat_trace._parse_data_row(_SAMPLE_VMSTAT_DATA_LINE, 18)
+        self.assertEqual(entry, _SAMPLE_VMSTAT_ENTRY)
+
+    def test_parse_vmstat_output(self) -> None:
+        lines = [
+            "# Remember, remember, the 5th of November",  # ignore comment
+            "procs -----------memory---------- ---swap-- -----io---- -system-- -------cpu------- -----timestamp-----",
+            " r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa st gu                 UTC",
+            _SAMPLE_VMSTAT_DATA_LINE,
+        ]
+        entries = list(vmstat_trace._parse_vmstat_output(iter(lines)))
+        self.assertEqual(entries, [_SAMPLE_VMSTAT_ENTRY])
+
+    def test_print_chrome_trace_json_empty(self) -> None:
+        lines = list(vmstat_trace.print_chrome_trace_json(iter([])))
+        self.assertEqual(lines, ["[", "]"])
+
+    def test_print_chrome_trace_json_nonempty(self) -> None:
+        lines = list(
+            vmstat_trace.print_chrome_trace_json(
+                iter([_SAMPLE_VMSTAT_ENTRY] * 2)
+            )
+        )
+        self.assertEqual(lines[0], "[")
+        self.assertEqual(lines[-1], "]")
+        self.assertEqual(len(lines[1:-1]), 18 * 2)  # one per field
+
+
+class MainTests(unittest.TestCase):
+    def test_parse_and_print(self) -> None:
+        argv = ["vmstat.log"]
+        with mock.patch.object(
+            Path, "read_text", return_value=_SAMPLE_VMSTAT_DATA_LINE
+        ) as mock_read:
+            returncode = vmstat_trace.main(argv)
+        self.assertEqual(returncode, 0)
+        mock_read.assert_called_once_with()
+
+
+if __name__ == "__main__":
+    unittest.main()
