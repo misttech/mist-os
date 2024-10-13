85e7a6a00e0310d4a5792d41853db50b0494df37
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-13 11:07:13 -0300

    [mistos][starnix] Add ProcessEntry impl
    Change-Id: I408274a71b7b4c3a3b4ea629a7c4d7f8546ff4a6

--- vendor/misttech/zircon/kernel/lib/starnix/kernel/task/pid_table.cc
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/task/pid_table.cc
@@ -8,6 +8,9 @@
 #include <lib/mistos/starnix/kernel/task/task.h>
 #include <lib/mistos/starnix/kernel/task/thread_group.h>
 #include <lib/mistos/starnix/kernel/task/zombie_process.h>
+#include <lib/mistos/util/num.h>
+#include <lib/mistos/util/weak_wrapper.h>
+#include <zircon/assert.h>
 #include <zircon/compiler.h>
 #include <zircon/types.h>
 
@@ -17,9 +20,7 @@
 #include <fbl/ref_ptr.h>
 #include <ktl/optional.h>
 
-#include "lib/mistos/util/weak_wrapper.h"
-
-#include <ktl/enforce.h>
+// #include <ktl/enforce.h>
 
 namespace {
 #if 0
@@ -35,6 +36,27 @@ V&& get(const fbl::HashTable<K, V>& table, const K& key) {
 
 namespace starnix {
 
+ProcessEntry ProcessEntry::ThreadGroupCtor(util::WeakPtr<ThreadGroup> thread_group) {
+  return ProcessEntry(thread_group);
+}
+ProcessEntry ProcessEntry::ZombieProcessCtor(util::WeakPtr<ZombieProcess> zombie_process) {
+  return ProcessEntry(zombie_process);
+}
+
+ProcessEntry::ProcessEntry() = default;
+ProcessEntry::~ProcessEntry() = default;
+ProcessEntry::ProcessEntry(Variant variant) : process_(ktl::move(variant)) {}
+
+bool ProcessEntry::is_none() const { return ktl::holds_alternative<ktl::monostate>(process_); }
+
+ktl::optional<std::reference_wrapper<const util::WeakPtr<ThreadGroup>>> ProcessEntry::thread_group()
+    const {
+  if (auto* ptr = ktl::get_if<util::WeakPtr<ThreadGroup>>(&process_)) {
+    return std::reference_wrapper<const util::WeakPtr<ThreadGroup>>(*ptr);
+  }
+  return ktl::nullopt;
+}
+
 pid_t PidEntry::GetKey() const { return pid_; }
 
 size_t PidEntry::GetHash(pid_t pid) { return pid; }
@@ -44,8 +66,8 @@ PidEntry::PidEntry(pid_t pid) : pid_(pid) {}
 PidEntry::~PidEntry() = default;
 
 ktl::optional<const PidEntry*> PidTable::get_entry(pid_t pid) const {
-  const auto& entry = table.find(pid);
-  if (entry == table.end()) {
+  const auto& entry = table_.find(pid);
+  if (entry == table_.end()) {
     return ktl::nullopt;
   }
   return &(*entry);
@@ -55,32 +77,32 @@ PidEntry& PidTable::get_entry_mut(pid_t pid) {
   fbl::AllocChecker ac;
   auto default_ptr = ktl::make_unique<PidEntry>(&ac, pid);
   if (ac.check()) {
-    table.insert_or_find(ktl::move(default_ptr));
+    table_.insert_or_find(ktl::move(default_ptr));
   }
-  return *table.find(pid);
+  return *table_.find(pid);
 }
 
 pid_t PidTable::allocate_pid() {
-  pid_t p;
-  if (add_overflow(last_pid_, 1, &p)) {
+  auto result = mtl::checked_add(last_pid_, 1);
+  if (result.has_value()) {
+    last_pid_ = result.value();
+  } else {
     // NB: If/when we re-use pids, we need to check that PidFdFileObject is holding onto
     // the task correctly.
     // track_stub !(TODO("https://fxbug.dev/322874557"), "pid wraparound");
-    // last_pid_ = self.last_pid.overflowing_add(1) .0;
-  } else {
-    last_pid_ = p;
+    last_pid_ = mtl::overflowing_add(last_pid_, 1).first;
   }
   return last_pid_;
 }
 
 pid_t PidTable::last_pid() const { return last_pid_; }
 
-size_t PidTable::len() const { return table.size(); }
+size_t PidTable::len() const { return table_.size(); }
 
 util::WeakPtr<Task> PidTable::get_task(pid_t pid) const {
   auto entry = get_entry(pid);
   if (entry.has_value()) {
-    return entry.value()->task_.value_or(util::WeakPtr<Task>());
+    return (*entry)->task_.value_or(util::WeakPtr<Task>());
   }
   return util::WeakPtr<Task>();
 }
@@ -92,18 +114,16 @@ void PidTable::add_task(const fbl::RefPtr<Task>& task) {
 }
 
 void PidTable::remove_task(pid_t pid) {
-  const auto& entry = table.find(pid);
-  if (entry != table.end()) {
-    // if (!entry->process_group.has_value() && !entry->task.has_value()) {
-    //   table_.erase(pid);
-    // }
-  }
+  remove_item(pid, [](auto& entry) {
+    auto removed = ktl::move(entry.task_);
+    ZX_ASSERT(removed.has_value());
+  });
 }
 
 void PidTable::add_thread_group(const fbl::RefPtr<ThreadGroup>& thread_group) {
   auto& entry = get_entry_mut(thread_group->leader);
-  ASSERT(entry.process_.index() == 0);
-  entry.process_ = util::WeakPtr<ThreadGroup>(thread_group.get());
+  ASSERT(entry.process_.is_none());
+  entry.process_ = ProcessEntry::ThreadGroupCtor(util::WeakPtr<ThreadGroup>(thread_group.get()));
 }
 
 void PidTable::add_process_group(const fbl::RefPtr<ProcessGroup>& process_group) {
@@ -111,5 +131,15 @@ void PidTable::add_process_group(const fbl::RefPtr<ProcessGroup>& process_group)
   ASSERT(!entry.process_group_.has_value());
   entry.process_group_ = util::WeakPtr<ProcessGroup>(process_group.get());
 }
+
+ktl::optional<fbl::RefPtr<ProcessGroup>> PidTable::get_process_group(pid_t pid) const {
+  auto entry = get_entry(pid);
+  if (entry.has_value() && (*entry)->process_group_) {
+    return (*entry)->process_group_->Lock();
+  }
+  return ktl::nullopt;
+}
+
+void PidTable::remove_process_group(pid_t pid) {}
 
 }  // namespace starnix
