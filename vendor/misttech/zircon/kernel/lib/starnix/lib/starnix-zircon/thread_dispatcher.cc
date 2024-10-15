5aef43b213f0f8d55a3959443e796ad5de60ac5b
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-15 10:16:04 -0300

    [mistos][kernel] Fork Support
    Change-Id: I3d32ba9cc4179a423d21ef868c29cea9506a96da

--- vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-zircon/thread_dispatcher.cc
+++ vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-zircon/thread_dispatcher.cc
@@ -8,11 +8,39 @@
 #include "object/thread_dispatcher.h"
 
 #include <lib/starnix_zircon/task_wrapper.h>
+#include <trace.h>
 
 #include <ktl/move.h>
+
+#define LOCAL_TRACE 0
 
 void ThreadDispatcher::SetTask(fbl::RefPtr<TaskWrapper> task) { starnix_task_ = ktl::move(task); }
 
 // Get the associated Starnix Task.
 TaskWrapper* ThreadDispatcher::task() { return starnix_task_.get(); }
 const TaskWrapper* ThreadDispatcher::task() const { return starnix_task_.get(); }
+
+zx_status_t ThreadDispatcher::SetForkFrame(const zx_thread_state_general_regs_t& fork_frame) {
+  canary_.Assert();
+
+  LTRACE_ENTRY_OBJ;
+
+  Guard<CriticalMutex> guard{get_lock()};
+
+  switch (state_.lifecycle()) {
+    case ThreadState::Lifecycle::INITIAL:
+      // Unreachable, thread leaves INITIAL state before Create() returns.
+      DEBUG_ASSERT(false);
+      __UNREACHABLE;
+    case ThreadState::Lifecycle::INITIALIZED:
+      return core_thread_->SetForkFrame(fork_frame);
+    case ThreadState::Lifecycle::RUNNING:
+    case ThreadState::Lifecycle::SUSPENDED:
+    case ThreadState::Lifecycle::DYING:
+    case ThreadState::Lifecycle::DEAD:
+      return ZX_ERR_BAD_STATE;
+  }
+
+  DEBUG_ASSERT(false);
+  return ZX_ERR_BAD_STATE;
+}
