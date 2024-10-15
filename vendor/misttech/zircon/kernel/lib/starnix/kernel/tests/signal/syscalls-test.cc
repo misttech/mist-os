84e8fa382b2181c7dccd2e4144e1a155a1e0e395
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-15 15:44:09 -0300

    [mistos][starnix] Add signal syscall test:test_wait4_by_pgid
    Change-Id: I410a75fc53f2673b37d4c5eade8c7a94b34fb14b

---
+++ vendor/misttech/zircon/kernel/lib/starnix/kernel/tests/signal/syscalls-test.cc
@@ -0,0 +1,58 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/mistos/starnix/kernel/signals/syscalls.h>
+#include <lib/mistos/starnix/kernel/task/kernel.h>
+#include <lib/mistos/starnix/kernel/task/task.h>
+#include <lib/mistos/starnix/kernel/task/thread_group.h>
+#include <lib/mistos/starnix/testing/testing.h>
+#include <lib/mistos/util/default_construct.h>
+#include <lib/mistos/util/testing/unittest.h>
+#include <lib/unittest/unittest.h>
+
+#include <memory>
+
+#include <fbl/ref_ptr.h>
+#include <ktl/string_view.h>
+
+#include <linux/prctl.h>
+
+using namespace starnix::testing;
+
+namespace unit_testing {
+
+using starnix::ExitStatus;
+
+bool test_wait4_by_pgid() {
+  BEGIN_TEST;
+
+  auto [kernel, current_task] = create_kernel_task_and_unlocked();
+  auto child1 = (*current_task).clone_task_for_test(0, starnix_uapi::kSIGCHLD);
+  auto child1_pid = child1->id();
+  child1->thread_group()->exit(starnix::ExitStatusExit(42), ktl::nullopt);
+  child1.~AutoReleasableTask();
+  auto child2 = (*current_task).clone_task_for_test(0, starnix_uapi::kSIGCHLD);
+  ASSERT_TRUE(child2->thread_group()->setsid().is_ok(), "setsid");
+  auto child2_pid = child2->id();
+  child2->thread_group()->exit(starnix::ExitStatusExit(42), ktl::nullopt);
+  child2.~AutoReleasableTask();
+
+  auto result = sys_wait4(*current_task, -child2_pid, mtl::DefaultConstruct<UserRef<int32_t>>(), 0,
+                          mtl::DefaultConstruct<UserRef<struct ::rusage>>());
+  ASSERT_TRUE(result.is_ok());
+  ASSERT_EQ(child2_pid, result.value());
+
+  result = sys_wait4(*current_task, 0, mtl::DefaultConstruct<UserRef<int32_t>>(), 0,
+                     mtl::DefaultConstruct<UserRef<struct ::rusage>>());
+  ASSERT_TRUE(result.is_ok());
+  ASSERT_EQ(child1_pid, result.value());
+  END_TEST;
+}
+}  // namespace unit_testing
+
+UNITTEST_START_TESTCASE(starnix_signal_syscalls)
+UNITTEST("test wait4 by pgid", unit_testing::test_wait4_by_pgid)
+UNITTEST_END_TESTCASE(starnix_signal_syscalls, "starnix_signal_syscalls",
+                      "Tests for Signal Syscalls")
