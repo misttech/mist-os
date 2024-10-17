427b45a5cae97339073f74f8fb148e2046e41f8b
Rui Qi Sim <rqsim@google.com>
2024-10-17 03:56:47 +0000

    [system-updater] Replace deprecated fuchsia-fs functions
    Bug: 361450366
    Change-Id: I2150f3c8022c748b0e921c53f29d6d2c3961a67f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1135157
    Reviewed-by: Ben Keller <galbanum@google.com>
    Commit-Queue: Rui Qi Sim <rqsim@google.com>

--- src/sys/pkg/bin/system-updater/src/update/environment.rs
+++ src/sys/pkg/bin/system-updater/src/update/environment.rs
@@ -111,18 +111,12 @@ pub struct NamespaceBuildInfo;
 
 impl NamespaceBuildInfo {
     async fn read_file(&self, name: &str) -> Result<Option<String>, Error> {
-        let build_info = fuchsia_fs::directory::open_in_namespace_deprecated(
-            "/config/build-info",
-            fuchsia_fs::OpenFlags::RIGHT_READABLE,
-        )
-        .context("while opening /config/build-info")?;
+        let build_info =
+            fuchsia_fs::directory::open_in_namespace("/config/build-info", fio::PERM_READABLE)
+                .context("while opening /config/build-info")?;
 
-        let file = match fuchsia_fs::directory::open_file_deprecated(
-            &build_info,
-            name,
-            fuchsia_fs::OpenFlags::RIGHT_READABLE,
-        )
-        .await
+        let file = match fuchsia_fs::directory::open_file(&build_info, name, fio::PERM_READABLE)
+            .await
         {
             Ok(file) => file,
             Err(fuchsia_fs::node::OpenError::OpenError(zx::Status::NOT_FOUND)) => return Ok(None),
@@ -154,10 +148,9 @@ pub struct NamespaceSystemInfo;
 #[async_trait]
 impl SystemInfo for NamespaceSystemInfo {
     async fn system_image_hash(&self) -> Result<Option<fuchsia_hash::Hash>, Error> {
-        let proxy = if let Ok(proxy) = fuchsia_fs::directory::open_in_namespace_deprecated(
-            "/system",
-            fio::OpenFlags::RIGHT_READABLE,
-        ) {
+        let proxy = if let Ok(proxy) =
+            fuchsia_fs::directory::open_in_namespace("/system", fio::PERM_READABLE)
+        {
             proxy
         } else {
             // system-updater will always have /system in its namespace because its manifest
