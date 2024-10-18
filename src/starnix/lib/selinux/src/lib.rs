1ad7a06b3103afdad9198f4e801627d005115ea4
Wez <wez@google.com>
2024-10-18 23:35:46 +0000

    [sestarnix] Apply the keep-sorted lint to class & permissions enums
    Keep-sorted will keep the kernel object class & permission enum
    values in lexicographical order, making it easier to find them at
    a glance.
    
    The reference SIDs are not keep-sorted, so that they will remain
    ordered by their assigned values, as is the case in the reference
    policy's initial-SIDs fragment.
    
    Change-Id: I32af600c27170a1b6a0d2486e2ec4b842122e347
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1139998
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Wez <wez@google.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Reviewed-by: David Song <wintermelons@google.com>

--- src/starnix/lib/selinux/src/lib.rs
+++ src/starnix/lib/selinux/src/lib.rs
@@ -69,39 +69,43 @@ enumerable_enum! {
     /// hooks.
     #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
     ObjectClass {
-        /// The SELinux "process" object class.
-        Process,
-        /// The SELinux "file" object class.
-        File,
-        /// The SELinux "dir" object class.
-        Dir,
+        // keep-sorted start
         /// The SELinux "blk_file" object class.
         Block,
         /// The SELinux "chr_file" object class.
         Character,
-        /// The SELinux "lnk_file" object class.
-        Link,
+        /// The SELinux "dir" object class.
+        Dir,
         /// The SELinux "fifo_file" object class.
         Fifo,
-        /// The SELinux "sock_file" object class.
-        Socket,
+        /// The SELinux "file" object class.
+        File,
+        /// The SELinux "lnk_file" object class.
+        Link,
+        /// The SELinux "process" object class.
+        Process,
         /// The SELinux "security" object class.
         Security,
+        /// The SELinux "sock_file" object class.
+        Socket,
+        // keep-sorted end
     }
 }
 
 impl ObjectClass {
     pub fn name(&self) -> &'static str {
         match self {
-            Self::Process => "process",
-            Self::File => "file",
-            Self::Dir => "dir",
+            // keep-sorted start
             Self::Block => "blk_file",
             Self::Character => "chr_file",
-            Self::Link => "lnk_file",
+            Self::Dir => "dir",
             Self::Fifo => "fifo_file",
-            Self::Socket => "sock_file",
+            Self::File => "file",
+            Self::Link => "lnk_file",
+            Self::Process => "process",
             Self::Security => "security",
+            Self::Socket => "sock_file",
+            // keep-sorted end
         }
     }
 }
@@ -123,33 +127,37 @@ enumerable_enum! {
     /// enforcement hooks.
     #[derive(Clone, Debug, Eq, Hash, PartialEq)]
     FileClass {
-        /// The SELinux "file" object class.
-        File,
-        /// The SELinux "dir" object class.
-        Dir,
+        // keep-sorted start
         /// The SELinux "blk_file" object class.
         Block,
         /// The SELinux "chr_file" object class.
         Character,
-        /// The SELinux "lnk_file" object class.
-        Link,
+        /// The SELinux "dir" object class.
+        Dir,
         /// The SELinux "fifo_file" object class.
         Fifo,
+        /// The SELinux "file" object class.
+        File,
+        /// The SELinux "lnk_file" object class.
+        Link,
         /// The SELinux "sock_file" object class.
         Socket,
+        // keep-sorted end
     }
 }
 
 impl From<FileClass> for ObjectClass {
     fn from(file_class: FileClass) -> Self {
         match file_class {
-            FileClass::File => Self::File,
-            FileClass::Dir => Self::Dir,
+            // keep-sorted start
             FileClass::Block => Self::Block,
             FileClass::Character => Self::Character,
-            FileClass::Link => Self::Link,
+            FileClass::Dir => Self::Dir,
             FileClass::Fifo => Self::Fifo,
+            FileClass::File => Self::File,
+            FileClass::Link => Self::Link,
             FileClass::Socket => Self::Socket,
+            // keep-sorted end
         }
     }
 }
@@ -224,12 +232,14 @@ permission_enum! {
     /// policy enforcement hooks.
     #[derive(Clone, Debug, Eq, Hash, PartialEq)]
     Permission {
-        /// Permissions for the well-known SELinux "process" object class.
-        Process(ProcessPermission),
+        // keep-sorted start
         /// Permissions for the well-known SELinux "file" object class.
         File(FilePermission),
+        /// Permissions for the well-known SELinux "process" object class.
+        Process(ProcessPermission),
         /// Permissions for access to parts of the "selinuxfs" used to administer and query SELinux.
         Security(SecurityPermission),
+        // keep-sorted end
     }
 }
 
@@ -260,34 +270,44 @@ macro_rules! class_permission_enum {
 }
 
 class_permission_enum! {
+    /// A well-known "file" class permission in SELinux policy that has a particular meaning in
+    /// policy enforcement hooks.
+    #[derive(Clone, Debug, Eq, Hash, PartialEq)]
+    FilePermission {
+        // keep-sorted start
+        /// Permission to create a file.
+        Create("create"),
+        /// Permission to use a file as an entry point into the new domain on transition.
+        Entrypoint("entrypoint"),
+        /// Permission to use a file as an entry point to the calling domain without performing a
+        /// transition.
+        ExecuteNoTrans("execute_no_trans"),
+        /// Permission to open a file.
+        Open("open"),
+        // keep-sorted end
+    }
+}
+
+class_permission_enum! {
     /// A well-known "process" class permission in SELinux policy that has a particular meaning in
     /// policy enforcement hooks.
     #[derive(Clone, Debug, Eq, Hash, PartialEq)]
     ProcessPermission {
+        // keep-sorted start
+        /// Permission to dynamically transition a process to a different security domain.
+        DynTransition("dyntransition"),
         /// Permission to fork the current running process.
         Fork("fork"),
-        /// Permission to transition to a different security domain.
-        Transition("transition"),
-        /// Permission to get scheduling policy currently applied to a process.
-        GetSched("getsched"),
-        /// Permission to set scheduling policy for a process.
-        SetSched("setsched"),
         /// Permission to get the process group ID.
         GetPgid("getpgid"),
-        /// Permission to set the process group ID.
-        SetPgid("setpgid"),
-        /// Permission to send a signal other than SIGKILL, SIGSTOP, or SIGCHLD to a process.
-        Signal("signal"),
-        /// Permission to send SIGKILL to a process.
-        SigKill("sigkill"),
-        /// Permission to send SIGSTOP to a process.
-        SigStop("sigstop"),
-        /// Permission to send SIGCHLD to a process.
-        SigChld("sigchld"),
-        /// Permission to trace a process.
-        Ptrace("ptrace"),
+        /// Permission to get the resource limits on a process.
+        GetRlimit("getrlimit"),
+        /// Permission to get scheduling policy currently applied to a process.
+        GetSched("getsched"),
         /// Permission to get the session ID.
         GetSession("getsession"),
+        /// Permission to trace a process.
+        Ptrace("ptrace"),
         /// Permission to set the calling task's current Security Context.
         /// The "dyntransition" permission separately limits which Contexts "setcurrent" may be used to transition to.
         SetCurrent("setcurrent"),
@@ -297,31 +317,25 @@ class_permission_enum! {
         SetFsCreate("setfscreate"),
         /// Permission to set the Security Context used when creating kernel keyrings.
         SetKeyCreate("setkeycreate"),
-        /// Permission to set the Security Context used when creating new labeled sockets.
-        SetSockCreate("setsockcreate"),
-        /// Permission to get the resource limits on a process.
-        GetRlimit("getrlimit"),
+        /// Permission to set the process group ID.
+        SetPgid("setpgid"),
         /// Permission to set the resource limits on a process.
         SetRlimit("setrlimit"),
-        /// Permission to dynamically transition a process to a different security domain.
-        DynTransition("dyntransition"),
-    }
-}
-
-class_permission_enum! {
-    /// A well-known "file" class permission in SELinux policy that has a particular meaning in
-    /// policy enforcement hooks.
-    #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-    FilePermission {
-        /// Permission to create a file.
-        Create("create"),
-        /// Permission to open a file.
-        Open("open"),
-        /// Permission to use a file as an entry point to the calling domain without performing a
-        /// transition.
-        ExecuteNoTrans("execute_no_trans"),
-        /// Permission to use a file as an entry point into the new domain on transition.
-        Entrypoint("entrypoint"),
+        /// Permission to set scheduling policy for a process.
+        SetSched("setsched"),
+        /// Permission to set the Security Context used when creating new labeled sockets.
+        SetSockCreate("setsockcreate"),
+        /// Permission to send SIGCHLD to a process.
+        SigChld("sigchld"),
+        /// Permission to send SIGKILL to a process.
+        SigKill("sigkill"),
+        /// Permission to send SIGSTOP to a process.
+        SigStop("sigstop"),
+        /// Permission to send a signal other than SIGKILL, SIGSTOP, or SIGCHLD to a process.
+        Signal("signal"),
+        /// Permission to transition to a different security domain.
+        Transition("transition"),
+        // keep-sorted end
     }
 }
 
@@ -330,6 +344,7 @@ class_permission_enum! {
     /// sensitive administrative and query API surfaces in the "selinuxfs".
     #[derive(Clone, Debug, Eq, Hash, PartialEq)]
     SecurityPermission {
+        // keep-sorted start
         /// Permission to validate Security Context using the "context" API.
         CheckContext("check_context"),
         /// Permission to compute access vectors via the "access" API.
@@ -344,6 +359,7 @@ class_permission_enum! {
         SetCheckReqProt("setcheckreqprot"),
         /// Permission to switch the system between permissive and enforcing modes, via "enforce".
         SetEnforce("setenforce"),
+        // keep-sorted end
      }
 }
 
@@ -402,9 +418,11 @@ initial_sid_enum! {
 /// These must be present in the policy, for it to be valid.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
     InitialSid {
+        // keep-sorted start
         File("file"),
         Kernel("kernel"),
         Unlabeled("unlabeled"),
+        // keep-sorted end
     }
 }
 
