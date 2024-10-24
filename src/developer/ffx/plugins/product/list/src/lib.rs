b6a3945e63d2f1a983b88b83eba4e967b842577f
Jiaming Li <lijiaming@google.com>
2024-10-24 18:41:25 +0000

    [ffx][product] Support ffx product list for in-tree
    In tree we will create product_bundles.json. We teach 'ffx product list'
    to parse this file and list products.
    
    Tests: fx build && ffx product list
    
    Bug: 371199456
    Change-Id: I298b17233c094ef510075ecac410c4cd385f567f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1143903
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Commit-Queue: Jiaming Li <lijiaming@google.com>

--- src/developer/ffx/plugins/product/list/src/lib.rs
+++ src/developer/ffx/plugins/product/list/src/lib.rs
@@ -8,6 +8,7 @@
 use ::gcs::client::{Client, ProgressResponse};
 use anyhow::{Context, Result};
 use ffx_config::sdk::SdkVersion;
+use ffx_config::EnvironmentContext;
 use ffx_product_list_args::ListCommand;
 use fho::{bug, return_user_error, FfxMain, FfxTool, MachineWriter, ToolIO as _};
 use gcs::gs_url::split_gs_url;
@@ -19,11 +20,13 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::io::{stderr, stdin, stdout, Write};
+use std::path::Path;
 use std::str::FromStr;
 use structured_ui::{Notice, Presentation};
 
 const PB_MANIFEST_NAME: &'static str = "product_bundles.json";
 const CONFIG_BASE_URLS: &'static str = "pbms.base_urls";
+const PRODUCT_BUNDLE_INDEX_KEY: &str = "product.index";
 
 lazy_static! {
     static ref BRANCH_TO_PREFIX_MAPPING: HashMap<&'static str, &'static str> = hashmap! {
@@ -53,11 +56,13 @@ type ProductManifest = Vec<ProductBundle>;
 pub struct ProductListTool {
     #[command]
     pub cmd: ListCommand,
+
+    pub context: EnvironmentContext,
 }
 
 fho::embedded_plugin!(ProductListTool);
 
-/// This plugin will get the info of repository inside product bundle.
+/// This plugin will get list the available product bundles.
 #[async_trait::async_trait(?Send)]
 impl FfxMain for ProductListTool {
     type Writer = MachineWriter<Vec<ProductBundle>>;
@@ -72,9 +77,15 @@ impl FfxMain for ProductListTool {
         let mut err_out = stderr();
         let ui = structured_ui::TextUi::new(&mut input, &mut output, &mut err_out);
 
-        let pbs =
-            pb_list_impl(&self.cmd.auth, self.cmd.base_url, self.cmd.version, self.cmd.branch, &ui)
-                .await?;
+        let pbs = pb_list_impl(
+            &self.cmd.auth,
+            self.cmd.base_url,
+            self.cmd.version,
+            self.cmd.branch,
+            &ui,
+            &self.context,
+        )
+        .await?;
         if writer.is_machine() {
             writer.machine(&pbs)?;
         } else {
@@ -93,23 +104,20 @@ pub async fn resolve_branch_to_base_urls<I>(
     auth: &AuthFlowChoice,
     ui: &I,
     client: &Client,
+    context: &EnvironmentContext,
 ) -> fho::Result<Vec<String>>
 where
     I: structured_ui::Interface,
 {
-    let base_urls = ffx_config::get::<Vec<String>, _>(CONFIG_BASE_URLS)
-        .context("get config CONFIG_BASE_URLS")?;
+    let base_urls: Vec<String> =
+        context.get(CONFIG_BASE_URLS).context("get config CONFIG_BASE_URLS")?;
 
     // If branch is not provided, use version to build base_urls
     if branch.is_none() {
         let version = match version {
             Some(version) => version,
             None => {
-                let sdk = ffx_config::global_env_context()
-                    .context("loading global environment context")?
-                    .get_sdk()
-                    .await
-                    .context("getting sdk env context")?;
+                let sdk = context.get_sdk().await.context("getting sdk env context")?;
                 match sdk.get_version() {
                     SdkVersion::Version(version) => version.to_string(),
                     SdkVersion::InTree => {
@@ -183,17 +191,26 @@ pub async fn pb_list_impl<I>(
     version: Option<String>,
     branch: Option<String>,
     ui: &I,
+    context: &EnvironmentContext,
 ) -> Result<Vec<ProductBundle>>
 where
     I: structured_ui::Interface,
 {
-    let client = Client::initial()?;
+    // If the product.index is specified, we will use local product_bundles.json
+    let index: String = context.get(PRODUCT_BUNDLE_INDEX_KEY)?;
+    if Path::new(&index).is_file() {
+        let pm = std::fs::read_to_string(index)?;
+        return Ok(serde_json::from_str::<ProductManifest>(&pm)
+            .map_err(|e| bug!("Parsing json {:?}: {e}", pm))?);
+    }
 
+    let client = Client::initial()?;
     let mut products = Vec::new();
+
     let base_urls = if let Some(base_url) = override_base_url {
         vec![base_url]
     } else {
-        resolve_branch_to_base_urls(version, branch, auth, ui, &client).await?
+        resolve_branch_to_base_urls(version, branch, auth, ui, &client, &context).await?
     };
 
     for base_url in &base_urls {
@@ -251,14 +268,29 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+    use ffx_config::{ConfigLevel, TestEnv};
     use fho::{Format, TestBuffers};
-    use temp_test_env::TempTestEnv;
+    use std::fs::File;
+    use std::path::Path;
+
+    async fn setup_test_env(path: &Path) -> TestEnv {
+        let env = ffx_config::test_init().await.unwrap();
+        env.context
+            .query(PRODUCT_BUNDLE_INDEX_KEY)
+            .level(Some(ConfigLevel::User))
+            .set(path.to_str().unwrap().into())
+            .await
+            .unwrap();
+
+        env
+    }
 
     #[fuchsia::test]
     async fn test_pb_list_impl() {
-        let test_env = TempTestEnv::new().expect("test_env");
-        let mut f =
-            std::fs::File::create(test_env.home.join(PB_MANIFEST_NAME)).expect("file create");
+        let tmp = tempfile::tempdir().unwrap();
+        let path = tmp.path().join(PB_MANIFEST_NAME);
+        let env = setup_test_env(&path).await;
+        let mut f = File::create(&path).expect("file create");
         f.write_all(
             r#"[{
             "name": "fake_name",
@@ -272,10 +304,11 @@ mod test {
         let ui = structured_ui::MockUi::new();
         let pbs = pb_list_impl(
             &AuthFlowChoice::Default,
-            Some(format!("file:{}", test_env.home.display())),
+            Some(format!("file:{}", tmp.path().display())),
             Some(String::from("fake_version")),
             None,
             &ui,
+            &env.context,
         )
         .await
         .expect("testing list");
@@ -291,9 +324,10 @@ mod test {
 
     #[fuchsia::test]
     async fn test_pb_list_impl_machine_code() {
-        let test_env = TempTestEnv::new().expect("test_env");
-        let mut f =
-            std::fs::File::create(test_env.home.join(PB_MANIFEST_NAME)).expect("file create");
+        let tmp = tempfile::tempdir().unwrap();
+        let path = tmp.path().join(PB_MANIFEST_NAME);
+        let env = setup_test_env(&path).await;
+        let mut f = File::create(&path).expect("file create");
         f.write_all(
             r#"[{
             "name": "fake_name",
@@ -309,10 +343,53 @@ mod test {
         let tool = ProductListTool {
             cmd: ListCommand {
                 auth: AuthFlowChoice::Default,
-                base_url: Some(format!("file:{}", test_env.home.display())),
+                base_url: Some(format!("file:{}", tmp.path().display())),
                 version: Some("fake_version".into()),
                 branch: None,
             },
+            context: env.context.clone(),
+        };
+
+        tool.main(writer).await.expect("testing list");
+
+        let pbs: Vec<ProductBundle> = serde_json::from_str(&buffers.into_stdout_str()).unwrap();
+        assert_eq!(
+            vec![ProductBundle {
+                name: String::from("fake_name"),
+                product_version: String::from("fake_version"),
+                transfer_manifest_url: String::from("fake_url")
+            }],
+            pbs
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_pb_list_impl_machine_code_ignore_unknown_fields() {
+        let tmp = tempfile::tempdir().unwrap();
+        let path = tmp.path().join(PB_MANIFEST_NAME);
+        let env = setup_test_env(&path).await;
+        let mut f = File::create(&path).expect("file create");
+        f.write_all(
+            r#"[{
+            "name": "fake_name",
+            "product_version": "fake_version",
+            "transfer_manifest_url": "fake_url",
+            "transfer_manifest_path": "not_used_path"
+            }]"#
+            .as_bytes(),
+        )
+        .expect("write_all");
+
+        let buffers = TestBuffers::default();
+        let writer = MachineWriter::new_test(Some(Format::Json), &buffers);
+        let tool = ProductListTool {
+            cmd: ListCommand {
+                auth: AuthFlowChoice::Default,
+                base_url: Some(format!("file:{}", tmp.path().display())),
+                version: Some("fake_version".into()),
+                branch: None,
+            },
+            context: env.context.clone(),
         };
 
         tool.main(writer).await.expect("testing list");
--- src/developer/ffx/plugins/product/lookup/src/lib.rs
+++ src/developer/ffx/plugins/product/lookup/src/lib.rs
@@ -5,6 +5,7 @@
 //! A tool to:
 //! - lookup product bundle description information to find the transfer URL.
 
+use ffx_config::EnvironmentContext;
 use ffx_product::{CommandStatus, MachineOutput, MachineUi};
 use ffx_product_list::{pb_list_impl, ProductBundle};
 use ffx_product_lookup_args::LookupCommand;
@@ -16,6 +17,8 @@ use std::io::{stdin, stdout, Write};
 pub struct PbLookupTool {
     #[command]
     cmd: LookupCommand,
+
+    context: EnvironmentContext,
 }
 
 fho::embedded_plugin!(PbLookupTool);
@@ -42,6 +45,7 @@ impl PbLookupTool {
             &self.cmd.name,
             &self.cmd.version,
             &ui,
+            &self.context,
         )
         .await
         {
@@ -70,6 +74,7 @@ impl PbLookupTool {
             &self.cmd.name,
             &self.cmd.version,
             &ui,
+            &self.context,
         )
         .await?;
 
@@ -84,6 +89,7 @@ pub async fn pb_lookup_impl<I>(
     name: &str,
     version: &str,
     ui: &I,
+    context: &EnvironmentContext,
 ) -> Result<ProductBundle>
 where
     I: structured_ui::Interface,
@@ -92,7 +98,8 @@ where
     tracing::info!("---------------------- Lookup Begin ----------------------------");
 
     let products =
-        pb_list_impl(auth, override_base_url.clone(), Some(version.to_string()), None, ui).await?;
+        pb_list_impl(auth, override_base_url.clone(), Some(version.to_string()), None, ui, context)
+            .await?;
 
     tracing::debug!("Looking for product bundle {}, version {}", name, version);
     let mut products = products
@@ -122,16 +129,32 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+    use ffx_config::{ConfigLevel, TestEnv};
     use fho::{Format, TestBuffers};
-    use temp_test_env::TempTestEnv;
+    use std::fs::File;
+    use std::path::Path;
 
     const PB_MANIFEST_NAME: &'static str = "product_bundles.json";
+    const PRODUCT_BUNDLE_INDEX_KEY: &str = "product.index";
+
+    async fn setup_test_env(path: &Path) -> TestEnv {
+        let env = ffx_config::test_init().await.unwrap();
+        env.context
+            .query(PRODUCT_BUNDLE_INDEX_KEY)
+            .level(Some(ConfigLevel::User))
+            .set(path.to_str().unwrap().into())
+            .await
+            .unwrap();
+
+        env
+    }
 
     #[fuchsia::test]
     async fn test_pb_lookup_impl() {
-        let test_env = TempTestEnv::new().expect("test_env");
-        let mut f =
-            std::fs::File::create(test_env.home.join(PB_MANIFEST_NAME)).expect("file create");
+        let tmp = tempfile::tempdir().unwrap();
+        let path = tmp.path().join(PB_MANIFEST_NAME);
+        let env = setup_test_env(&path).await;
+        let mut f = File::create(&path).expect("file create");
         f.write_all(
             r#"[{
             "name": "fake_name",
@@ -145,10 +168,11 @@ mod test {
         let ui = structured_ui::MockUi::new();
         let product = pb_lookup_impl(
             &AuthFlowChoice::Default,
-            &Some(format!("file:{}", test_env.home.display())),
+            &Some(format!("file:{}", tmp.path().display())),
             "fake_name",
             "fake_version",
             &ui,
+            &env.context,
         )
         .await
         .expect("testing lookup");
@@ -165,9 +189,10 @@ mod test {
 
     #[fuchsia::test]
     async fn test_bp_lookup_machine_mode() {
-        let test_env = TempTestEnv::new().expect("test_env");
-        let mut f =
-            std::fs::File::create(test_env.home.join(PB_MANIFEST_NAME)).expect("file create");
+        let tmp = tempfile::tempdir().unwrap();
+        let path = tmp.path().join(PB_MANIFEST_NAME);
+        let env = setup_test_env(&path).await;
+        let mut f = File::create(&path).expect("file create");
         f.write_all(
             r#"[{
             "name": "fake_name",
@@ -183,10 +208,11 @@ mod test {
         let tool = PbLookupTool {
             cmd: LookupCommand {
                 auth: AuthFlowChoice::Default,
-                base_url: Some(format!("file:{}", test_env.home.display())),
+                base_url: Some(format!("file:{}", tmp.path().display())),
                 name: "fake_name".into(),
                 version: "fake_version".into(),
             },
+            context: env.context.clone(),
         };
 
         tool.main(writer).await.expect("testing lookup");
