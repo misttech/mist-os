9a48a27bc6cffc896621dabfe1c94b4eb12e6ed0
James Robinson <jamesr@google.com>
2024-10-25 00:05:13 +0000

    [tee] Add binder-proxy component hosting rpcbinder server
    This adds a binder-proxy component which hosts a binder rpc server on
    port 5680 implementing the IAccessorMicrofuchsia protocol. In this
    change the protocol definition is empty and only base binder operations
    like "ping" are supported.
    
    Bug: 369435662
    Change-Id: I11512afe69a4442669a2b65a2a676f060da381fc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1126628
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Fuchsia-Auto-Submit: James Robinson <jamesr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Adam Perry <adamperry@google.com>

--- src/tee/binder-proxy/README.md
+++ src/tee/binder-proxy/README.md
@@ -1,5 +1,6 @@
 # binder-proxy
 
 This component will be the Binder RPC proxy between the TEE and external clients
-including the host VM and (one day) other VMs. The implementation is stubbed out
-currently.
+including the host VM and (one day) other VMs. The proxy exposes the
+Binder RPC protocol IAccessorMicrofuchsia over virtio-socket on the port
+specified in the binder definition (currently 5680).
---
+++ src/tee/binder-proxy/tests/binder_proxy_host_tool.cc
@@ -0,0 +1,92 @@
+// Copyright 2024 The Fuchsia Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/syslog/cpp/macros.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+
+#include <android/system/microfuchsia/vm_service/IAccessorMicrofuchsia.h>
+#include <binder/Binder.h>
+#include <binder/RpcSession.h>
+#include <binder/unique_fd.h>
+
+#include "src/lib/fxl/command_line.h"
+#include "src/lib/fxl/log_settings_command_line.h"
+
+namespace {
+
+using IAccessorMicrofuchsia = android::system::microfuchsia::vm_service::IAccessorMicrofuchsia;
+
+int BinderProxyHostTool() {
+  constexpr int kLoopbackPort = 5680;
+
+  auto session = android::RpcSession::make();
+  session->setMaxIncomingThreads(1);
+
+  auto status = session->setupPreconnectedClient({}, [] {
+    FX_LOGS(TRACE) << "preconnected client callback invoked";
+    auto socket_fd = android::binder::unique_fd(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0));
+    if (!socket_fd.ok()) {
+      FX_LOGS(ERROR) << "socket failed: " << errno << ": " << strerror(errno);
+      return android::binder::unique_fd();
+    }
+
+    int no_delay = 1;
+    int rv = setsockopt(socket_fd.get(), IPPROTO_TCP, TCP_NODELAY, &no_delay, sizeof(no_delay));
+    if (rv < 0) {
+      FX_LOGS(ERROR) << "setsockopt failed: " << errno << ": " << strerror(errno);
+      return android::binder::unique_fd();
+    }
+
+    sockaddr_in loopback_addr{
+        .sin_family = AF_INET,
+        .sin_port = htons(kLoopbackPort),
+        .sin_addr =
+            {
+                .s_addr = htonl(INADDR_LOOPBACK),
+            },
+    };
+
+    for (int tries = 0; tries < 5; ++tries) {
+      FX_LOGS(TRACE) << "connecting";
+      rv = connect(socket_fd.get(), reinterpret_cast<struct sockaddr*>(&loopback_addr),
+                   sizeof(loopback_addr));
+      if (rv == 0) {
+        break;
+      }
+      // If we can't connect, sleep for a bit to let the target port come up.
+      sleep(2);
+    }
+    if (rv != 0) {
+      FX_LOGS(ERROR) << "Could not connect: " << errno << ": " << strerror(errno);
+      return android::binder::unique_fd();
+    }
+
+    return socket_fd;
+  });
+
+  if (status != android::OK) {
+    FX_LOGS(ERROR) << "setupPreconnectedClient failed: " << android::statusToString(status);
+    return EXIT_FAILURE;
+  }
+
+  auto root_object = session->getRootObject();
+  status = root_object->pingBinder();
+  if (status != android::OK) {
+    FX_LOGS(ERROR) << "pingBinder failed: " << android::statusToString(status);
+    return EXIT_FAILURE;
+  }
+
+  FX_LOGS(INFO) << "binder ping received";
+
+  return EXIT_SUCCESS;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  fxl::SetLogSettingsFromCommandLine(fxl::CommandLineFromArgcArgv(argc, argv));
+  return BinderProxyHostTool();
+}
