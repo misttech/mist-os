c4c8e8380d0ec90d2bb4c656e8a0e843b26ed2fd
Charles Celerier <chcl@google.com>
2024-10-28 15:16:46 +0000

    [fuchsia-controller] Change example.py to print info for provided IPs
    The provided example.py now instead accepts any number of target IPs and
    prints the information for each.
    
    Test: Ran the commands provided in the README.md
    Bug: 356185684
    Change-Id: If4101aefb2db7da753d3f60e37999f1516b75b25
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1144113
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

---
+++ src/developer/ffx/lib/fuchsia-controller/README.md
@@ -0,0 +1,24 @@
+# Fuchsia Controller
+
+The fuchsia-controller Python library is a host library for interacting with
+target devices from a host.
+
+## Example
+
+The [example.py](/src/developer/ffx/lib/fuchsia-controller/python/example.py)
+program illustrates how to get information from a target with
+fuchsia-controller. You can build and run the program with the following
+commands:
+
+```sh
+fx set --auto-dir minimal.x64 --with-host "//src/developer/ffx/lib/fuchsia-controller:example"
+fx build
+ffx emu start -H --net user --name 'emulator-a'
+ffx emu start -H --net user --name 'emulator-b'
+ffx emu start -H --net user --name 'emulator-c'
+ffx target list
+fx run-in-build host_x64/obj/src/developer/ffx/lib/fuchsia-controller/example.pyz \
+  '127.0.0.1:<emulator-a-port>' \
+  '127.0.0.1:<emulator-b-port>' \
+  '127.0.0.1:<emulator-c-port>'
+```
--- src/developer/ffx/lib/fuchsia-controller/python/example.py
+++ src/developer/ffx/lib/fuchsia-controller/python/example.py
@@ -1,71 +1,44 @@
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import argparse
 import asyncio
 import logging
+import pprint
 
-import fidl.fuchsia_developer_ffx as fd_ffx
-from fuchsia_controller_py import Context, IsolateDir
+import fidl.fuchsia_developer_remotecontrol as fd_rcs
+from fuchsia_controller_py import Context
 
 logging.basicConfig(level=logging.DEBUG)
 
+USAGE = "This script prints the result of RemoteControl.IdentifyHost on Fuchsia targets."
 
-async def echo() -> None:
-    ctx = Context()
-    echo_proxy = fd_ffx.Echo.Client(
-        ctx.connect_daemon_protocol(fd_ffx.Echo.MARKER)
+
+async def main() -> None:
+    params = argparse.ArgumentParser(usage=USAGE)
+    params.add_argument(
+        "target_ips",
+        help="Fuchsia target IP addresses",
+        nargs="+",
     )
-    result = await echo_proxy.echo_string(value="foobar")
-    print(f"Echo Result: {result}")
+    args = params.parse_args()
 
+    # Return the result of RemoteControl.IdentifyHost on a Fuchsia target.
+    async def identify_host(target_ip: str) -> str:
+        ctx = Context(
+            config={"sdk.root": "."},
+            target=target_ip,
+        )
+        remote_control_proxy = ctx.connect_remote_control_proxy()
+        proxy = fd_rcs.RemoteControl.Client(remote_control_proxy)
+        return await proxy.identify_host()
 
-async def target_info_multi_target_isolated() -> None:
-    isolate = IsolateDir()  # Will create a random tmpdir
-    ctx = Context(
-        config={"sdk.root": "."}, isolate_dir=isolate, target="fuchsia-emulator"
-    )
-    ctx2 = Context(
-        config={"sdk.root": "."}, isolate_dir=isolate, target="emu-two"
-    )
-    ch = ctx.connect_target_proxy()
-    proxy = fd_ffx.Target.Client(ch)
-    proxy2 = fd_ffx.Target.Client(ctx2.connect_target_proxy())
-    result2 = proxy2.identity()
-    result1 = proxy.identity()
-    results = await asyncio.gather(*[result1, result2])
-    print(f"Target Info Received: {results}")
+    # Asynchronously await each RemoteControl.IdentifyHost call.
+    results = await asyncio.gather(*map(identify_host, args.target_ips))
 
-
-async def multi_echo() -> None:
-    ctx = Context()
-    echo_proxy = fd_ffx.Echo.Client(
-        ctx.connect_daemon_protocol(fd_ffx.Echo.MARKER)
-    )
-    result1 = echo_proxy.echo_string(value="1foobington")
-    result2 = echo_proxy.echo_string(value="2frobination")
-    result3 = echo_proxy.echo_string(value="3frobinationator")
-    result4 = echo_proxy.echo_string(value="4frobinationatorawoihoiwf")
-    result5 = echo_proxy.echo_string(value="5frobin")
-    results = await asyncio.gather(result1, result2, result3, result4, result5)
-    print(f"Multi-echo results: {results}")
-
-
-async def async_main() -> None:
-    await echo()
-    await multi_echo()
-    await target_info_multi_target_isolated()
-
-
-def main() -> None:
-    print("Testing asynchronous calls.")
-    asyncio.run(async_main())
-    for x in range(5):
-        print()
-        print(f"Testing synchronous calls, iteration {x}.")
-        asyncio.run(echo())
-        asyncio.run(multi_echo())
-        asyncio.run(target_info_multi_target_isolated())
+    print("Target Info Received:")
+    pprint.pp(results)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
