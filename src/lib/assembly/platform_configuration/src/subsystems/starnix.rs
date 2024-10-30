377c2a30ce0fe4fb4dcc8ed7f2f5eba689711efd
Suraj Malhotra <surajmalhotra@google.com>
2024-10-30 01:11:27 +0000

    [starnix][sockettunnel] Add a broker for the socketunnel service
    This broker provides a simple policy to pluck out the device protocol
    from the first service instance that enumerates. This allows an eventual Starnix module to connect to this Service as a Protocol, simplifying the code.
    
    Additionally, define a "nanohub_support" assembly target that can be used to control the inclusion of the service broker + Starnix enablement flags.
    
    Follow-up assembly ticket: b/376318780
    
    Change-Id: Iee23d66e3fb47a9c12610d859c2b142cbbd1e235
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1137955
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Eric Rahm <erahm@google.com>
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/platform_configuration/src/subsystems/starnix.rs
+++ src/lib/assembly/platform_configuration/src/subsystems/starnix.rs
@@ -40,6 +40,7 @@ impl DefineSubsystemConfiguration<PlatformStarnixConfig> for StarnixSubsystem {
                     ),
                 )?;
                 builder.platform_bundle("adb_support");
+                builder.platform_bundle("nanohub_support");
             } else {
                 builder.set_config_capability(
                     "fuchsia.starnix.runner.EnableDataCollection",
