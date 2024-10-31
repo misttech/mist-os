24f2fd3461b77a52ab3c2b1f79879227f3764894
Bruno Herrera <bruno.herrera@mist-os.com>
2024-10-31 15:19:16 -0300

    [mistos][starnix] Initial module support
    Change-Id: I935691bb4141c88ec1064cfe23aad45aa250548a

---
+++ vendor/misttech/zircon/kernel/lib/starnix/modules/modules.cc
@@ -0,0 +1,19 @@
+// Copyright 2024 Mist Tecnologia LTDA. All rights reserved.
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "lib/starnix/modules/modules.h"
+
+#include <lib/mistos/starnix/kernel/task/current_task.h>
+#include <lib/mistos/starnix/kernel/task/kernel.h>
+
+#include <fbl/ref_ptr.h>
+
+namespace starnix_modules {
+
+void init_common_devices(const starnix::CurrentTask& system_task) {}
+
+void register_common_file_systems(const fbl::RefPtr<starnix::Kernel>& kernel) {}
+
+}  // namespace starnix_modules
