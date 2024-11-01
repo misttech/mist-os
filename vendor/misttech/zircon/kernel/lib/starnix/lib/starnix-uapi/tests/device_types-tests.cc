a2f1ee89de3935ca9d661ebb301b64485b3ba9a9
Bruno Herrera <bruno.herrera@mist-os.com>
2024-11-01 10:49:22 -0300

    fixup! [mistos][starnix-uapi] Update DeviceType from rust

--- vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-uapi/tests/device_types-tests.cc
+++ vendor/misttech/zircon/kernel/lib/starnix/lib/starnix-uapi/tests/device_types-tests.cc
@@ -4,21 +4,31 @@
 // found in the LICENSE file.
 
 #include <lib/mistos/starnix_uapi/device_type.h>
+#include <lib/unittest/unittest.h>
 
-#include <zxtest/zxtest.h>
-
-using namespace starnix_uapi;
+namespace unit_testing {
 
 namespace {
 
-TEST(DeviceType, test_device_type) {
+using starnix_uapi::DeviceType;
+
+bool test_device_type() {
+  BEGIN_TEST;
+
   auto dev = DeviceType::New(21, 17);
-  ASSERT_EQ(dev.major(), 21);
-  ASSERT_EQ(dev.minor(), 17);
+  ASSERT_EQ(21u, dev.major());
+  ASSERT_EQ(17u, dev.minor());
 
   dev = DeviceType::New(0x83af83fe, 0xf98ecba1);
-  ASSERT_EQ(dev.major(), 0x83af83fe);
-  ASSERT_EQ(dev.minor(), 0xf98ecba1);
-}
+  ASSERT_EQ(0x83af83fe, dev.major());
+  ASSERT_EQ(0xf98ecba1, dev.minor());
 
+  END_TEST;
+}
 }  // namespace
+
+}  // namespace unit_testing
+
+UNITTEST_START_TESTCASE(starnix_uapi_devicetype)
+UNITTEST("test device type", unit_testing::test_device_type)
+UNITTEST_END_TESTCASE(starnix_uapi_devicetype, "starnix_uapi_devicetype", "Tests Device Type")
