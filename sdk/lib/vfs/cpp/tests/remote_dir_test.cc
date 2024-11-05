d40361c9dffdee51695a2e70cc4b746edd0571f1
Rui Qi Sim <rqsim@google.com>
2024-11-05 23:28:01 +0000

    [sdk][vfs] Fix incorrect use of POSIX O_* flags with fdio function
    The `fdio_open*(.., flags, ..)` functions expects fuchsia.io Flags. Fix
    a case where POSIX O_*flags were used.
    
    Also, switch over to using FDIO open3.
    
    Bug: 376575307
    Change-Id: I54308f107b7d80eb5d96b8c567c7ee7e532835f9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1151135
    Reviewed-by: Brandon Castellano <bcastell@google.com>
    Commit-Queue: Rui Qi Sim <rqsim@google.com>

--- sdk/lib/vfs/cpp/tests/remote_dir_test.cc
+++ sdk/lib/vfs/cpp/tests/remote_dir_test.cc
@@ -78,11 +78,15 @@ TEST_F(RemoteDirTest, Open) {
     auto root = open_root_fd();
     // We should be able to cross the remote mount point and open the file as read-only.
     fbl::unique_fd file;
-    zx_status_t status =
-        fdio_open_fd_at(root.get(), "remote_dir/file", O_RDONLY, file.reset_and_get_address());
+    zx_status_t status = fdio_open3_fd_at(root.get(), "remote_dir/file",
+                                          static_cast<uint64_t>(fuchsia_io::Flags::kPermRead),
+                                          file.reset_and_get_address());
     ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
     // We should not be able to open the file as writable since the remote only has read access.
-    status = fdio_open_fd_at(root.get(), "remote_dir/file", O_RDWR, file.reset_and_get_address());
+    status = fdio_open3_fd_at(
+        root.get(), "remote_dir/file",
+        static_cast<uint64_t>(fuchsia_io::Flags::kPermRead | fuchsia_io::Flags::kPermWrite),
+        file.reset_and_get_address());
     ASSERT_EQ(status, ZX_ERR_ACCESS_DENIED) << zx_status_get_string(status);
   });
 }
