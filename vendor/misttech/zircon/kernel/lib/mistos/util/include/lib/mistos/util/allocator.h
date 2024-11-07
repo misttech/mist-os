faf97b19076a51b0ad4065a617ad59a0f9ffb841
Bruno Herrera <bruno.herrera@mist-os.com>
2024-11-07 08:56:41 -0300

    [mistos][util] Refactor allocator to use fbl::AllocChecker and add comparison operators
    Change-Id: I1d35bd1d99f1b6c87b72825afa2dbccec4aa517f

--- vendor/misttech/zircon/kernel/lib/mistos/util/include/lib/mistos/util/allocator.h
+++ vendor/misttech/zircon/kernel/lib/mistos/util/include/lib/mistos/util/allocator.h
@@ -5,10 +5,11 @@
 #ifndef VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_ALLOCATOR_H_
 #define VENDOR_MISTTECH_ZIRCON_KERNEL_LIB_MISTOS_UTIL_INCLUDE_LIB_MISTOS_UTIL_ALLOCATOR_H_
 
-#include <lib/heap.h>
 #include <zircon/assert.h>
 
 #include <cstddef>
+
+#include <fbl/alloc_checker.h>
 
 namespace util {
 
@@ -16,21 +17,39 @@ template <typename T>
 struct Allocator {
   using value_type = T;
 
-  Allocator() noexcept = default;
+  Allocator() = default;
 
+  // Converting constructor
   template <class U>
-  Allocator(const Allocator<U>&) noexcept {}
+  Allocator(const Allocator<U>&) {}
 
+  // Allocate memory
   T* allocate(std::size_t n) {
-    if (n > std::size_t(-1) / sizeof(T)) {
+    if (n > static_cast<std::size_t>(-1) / sizeof(T)) {
       ZX_PANIC("allocate");
     }
-    auto p = static_cast<T*>(malloc(n * sizeof(T)));
-    ASSERT(p != nullptr);
-    return p;
+
+    fbl::AllocChecker ac;
+    void* raw = new (&ac) char[n * sizeof(T)];
+    if (!ac.check()) {
+      ZX_PANIC("failed to allocate memory");
+    }
+    return static_cast<T*>(raw);
   }
 
-  void deallocate(T* p, std::size_t) noexcept { free(p); }
+  // Deallocate memory
+  void deallocate(T* p, std::size_t) noexcept { delete[] reinterpret_cast<char*>(p); }
+
+  // Equality comparison for compatibility with std::allocator_traits
+  template <typename U>
+  bool operator==(const Allocator<U>&) const noexcept {
+    return true;
+  }
+
+  template <typename U>
+  bool operator!=(const Allocator<U>& other) const noexcept {
+    return false;
+  }
 };
 
 }  // namespace util
