0b7af4e1c99508a2ebced6313d61c3c88a1764aa
Adam Perry <adamperry@google.com>
2024-11-08 12:14:56 +0000

    [symbolizer] More debugging logs.
    Bug: 359658592
    Change-Id: Ie284f10ea4f9582d4869ee91b22e7ab3bbd5b400
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1153345
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Reviewed-by: Jacob Rutherford <jruthe@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- tools/symbolizer/symbolizer_impl.cc
+++ tools/symbolizer/symbolizer_impl.cc
@@ -50,6 +50,7 @@ void SetupCommandLineOptions(const CommandLineOptions& options, zxdb::MapSetting
 
   const char* symbol_index_from_env = getenv("SYMBOL_INDEX_INCLUDE");
   if (symbol_index_from_env) {
+    FX_LOGS(DEBUG) << "Setting symbol index contents from environment variable.";
     settings.SetList(Settings::System::kSymbolIndexInclude, {symbol_index_from_env});
   }
 
@@ -330,6 +331,7 @@ void SymbolizerImpl::MMap(uint64_t address, uint64_t size, uint64_t module_id,
 }
 
 void SymbolizerImpl::Backtrace(uint64_t address, AddressType type, LocationOutputFn output) {
+  FX_LOGS(TRACE) << "Symbolizing backtrace for address 0x" << std::hex << address;
   InitProcess();
   analytics_builder_.IncreaseNumberOfFrames();
 
@@ -355,6 +357,7 @@ void SymbolizerImpl::Backtrace(uint64_t address, AddressType type, LocationOutpu
   }
 
   if (!module) {
+    FX_LOGS(TRACE) << "Could not find matching module.";
     analytics_builder_.IncreaseNumberOfFramesInvalid();
     analytics_builder_.TotalTimerStop();
     return;
@@ -380,11 +383,15 @@ void SymbolizerImpl::Backtrace(uint64_t address, AddressType type, LocationOutpu
   previous_modules_.clear();
 
   bool symbolized = false;
+  FX_LOGS(TRACE) << "Have " << stack.size() << " frames for address 0x" << std::hex << address;
   for (size_t i = 0; i < stack.size(); i++) {
     // Function name.
     const zxdb::Location location = stack[i]->GetLocation();
     if (location.symbol().is_valid()) {
       symbolized = true;
+    } else {
+      FX_LOGS(WARNING) << "Invalid symbol for stack frame " << i << " of address 0x" << std::hex
+                       << address;
     }
 
     output(stack.size() - i - 1, location, *module);
@@ -393,7 +400,10 @@ void SymbolizerImpl::Backtrace(uint64_t address, AddressType type, LocationOutpu
   // One physical frame could be symbolized to multiple inlined frames. We're only counting the
   // number of physical frames symbolized.
   if (symbolized) {
+    FX_LOGS(TRACE) << "Successfully symbolized address 0x" << std::hex << address;
     analytics_builder_.IncreaseNumberOfFramesSymbolized();
+  } else {
+    FX_LOGS(WARNING) << "No stack frames symbolized for address 0x" << std::hex << address;
   }
   analytics_builder_.TotalTimerStop();
 }
@@ -549,6 +559,7 @@ void SymbolizerImpl::DumpFile(std::string_view type, std::string_view name) {
 }
 
 void SymbolizerImpl::OnDownloadsStarted() {
+  FX_LOGS(DEBUG) << "Starting downloads";
   if (remote_symbol_lookup_enabled_) {
     analytics_builder_.DownloadTimerStart();
   }
@@ -556,6 +567,7 @@ void SymbolizerImpl::OnDownloadsStarted() {
 }
 
 void SymbolizerImpl::OnDownloadsStopped(size_t num_succeeded, size_t num_failed) {
+  FX_LOGS(DEBUG) << "Downloads stopped";
   // Even if no symbol server is configured, this function could still be invoked but all
   // downloadings will be failed.
   if (remote_symbol_lookup_enabled_) {
@@ -568,13 +580,16 @@ void SymbolizerImpl::OnDownloadsStopped(size_t num_succeeded, size_t num_failed)
 }
 
 void SymbolizerImpl::DidCreateSymbolServer(zxdb::SymbolServer* server) {
+  FX_LOGS(DEBUG) << "Created symbol server " << server->name();
   if (server->state() == zxdb::SymbolServer::State::kInitializing ||
       server->state() == zxdb::SymbolServer::State::kBusy) {
     waiting_auth_ = true;
   }
 }
 
-void SymbolizerImpl::OnSymbolServerStatusChanged(zxdb::SymbolServer* unused_server) {
+void SymbolizerImpl::OnSymbolServerStatusChanged(zxdb::SymbolServer* server) {
+  FX_LOGS(DEBUG) << "Symbol server " << server->name() << " status changed to "
+                 << zxdb::ServerStateToString(server->state()) << ".";
   if (!waiting_auth_) {
     return;
   }
