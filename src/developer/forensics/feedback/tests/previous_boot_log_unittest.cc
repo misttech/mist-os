4d57c238bd5e97d764113c3c33512abee4e47bd7
Thomas Gales <tgales@google.com>
2024-11-08 02:15:45 +0000

    [feedback] Lazily delete previous boot logs
    Once the monotonic clock pauses, it's possible for the previous boot
    logs to stick around longer than intended because async work is
    scheduled based on the monotonic clock.
    
    This CL adds lazy deletion based on the boot clock. Whenever the
    previous boot logs are requested, Feedback will first check if more than
    24 hours of uptime has passed and if so will delete the previous boot
    logs.
    
    Feedback will still also proactively delete the previous boot logs after
    24 hours of runtime to save space.
    
    Bug: 360946313
    Tested: `fx test //src/developer/forensics`
    Tested: Temporarily changed deletion period to 4 minutes, observed
    proactive deletion @ 8d72854b18feed4d
    Tested: Temporarily commented out proactive deletion code and changed
    deletion period to 5 minutes, observed lazy deletion @ cef5874415719fbc
    
    Change-Id: Ibf355bde50b124bf5da0cb34c927552e1a0b34a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1151692
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Commit-Queue: Thomas Gales <tgales@google.com>

--- src/developer/forensics/feedback/tests/previous_boot_log_unittest.cc
+++ src/developer/forensics/feedback/tests/previous_boot_log_unittest.cc
@@ -7,16 +7,14 @@
 #include <lib/async/cpp/executor.h>
 
 #include "src/developer/forensics/feedback/attachments/types.h"
-#include "src/developer/forensics/testing/gmatchers.h"
 #include "src/developer/forensics/testing/gpretty_printers.h"
 #include "src/developer/forensics/testing/unit_test_fixture.h"
 #include "src/developer/forensics/utils/errors.h"
-#include "src/lib/files/directory.h"
 #include "src/lib/files/file.h"
-#include "src/lib/files/path.h"
 #include "src/lib/files/scoped_temp_dir.h"
 #include "src/lib/timekeeper/async_test_clock.h"
 #include "src/lib/timekeeper/clock.h"
+#include "src/lib/timekeeper/test_clock.h"
 
 namespace forensics::feedback {
 
@@ -109,6 +107,44 @@ TEST_F(PreviousBootLogTest, NoPreviousBootLog) {
   RunLoopFor(zx::hour(25));
 
   EXPECT_TRUE(files::IsFile(path));
+}
+
+TEST_F(PreviousBootLogTest, LazilyDeleted) {
+  const uint64_t kTicket = 21;
+  const std::string path = NewFile();
+  files::WriteFile(path, "test data");
+
+  timekeeper::TestClock clock;
+  PreviousBootLog previous_boot_log_(dispatcher(), &clock, zx::sec(5), path);
+  EXPECT_TRUE(files::IsFile(path));
+
+  std::optional<AttachmentValue> result = std::nullopt;
+  GetExecutor().schedule_task(previous_boot_log_.Get(kTicket)
+                                  .and_then([&result](AttachmentValue& promise_result) mutable {
+                                    result = std::move(promise_result);
+                                  })
+                                  .or_else([] { FX_LOGS(FATAL) << "Logic error"; }));
+  RunLoopUntilIdle();
+
+  ASSERT_TRUE(result.has_value());
+  ASSERT_TRUE(result->HasValue());
+  EXPECT_TRUE(files::IsFile(path));
+
+  clock.SetBoot(clock.BootNow() + zx::sec(5));
+  result = std::nullopt;
+  GetExecutor().schedule_task(previous_boot_log_.Get(kTicket)
+                                  .and_then([&result](AttachmentValue& promise_result) {
+                                    result = std::move(promise_result);
+                                  })
+                                  .or_else([] { FX_LOGS(FATAL) << "Logic error"; }));
+  RunLoopUntilIdle();
+
+  ASSERT_TRUE(result.has_value());
+  ASSERT_TRUE(result->HasError());
+  EXPECT_EQ(result->Error(), Error::kCustom);
+
+  // Check that the file is deleted after 5 seconds.
+  EXPECT_FALSE(files::IsFile(path));
 }
 
 }  // namespace forensics::feedback
