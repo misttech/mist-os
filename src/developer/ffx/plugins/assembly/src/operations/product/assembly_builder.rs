feb593a2895c6ab00aeedb0735ae3322d8dd0ee7
John Wittrock <wittrock@google.com>
2024-11-12 16:49:11 +0000

    [assembly] Compiled packages cannot be dups
    Cleans up after b/361426282
    
    Bug: 361426282
    Test: existing tests
    Change-Id: I417923c4294842d269feacf102fdc364a96eb922
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1152099
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Commit-Queue: John Wittrock <wittrock@google.com>

--- src/developer/ffx/plugins/assembly/src/operations/product/assembly_builder.rs
+++ src/developer/ffx/plugins/assembly/src/operations/product/assembly_builder.rs
@@ -823,20 +823,7 @@ impl ImageAssemblyConfigBuilder {
                         package_name.clone(),
                     ));
                     let (_d, package_entry) =
-                        PackageEntry::parse_from(PackageOrigin::AIB, PackageSet::Base, p.clone())?;
-
-                    // TODO(b/361426282): remove this block once we've migrated tee_manager
-                    if let Ok((board_origin_destination, _)) =
-                        PackageEntry::parse_from(PackageOrigin::Board, PackageSet::Base, &p)
-                    {
-                        if let Some(PackageSetDestination::Blob(PackageDestination::FromBoard(_))) =
-                            packages.existing_key(board_origin_destination)
-                        {
-                            // We only want to continue if this package exists and it was added by the
-                            // _board_ previously.
-                            continue;
-                        }
-                    }
+                        PackageEntry::parse_from(PackageOrigin::AIB, PackageSet::Base, p)?;
 
                     packages
                         .try_insert_unique(d, package_entry)
