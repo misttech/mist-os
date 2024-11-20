d886ceb413b002ab95b230692faba7b6c3beab86
Jiaming Li <lijiaming@google.com>
2024-11-20 22:00:54 +0000

    [kernel][assembly] Move kernel.arm64.event-stream.enable to board configuration
    Bug: 370241090
    Change-Id: Iff00d3307dba0090de6522cdee10af37d635aae2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1159336
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Ali Zhang <alizhang@google.com>
    Commit-Queue: Jiaming Li <lijiaming@google.com>

--- src/lib/assembly/config_schema/src/board_config.rs
+++ src/lib/assembly/config_schema/src/board_config.rs
@@ -206,6 +206,12 @@ pub struct BoardKernelConfig {
     #[serde(default)]
     pub quiet_early_boot: bool,
 
+    /// When enabled, each ARM cpu will enable an event stream generator, which
+    /// per-cpu sets the hidden event flag at a particular rate. This has the
+    /// effect of kicking cpus out of any WFE states they may be sitting in.
+    #[serde(default = "arm64_event_stream_enable_default")]
+    pub arm64_event_stream_enable: bool,
+
     /// This controls what serial port is used.  If provided, it overrides the
     /// serial port described by the system's bootdata.  The kernel debug serial
     /// port is a reserved resource and may not be used outside of the kernel.
@@ -229,6 +235,10 @@ pub struct BoardKernelConfig {
     /// OOM related configurations.
     #[serde(default)]
     pub oom: Option<OOM>,
+}
+
+fn arm64_event_stream_enable_default() -> bool {
+    true
 }
 
 /// This struct defines supported Out of memory features.
@@ -414,6 +424,7 @@ mod test {
             "kernel": {
                 "contiguous_physical_pages": true,
                 "scheduler_prefer_little_cpus": true,
+                "arm64_event_stream_enable": false,
             },
             "platform": {
                 "development_support": {
@@ -447,6 +458,7 @@ mod test {
                 scheduler_prefer_little_cpus: true,
                 halt_on_panic: false,
                 oom: None,
+                arm64_event_stream_enable: false,
             },
             platform: PlatformConfig {
                 connectivity: ConnectivityConfig::default(),
