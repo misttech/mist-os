084845017b79a7580c368ca17a64b7836a352683
Chris Phoenix <cphoenix@google.com>
2024-11-21 06:48:43 +0000

    [trace] Add trace_provider_create_with_service_rust()
    Rust programs today let the tracing library use FDIO to find the
    trace provider service.
    Component Manager needs to get the service itself, and pass it to
    the tracing library.
    
    Bug: 379766709
    Change-Id: I49d78319e8fea677a515598a3323c708c835ebee
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1159134
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Chris Phoenix <cphoenix@google.com>

--- src/lib/trace/rust/trace-provider/trace_provider.cc
+++ src/lib/trace/rust/trace-provider/trace_provider.cc
@@ -17,6 +17,8 @@ constexpr char PROVIDER_THREAD_NAME[] = "tracing:provider";
 __BEGIN_CDECLS
 
 void trace_provider_create_with_fdio_rust() __attribute__((visibility("default")));
+void trace_provider_create_with_service_rust(zx_handle_t to_service_h)
+    __attribute__((visibility("default")));
 void trace_provider_wait_for_init() __attribute__((visibility("default")));
 
 __END_CDECLS
@@ -32,26 +34,47 @@ namespace {
 // trace-provider implementation.
 //
 // Shouldn't be calling this more than once during the lifetime of your program.
-void trace_provider_with_fdio_thread_entry() {
-  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
-  std::unique_ptr<trace::TraceProviderWithFdio> trace_provider;
-  bool result = trace::TraceProviderWithFdio::CreateSynchronously(loop.dispatcher(), nullptr,
-                                                                  &trace_provider, nullptr);
 
-  if (result && trace_provider && trace_provider->is_valid()) {
-    // Make sure the provider has a chance to acknowledge any already-running traces.
-    loop.RunUntilIdle();
+// Given a loop containing a newly created valid trace provider, start it running.
+static void run_trace_provider(async::Loop &loop) {
+  // Make sure the provider has a chance to acknowledge any already-running traces.
+  loop.RunUntilIdle();
 
-    // Tell the spawning thread that the provider has been fully initialized.
-    provider_initialized.count_down();
-
-    // Continue running the loop to handle future trace start/stop messages.
-    loop.Run();
-  }
-
-  // If the initialization failed, unblock waiters regardless.
+  // Tell the spawning thread that the provider has been fully initialized.
   provider_initialized.count_down();
+
+  // Continue running the loop to handle future trace start/stop messages.
+  loop.Run();
 }
+
+static void trace_provider_with_service_thread_entry(zx_handle_t to_service_h) {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+  std::unique_ptr<trace::TraceProvider> trace_provider = nullptr;
+  bool result = trace::TraceProvider::CreateSynchronously(
+      zx::channel{to_service_h}, loop.dispatcher(), nullptr, &trace_provider, nullptr);
+  bool is_valid = result && trace_provider && trace_provider->is_valid();
+  if (is_valid) {
+    run_trace_provider(loop);
+  } else {
+    // If the initialization failed, unblock waiters regardless.
+    provider_initialized.count_down();
+  }
+}
+
+static void trace_provider_with_fdio_thread_entry() {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+  std::unique_ptr<trace::TraceProviderWithFdio> trace_provider_with_fdio = nullptr;
+  bool result = trace::TraceProviderWithFdio::CreateSynchronously(
+      loop.dispatcher(), nullptr, &trace_provider_with_fdio, nullptr);
+  bool is_valid = result && trace_provider_with_fdio && trace_provider_with_fdio->is_valid();
+  if (is_valid) {
+    run_trace_provider(loop);
+  } else {
+    // If the initialization failed, unblock waiters regardless.
+    provider_initialized.count_down();
+  }
+}
+
 }  // namespace
 
 // Calling this function multiple times is idempotent, to ensure that resources
@@ -59,6 +82,18 @@ void trace_provider_with_fdio_thread_entry() {
 void trace_provider_create_with_fdio_rust() {
   std::call_once(init_once, [] {
     std::thread thread(trace_provider_with_fdio_thread_entry);
+    zx_handle_t thread_handle = native_thread_get_zx_handle(thread.native_handle());
+    zx_object_set_property(thread_handle, ZX_PROP_NAME, PROVIDER_THREAD_NAME,
+                           sizeof(PROVIDER_THREAD_NAME));
+    thread.detach();
+  });
+}
+
+// Calling this function multiple times is idempotent, to ensure that resources
+// for the trace provider are created only once.
+void trace_provider_create_with_service_rust(zx_handle_t to_service_h) {
+  std::call_once(init_once, [to_service_h] {
+    std::thread thread(trace_provider_with_service_thread_entry, to_service_h);
     zx_handle_t thread_handle = native_thread_get_zx_handle(thread.native_handle());
     zx_object_set_property(thread_handle, ZX_PROP_NAME, PROVIDER_THREAD_NAME,
                            sizeof(PROVIDER_THREAD_NAME));
