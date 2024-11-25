8acacc92bc31ce75aca6663538e969340c736b88
David Gilhooley <dgilhooley@google.com>
2024-11-25 16:55:47 +0000

    [magma][platform] Create PlatformDeviceDfv2
    This is a DFv2 version of the platform device which makes FIDL
    calls instead of banjo.
    
    Use this in the msd-arm-mali driver first. It will also be used
    in the msd-vsi-vip once it migrates to DFv2.
    
    This should not have any functional changes to the msd-arm-mali driver.
    
    Bug: b/301005872
    Change-Id: I21c70fa4ae72bcefd834fe21cf1e6f0d0334692d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1158115
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/graphics/magma/lib/magma/platform/zircon/zircon_platform_device_dfv2.h
@@ -0,0 +1,40 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_DEVICE_DFV2_H_
+#define SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_DEVICE_DFV2_H_
+
+#include <fidl/fuchsia.hardware.platform.device/cpp/wire.h>
+#include <lib/magma/platform/platform_device.h>
+
+namespace magma {
+
+class ZirconPlatformDeviceDfv2 final : public PlatformDevice {
+ public:
+  explicit ZirconPlatformDeviceDfv2(
+      fidl::WireSyncClient<fuchsia_hardware_platform_device::Device> pdev)
+      : pdev_(std::move(pdev)) {}
+
+  void* GetDeviceHandle() override { return &pdev_; }
+
+  std::unique_ptr<PlatformHandle> GetBusTransactionInitiator() const override;
+
+  std::unique_ptr<PlatformMmio> CpuMapMmio(unsigned int index,
+                                           PlatformMmio::CachePolicy cache_policy) override;
+
+  uint32_t GetMmioCount() const override;
+
+  std::unique_ptr<PlatformBuffer> GetMmioBuffer(unsigned int index) override;
+
+  std::unique_ptr<PlatformInterrupt> RegisterInterrupt(unsigned int index) override;
+
+  fidl::WireSyncClient<fuchsia_hardware_platform_device::Device>& fidl() { return pdev_; }
+
+ private:
+  fidl::WireSyncClient<fuchsia_hardware_platform_device::Device> pdev_;
+};
+
+}  // namespace magma
+
+#endif  // SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_DEVICE_DFV2_H_
