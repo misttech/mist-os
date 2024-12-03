c585411b91930bc60f9a0441547f6eb30de75e39
Nick Eaton <nickeaton@google.com>
2024-12-03 14:58:47 +0000

    [pci-sdhci] Serve sdhci fidl protocol
    Make the pci-sdhci driver serve the fuchsia.hardware.sdhci fidl
    protocol.
    
    Bug: 374152849
    Change-Id: Ic7d0f1a2207af907622cef2e3ca922b1f6ee4f51
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1163252
    Reviewed-by: HanBin Yoon <hanbinyoon@google.com>
    Commit-Queue: Nick Eaton <nickeaton@google.com>

--- src/devices/block/drivers/pci-sdhci/pci-sdhci.cc
+++ src/devices/block/drivers/pci-sdhci/pci-sdhci.cc
@@ -45,6 +45,15 @@ zx_status_t PciSdhci::SdhciGetInterrupt(zx::interrupt* interrupt_out) {
   return status;
 }
 
+void PciSdhci::GetInterrupt(fdf::Arena& arena, GetInterruptCompleter::Sync& completer) {
+  zx::interrupt irq;
+  if (zx_status_t status = SdhciGetInterrupt(&irq); status != ZX_OK) {
+    completer.buffer(arena).ReplyError(status);
+    return;
+  }
+  completer.buffer(arena).ReplySuccess(std::move(irq));
+}
+
 zx_status_t PciSdhci::SdhciGetMmio(zx::vmo* out, zx_off_t* out_offset) {
   if (!mmio_.has_value()) {
     zx_status_t status = pci_.MapMmio(0u, ZX_CACHE_POLICY_UNCACHED_DEVICE, &mmio_);
@@ -57,6 +66,16 @@ zx_status_t PciSdhci::SdhciGetMmio(zx::vmo* out, zx_off_t* out_offset) {
   return mmio_->get_vmo()->duplicate(ZX_RIGHT_SAME_RIGHTS, out);
 }
 
+void PciSdhci::GetMmio(fdf::Arena& arena, GetMmioCompleter::Sync& completer) {
+  zx::vmo vmo;
+  zx_off_t offset;
+  if (zx_status_t status = SdhciGetMmio(&vmo, &offset); status != ZX_OK) {
+    completer.buffer(arena).ReplyError(status);
+    return;
+  }
+  completer.buffer(arena).ReplySuccess(std::move(vmo), offset);
+}
+
 zx_status_t PciSdhci::SdhciGetBti(uint32_t index, zx::bti* out_bti) {
   if (!bti_.is_valid()) {
     zx_status_t st = pci_.GetBti(index, &bti_);
@@ -67,11 +86,31 @@ zx_status_t PciSdhci::SdhciGetBti(uint32_t index, zx::bti* out_bti) {
   return bti_.duplicate(ZX_RIGHT_SAME_RIGHTS, out_bti);
 }
 
+void PciSdhci::GetBti(GetBtiRequestView request, fdf::Arena& arena,
+                      GetBtiCompleter::Sync& completer) {
+  zx::bti bti;
+  if (zx_status_t status = SdhciGetBti(request->index, &bti); status != ZX_OK) {
+    completer.buffer(arena).ReplyError(status);
+    return;
+  }
+  completer.buffer(arena).ReplySuccess(std::move(bti));
+}
+
 uint32_t PciSdhci::SdhciGetBaseClock() { return 0; }
+
+void PciSdhci::GetBaseClock(fdf::Arena& arena, GetBaseClockCompleter::Sync& completer) {
+  completer.buffer(arena).Reply(SdhciGetBaseClock());
+}
 
 uint64_t PciSdhci::SdhciGetQuirks(uint64_t* out_dma_boundary_alignment) {
   *out_dma_boundary_alignment = 0;
   return SDHCI_QUIRK_STRIP_RESPONSE_CRC_PRESERVE_ORDER;
+}
+
+void PciSdhci::GetQuirks(fdf::Arena& arena, GetQuirksCompleter::Sync& completer) {
+  uint64_t dma_boundary_alignment;
+  auto quirks = SdhciGetQuirks(&dma_boundary_alignment);
+  completer.buffer(arena).Reply(quirks, dma_boundary_alignment);
 }
 
 void PciSdhci::SdhciHwReset() {
@@ -89,7 +128,21 @@ void PciSdhci::SdhciHwReset() {
   zx_nanosleep(zx_deadline_after(ZX_USEC(300)));
 }
 
+void PciSdhci::HwReset(fdf::Arena& arena, HwResetCompleter::Sync& completer) {
+  SdhciHwReset();
+  completer.buffer(arena).Reply();
+}
+
 zx_status_t PciSdhci::SdhciVendorSetBusClock(uint32_t frequency_hz) { return ZX_ERR_STOP; }
+
+void PciSdhci::VendorSetBusClock(VendorSetBusClockRequestView request, fdf::Arena& arena,
+                                 VendorSetBusClockCompleter::Sync& completer) {
+  if (zx_status_t status = SdhciVendorSetBusClock(request->frequency_hz)) {
+    completer.buffer(arena).ReplyError(status);
+    return;
+  }
+  completer.buffer(arena).ReplySuccess();
+}
 
 void PciSdhci::DdkUnbind(ddk::UnbindTxn txn) { device_unbind_reply(zxdev()); }
 
@@ -100,27 +153,69 @@ zx_status_t PciSdhci::Bind(void* /* unused */, zx_device_t* parent) {
     return ZX_ERR_NO_MEMORY;
   }
 
-  dev->pci_ = ddk::Pci(parent, "pci");
-  if (!dev->pci_.is_valid()) {
-    zxlogf(ERROR, "%s: could not get PCI protocol", kTag);
-    return ZX_ERR_INTERNAL;
-  }
-
-  zx_status_t status = dev->pci_.SetBusMastering(true);
-  if (status < 0) {
-    zxlogf(ERROR, "%s: error in enable bus master: %s", kTag, zx_status_get_string(status));
-    return status;
-  }
-
-  status = dev->DdkAdd(ddk::DeviceAddArgs("pci-sdhci").set_proto_id(ZX_PROTOCOL_SDHCI));
+  zx_status_t status = dev->Init();
   if (status != ZX_OK) {
-    zxlogf(ERROR, "%s: error adding device: %s", kTag, zx_status_get_string(status));
+    zxlogf(ERROR, "Failed to initialize device: %s", zx_status_get_string(status));
     return status;
   }
 
   // The object is owned by the DDK, now that it has been added. It will be deleted
   // when the device is released.
   [[maybe_unused]] auto ptr = dev.release();
+
+  return ZX_OK;
+}
+
+zx_status_t PciSdhci::Init() {
+  pci_ = ddk::Pci(parent_, "pci");
+  if (!pci_.is_valid()) {
+    zxlogf(ERROR, "Failed to connect to PCI protocol");
+    return ZX_ERR_INTERNAL;
+  }
+
+  zx_status_t status = pci_.SetBusMastering(true);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failed to set bus mastering: %s", zx_status_get_string(status));
+    return status;
+  }
+
+  {
+    fuchsia_hardware_sdhci::Service::InstanceHandler handler(
+        {.device = bindings_.CreateHandler(this, fdf::Dispatcher::GetCurrent()->get(),
+                                           fidl::kIgnoreBindingClosure)});
+
+    zx::result result = outgoing_.AddService<fuchsia_hardware_sdhci::Service>(std::move(handler));
+    if (result.is_error()) {
+      zxlogf(ERROR, "Failed to add sdhci fidl service to outgoing directory: %s",
+             result.status_string());
+      return result.error_value();
+    }
+  }
+
+  auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+  if (endpoints.is_error()) {
+    zxlogf(ERROR, "Failed to create endpoints: %s", endpoints.status_string());
+    return endpoints.status_value();
+  }
+
+  zx::result result = outgoing_.Serve(std::move(endpoints->server));
+  if (result.is_error()) {
+    zxlogf(ERROR, "Failed to serve outgoing directory: %s", result.status_string());
+    return result.error_value();
+  }
+
+  std::array offers = {
+      fuchsia_hardware_sdhci::Service::Name,
+  };
+
+  status = DdkAdd(ddk::DeviceAddArgs("pci-sdhci")
+                      .set_proto_id(ZX_PROTOCOL_SDHCI)
+                      .set_runtime_service_offers(offers)
+                      .set_outgoing_dir(endpoints->client.TakeChannel()));
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "Failed to add device: %s", zx_status_get_string(status));
+    return status;
+  }
 
   return ZX_OK;
 }
