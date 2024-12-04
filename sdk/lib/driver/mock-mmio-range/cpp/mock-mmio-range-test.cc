17e174392a79b8c9ef81f14160ddcf261f88e927
Sarah Chan <spqchan@google.com>
2024-12-04 22:39:57 +0000

    [fdf] Fork mock-mmio-range to sdk/lib/driver
    Fork src/devices/testing/mock-mmio-range to sdk/lib/driver.
    Remove the gtest and fbl dependencies on the forked library.
    
    Bug: 373899421
    Tests: mock-mmio-range-cpp-test
    Change-Id: I2626ea2ef16d5b1c8c4dc334a5092793d4512025
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1158046
    Reviewed-by: Yilong Li <liyl@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>

---
+++ sdk/lib/driver/mock-mmio-range/cpp/mock-mmio-range-test.cc
@@ -0,0 +1,224 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/driver/mock-mmio-range/cpp/mock-mmio-range.h>
+#include <lib/mmio/mmio.h>
+
+#include <gtest/gtest.h>
+
+namespace mock_mmio {
+
+namespace {
+
+class MockMmioRangeTest : public ::testing::Test {
+ public:
+  void TearDown() override { mmio_range_.CheckAllAccessesReplayed(); }
+
+ protected:
+  MockMmioRange mmio_range_{0x4000, MockMmioRange::Size::k32};
+  fdf::MmioBuffer mmio_buffer_{mmio_range_.GetMmioBuffer()};
+};
+
+TEST_F(MockMmioRangeTest, NoOperations) {
+  MockMmioRange mmio_range(0x1000, MockMmioRange::Size::k32);
+  mmio_range_.CheckAllAccessesReplayed();
+  SUCCEED();
+}
+
+TEST_F(MockMmioRangeTest, ConstructorRangeSize) {
+  MockMmioRange mmio_range1(0x1000, MockMmioRange::Size::k32);
+  MockMmioRange mmio_range2(0x4000, MockMmioRange::Size::k32);
+
+  fdf::MmioBuffer mmio_range1_buffer = mmio_range1.GetMmioBuffer();
+  fdf::MmioBuffer mmio_range2_buffer = mmio_range2.GetMmioBuffer();
+
+  EXPECT_EQ(0x1000u, mmio_range1_buffer.get_size());
+  EXPECT_EQ(0x4000u, mmio_range2_buffer.get_size());
+}
+
+TEST_F(MockMmioRangeTest, ConstructorDefaultOperationSize) {
+  MockMmioRange mmio_range1(0x1000, MockMmioRange::Size::k32);
+  MockMmioRange mmio_range2(0x1000, MockMmioRange::Size::k16);
+
+  fdf::MmioBuffer mmio_range1_buffer = mmio_range1.GetMmioBuffer();
+  fdf::MmioBuffer mmio_range2_buffer = mmio_range2.GetMmioBuffer();
+
+  mmio_range1.Expect({.address = 0x100, .value = 0x01});
+  mmio_range2.Expect({.address = 0x100, .value = 0x01});
+
+  EXPECT_EQ(0x01u, mmio_range1_buffer.Read32(0x100));
+  EXPECT_EQ(0x01u, mmio_range2_buffer.Read16(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadOnce) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445});
+  EXPECT_EQ(0x42434445u, mmio_buffer_.Read32(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadOnceNonDefaultSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445, .size = MockMmioRange::Size::k64});
+  EXPECT_EQ(0x42434445u, mmio_buffer_.Read64(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadOnceExplicitSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445, .size = MockMmioRange::Size::k32});
+  EXPECT_EQ(0x42434445u, mmio_buffer_.Read32(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadRepeated) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42});
+  mmio_range_.Expect({.address = 0x100, .value = 0x43});
+  mmio_range_.Expect({.address = 0x100, .value = 0x44});
+  mmio_range_.Expect({.address = 0x100, .value = 0x45});
+
+  EXPECT_EQ(0x42u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x43u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x44u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x45u, mmio_buffer_.Read32(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadRepeatedFromAccessList) {
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x100, .value = 0x42},
+      {.address = 0x100, .value = 0x43},
+      {.address = 0x100, .value = 0x44},
+      {.address = 0x100, .value = 0x45},
+  }));
+
+  EXPECT_EQ(0x42u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x43u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x44u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x45u, mmio_buffer_.Read32(0x100));
+}
+
+TEST_F(MockMmioRangeTest, ReadVaryingAddressSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42});
+  mmio_range_.Expect({.address = 0x104, .value = 0x43, .size = MockMmioRange::Size::k16});
+  mmio_range_.Expect({.address = 0x106, .value = 0x44, .size = MockMmioRange::Size::k8});
+  mmio_range_.Expect({.address = 0x108, .value = 0x45, .size = MockMmioRange::Size::k64});
+
+  EXPECT_EQ(0x42u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x43u, mmio_buffer_.Read16(0x104));
+  EXPECT_EQ(0x44u, mmio_buffer_.Read8(0x106));
+  EXPECT_EQ(0x45u, mmio_buffer_.Read64(0x108));
+}
+
+TEST_F(MockMmioRangeTest, ReadVaryingAddressSizeFromAccessLists) {
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x100, .value = 0x42},
+      {.address = 0x104, .value = 0x43, .size = MockMmioRange::Size::k16},
+  }));
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x106, .value = 0x44, .size = MockMmioRange::Size::k8},
+      {.address = 0x108, .value = 0x45, .size = MockMmioRange::Size::k64},
+  }));
+
+  EXPECT_EQ(0x42u, mmio_buffer_.Read32(0x100));
+  EXPECT_EQ(0x43u, mmio_buffer_.Read16(0x104));
+  EXPECT_EQ(0x44u, mmio_buffer_.Read8(0x106));
+  EXPECT_EQ(0x45u, mmio_buffer_.Read64(0x108));
+}
+
+TEST_F(MockMmioRangeTest, WriteOnce) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445, .write = true});
+  mmio_buffer_.Write32(0x42434445, 0x100);
+  mmio_range_.CheckAllAccessesReplayed();
+  SUCCEED();
+}
+
+TEST_F(MockMmioRangeTest, WriteOnceNonDefaultSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445, .size = MockMmioRange::Size::k64});
+  EXPECT_EQ(0x42434445u, mmio_buffer_.Read64(0x100));
+}
+
+TEST_F(MockMmioRangeTest, WriteOnceExplicitSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42434445, .size = MockMmioRange::Size::k32});
+  EXPECT_EQ(0x42434445u, mmio_buffer_.Read32(0x100));
+}
+
+TEST_F(MockMmioRangeTest, WriteRepeated) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42, .write = true});
+  mmio_range_.Expect({.address = 0x100, .value = 0x43, .write = true});
+  mmio_range_.Expect({.address = 0x100, .value = 0x44, .write = true});
+  mmio_range_.Expect({.address = 0x100, .value = 0x45, .write = true});
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  mmio_buffer_.Write32(0x43, 0x100);
+  mmio_buffer_.Write32(0x44, 0x100);
+  mmio_buffer_.Write32(0x45, 0x100);
+}
+
+TEST_F(MockMmioRangeTest, WriteRepeatedFromAccessList) {
+  mmio_range_.Expect(MockMmioRange::AccessList({{.address = 0x100, .value = 0x42, .write = true},
+                                                {.address = 0x100, .value = 0x43, .write = true},
+                                                {.address = 0x100, .value = 0x44, .write = true},
+                                                {.address = 0x100, .value = 0x45, .write = true}}));
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  mmio_buffer_.Write32(0x43, 0x100);
+  mmio_buffer_.Write32(0x44, 0x100);
+  mmio_buffer_.Write32(0x45, 0x100);
+}
+
+TEST_F(MockMmioRangeTest, WriteVaryingAddressSize) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42, .write = true});
+  mmio_range_.Expect(
+      {.address = 0x104, .value = 0x43, .write = true, .size = MockMmioRange::Size::k16});
+  mmio_range_.Expect(
+      {.address = 0x106, .value = 0x44, .write = true, .size = MockMmioRange::Size::k8});
+  mmio_range_.Expect(
+      {.address = 0x108, .value = 0x45, .write = true, .size = MockMmioRange::Size::k64});
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  mmio_buffer_.Write16(0x43, 0x104);
+  mmio_buffer_.Write8(0x44, 0x106);
+  mmio_buffer_.Write64(0x45, 0x108);
+}
+
+TEST_F(MockMmioRangeTest, WriteVaryingAddressSizeFromAccessLists) {
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x100, .value = 0x42, .write = true},
+      {.address = 0x104, .value = 0x43, .write = true, .size = MockMmioRange::Size::k16},
+  }));
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x106, .value = 0x44, .write = true, .size = MockMmioRange::Size::k8},
+      {.address = 0x108, .value = 0x45, .write = true, .size = MockMmioRange::Size::k64},
+  }));
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  mmio_buffer_.Write16(0x43, 0x104);
+  mmio_buffer_.Write8(0x44, 0x106);
+  mmio_buffer_.Write64(0x45, 0x108);
+}
+
+TEST_F(MockMmioRangeTest, InterleavedReadAndWrite) {
+  mmio_range_.Expect({.address = 0x100, .value = 0x42, .write = true});
+  mmio_range_.Expect({.address = 0x104, .value = 0x43, .size = MockMmioRange::Size::k16});
+  mmio_range_.Expect(
+      {.address = 0x106, .value = 0x44, .write = true, .size = MockMmioRange::Size::k8});
+  mmio_range_.Expect({.address = 0x108, .value = 0x45, .size = MockMmioRange::Size::k64});
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  EXPECT_EQ(0x43u, mmio_buffer_.Read16(0x104));
+  mmio_buffer_.Write8(0x44, 0x106);
+  EXPECT_EQ(0x45u, mmio_buffer_.Read64(0x108));
+}
+
+TEST_F(MockMmioRangeTest, InterleavedReadAndWriteFromAccessList) {
+  mmio_range_.Expect(MockMmioRange::AccessList({
+      {.address = 0x100, .value = 0x42, .write = true},
+      {.address = 0x104, .value = 0x43, .size = MockMmioRange::Size::k16},
+      {.address = 0x106, .value = 0x44, .write = true, .size = MockMmioRange::Size::k8},
+      {.address = 0x108, .value = 0x45, .size = MockMmioRange::Size::k64},
+  }));
+
+  mmio_buffer_.Write32(0x42, 0x100);
+  EXPECT_EQ(0x43u, mmio_buffer_.Read16(0x104));
+  mmio_buffer_.Write8(0x44, 0x106);
+  EXPECT_EQ(0x45u, mmio_buffer_.Read64(0x108));
+}
+
+}  // namespace
+
+}  // namespace mock_mmio
---
+++ sdk/lib/driver/mock-mmio-range/cpp/mock-mmio-range.cc
@@ -0,0 +1,79 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/driver/mock-mmio-range/cpp/mock-mmio-range.h>
+
+namespace mock_mmio {
+
+namespace {
+fdf::MmioBuffer CreateMmioBuffer(size_t size, uint32_t cache_policy, const fdf::MmioBufferOps* ops,
+                                 void* ctx) {
+  zx::vmo vmo;
+  ZX_ASSERT(zx::vmo::create(/*size=*/size, 0, &vmo) == ZX_OK);
+  mmio_buffer_t mmio{};
+  ZX_ASSERT(mmio_buffer_init(&mmio, 0, size, vmo.release(), cache_policy) == ZX_OK);
+  return fdf::MmioBuffer(mmio, ops, ctx);
+}
+}  // namespace
+
+void MockMmioRange::Expect(cpp20::span<const MockMmioRange::Access> accesses) {
+  const std::lock_guard<std::mutex> lock(mutex_);
+  for (const auto& access : accesses) {
+    access_list_.push_back({
+        .address = access.address,
+        .value = access.value,
+        .write = access.write,
+        .size =
+            access.size == MockMmioRange::Size::kUseDefault ? default_access_size_ : access.size,
+    });
+  }
+}
+
+void MockMmioRange::CheckAllAccessesReplayed() {
+  const std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT(access_list_.size() == access_index_);
+}
+
+fdf::MmioBuffer MockMmioRange::GetMmioBuffer() {
+  static constexpr fdf::MmioBufferOps kMockMmioOps = {
+      .Read8 = MockMmioRange::Read8,
+      .Read16 = MockMmioRange::Read16,
+      .Read32 = MockMmioRange::Read32,
+      .Read64 = MockMmioRange::Read64,
+      .Write8 = MockMmioRange::Write8,
+      .Write16 = MockMmioRange::Write16,
+      .Write32 = MockMmioRange::Write32,
+      .Write64 = MockMmioRange::Write64,
+  };
+
+  return CreateMmioBuffer(range_size_, ZX_CACHE_POLICY_CACHED, &kMockMmioOps, this);
+}
+
+uint64_t MockMmioRange::Read(zx_off_t address, MockMmioRange::Size size) const {
+  const std::lock_guard<std::mutex> lock(mutex_);
+  if (access_index_ >= access_list_.size()) {
+    return 0;
+  }
+
+  const MockMmioRange::Access& expected_access = access_list_[access_index_];
+  ++access_index_;
+
+  ZX_ASSERT(expected_access.address == address);
+  ZX_ASSERT(expected_access.write == false);
+  return expected_access.value;
+}
+
+void MockMmioRange::Write(zx_off_t address, uint64_t value, MockMmioRange::Size size) const {
+  const std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT(access_index_ < access_list_.size());
+
+  const MockMmioRange::Access& expected_access = access_list_[access_index_];
+  ++access_index_;
+
+  ZX_ASSERT(expected_access.address == address);
+  ZX_ASSERT(expected_access.value == value);
+  ZX_ASSERT(expected_access.size == size);
+}
+
+}  // namespace mock_mmio
