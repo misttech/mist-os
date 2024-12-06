edc5e262c5331822f9e35a0cca35b314f89eb75e
Ankur Mittal <anmittal@google.com>
2024-12-06 22:28:06 +0000

    [reland][test_manager] Implement lifecycle controller.
    This is a reland of commit 0b05ef22db941fb418afe1ac37bdfdb526f0b308
    
    Multiply: test_manager_test.cm
    Multiply: elf_test_runner_integration_test.cm
    Multiply: test_manager_stress_test.cm
    
    Changes compared to land commit: https://fuchsia-review.googlesource.com/c/fuchsia/+/1169395/1..6
    
    Original change's description:
    > [test_manager] Implement lifecycle controller.
    >
    > This will make nested test manager complete all tasks before getting
    > killed and prevent leaking handles in memfs
    >
    > BUG=379961285
    > Fixed: 379741950
    >
    > Testing: Added stress tests and manually verified that we are no longer leaking handles or memory.
    >
    > Change-Id: I6ceadef258871e19df6d32832f426c32a5498a91
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1168237
    > Reviewed-by: Christopher Johnson <crjohns@google.com>
    > Commit-Queue: Ankur Mittal <anmittal@google.com>
    
    Bug: 379961285
    Change-Id: I3fffd0249edbe438fcb1145c3dc33154f3e12a4a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1169395
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Ankur Mittal <anmittal@google.com>

--- src/sys/test_manager/src/main.rs
+++ src/sys/test_manager/src/main.rs
@@ -3,9 +3,14 @@
 // found in the LICENSE file.
 
 use anyhow::Error;
+use fidl::endpoints::{ControlHandle, RequestStream};
+use fidl::handle::AsyncChannel;
+use fidl_fuchsia_process_lifecycle::{LifecycleRequest, LifecycleRequestStream};
 use fuchsia_component::client::connect_to_protocol;
 use fuchsia_component::server::ServiceFs;
+use fuchsia_runtime::{HandleInfo, HandleType};
 use futures::StreamExt;
+use futures_util::stream::TryStreamExt;
 use std::sync::Arc;
 use test_manager_lib::{constants, AboveRootCapabilitiesForTest, RootDiagnosticNode};
 use tracing::{info, warn};
@@ -43,6 +48,8 @@ impl TestManagerArgs {
 #[fuchsia::main]
 async fn main() -> Result<(), Error> {
     info!("started");
+    let lifecycle_handle =
+        fuchsia_runtime::take_startup_handle(HandleInfo::new(HandleType::Lifecycle, 0)).unwrap();
     let args: TestManagerArgs = std::env::args().try_into()?;
     let mut fs = ServiceFs::new();
 
@@ -85,14 +92,23 @@ async fn main() -> Result<(), Error> {
     let root_inspect_for_test_case_enumerator = root_inspect_for_run_builder.clone();
     let root_inspect_for_suite_runner = root_inspect_for_run_builder.clone();
 
+    let server_scope = fasync::Scope::new();
+    // Create a Arc wrapped child scope and pass its clone to various closures so that this scope
+    // is not terminated along with those closures.
+    let child_scope: Arc<fasync::Scope> = Arc::new(server_scope.new_child());
+    let scope_for_run_builder = child_scope.clone();
+    let scope_for_query = child_scope.clone();
+    let scope_for_test_case_enumerator = child_scope.clone();
+    let scope_for_suite_runner = child_scope.clone();
+    let scope_for_early_boot_profiles = child_scope.clone();
+
     fs.dir("svc")
         .add_fidl_service(move |stream| {
             let resolver = resolver_for_run_builder.clone();
             let pkg_resolver = pkg_resolver_for_run_builder.clone();
             let routing_info = routing_info_for_run_builder.clone();
             let root_inspect = root_inspect_for_run_builder.clone();
-
-            fasync::Task::spawn(async move {
+            scope_for_run_builder.spawn(async move {
                 test_manager_lib::run_test_manager_run_builder_server(
                     stream,
                     resolver,
@@ -102,8 +118,7 @@ async fn main() -> Result<(), Error> {
                 )
                 .await
                 .unwrap_or_else(|error| warn!(?error, "test manager returned error"))
-            })
-            .detach();
+            });
         })
         .add_fidl_service(move |stream| {
             let resolver = resolver_for_query.clone();
@@ -111,7 +126,7 @@ async fn main() -> Result<(), Error> {
             let routing_info = routing_info_for_query.clone();
             let root_inspect = root_inspect_for_query.clone();
 
-            fasync::Task::local(async move {
+            scope_for_query.spawn_local(async move {
                 test_manager_lib::run_test_manager_query_server(
                     stream,
                     resolver,
@@ -121,16 +136,14 @@ async fn main() -> Result<(), Error> {
                 )
                 .await
                 .unwrap_or_else(|error| warn!(?error, "test manager returned error"))
-            })
-            .detach();
+            });
         })
         .add_fidl_service(move |stream| {
-            fasync::Task::spawn(async move {
+            scope_for_early_boot_profiles.spawn(async move {
                 test_manager_lib::serve_early_boot_profiles(stream)
                     .await
                     .unwrap_or_else(|error| warn!(?error, "test manager returned error"))
-            })
-            .detach();
+            });
         })
         .add_fidl_service(move |stream| {
             let resolver = resolver_for_test_case_enumerator.clone();
@@ -138,7 +151,7 @@ async fn main() -> Result<(), Error> {
             let routing_info = routing_info_for_task_test_case_enumerator.clone();
             let root_inspect = root_inspect_for_test_case_enumerator.clone();
 
-            fasync::Task::local(async move {
+            scope_for_test_case_enumerator.spawn_local(async move {
                 test_manager_lib::run_test_manager_test_case_enumerator_server(
                     stream,
                     resolver,
@@ -148,8 +161,7 @@ async fn main() -> Result<(), Error> {
                 )
                 .await
                 .unwrap_or_else(|error| warn!(?error, "test manager returned error"))
-            })
-            .detach();
+            });
         })
         .add_fidl_service(move |stream| {
             let resolver = resolver_for_suite_runner.clone();
@@ -157,7 +169,7 @@ async fn main() -> Result<(), Error> {
             let routing_info = routing_info_for_suite_runner.clone();
             let root_inspect = root_inspect_for_suite_runner.clone();
 
-            fasync::Task::spawn(async move {
+            scope_for_suite_runner.spawn(async move {
                 test_manager_lib::run_test_manager_suite_runner_server(
                     stream,
                     resolver,
@@ -167,10 +179,35 @@ async fn main() -> Result<(), Error> {
                 )
                 .await
                 .unwrap_or_else(|error| warn!(?error, "test manager returned error"))
-            })
-            .detach();
+            });
         });
+
     fs.take_and_serve_directory_handle()?;
-    fs.collect::<()>().await;
+    let fs_task = fasync::Task::spawn(async move { fs.collect::<()>().await });
+
+    let mut req_stream = LifecycleRequestStream::from_channel(AsyncChannel::from(
+        fuchsia_async::Channel::from_channel(lifecycle_handle.into()),
+    ));
+    if let Some(request) =
+        req_stream.try_next().await.expect("Failure receiving lifecycle FIDL message")
+    {
+        match request {
+            LifecycleRequest::Stop { control_handle: c } => {
+                info!("Received request to stop. Shutting down.");
+
+                // Cancel servicing outgoing directory. This will drop any current requests in
+                // flight, should not be directly used in prod code. It works here because
+                // this line is only executed when all of test manager clients go away when used as
+                // nested test manager in some tests.
+                fs_task.cancel().await;
+
+                // Complete all running tasks.
+                server_scope.join().await;
+
+                c.shutdown();
+            }
+        }
+    }
+
     Ok(())
 }
