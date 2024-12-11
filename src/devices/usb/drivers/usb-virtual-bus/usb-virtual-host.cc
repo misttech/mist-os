9445bcc85ba08eaa3cf38dc07e46c0f650344b18
Sam Hansen <hansens@google.com>
2024-12-11 08:59:51 -0800

    [usb][virtual-bus] Add missing dep on usb-fidl
    Somehow, the virtual-bus was relying on a transitive dep on the
    usb-fidl lib. This plugs the transitive hole.
    
    Bug: b/376724095
    Change-Id: Ia67348a55d216ae4531723315fe8ec12d5b0729a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1171418
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Christopher Anderson <cja@google.com>

--- src/devices/usb/drivers/usb-virtual-bus/usb-virtual-host.cc
+++ src/devices/usb/drivers/usb-virtual-bus/usb-virtual-host.cc
@@ -14,6 +14,7 @@
 #include <string.h>
 
 #include <fbl/auto_lock.h>
+#include <usb/request-fidl.h>
 #include <usb/usb-request.h>
 
 #include "src/devices/usb/drivers/usb-virtual-bus/usb-virtual-bus.h"
