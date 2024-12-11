cfeb4731e3bf72f341bf6dd58f0e1976a4d6b40e
Martin Puryear <mpuryear@google.com>
2024-12-11 12:45:47 -0800

    [audio] Auto-format JSX schema files
    Baseline audio_core's JSX files with an effective auto-
    formatter. RapidJson uses JSX to schema-validate JSON
    content, and these JSX files contain the schema for
    audio policy settings, delivered via product-specific
    static configuration files.
    
    Bug: b/371061877
    
    Change-Id: Ic3b0f94dd123b5eae4fd9d92da48464a4d8ce425
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1171081
    Fuchsia-Auto-Submit: Martin Puryear <mpuryear@google.com>
    Reviewed-by: Yegor Pomortsev <ypomortsev@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/media/audio/audio_core/shared/schema/audio_policy_schema.jsx
+++ src/media/audio/audio_core/shared/schema/audio_policy_schema.jsx
@@ -3,31 +3,48 @@
     // behaviors, render_usage, and capture_usage should be enum's, but rapidjson
     // does not validate against the enum, which means there's no 'breaking' reason
     // that a person would know to update the schema.
-    "behaviors": { "type": "string" },
-    "render_usage": { "type": "string" },
-    "capture_usage": { "type": "string" },
+    "behaviors": {
+      "type": "string"
+    },
+    "render_usage": {
+      "type": "string"
+    },
+    "capture_usage": {
+      "type": "string"
+    },
     "rule": {
       "active": {
         "oneOf": [
-          {"$ref": "#/definitions/render_usage"},
-          {"$ref": "#/definitions/capture_usage"}
+          {
+            "$ref": "#/definitions/render_usage"
+          },
+          {
+            "$ref": "#/definitions/capture_usage"
+          }
         ]
-       },
-       "affected": {
+      },
+      "affected": {
         "oneOf": [
-          {"$ref": "#/definitions/render_usage"},
-          {"$ref": "#/definitions/capture_usage"}
+          {
+            "$ref": "#/definitions/render_usage"
+          },
+          {
+            "$ref": "#/definitions/capture_usage"
+          }
         ]
-       },
-       "behavior": {"$ref": "#/definitions/behaviors"}
+      },
+      "behavior": {
+        "$ref": "#/definitions/behaviors"
+      }
     }
-   },
-
+  },
   "type": "object",
   "properties": {
     "audio_policy_rules": {
       "type": "array",
-      "items": { "$ref": "#/definitions/rule" }
+      "items": {
+        "$ref": "#/definitions/rule"
+      }
     },
     "idle_countdown_milliseconds": {
       "type": "integer",
@@ -41,6 +58,8 @@
       "type": "boolean"
     }
   },
-  "required": ["audio_policy_rules"],
+  "required": [
+    "audio_policy_rules"
+  ],
   "additionalProperties": true
 }
