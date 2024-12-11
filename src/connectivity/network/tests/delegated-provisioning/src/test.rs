a0443ccc22424e11b01b5c6f0f33ba91e65e688d
Evan Wildenhain <wildenhain@google.com>
2024-12-11 01:18:24 +0000

    [network/tests] Log guidance for triagers
    Logs a warning if the delegated-provisioning test is taking too long
    with instructions for triagers on what the test covers, why it might
    be taking so long, and how it depends on the product inside the starnix
    container booting successfully in order to pass, meaning that if there
    are other indications that the contained product cannot startup
    properly, this test failing does not necessarily indicate an issue with
    networking code specifically.
    
    Bug: 383358834
    Change-Id: I9f356b75f7d06a5f63115507bfc4c672037f6e01
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1171532
    Reviewed-by: Jeff Martin <martinjeffrey@google.com>
    Commit-Queue: Evan Wildenhain <wildenhain@google.com>

--- src/connectivity/network/tests/delegated-provisioning/src/test.rs
+++ src/connectivity/network/tests/delegated-provisioning/src/test.rs
@@ -9,12 +9,16 @@
 
 use std::collections::HashMap;
 
-use tracing::info;
+use futures::StreamExt as _;
+use tracing::{info, warn};
 use {
     delegated_provisioning_constants as constants, fidl_fuchsia_net as fnet,
     fidl_fuchsia_net_interfaces as fnet_interfaces,
-    fidl_fuchsia_net_interfaces_ext as fnet_interfaces_ext,
+    fidl_fuchsia_net_interfaces_ext as fnet_interfaces_ext, fuchsia_async as fasync,
 };
+
+const START_LOGGING_WARNINGS_AFTER: std::time::Duration = std::time::Duration::from_secs(3 * 60);
+const WARNING_LOG_INTERVAL: std::time::Duration = std::time::Duration::from_secs(30);
 
 #[fuchsia::test]
 async fn delegated_provisioning_test() {
@@ -33,6 +37,31 @@ async fn delegated_provisioning_test() {
         constants::CLIENT_IFACE_NAME,
         constants::DHCP_DYNAMIC_IP
     );
+
+    let _log_warning_if_taking_too_log_task = fasync::Task::spawn(async {
+        // NB: Don't move the timer into the `futures::stream::once` as we want to defer actually
+        // invoking `fasync::Interval::new` until after the initial timer expires.
+        fasync::Timer::new(START_LOGGING_WARNINGS_AFTER).await;
+        futures::stream::once(futures::future::ready(()))
+            .chain(fasync::Interval::new(WARNING_LOG_INTERVAL.into()))
+            .for_each(|()| async move {
+                warn!(
+                    "still waiting for interface ({}) to be assigned address ({:?})...",
+                    constants::CLIENT_IFACE_NAME,
+                    constants::DHCP_DYNAMIC_IP
+                );
+                warn!(
+                    "NOTE FOR TRIAGERS: this test simply waits for the DhcpClient of the product \
+                    running in the container to successfully acquire an IPv4 address.\n\
+                    This means that if the session running in the starnix container is unable \
+                    to start up properly, this test will fail via timing out.\n\
+                    Please check whether other end-to-end tests for your product indicate failure\n\
+                    to start up properly before assuming that this is failing due to specifically\n\
+                    a networking issue."
+                );
+            })
+            .await;
+    });
 
     let mut interfaces = HashMap::<u64, fnet_interfaces_ext::PropertiesAndState<(), _>>::new();
     fnet_interfaces_ext::wait_interface(event_stream, &mut interfaces, |interfaces| {
