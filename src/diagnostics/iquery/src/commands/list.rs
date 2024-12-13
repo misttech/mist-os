72ce3362e1b06d27c58a2a8cab1d09fc1536757b
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2024-12-13 12:11:36 -0800

    [iquery] Continue removal of --manifest by making it fail
    Similar to what we did it for other instances of this flag in
    I710e06408831b54498255e7d634b5d6bc2911b7c .
    
    Bug: 381285460
    Change-Id: I44da7c41531c7074aa274d2d2939bbb86ad2a99d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1169274
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Clayton McCray <claytonmccray@google.com>

--- src/diagnostics/iquery/src/commands/list.rs
+++ src/diagnostics/iquery/src/commands/list.rs
@@ -104,18 +104,10 @@ fn components_from_inspect_data(
 }
 
 pub fn list_response_items(
-    manifest: Option<&str>,
     with_url: bool,
     components: impl Iterator<Item = ListResultItem>,
 ) -> Vec<ListResultItem> {
     components
-        .filter(|result| match manifest {
-            None => true,
-            Some(manifest) => match result {
-                ListResultItem::Moniker(_) => true,
-                ListResultItem::MonikerWithUrl(url) => url.component_url.contains(manifest),
-            },
-        })
         .map(|result| {
             if with_url {
                 result
@@ -169,32 +161,22 @@ impl Command for ListCommand {
     type Result = ListResult;
 
     async fn execute<P: DiagnosticsProvider>(self, provider: &P) -> Result<Self::Result, Error> {
-        let (inspect, manifest) = if self.manifest.is_some() {
-            eprintln!(
+        if self.manifest.is_some() {
+            panic!(
                 "WARNING: --manifest is DEPRECATED and will be removed soon. Please use --component only."
             );
-            if self.component.is_some() {
-                eprintln!("WARNING: --component ignored due to usage of deprecated --manifest. Please use --component only.")
-            }
-            let inspect =
-                provider.snapshot::<Inspect>(self.accessor.as_deref(), std::iter::empty()).await?;
-            (inspect, self.manifest)
+        }
+        let mut selectors = if let Some(query) = self.component {
+            let instances = find_components(provider.realm_query(), &query).await?;
+            instances_to_root_selectors(instances)?
         } else {
-            let mut selectors = if let Some(query) = self.component {
-                let instances = find_components(provider.realm_query(), &query).await?;
-                instances_to_root_selectors(instances)?
-            } else {
-                vec![]
-            };
-            utils::ensure_tree_field_is_set(&mut selectors, None)?;
-            let inspect = provider
-                .snapshot::<Inspect>(self.accessor.as_deref(), selectors.into_iter())
-                .await?;
-            (inspect, None)
+            vec![]
         };
-
+        utils::ensure_tree_field_is_set(&mut selectors, None)?;
+        let inspect =
+            provider.snapshot::<Inspect>(self.accessor.as_deref(), selectors.into_iter()).await?;
         let components = components_from_inspect_data(inspect);
-        let results = list_response_items(manifest.as_deref(), self.with_url, components);
+        let results = list_response_items(self.with_url, components);
         Ok(ListResult(results))
     }
 }
