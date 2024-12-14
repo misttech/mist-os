380240157d5c24f26d1392e68c3afcabd3ec4e88
James Robinson <jamesr@google.com>
2024-12-13 18:24:08 -0800

    [binder] Update copy of generated bindings
    This updates our vendored copy of the bindgen generated bindings for the
    binder_ndk crate. This file is copied from an Android build at the path:
    
    out/soong/.intermediates/frameworks/native/libs/binder/rust/libbinder_ndk_sys/android_arm64_armv8-2a_cortex-a55_dylib/out/bindings.rs
    
    with an Android copyright header added.
    
    This also updates our generated bindings for rpcbinder by running the
    script //src/lib/android/binder/rust/rpcbinder/generate.py
    
    Lastly, this updates binder proxy code for changes in the API as the
    StatusCode type no longer implements Error.
    
    Change-Id: I83bb67037f004c4543cf21e2e315f20e5f3c833c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1172293
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Reviewed-by: Mike Comfoltey <comfoltey@google.com>
    Reviewed-by: Prashanth Swaminathan <prashanthsw@google.com>

--- src/tee/binder-proxy/tests/src/binder_proxy_enumerate.rs
+++ src/tee/binder-proxy/tests/src/binder_proxy_enumerate.rs
@@ -5,7 +5,7 @@
 use android_system_microfuchsia_vm_service::aidl::android::system::microfuchsia::vm_service::IMicrofuchsia::{
     IMicrofuchsia, GUEST_PORT,
 };
-use anyhow::{self, Error};
+use anyhow::Error;
 use rpcbinder::RpcSession;
 use std::{os::fd::{AsRawFd, FromRawFd, IntoRawFd, OwnedFd}, sync::Once};
 use vsock_sys::{create_virtio_stream_socket, sockaddr_vm};
@@ -33,42 +33,45 @@ async fn main() -> Result<(), Error> {
     init_dev_urandom_compat_once();
 
     let session = RpcSession::new();
-    let service: binder::Strong<dyn IMicrofuchsia> = session.setup_preconnected_client(|| {
-        // TODO: Why doesn't the polling loop in the binder library work?
-        // We are probably returning the wrong error on connection failure.
-        std::thread::sleep(std::time::Duration::from_secs(2));
-        // Make a virtio stream socket.
-        let mut socket_fd = 0;
-        let status = zx::Status::from_raw(unsafe { create_virtio_stream_socket(&mut socket_fd) });
-        if status != zx::Status::OK {
-            tracing::error!("Could not create virtio stream socket: {status:?}");
-            return None;
-        }
-        let socket_fd = unsafe { OwnedFd::from_raw_fd(socket_fd) };
+    let service: binder::Strong<dyn IMicrofuchsia> = session
+        .setup_preconnected_client(|| {
+            // TODO: Why doesn't the polling loop in the binder library work?
+            // We are probably returning the wrong error on connection failure.
+            std::thread::sleep(std::time::Duration::from_secs(2));
+            // Make a virtio stream socket.
+            let mut socket_fd = 0;
+            let status =
+                zx::Status::from_raw(unsafe { create_virtio_stream_socket(&mut socket_fd) });
+            if status != zx::Status::OK {
+                tracing::error!("Could not create virtio stream socket: {status:?}");
+                return None;
+            }
+            let socket_fd = unsafe { OwnedFd::from_raw_fd(socket_fd) };
 
-        // connect to loopback 5680
-        let addr = sockaddr_vm {
-            svm_family: libc::AF_VSOCK as u16,
-            svm_port: GUEST_PORT as u32,
-            svm_cid: 1, // VMADDR_CID_LOCAL
-            ..Default::default()
-        };
+            // connect to loopback 5680
+            let addr = sockaddr_vm {
+                svm_family: libc::AF_VSOCK as u16,
+                svm_port: GUEST_PORT as u32,
+                svm_cid: 1, // VMADDR_CID_LOCAL
+                ..Default::default()
+            };
 
-        let r = unsafe {
-            let addr_ptr = &addr as *const sockaddr_vm as *const libc::sockaddr;
-            libc::connect(
-                socket_fd.as_raw_fd(),
-                addr_ptr,
-                std::mem::size_of::<sockaddr_vm>() as libc::socklen_t,
-            )
-        };
-        if r == -1 {
-            tracing::error!("connect failed: {:?}", std::io::Error::last_os_error());
-            return None;
-        }
+            let r = unsafe {
+                let addr_ptr = &addr as *const sockaddr_vm as *const libc::sockaddr;
+                libc::connect(
+                    socket_fd.as_raw_fd(),
+                    addr_ptr,
+                    std::mem::size_of::<sockaddr_vm>() as libc::socklen_t,
+                )
+            };
+            if r == -1 {
+                tracing::error!("connect failed: {:?}", std::io::Error::last_os_error());
+                return None;
+            }
 
-        Some(socket_fd.into_raw_fd())
-    })?;
+            Some(socket_fd.into_raw_fd())
+        })
+        .map_err(|e| anyhow::anyhow!("setup_preconnected_client error {e:?}"))?;
     let tee_uuid = service.trustedAppUuids()?;
 
     let config = binder_proxy_tests_config::Config::take_from_startup_handle();
