78afbea9c2a473a007d478f237d4d51d68e1bc1a
Charles Celerier <chcl@google.com>
2024-12-16 20:13:05 -0800

    [fuchsia-controller] Fix and enable mypy errors in fidl/_import.py
    Bug: 346628306
    Change-Id: I83f472ffafc0386314aae4246b047fb29352e943
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1173013
    Reviewed-by: Sam Balana <sbalana@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/developer/ffx/lib/fuchsia-controller/python/fidl/_import.py
+++ src/developer/ffx/lib/fuchsia-controller/python/fidl/_import.py
@@ -3,37 +3,53 @@
 # found in the LICENSE file.
 """Defines the import hooks for when a user writes `import fidl.[fidl_library]`."""
 
-# TODO(https://fxbug.dev/346628306): Remove this comment to ignore mypy errors.
-# mypy: ignore-errors
-
 # autoflake: skip_file
-import importlib.abc
+import importlib.util
 import sys
+from collections.abc import Sequence
+from importlib.abc import MetaPathFinder
+from importlib.machinery import ModuleSpec
+from types import ModuleType
 
 from ._async_socket import AsyncSocket
 from ._fidl_common import EpitaphError, FrameworkError
 from ._ipc import GlobalHandleWaker, HandleWaker
-from ._library import load_module
+from ._library import FIDLLibraryModule, load_fidl_module
 
 
-class FIDLImportFinder(importlib.abc.MetaPathFinder):
+class FIDLImportFinder(MetaPathFinder):
     """The main import hook class."""
 
-    def find_module(self, fullname: str, path=None):
-        """Override from abc.MetaPathFinder."""
+    def find_spec(
+        self,
+        fullname: str,
+        path: Sequence[str] | None = None,
+        target: ModuleType | None = None,
+    ) -> ModuleSpec | None:
         # TODO(https://fxbug.dev/42061151): Remove "TransportError".
         if (
             fullname.startswith("fidl._")
             or fullname == "fidl.FrameworkError"
             or fullname == "fidl.TransportError"
         ):
-            return __loader__
-        elif fullname.startswith("fidl."):
-            return self
+            return None
+        if fullname.startswith("fidl."):
+            # TODO(https://fxbug.dev/346628306): Need to determine how to make mypy
+            # correctly check the types for a Loader.
+            return importlib.util.spec_from_loader(fullname, FIDLImportLoader)  # type: ignore
+        return None
 
-    def load_module(self, fullname: str):
-        """Override from abc.MetaPathFinder."""
-        return load_module(fullname)
+
+class FIDLImportLoader:
+    # TODO(https://fxbug.dev/346628306): Need to determine how to make mypy
+    # correctly check the types for a Loader.
+    def create_module(spec: ModuleSpec) -> ModuleType:  # type: ignore
+        return load_fidl_module(spec.name)
+
+    # TODO(https://fxbug.dev/346628306): Need to determine how to make mypy
+    # correctly check the types for a Loader.
+    def exec_module(module: ModuleType) -> None:  # type: ignore
+        pass
 
 
 meta_hook = FIDLImportFinder()
