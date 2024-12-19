3e58d3be5807b487e2142bef0a7f955722de50af
Puneetha Ramachandra <puneetha@google.com>
2024-12-18 18:35:31 -0800

    [devicetree] Omit newline for compiler output
    This helps reduce the build log churn when compiler has no errors to
    report.
    
    Change-Id: I0f3567d1a3892c261217a6716d7e179ac245148a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1177312
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Puneetha Ramachandra <puneetha@google.com>

--- build/devicetree/dtc.sh
+++ build/devicetree/dtc.sh
@@ -18,7 +18,7 @@ fi
 
 # Treat DTS warnings as build failures.
 DTC_OUTPUT=$($DTC_PATH $@ $DTC_ARGS 2>&1)
-echo -e "$DTC_OUTPUT"
+echo -en "$DTC_OUTPUT"
 if [[ "$DTC_OUTPUT" == *"Warning"* ]]; then
   echo "$0: Returning failure due to warnings in dts compilation."
   exit 1
