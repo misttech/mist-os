0b235245e637d770a2873d313e73ac095ea282c1
Victor Costan <costan@google.com>
2024-12-30 17:36:49 -0800

    [display][api-protocols] Add tests for DisplayEngineEventsBanjo.
    Bug: 42079190
    Test: fx test //src/graphics/display/lib/api-protocols
    Change-Id: I465a02983699e034dba094341e4a476da8a639ae
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1180053
    Commit-Queue: Victor Costan <costan@google.com>
    Reviewed-by: Yilong Li <liyl@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

--- src/graphics/display/lib/api-protocols/cpp/display-engine-events-banjo-test.cc
+++ src/graphics/display/lib/api-protocols/cpp/display-engine-events-banjo-test.cc
@@ -4,4 +4,143 @@
 
 #include "src/graphics/display/lib/api-protocols/cpp/display-engine-events-banjo.h"
 
-namespace display {}  // namespace display
+#include <fuchsia/hardware/display/controller/cpp/banjo.h>
+#include <lib/driver/testing/cpp/scoped_global_logger.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "src/graphics/display/lib/api-protocols/cpp/mock-banjo-display-engine-listener.h"
+#include "src/graphics/display/lib/api-types/cpp/config-stamp.h"
+#include "src/graphics/display/lib/api-types/cpp/display-id.h"
+#include "src/graphics/display/lib/api-types/cpp/mode-and-id.h"
+#include "src/graphics/display/lib/api-types/cpp/mode-id.h"
+#include "src/graphics/display/lib/api-types/cpp/mode.h"
+#include "src/graphics/display/lib/api-types/cpp/pixel-format.h"
+
+namespace display {
+
+namespace {
+
+class DisplayEngineEventsBanjoTest : public ::testing::Test {
+ public:
+  void SetUp() override { banjo_adapter_.SetListener(&display_engine_listener_protocol); }
+
+  void TearDown() override {
+    banjo_adapter_.SetListener(nullptr);
+    mock_.CheckAllCallsReplayed();
+  }
+
+ protected:
+  fdf_testing::ScopedGlobalLogger logger_;
+
+  display::testing::MockBanjoDisplayEngineListener mock_;
+  const display_engine_listener_protocol_t display_engine_listener_protocol = mock_.GetProtocol();
+  DisplayEngineEventsBanjo banjo_adapter_;
+  DisplayEngineEventsInterface& interface_{banjo_adapter_};
+};
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayAddedWithPreferredMode) {
+  static constexpr display::DisplayId kDisplayId(42);
+  static constexpr display::ModeAndId kDisplayModeAndId({
+      .id = display::ModeId(1),
+      .mode = display::Mode({
+          .active_width = 640,
+          .active_height = 480,
+          .refresh_rate_millihertz = 60'000,
+      }),
+  });
+  static constexpr cpp20::span<const display::ModeAndId> kPreferredModes(&kDisplayModeAndId, 1);
+  static constexpr display::PixelFormat kPixelFormat = display::PixelFormat::kB8G8R8A8;
+  static constexpr cpp20::span<const display::PixelFormat> kPixelFormats(&kPixelFormat, 1);
+
+  static constexpr uint64_t banjo_display_id = display::ToBanjoDisplayId(kDisplayId);
+  static constexpr fuchsia_images2_pixel_format_enum_value_t expected_banjo_pixel_format =
+      kPixelFormat.ToBanjo();
+  static constexpr cpp20::span<const fuchsia_images2_pixel_format_enum_value_t>
+      expected_banjo_pixel_formats(&expected_banjo_pixel_format, 1);
+
+  mock_.ExpectOnDisplayAdded([&](const raw_display_info_t* info) {
+    EXPECT_EQ(banjo_display_id, info->display_id);
+
+    ASSERT_EQ(1u, info->preferred_modes_count);
+    EXPECT_EQ(640u, info->preferred_modes_list[0].h_addressable);
+    EXPECT_EQ(480u, info->preferred_modes_list[0].v_addressable);
+    EXPECT_EQ(640 * 480 * 60u, info->preferred_modes_list[0].pixel_clock_hz);
+
+    cpp20::span<const fuchsia_images2_pixel_format_enum_value_t> banjo_pixel_formats(
+        info->pixel_formats_list, info->pixel_formats_count);
+    EXPECT_THAT(banjo_pixel_formats, ::testing::ElementsAreArray(expected_banjo_pixel_formats));
+  });
+  interface_.OnDisplayAdded(kDisplayId, kPreferredModes, kPixelFormats);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayAddedWithNoListener) {
+  static constexpr display::DisplayId kDisplayId(42);
+  static constexpr display::ModeAndId kDisplayModeAndId({
+      .id = display::ModeId(1),
+      .mode = display::Mode({
+          .active_width = 640,
+          .active_height = 480,
+          .refresh_rate_millihertz = 60'000,
+      }),
+  });
+  static constexpr cpp20::span<const display::ModeAndId> kPreferredModes(&kDisplayModeAndId, 1);
+  static constexpr display::PixelFormat kPixelFormat = display::PixelFormat::kB8G8R8A8;
+  static constexpr cpp20::span<const display::PixelFormat> kPixelFormats(&kPixelFormat, 1);
+
+  banjo_adapter_.SetListener(nullptr);
+  interface_.OnDisplayAdded(kDisplayId, kPreferredModes, kPixelFormats);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayRemoved) {
+  static constexpr display::DisplayId kDisplayId(42);
+
+  mock_.ExpectOnDisplayRemoved(
+      [&](uint64_t banjo_display_id) { EXPECT_EQ(42u, banjo_display_id); });
+  interface_.OnDisplayRemoved(kDisplayId);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayRemovedWithNoListener) {
+  static constexpr display::DisplayId kDisplayId(42);
+
+  banjo_adapter_.SetListener(nullptr);
+  interface_.OnDisplayRemoved(kDisplayId);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayVsync) {
+  static constexpr display::DisplayId kDisplayId(42);
+  static constexpr zx::time kTimestamp(zx_time_t{42'4242});
+  static constexpr display::ConfigStamp kConfigStamp(4242'4242);
+
+  mock_.ExpectOnDisplayVsync([&](uint64_t banjo_display_id, zx_time_t banjo_timestamp,
+                                 const config_stamp_t* banjo_config_stamp) {
+    EXPECT_EQ(42u, banjo_display_id);
+    EXPECT_EQ(42'4242u, banjo_timestamp);
+    EXPECT_EQ(4242'4242u, banjo_config_stamp->value);
+  });
+  interface_.OnDisplayVsync(kDisplayId, kTimestamp, kConfigStamp);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnDisplayVsyncWithNoListener) {
+  static constexpr display::DisplayId kDisplayId(42);
+  static constexpr zx::time kTimestamp(zx_time_t{42'4242});
+  static constexpr display::ConfigStamp kConfigStamp(4242'4242);
+
+  banjo_adapter_.SetListener(nullptr);
+  interface_.OnDisplayVsync(kDisplayId, kTimestamp, kConfigStamp);
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnCaptureComplete) {
+  mock_.ExpectOnCaptureComplete([]() {});
+  interface_.OnCaptureComplete();
+}
+
+TEST_F(DisplayEngineEventsBanjoTest, OnCaptureCompleteWithNoListener) {
+  banjo_adapter_.SetListener(nullptr);
+  interface_.OnCaptureComplete();
+}
+
+}  // namespace
+
+}  // namespace display
---
+++ src/graphics/display/lib/api-protocols/cpp/mock-banjo-display-engine-listener.cc
@@ -0,0 +1,114 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/graphics/display/lib/api-protocols/cpp/mock-banjo-display-engine-listener.h"
+
+#include <fuchsia/hardware/display/controller/cpp/banjo.h>
+#include <zircon/assert.h>
+#include <zircon/compiler.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <mutex>
+#include <vector>
+
+namespace display::testing {
+
+// Exactly one of the members is non-null.
+struct MockBanjoDisplayEngineListener::Expectation {
+  OnDisplayAddedChecker on_display_added_checker;
+  OnDisplayRemovedChecker on_display_removed_checker;
+  OnDisplayVsyncChecker on_display_vsync_checker;
+  OnCaptureCompleteChecker on_capture_complete_checker;
+};
+
+MockBanjoDisplayEngineListener::MockBanjoDisplayEngineListener() = default;
+
+MockBanjoDisplayEngineListener::~MockBanjoDisplayEngineListener() {
+  ZX_ASSERT_MSG(check_all_calls_replayed_called_, "CheckAllCallsReplayed() not called on a mock");
+}
+
+void MockBanjoDisplayEngineListener::ExpectOnDisplayAdded(OnDisplayAddedChecker checker) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  expectations_.push_back({.on_display_added_checker = std::move(checker)});
+}
+
+void MockBanjoDisplayEngineListener::ExpectOnDisplayRemoved(OnDisplayRemovedChecker checker) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  expectations_.push_back(Expectation{.on_display_removed_checker = std::move(checker)});
+}
+
+void MockBanjoDisplayEngineListener::ExpectOnDisplayVsync(OnDisplayVsyncChecker checker) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  expectations_.push_back(Expectation{.on_display_vsync_checker = std::move(checker)});
+}
+
+void MockBanjoDisplayEngineListener::ExpectOnCaptureComplete(OnCaptureCompleteChecker checker) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  expectations_.push_back(Expectation{.on_capture_complete_checker = std::move(checker)});
+}
+
+void MockBanjoDisplayEngineListener::CheckAllCallsReplayed() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT_MSG(expectations_.size() == call_index_, "%zu expected calls were not received",
+                expectations_.size() - call_index_);
+  check_all_calls_replayed_called_ = true;
+}
+
+display_engine_listener_protocol_t MockBanjoDisplayEngineListener::GetProtocol() {
+  return display_engine_listener_protocol_t{
+      .ops = &display_engine_listener_protocol_ops_,
+      .ctx = this,
+  };
+}
+
+void MockBanjoDisplayEngineListener::DisplayEngineListenerOnDisplayAdded(
+    const raw_display_info_t* banjo_raw_display_info) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT_MSG(call_index_ < expectations_.size(), "All expected calls were already received");
+  Expectation& call_expectation = expectations_[call_index_];
+  ++call_index_;
+
+  ZX_ASSERT_MSG(call_expectation.on_display_added_checker != nullptr,
+                "Received call type does not match expected call type");
+  call_expectation.on_display_added_checker(banjo_raw_display_info);
+}
+
+void MockBanjoDisplayEngineListener::DisplayEngineListenerOnDisplayRemoved(
+    uint64_t banjo_display_id) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT_MSG(call_index_ < expectations_.size(), "All expected calls were already received");
+  Expectation& call_expectation = expectations_[call_index_];
+  ++call_index_;
+
+  ZX_ASSERT_MSG(call_expectation.on_display_removed_checker != nullptr,
+                "Received call type does not match expected call type");
+  call_expectation.on_display_removed_checker(banjo_display_id);
+}
+
+void MockBanjoDisplayEngineListener::DisplayEngineListenerOnDisplayVsync(
+    uint64_t banjo_display_id, zx_time_t banjo_timestamp,
+    const config_stamp_t* banjo_config_stamp) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT_MSG(call_index_ < expectations_.size(), "All expected calls were already received");
+  Expectation& call_expectation = expectations_[call_index_];
+  ++call_index_;
+
+  ZX_ASSERT_MSG(call_expectation.on_display_vsync_checker != nullptr,
+                "Received call type does not match expected call type");
+  call_expectation.on_display_vsync_checker(banjo_display_id, banjo_timestamp, banjo_config_stamp);
+}
+
+void MockBanjoDisplayEngineListener::DisplayEngineListenerOnCaptureComplete() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  ZX_ASSERT_MSG(call_index_ < expectations_.size(), "All expected calls were already received");
+  Expectation& call_expectation = expectations_[call_index_];
+  ++call_index_;
+
+  ZX_ASSERT_MSG(call_expectation.on_capture_complete_checker != nullptr,
+                "Received call type does not match expected call type");
+  call_expectation.on_capture_complete_checker();
+}
+
+}  // namespace display::testing
