54a0f2192ae8600493f233b4a467c9b48b0d7dd0
Sarah Chan <spqchan@google.com>
2025-01-02 13:53:53 -0800

    [driver] Add documentation and remove exports in fake-object
    Add documentation to the fake-object header file
    on how to use the class and its functions. Remove
    unnecessary exports for FakeHandleTable.
    
    Bug: 373444405
    Change-Id: I1f94f673c54f669e60a0305d468417e3e108acbe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1180471
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Fuchsia-Auto-Submit: Sarah Chan <spqchan@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- sdk/lib/driver/fake-object/cpp/fake-object.h
+++ sdk/lib/driver/fake-object/cpp/fake-object.h
@@ -18,14 +18,42 @@
 
 namespace fake_object {
 
+// This class is a fake Object base class that's provided to build fake
+// derived object types from //zircon/system/ulib/zx/include/lib/zx/object.h.
+//
+// The library provides strong symbols for the following zx::Object syscalls so that they
+// can be dispatched by the linker to virtual methods within the implemented Object:
+// - zx_object_get_child()
+// - zx_object_get_info()
+// - zx_object_get_property()
+// - zx_object_set_profile()
+// - zx_object_set_property()
+// - zx_object_signal()
+// - zx_object_signal_peer()
+// - zx_object_wait_one()
+// - zx_object_wait_async()
+//
+// To use this library, create a derived class that inherits from FakeObject. The derived
+// class can override the virtual methods associated with the syscall. For instance, if the
+// class wants to implement zx_object_get_info(), it should override get_info().
+//
+// All objects derived from the FakeObject class need to be added to the FakeHandleTable singleton
+// after they're instantiated. For example, say FakeBti is a derived class from FakeObject. After
+// a FakeBti object is instantiated, the FakeHandleTable singleton is retrieved via
+// FakeHandleTable() and the FakeBti object is added to it via its Add() function:
+//
+// std::shared_ptr<fake_object::FakeObject> new_bti = FakeBti::Create(paddrs, &new_bti);
+// zx::result result = fake_object::FakeHandleTable().Add(std::move(new_bti));
+//
 class FakeObject {
  public:
   FakeObject() = delete;
   explicit FakeObject(zx_obj_type_t type) : type_(type) {}
+
   // For each object-related syscall we stub out a fake-specific version that
-  // can be implemented within the derived fake objects. syscall symbols defined
-  // in the fake-object source will route to the fake impl or real impl
-  // depending on the handle's validity.
+  // can be overridden and implemented by the derived fake objects. These functions
+  // don't follow the C++ style guide since they need match the zx::Object class function
+  // names defined in //zircon/system/ulib/zx/include/lib/zx/object.h.
   virtual zx_status_t get_child(zx_handle_t /* handle */, uint64_t /* koid */,
                                 zx_rights_t /* rights */, zx_handle_t* /* out */) {
     return ZX_ERR_NOT_SUPPORTED;
@@ -67,9 +95,6 @@ class FakeObject {
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  // |zx_object_wait_many| is omitted because we would need to define what it means to wait on
-  // both real objects and fake objects at the same time due to it taking a handle table parameter.
-
   virtual zx_status_t wait_async(zx_handle_t /* handle */, zx_handle_t /* port */,
                                  uint64_t /* key */, zx_signals_t /* signals */,
                                  uint32_t /* options */) {
@@ -86,6 +111,10 @@ class FakeObject {
   zx_obj_type_t type_;
 };
 
+// FakeHandleTable manipulates handle-related syscalls to use the fake handles
+// vended by the fake-object library. All fake object instances are expected to be
+// added to the FakeHandleTable singleton.
+// See https://fuchsia.dev/fuchsia-src/concepts/kernel/handles for more details on handles.
 class FakeHandleTable {
  public:
   FakeHandleTable() = default;
@@ -96,11 +125,21 @@ class FakeHandleTable {
   FakeHandleTable(FakeHandleTable&&) = delete;
   FakeHandleTable& operator=(FakeHandleTable&&) = delete;
 
+  // Returns true if |handle| routes to a fake object added to a FakeHandleTable. This function
+  // is used decide whether the real or fake function should be called based on the handle.
   static bool IsValidFakeHandle(zx_handle_t handle);
 
-  zx::result<std::shared_ptr<FakeObject>> Get(zx_handle_t handle) __TA_EXCLUDES(lock_);
-  zx::result<> Remove(zx_handle_t handle) __TA_EXCLUDES(lock_);
+  // Creates and adds a fake handle for |obj| into FakeHandleTable. |obj| is moved and stored
+  // into FakeHandleTable.
   zx::result<zx_handle_t> Add(std::shared_ptr<FakeObject> obj) __TA_EXCLUDES(lock_);
+
+  // Returns a shared pointer to the FakeObject that maps to |handle|.
+  zx::result<std::shared_ptr<FakeObject>> Get(zx_handle_t handle) __TA_EXCLUDES(lock_);
+
+  // Remove the handle from FakeHandleTable.
+  zx::result<> Remove(zx_handle_t handle) __TA_EXCLUDES(lock_);
+
+  // Removes all handles in FakeHandleTable.
   void Clear() __TA_EXCLUDES(lock_);
 
   // Walks the handle table and calls |cb| on each handle that matches the
@@ -120,22 +159,26 @@ class FakeHandleTable {
     }
   }
 
+  // Prints information on all the handles in FakeHandleTable.
   void Dump() __TA_EXCLUDES(lock_);
-  // We use the overall size of the vector to calculate new indices
-  // so to determine the occupied size we have to verify each element.
+
   size_t size() __TA_EXCLUDES(lock_) {
     std::lock_guard lock(lock_);
     return handles_.size();
   }
 
  private:
+  // This prop name is used as a way to validate if a handle is backing a fake
+  // object. This allows us to check validity at any point in a process's lifecycle,
+  // including when it has begun tearing down various sorts of storage.
   static constexpr const char kFakeObjectPropName[ZX_MAX_NAME_LEN] = "FAKEOBJECT";
 
   std::mutex lock_;
   std::unordered_map<zx_handle_t, std::shared_ptr<FakeObject>> handles_ __TA_GUARDED(lock_);
 };
 
-// Singleton accessor for tests and any derived fake object type.
+// Singleton accessor to a FakeHandleTable for tests and any derived fake object type. All
+// fake object instances are expected to be added to this singleton.
 FakeHandleTable& FakeHandleTable();
 
 // Creates a base object for testing handle methods.
