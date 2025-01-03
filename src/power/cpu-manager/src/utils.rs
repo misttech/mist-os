211a56c5ced2c4a7ebd027c7d925bc2d1f099e8b
Suraj Malhotra <surajmalhotra@google.com>
2025-01-03 11:11:50 -0800

    [cpu-manager] Use new connect_to_services function
    This simplifies connection logic.
    
    Change-Id: Ib4e16c5b0f7b5703c0d37378cf79a0b5f5f639c8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1146699
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Didi She <didis@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/power/cpu-manager/src/utils.rs
+++ src/power/cpu-manager/src/utils.rs
@@ -5,7 +5,7 @@
 use anyhow::Error;
 use fidl_fuchsia_hardware_cpu_ctrl as fcpu_ctrl;
 use fuchsia_async::{DurationExt, TimeoutExt};
-use fuchsia_component::client::{self as fclient, connect_to_service_instance};
+use fuchsia_component::client::Service;
 use futures::{TryFutureExt, TryStreamExt};
 use std::cmp::Reverse;
 use std::collections::HashMap;
@@ -18,64 +18,42 @@ pub async fn get_cpu_ctrl_proxy(
     total_domain_count: u8,
     perf_rank: u8,
 ) -> Result<fcpu_ctrl::DeviceProxy, Error> {
-    let dir = fclient::open_service::<fcpu_ctrl::ServiceMarker>()
-        .expect("failed to open fuchsia.hardware.cpu.ctrl service directory");
-
-    let mut watcher = fuchsia_fs::directory::Watcher::new(&dir)
+    let mut instances = Service::open(fcpu_ctrl::ServiceMarker)
+        .expect("failed to open fuchsia.hardware.cpu.ctrl service directory")
+        .watch()
         .await
         .map_err(|e| anyhow::anyhow!("Failed to create watcher: {:?}", e))?;
-
     let mut proxies = HashMap::new();
+    while let Some(instance) = instances
+        .try_next()
+        .map_err(|e| anyhow::anyhow!("Failed to get service instance: {e:?}"))
+        .on_timeout(CPU_DRIVER_TIMEOUT.after_now(), || {
+            Err(anyhow::anyhow!("Timeout waiting for next service instance."))
+        })
+        .await?
+    {
+        let proxy = instance
+            .connect_to_device()
+            .map_err(|e| anyhow::anyhow!("Failed to connect to device: {:?}", e))?;
 
-    loop {
-        let next = watcher
-            .try_next()
-            .map_err(|e| anyhow::anyhow!("Failed to get next watch message: {e:?}"))
-            .on_timeout(CPU_DRIVER_TIMEOUT.after_now(), || {
-                Err(anyhow::anyhow!("Timeout waiting for next watcher message."))
-            })
-            .await?;
+        let relative_perf = proxy
+            .get_relative_performance()
+            .await?
+            .map_err(|e| anyhow::anyhow!("GetRelativePerformance returned err: {:?}", e))?;
+        log::info!(node_info:?, relative_perf:?; "CPU device detected");
+        if proxies.insert(relative_perf, proxy).is_some() {
+            log::warn!(
+                "CPU driver of relative performance {:?} showed up more than once",
+                relative_perf
+            );
+        }
 
-        if let Some(watch_msg) = next {
-            let filename = watch_msg
-                .filename
-                .as_path()
-                .to_str()
-                .ok_or_else(|| anyhow::anyhow!("Failed to convert filename to string"))?;
-            if filename != "." {
-                if watch_msg.event == fuchsia_fs::directory::WatchEvent::ADD_FILE
-                    || watch_msg.event == fuchsia_fs::directory::WatchEvent::EXISTING
-                {
-                    let proxy = connect_to_service_instance::<fcpu_ctrl::ServiceMarker>(filename)?
-                        .connect_to_device()
-                        .map_err(|e| anyhow::anyhow!("Failed to connect to device: {:?}", e))?;
-
-                    let relative_perf = proxy.get_relative_performance().await?.map_err(|e| {
-                        anyhow::anyhow!("GetRelativePerformance returned err: {:?}", e)
-                    })?;
-                    log::info!(
-                        node_info,
-                        instance_filename = filename,
-                        relative_perf;
-                        "CPU device detected"
-                    );
-                    if proxies.insert(relative_perf, proxy).is_some() {
-                        log::warn!(
-                            "CPU driver of relative performance {:?} showed up more than once",
-                            relative_perf
-                        );
-                    }
-
-                    if proxies.len() == total_domain_count as usize {
-                        // Sort by relative_perf from highest to lowest.
-                        let mut proxies_sort = proxies.into_iter().collect::<Vec<_>>();
-                        proxies_sort.sort_by_key(|r| Reverse(r.0));
-                        return Ok(proxies_sort[perf_rank as usize].1.clone());
-                    }
-                }
-            }
-        } else {
-            return Err(anyhow::anyhow!("Directory watcher returned None entry."));
+        if proxies.len() == total_domain_count as usize {
+            // Sort by relative_perf from highest to lowest.
+            let mut proxies_sort = proxies.into_iter().collect::<Vec<_>>();
+            proxies_sort.sort_by_key(|r| Reverse(r.0));
+            return Ok(proxies_sort[perf_rank as usize].1.clone());
         }
     }
+    Err(anyhow::anyhow!("Failed to get all devices"))
 }
