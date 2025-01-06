ecc0b4df5ca28ded72f5023c717316aac6fab39a
Steven Grady <slgrady@google.com>
2025-01-06 11:47:27 -0800

    [ffx] Update strict tests
    Added a new test to validate that `ffx target list` will correctly
    return `RCS=N` if it can't make a connection.
    
    In addition, strict tests were a bit of a mess. Cleaned up the handling
    of configs.
    
    Change-Id: Ifc31c3a7efe4bee1288f4df15adaf7a6d0cb15ac
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1180414
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Steven Grady <slgrady@google.com>

--- src/developer/ffx/tests/e2e/python/ffx_strict.py
+++ src/developer/ffx/tests/e2e/python/ffx_strict.py
@@ -9,43 +9,13 @@ import json
 import logging
 import os
 import subprocess
-from typing import Any, List, Optional, Text, Tuple
+from typing import Any, List, Optional, Text
 
 import ffxtestcase
 from honeydew.errors import FfxCommandError
 from mobly import asserts, test_runner
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
-
-
-def build_strict_config_args(
-    ssh_pub_key: Text, ssh_private_key: Text
-) -> List[Text]:
-    environ = os.environ
-    config = "--config"
-    retval = []
-    retval.append(config)
-    out_dir = os.environ.get("TEST_UNDECLARED_OUTPUTS_DIR")
-    caller_frame = inspect.currentframe()
-    if caller_frame:
-        caller_frame = caller_frame.f_back
-    if out_dir and caller_frame:
-        out_dir = os.path.join(out_dir, f"{caller_frame.f_code.co_name}.log")
-    if not out_dir:
-        out_dir = "/dev/null"
-    _LOGGER.info(f"Setting ffx config log dir to {out_dir}")
-    retval.append(f"test.output_path={out_dir}")
-    retval.append(config)
-    retval.append(f"ssh.pub={ssh_pub_key}")
-    retval.append(config)
-    retval.append(f"ssh.priv={ssh_private_key}")
-    retval.append(config)
-    retval.append(
-        f"fastboot.devices_file.path={environ['HOME']}/.fastboot/devices"
-    )
-    retval.append(config)
-    retval.append(f"log.dir={environ['FUCHSIA_TEST_OUTDIR']}/ffx_logs")
-    return retval
 
 
 class FfxStrictTest(ffxtestcase.FfxTestCase):
@@ -57,13 +27,15 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
         super().setup_class()
         self.dut_ssh_address = self.dut.ffx.get_target_ssh_address()
         self.dut_name = self.dut.ffx.get_target_name()
+        self.ssh_private_key: Optional[str] = None
 
     def setup_test(self) -> None:
         """Each test must run without the daemon."""
         super().setup_test()
         self.dut.ffx.run(["daemon", "stop"])
 
-    def _get_config_str(self, keys: List[str]) -> str:
+    # Return list of ["key=val"]
+    def _get_configs(self, keys: List[str]) -> List[str]:
         outputs = []
         for key in keys:
             output = json.loads(
@@ -75,54 +47,83 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
             )
             output = output.strip().replace('"', "")
             outputs.append(f"{key}={output}")
-        return ",".join(outputs)
+        return outputs
 
-    def _get_ssh_key_information(self) -> Tuple[Text, Text]:
-        ssh_pub_output = json.loads(
-            self.run_ffx(["config", "get", "-s", "first", "ssh.pub"])
-        )
-        if isinstance(ssh_pub_output, List):
-            print("pub is list")
-            ssh_pub = ssh_pub_output[0].strip().replace('"', "")
-        elif isinstance(ssh_pub_output, Text):
-            ssh_pub = ssh_pub_output.strip().replace('"', "")
-
-        print(ssh_pub)
-
+    # Look up and store the user's private key
+    def _get_ssh_private_key(self) -> None:
+        if self.ssh_private_key:
+            return
         ssh_priv_output = json.loads(
             self.run_ffx(["config", "get", "-s", "first", "ssh.priv"])
         )
         ssh_priv = ""
         if isinstance(ssh_priv_output, List):
-            print("priv is List")
             ssh_priv = ssh_priv_output[0].strip().replace('"', "")
         elif isinstance(ssh_priv_output, Text):
             ssh_priv = ssh_priv_output.strip().replace('"', "")
 
-        print(ssh_priv)
+        self.ssh_private_key = ssh_priv
 
-        return (ssh_pub, ssh_priv)
+    # Build the default configs passed to strict invocations of ffx
+    def _build_strict_config_args(self, extra_configs: List[str]) -> List[Text]:
+        environ = os.environ
+        configs = extra_configs
+        # Get output directory
+        out_dir = os.environ.get("TEST_UNDECLARED_OUTPUTS_DIR")
+        caller_frame = inspect.currentframe()
+        if caller_frame:
+            caller_frame = caller_frame.f_back
+        if out_dir and caller_frame:
+            out_dir = os.path.join(
+                out_dir, f"{caller_frame.f_code.co_name}.log"
+            )
+        if not out_dir:
+            out_dir = "/dev/null"
+        _LOGGER.info(f"Setting ffx config log dir to {out_dir}")
+        # Get other required configs
+        configs.append(f"test.output_path={out_dir}")
+        self._get_ssh_private_key()
+        configs.append(f"ssh.priv={self.ssh_private_key}")
+        configs.append(
+            f"fastboot.devices_file.path={environ['HOME']}/.fastboot/devices"
+        )
+        configs.append(f"log.dir={environ['FUCHSIA_TEST_OUTDIR']}/ffx_logs")
+        # Return as list of args: ["-c, "key1=val1", "-c", "key2=val2", ...]
+        retval = []
+        for c in configs:
+            retval.append("--config")
+            retval.append(c)
+        return retval
 
-    def _run_strict_ffx(
-        self, cmd: List[str], target: Optional[str] = None
+    # Run ffx --strict <cmd> with the specified configs, and
+    # optionally with a target
+    def _run_strict_ffx_with_configs(
+        self, cmd: List[str], configs: List[str], target: Optional[str]
     ) -> Any:
-        ssh_args = self._get_ssh_key_information()
         all_args = [
             "--strict",
             "--machine",
             "json",
             "-o",
             "/dev/null",
-            *build_strict_config_args(*ssh_args),
+            *configs,
         ]
         if target is not None:
             all_args += ["-t", target]
         all_args += cmd
         return json.loads(self.run_ffx(all_args))
 
+    # Run ffx --strict <cmd> with the default configs, and
+    # optionally with a target
+    def _run_strict_ffx(
+        self, cmd: List[str], target: Optional[str] = None
+    ) -> Any:
+        return self._run_strict_ffx_with_configs(
+            cmd, self._build_strict_config_args([]), target
+        )
+
     def test_target_echo_no_start_daemon(self) -> None:
         """Test `ffx --strict target echo` does not affect daemon state."""
-        self._get_ssh_key_information()
         output = self._run_strict_ffx(
             [
                 "target",
@@ -138,7 +139,6 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
 
     def test_strict_errors_with_target_name(self) -> None:
         """Test `ffx --strict target echo` fails when attempt discovery."""
-        self._get_ssh_key_information()
         with asserts.assert_raises(subprocess.CalledProcessError):
             self._run_strict_ffx(
                 [
@@ -151,7 +151,6 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
 
     def test_strict_can_check_for_no_target(self) -> None:
         """Test `ffx --strict target echo` requires a target."""
-        self._get_ssh_key_information()
         with asserts.assert_raises(subprocess.CalledProcessError):
             try:
                 self._run_strict_ffx(["target", "echo"], None)
@@ -165,7 +164,6 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
 
     def test_strict_can_accept_no_target(self) -> None:
         """Test `ffx --strict product download` doesn't require a target."""
-        self._get_ssh_key_information()
         with asserts.assert_raises(subprocess.CalledProcessError):
             try:
                 self._run_strict_ffx(
@@ -181,22 +179,42 @@ class FfxStrictTest(ffxtestcase.FfxTestCase):
 
     def test_target_list_strict(self) -> None:
         """Test `ffx --strict target list` does not affect daemon state."""
-        self._get_ssh_key_information()
-        keys = ["emu.instance_dir"]
-        configs = self._get_config_str(keys)
-        output = self._run_strict_ffx(
+        emu_config = self._get_configs(["emu.instance_dir"])
+        configs = self._build_strict_config_args(emu_config)
+        output = self._run_strict_ffx_with_configs(
             [
-                "-c",
-                configs,
                 "target",
                 "list",
                 self.dut_name,
             ],
+            configs,
             None,
         )
         asserts.assert_equal(output[0]["rcs_state"], "Y")
         with asserts.assert_raises(FfxCommandError):
             self.dut.ffx.run(["-c", "daemon.autostart=false", "daemon", "echo"])
+
+    def test_target_list_strict_fails(self) -> None:
+        """Test `ffx --strict target list` correctly reports RCS=N."""
+        emu_config = self._get_configs(["emu.instance_dir"])
+        configs = self._build_strict_config_args(emu_config)
+        # Ensure that we cannot find the ssh.priv file
+        new_configs = []
+        for c in configs:
+            if c.startswith("ssh.priv="):
+                new_configs.append(c + "NONEXISTENT")
+            else:
+                new_configs.append(c)
+        output = self._run_strict_ffx_with_configs(
+            [
+                "target",
+                "list",
+                self.dut_name,
+            ],
+            new_configs,
+            None,
+        )
+        asserts.assert_equal(output[0]["rcs_state"], "N")
 
 
 if __name__ == "__main__":
