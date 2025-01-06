7ae0a95114b53d5fdfa555e38a4bda0a50c0e6a4
Joshua Seaton <joshuaseaton@google.com>
2025-01-06 13:16:29 -0800

    [kernel][phys] Undo trampoline-related hand-off hacks
    Fixed-address booting of the x86 kernel is no longer a thing!
    Accordingly, we can get rid of a number of hacks that futzed with
    allocations to move them out of the way of the fixed-address load image.
    
    Bug: 42164859
    Change-Id: Ib78c1184e49ea5fc35c94222d3dd54ff56108a4d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1173979
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

--- zircon/kernel/phys/handoff-prep.cc
+++ zircon/kernel/phys/handoff-prep.cc
@@ -266,65 +266,33 @@ void HandoffPrep::PublishLog(ktl::string_view name, Log&& log) {
     return;
   }
 
-  // TODO(https://fxbug.dev/42164859): While trampoline booting is in effect in
-  // the x86 codepath, care needs to be taken with `Allocation`s made before
-  // the fixed-address image recharacterization that TrampolineBoot does. In
-  // particular, we cannot turn the log in its current location into a PhysVmo
-  // as that might lie within the fixed-address image location. Accordingly, we
-  // allocate a copy of it and register that.
   const size_t content_size = log.size_bytes();
   Allocation buffer = ktl::move(log).TakeBuffer();
   ZX_ASSERT(content_size <= buffer.size_bytes());
 
-  size_t aligned_size = (content_size + ZX_PAGE_SIZE - 1) & -ZX_PAGE_SIZE;
-  fbl::AllocChecker ac;
-  ktl::span copy =
-      Allocation::New(ac, memalloc::Type::kPhysLog, aligned_size, ZX_PAGE_SIZE).release();
-  ZX_ASSERT_MSG(ac.check(), "cannot allocate %zu bytes for phys log", aligned_size);
-  memcpy(copy.data(), buffer.get(), content_size);
+  PublishExtraVmo(MakePhysVmo(buffer.data(), name, content_size));
 
-  PublishExtraVmo(MakePhysVmo(copy, name, content_size));
-
-  // TODO(https://fxbug.dev/42164859): As above, we do not want to
-  // absent-mindedly free a region that occupies space within the fixed-address
-  // image location. Until TrampolineBoot is removed from kernel boot, we
-  // defensively leak the log buffer allocation.
+  // Intentionally leak as the PhysVmo now tracks this memory.
   ktl::ignore = buffer.release();
 }
 
 void HandoffPrep::UsePackageFiles(KernelStorage::Bootfs kernel_package) {
+  auto& pool = Allocation::GetPool();
   for (auto it = kernel_package.begin(); it != kernel_package.end(); ++it) {
     ktl::span data = it->data;
+    uintptr_t start = reinterpret_cast<uintptr_t>(data.data());
+    // These are decompressed BOOTFS payloads, so there is only padding up to
+    // the next page boundary.
+    ktl::span aligned_data{data.data(), (data.size_bytes() + ZX_PAGE_SIZE - 1) & -ZX_PAGE_SIZE};
     if (it->name == "version-string.txt"sv) {
       ktl::string_view version{reinterpret_cast<const char*>(data.data()), data.size()};
       SetVersionString(version);
     } else if (it->name == "vdso"sv) {
-      // TODO(https://fxbug.dev/42164859): Ideally we would just recharacterize
-      // `data` as a kVdso range. However, while TrampolineBoot is in use, this
-      // range might overlap with the fixed-address kernel image. Accordingly
-      // we side-step this by always copying the contents over to a newly
-      // allocated range guaranteed not to overlap.
-      size_t aligned_size = (data.size() + ZX_PAGE_SIZE - 1) & -ZX_PAGE_SIZE;
-      fbl::AllocChecker ac;
-      Allocation vdso = Allocation::New(ac, memalloc::Type::kVdso, aligned_size, ZX_PAGE_SIZE);
-      ZX_ASSERT(ac.check());
-      memcpy(vdso.get(), data.data(), data.size());
-      handoff_->vdso = MakePhysVmo(vdso.data(), "vdso/next"sv, data.size());
-      ktl::ignore = vdso.release();
+      ZX_ASSERT(pool.UpdateRamSubranges(memalloc::Type::kVdso, start, data.size()).is_ok());
+      handoff_->vdso = MakePhysVmo(aligned_data, "vdso/next"sv, data.size());
     } else if (it->name == "userboot"sv) {
-      // TODO(https://fxbug.dev/42164859): Ideally we would just recharacterize
-      // `data` as a kUserboot range. However, while TrampolineBoot is in use,
-      // this range might overlap with the fixed-address kernel image.
-      // Accordingly we side-step this by always copying the contents over to a
-      // newly allocated range guaranteed not to overlap.
-      size_t aligned_size = (data.size() + ZX_PAGE_SIZE - 1) & -ZX_PAGE_SIZE;
-      fbl::AllocChecker ac;
-      Allocation userboot =
-          Allocation::New(ac, memalloc::Type::kUserboot, aligned_size, ZX_PAGE_SIZE);
-      ZX_ASSERT(ac.check());
-      memcpy(userboot.get(), data.data(), data.size());
-      handoff_->userboot = MakePhysVmo(userboot.data(), "userboot"sv, data.size());
-      ktl::ignore = userboot.release();
+      ZX_ASSERT(pool.UpdateRamSubranges(memalloc::Type::kUserboot, start, data.size()).is_ok());
+      handoff_->userboot = MakePhysVmo(aligned_data, "userboot"sv, data.size());
     }
   }
   if (auto result = kernel_package.take_error(); result.is_error()) {
