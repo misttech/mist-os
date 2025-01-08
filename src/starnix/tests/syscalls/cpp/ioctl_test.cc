c31d5c9e2d06517d8dedfcc91f3f9b2528ac3bca
Evan Wildenhain <wildenhain@google.com>
2025-01-08 13:10:28 -0800

    [starnix] Implement netmask ioctls
    Implements the SIOCGIFNETMASK and SIOCSIFNETMASK ioctls, and adds test
    cases for them.
    
    Fixed: 388247541
    Bug: 385171649
    Change-Id: I64ff315e5c55c7e716b9e298c0748aa696497284
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1183634
    Commit-Queue: Evan Wildenhain <wildenhain@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@google.com>

--- src/starnix/tests/syscalls/cpp/ioctl_test.cc
+++ src/starnix/tests/syscalls/cpp/ioctl_test.cc
@@ -149,8 +149,6 @@ void SetIfAddr(fbl::unique_fd& fd, in_addr_t addr) {
   };
   strncpy(ifr.ifr_name, kLoopbackIfName, IFNAMSIZ);
   ASSERT_EQ(ioctl(fd.get(), SIOCSIFADDR, &ifr), 0) << strerror(errno);
-
-  ASSERT_NO_FATAL_FAILURE(GetIfAddr(fd, addr));
 }
 
 TEST_F(IoctlTest, SIOCSIFADDR_Success) {
@@ -165,7 +163,99 @@ TEST_F(IoctlTest, SIOCSIFADDR_Success) {
   ASSERT_NO_FATAL_FAILURE(GetIfAddr(fd, htonl(INADDR_LOOPBACK)));
 }
 
-TEST_F(IoctlTest, SIOCSIFADDR_AgreesWithNetlink) {
+// Uses netlink to dump all interface addresses and prefix lens into the given vector of in_addr_t.
+void DumpIpv4AddressesOnInterface(uint32_t if_index,
+                                  std::vector<std::pair<in_addr_t, uint8_t>>& addresses) {
+  fbl::unique_fd nlsock(socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE));
+  ASSERT_TRUE(nlsock) << strerror(errno);
+
+  struct {
+    nlmsghdr hdr;
+    ifaddrmsg ifa;
+  } req = {};
+
+  req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(ifaddrmsg));
+  req.hdr.nlmsg_type = RTM_GETADDR;
+  req.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+  req.ifa.ifa_family = AF_INET;
+
+  ASSERT_EQ(send(nlsock.get(), &req, req.hdr.nlmsg_len, 0), static_cast<int>(req.hdr.nlmsg_len))
+      << strerror(errno);
+
+  constexpr size_t kBufSize = 4096;
+  char buf[kBufSize];
+
+  while (true) {
+    ssize_t len = recv(nlsock.get(), &buf, kBufSize, 0);
+    ASSERT_GT(len, 0) << strerror(errno);
+    for (nlmsghdr* hdr = reinterpret_cast<nlmsghdr*>(buf); NLMSG_OK(hdr, len);
+         hdr = NLMSG_NEXT(hdr, len)) {
+      if (hdr->nlmsg_type == NLMSG_DONE) {
+        return;
+      }
+      if (hdr->nlmsg_type == NLMSG_ERROR) {
+        FAIL() << "netlink error";
+      }
+
+      ifaddrmsg* ifa = static_cast<ifaddrmsg*>(NLMSG_DATA(hdr));
+      if (ifa->ifa_family != AF_INET || ifa->ifa_index != if_index) {
+        continue;
+      }
+
+      rtattr* rta = IFA_RTA(ifa);
+      in_addr addr;
+      memcpy(&addr, RTA_DATA(rta), sizeof(addr));
+      addresses.emplace_back(addr.s_addr, ifa->ifa_prefixlen);
+    }
+  }
+}
+
+// Uses netlink to install the given IPv4 address on the given interface.
+void InstallIpv4AddressOnInterface(const char* if_name, in_addr_t addr, uint8_t prefix_len) {
+  fbl::unique_fd nlsock(socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE));
+  ASSERT_TRUE(nlsock) << strerror(errno);
+
+  // Prepare the netlink request.
+  struct {
+    nlmsghdr hdr;
+    ifaddrmsg ifa;
+    rtattr rta;
+    in_addr in_addr;
+  } req;
+
+  req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(ifaddrmsg)) + RTA_LENGTH(sizeof(in_addr));
+  req.hdr.nlmsg_type = RTM_NEWADDR;
+  req.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL | NLM_F_ACK;
+  req.hdr.nlmsg_seq = 1;  // Arbitrary non-zero value.
+
+  req.ifa.ifa_family = AF_INET;
+  req.ifa.ifa_prefixlen = prefix_len;
+  req.ifa.ifa_flags = 0;
+  req.ifa.ifa_scope = RT_SCOPE_UNIVERSE;
+  req.ifa.ifa_index = if_nametoindex(if_name);
+  assert(req.ifa.ifa_index != 0);  // Ensure the interface exists.
+
+  req.rta.rta_type = IFA_LOCAL;
+  req.rta.rta_len = RTA_LENGTH(sizeof(in_addr));
+  req.in_addr.s_addr = addr;
+
+  ASSERT_EQ(send(nlsock.get(), &req, req.hdr.nlmsg_len, 0), static_cast<int>(req.hdr.nlmsg_len))
+      << strerror(errno);
+
+  constexpr size_t kBufSize = 4096;
+  char buf[kBufSize];
+  ssize_t len = recv(nlsock.get(), &buf, kBufSize, 0);
+  ASSERT_GT(len, 0) << strerror(errno);
+
+  nlmsghdr* response_hdr = reinterpret_cast<nlmsghdr*>(buf);
+  ASSERT_TRUE(NLMSG_OK(response_hdr, len)) << "Invalid netlink response";
+  ASSERT_EQ(response_hdr->nlmsg_type, NLMSG_ERROR) << "Unexpected netlink response type";
+
+  nlmsgerr* err = reinterpret_cast<nlmsgerr*>(NLMSG_DATA(response_hdr));
+  ASSERT_EQ(err->error, 0) << "Netlink error: " << strerror(-err->error);
+}
+
+TEST_F(IoctlTest, SIOCSIFADDR_WithMultipleAddressesOnInterface) {
   // TODO(https://fxbug.dev/317285180) don't skip on baseline
   if (!test_helper::HasSysAdmin()) {
     GTEST_SKIP() << "SIOCSIFADDR requires root, skipping...";
@@ -199,30 +289,114 @@ TEST_F(IoctlTest, SIOCSIFADDR_AgreesWithNetlink) {
   ssize_t len = recv(nlsock.get(), &buf, kBufSize, 0);
   ASSERT_GT(len, 0) << strerror(errno);
 
-  bool found = false;
-  for (nlmsghdr* hdr = reinterpret_cast<nlmsghdr*>(buf); NLMSG_OK(hdr, len);
-       hdr = NLMSG_NEXT(hdr, len)) {
-    if (hdr->nlmsg_type == NLMSG_DONE) {
-      break;
-    }
-    if (hdr->nlmsg_type == NLMSG_ERROR) {
-      FAIL() << "netlink error";
-    }
+  std::vector<std::pair<in_addr_t, uint8_t>> addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
 
-    ifaddrmsg* ifa = static_cast<ifaddrmsg*>(NLMSG_DATA(hdr));
-    if (ifa->ifa_family != AF_INET || ifa->ifa_index != if_nametoindex(kLoopbackIfName)) {
-      continue;
-    }
+  std::vector<std::pair<in_addr_t, uint8_t>> expected_addresses = {{htonl(INADDR_LOOPBACK), 8}};
 
-    rtattr* rta = IFA_RTA(ifa);
-    in_addr addr;
-    memcpy(&addr, RTA_DATA(rta), sizeof(addr));
+  EXPECT_EQ(addresses, expected_addresses);
 
-    EXPECT_EQ(addr.s_addr, htonl(INADDR_LOOPBACK));
-    found = true;
+  // Install another IP address so we can exercise what happens if there are multiple.
+  InstallIpv4AddressOnInterface("lo", inet_addr("1.2.3.4"), 24);
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+
+  expected_addresses = {{inet_addr("1.2.3.4"), 24}, {htonl(INADDR_LOOPBACK), 8}};
+  EXPECT_EQ(addresses, expected_addresses);
+
+  SetIfAddr(fd, inet_addr("5.6.7.8"));
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+
+  expected_addresses = {{inet_addr("5.6.7.8"), 8}, {htonl(INADDR_LOOPBACK), 8}};
+  EXPECT_EQ(addresses, expected_addresses);
+
+  // SIOCSIFADDR with the all-zeros address should remove the first address it finds.
+  SetIfAddr(fd, inet_addr("0.0.0.0"));
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+
+  expected_addresses = {{htonl(INADDR_LOOPBACK), 8}};
+  EXPECT_EQ(addresses, expected_addresses);
+}
+
+void GetIfNetmask(fbl::unique_fd& fd, in_addr_t expected_addr) {
+  ifreq ifr;
+  ifr.ifr_netmask = {.sa_family = AF_INET};
+  strncpy(ifr.ifr_name, kLoopbackIfName, IFNAMSIZ);
+  ASSERT_EQ(ioctl(fd.get(), SIOCGIFNETMASK, &ifr), 0) << strerror(errno);
+
+  EXPECT_EQ(strncmp(ifr.ifr_name, kLoopbackIfName, IFNAMSIZ), 0);
+  sockaddr_in* s = reinterpret_cast<sockaddr_in*>(&ifr.ifr_netmask);
+  EXPECT_EQ(s->sin_family, AF_INET);
+  EXPECT_EQ(s->sin_port, 0);
+  EXPECT_EQ(s->sin_addr.s_addr, expected_addr);
+}
+
+TEST_F(IoctlTest, SIOCGIFNETMASK_Success) {
+  in_addr_t expected_netmask = inet_addr("255.0.0.0");
+  ASSERT_NO_FATAL_FAILURE(GetIfNetmask(fd, expected_netmask));
+}
+
+void SetIfNetmask(fbl::unique_fd& fd, in_addr_t addr) {
+  ifreq ifr;
+  *(reinterpret_cast<sockaddr_in*>(&ifr.ifr_netmask)) = sockaddr_in{
+      .sin_family = AF_INET,
+      .sin_addr = {.s_addr = addr},
+  };
+  strncpy(ifr.ifr_name, kLoopbackIfName, IFNAMSIZ);
+  ASSERT_EQ(ioctl(fd.get(), SIOCSIFNETMASK, &ifr), 0) << strerror(errno);
+}
+
+TEST_F(IoctlTest, SIOCSIFNETMASK_Success) {
+  // TODO(https://fxbug.dev/317285180) don't skip on baseline
+  if (!test_helper::HasSysAdmin()) {
+    GTEST_SKIP() << "SIOCSIFNETMASK requires root, skipping...";
   }
 
-  EXPECT_TRUE(found);
+  ASSERT_NO_FATAL_FAILURE(SetIfNetmask(fd, inet_addr("255.255.0.0")));
+  ASSERT_NO_FATAL_FAILURE(GetIfNetmask(fd, inet_addr("255.255.0.0")));
+  ASSERT_NO_FATAL_FAILURE(SetIfNetmask(fd, inet_addr("255.0.0.0")));
+  ASSERT_NO_FATAL_FAILURE(GetIfNetmask(fd, inet_addr("255.0.0.0")));
+}
+
+TEST_F(IoctlTest, SIOCSIFNETMASK_WithMultipleAddressesOnInterface) {
+  // TODO(https://fxbug.dev/317285180) don't skip on baseline
+  if (!test_helper::HasSysAdmin()) {
+    GTEST_SKIP() << "SIOCSIFNETMASK requires root, skipping...";
+  }
+
+  std::vector<std::pair<in_addr_t, uint8_t>> addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+  std::vector<std::pair<in_addr_t, uint8_t>> expected_addresses = {{htonl(INADDR_LOOPBACK), 8}};
+  EXPECT_EQ(addresses, expected_addresses);
+
+  InstallIpv4AddressOnInterface("lo", inet_addr("1.2.3.4"), 24);
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+  expected_addresses = {
+      {inet_addr("1.2.3.4"), 24},
+      {htonl(INADDR_LOOPBACK), 8},
+  };
+  EXPECT_EQ(addresses, expected_addresses);
+
+  SetIfNetmask(fd, inet_addr("255.255.0.0"));
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+  expected_addresses = {
+      {inet_addr("1.2.3.4"), 16},
+      {htonl(INADDR_LOOPBACK), 8},
+  };
+  EXPECT_EQ(addresses, expected_addresses);
+
+  // Clear the address so that loopback is left in the same state as when we started.
+  SetIfAddr(fd, inet_addr("0.0.0.0"));
+  addresses = {};
+  ASSERT_NO_FATAL_FAILURE(DumpIpv4AddressesOnInterface(if_nametoindex(kLoopbackIfName), addresses));
+  expected_addresses = {
+      {htonl(INADDR_LOOPBACK), 8},
+  };
+  EXPECT_EQ(addresses, expected_addresses);
 }
 
 short GetLoopbackIfFlags(fbl::unique_fd& fd) {
