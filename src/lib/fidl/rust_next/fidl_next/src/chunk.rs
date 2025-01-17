9db740c4df1cb0dcaae71bd84d78ea62bce8e994
David Koloski <dkoloski@google.com>
2025-01-17 08:15:33 -0800

    [fidl][rust] Add macro to convert bytes to chunks
    Change-Id: Iaf7f188646cc926811cd8abb482d9a25c551a712
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1189213
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Commit-Queue: David Koloski <dkoloski@google.com>
    Fuchsia-Auto-Submit: David Koloski <dkoloski@google.com>

--- src/lib/fidl/rust_next/fidl_next/src/chunk.rs
+++ src/lib/fidl/rust_next/fidl_next/src/chunk.rs
@@ -10,3 +10,18 @@ pub const CHUNK_SIZE: usize = 8;
 
 /// A group of eight bytes, aligned to an 8-byte boundary.
 pub type Chunk = u64_le;
+
+/// Returns a slice of chunks with the same bytewise value as the given bytes.
+#[macro_export]
+macro_rules! chunks {
+    ($(
+        $b0:literal, $b1:literal, $b2:literal, $b3:literal,
+        $b4:literal, $b5:literal, $b6:literal, $b7:literal
+    ),* $(,)?) => {
+        [
+            $($crate::Chunk::from_native(u64::from_le_bytes([
+                $b0, $b1, $b2, $b3, $b4, $b5, $b6, $b7,
+            ])),)*
+        ]
+    }
+}
