25399f23481e23ec2dadad442488c8e92c10b2e2
James Robinson <jamesr@google.com>
2025-01-17 15:59:35 -0800

    [tee][storage] Single thread simplifications
    This updates the persistent storage types to be single threaded. This is
    mostly mechanical changes with a few exceptions. The type swaps are:
    
    std::sync::Arc -> std::rc::Rc
    fuchsia_sync::Mutex -> std::cell:RefCell
    std::atomic::AtomicU64 -> u64
    
    The guard types and access patterns are slightly different for these
    types so some callsites are slightly rewritten. Operations which used to
    access an atomic now require an exclusive reference.
    
    Bug: 360942417
    Change-Id: I62c1d13448f811bcb5fc0f72b06650c674926dd9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1189392
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: James Robinson <jamesr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

--- src/tee/runtime/api_impl/src/storage.rs
+++ src/tee/runtime/api_impl/src/storage.rs
@@ -2,17 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(https://fxbug.dev/360942417): Remove.
-#![allow(unused_variables)]
-
-use fuchsia_sync::Mutex;
+use std::cell::{Ref, RefCell, RefMut};
 use std::cmp::min;
 use std::collections::btree_map::Entry as BTreeMapEntry;
 use std::collections::hash_map::Entry as HashMapEntry;
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::ops::Bound;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::Arc;
+use std::rc::Rc;
 use tee_internal::{
     Attribute, AttributeId, BufferOrValue, Error, HandleFlags, MemRef, ObjectEnumHandle,
     ObjectHandle, ObjectInfo, Result as TeeResult, Storage as TeeStorage, Type, Usage, ValueFields,
@@ -145,20 +141,20 @@ impl Object for PersistentObject {
 
 // A handle's view into a persistent object.
 struct PersistentObjectView {
-    object: Arc<Mutex<PersistentObject>>,
+    object: Rc<RefCell<PersistentObject>>,
     flags: HandleFlags,
     data_position: usize,
 }
 
 impl PersistentObjectView {
     fn get_info(&self) -> ObjectInfo {
-        let obj = self.object.lock();
+        let obj = self.object.borrow();
         obj.get_info(obj.data_size, self.data_position)
     }
 
     // See read_object_data().
     fn read_data<'a>(&mut self, buffer: &'a mut [u8]) -> TeeResult<&'a [u8]> {
-        let obj = self.object.lock();
+        let obj = self.object.borrow();
         let read_size = min(obj.data_size - self.data_position, buffer.len());
         let written = &mut buffer[..read_size];
         if read_size > 0 {
@@ -173,7 +169,7 @@ impl PersistentObjectView {
         if data.is_empty() {
             return Ok(());
         }
-        let mut obj = self.object.lock();
+        let mut obj = self.object.borrow_mut();
         let write_end = self.data_position + data.len();
 
         if write_end > DATA_MAX_POSITION {
@@ -190,7 +186,7 @@ impl PersistentObjectView {
 
     // See truncate_object_data().
     fn truncate_data(&self, size: usize) -> TeeResult {
-        let mut obj = self.object.lock();
+        let mut obj = self.object.borrow_mut();
 
         // It's okay to set the size past the position in either direction.
         // However, the spec does not actually cover the case where the
@@ -207,7 +203,7 @@ impl PersistentObjectView {
     fn seek_data(&mut self, offset: isize, whence: Whence) -> TeeResult {
         let start = match whence {
             Whence::DataSeekCur => self.data_position,
-            Whence::DataSeekEnd => self.object.lock().data_size,
+            Whence::DataSeekEnd => self.object.borrow().data_size,
             Whence::DataSeekSet => 0,
         };
         let new_position = start.saturating_add_signed(offset);
@@ -229,23 +225,23 @@ struct EnumState {
 // A B-tree since enumeration needs to deal in key (i.e., ID) ordering.
 //
 // Further, the key represents a separately owned copy of the ID; we do this
-// instead of representing the key as an Arc<Vec<u8>> as then we would no
+// instead of representing the key as an Rc<Vec<u8>> as then we would no
 // longer be able to perform look-up with slices - since Borrow is not
-// implemented for Arc - and would instead have to dynamically allocate a new
+// implemented for Rc - and would instead have to dynamically allocate a new
 // key for the look-up. Better to not touch the heap when bad inputs are
 // provided.
-type PersistentIdMap = BTreeMap<Vec<u8>, Arc<Mutex<PersistentObject>>>;
+type PersistentIdMap = BTreeMap<Vec<u8>, Rc<RefCell<PersistentObject>>>;
 
-type PersistentHandleMap = HashMap<ObjectHandle, Mutex<PersistentObjectView>>;
-type PersistentEnumHandleMap = HashMap<ObjectEnumHandle, Mutex<EnumState>>;
+type PersistentHandleMap = HashMap<ObjectHandle, RefCell<PersistentObjectView>>;
+type PersistentEnumHandleMap = HashMap<ObjectEnumHandle, RefCell<EnumState>>;
 
 // A class abstraction implementing the persistent storage interface.
 struct PersistentObjects {
     by_id: PersistentIdMap,
     by_handle: PersistentHandleMap,
     enum_handles: PersistentEnumHandleMap,
-    next_handle_value: AtomicU64,
-    next_enum_handle_value: AtomicU64,
+    next_handle_value: u64,
+    next_enum_handle_value: u64,
 }
 
 impl PersistentObjects {
@@ -254,8 +250,8 @@ impl PersistentObjects {
             by_id: PersistentIdMap::new(),
             by_handle: PersistentHandleMap::new(),
             enum_handles: HashMap::new(),
-            next_handle_value: AtomicU64::new(1), // Always odd, per the described convention above
-            next_enum_handle_value: AtomicU64::new(1),
+            next_handle_value: 1, // Always odd, per the described convention above
+            next_enum_handle_value: 1,
         }
     }
 
@@ -296,7 +292,7 @@ impl PersistentObjects {
                     return Err(Error::AccessConflict);
                 }
                 {
-                    let mut obj_old = obj_ref.lock();
+                    let mut obj_old = obj_ref.borrow_mut();
                     for handle in obj_old.handles.iter() {
                         let removed = self.by_handle.remove(&handle).is_some();
                         debug_assert!(removed);
@@ -307,7 +303,7 @@ impl PersistentObjects {
             }
             None => {
                 let id = obj.id.clone();
-                let obj_ref = Arc::new(Mutex::new(obj));
+                let obj_ref = Rc::new(RefCell::new(obj));
                 let inserted = self.by_id.insert(id, obj_ref.clone());
                 debug_assert!(inserted.is_none());
                 obj_ref
@@ -326,13 +322,13 @@ impl PersistentObjects {
         }?;
 
         {
-            let mut obj = obj_ref.lock();
+            let mut obj = obj_ref.borrow_mut();
 
             // At any given time, the number of object references should be
             // greater than or equal to the number of handle map values + the
             // number of object ID map values, which should be equal to the #
             // of open handles to that object + 1.
-            debug_assert!(Arc::strong_count(obj_ref) >= obj.handles.len() + 1);
+            debug_assert!(Rc::strong_count(obj_ref) >= obj.handles.len() + 1);
 
             // If we previously closed the last handle to the object and are
             // now reopening its first active handle, overwrite the base flags
@@ -382,14 +378,14 @@ impl PersistentObjects {
     // expects that the operation has been validated.
     fn open_internal(
         &mut self,
-        object: Arc<Mutex<PersistentObject>>,
+        object: Rc<RefCell<PersistentObject>>,
         flags: HandleFlags,
     ) -> ObjectHandle {
         let handle = self.mint_handle();
-        let inserted = object.lock().handles.insert(handle);
+        let inserted = object.borrow_mut().handles.insert(handle);
         debug_assert!(inserted);
         let view = PersistentObjectView { object, flags, data_position: 0 };
-        let inserted = self.by_handle.insert(handle, Mutex::new(view)).is_none();
+        let inserted = self.by_handle.insert(handle, RefCell::new(view)).is_none();
         debug_assert!(inserted);
         handle
     }
@@ -401,8 +397,8 @@ impl PersistentObjects {
         match self.by_handle.entry(handle) {
             HashMapEntry::Occupied(entry) => {
                 {
-                    let view = &entry.get().lock();
-                    let mut obj = view.object.lock();
+                    let view = entry.get().borrow_mut();
+                    let mut obj = view.object.borrow_mut();
                     let removed = obj.handles.remove(&handle);
                     debug_assert!(removed);
                 }
@@ -421,11 +417,11 @@ impl PersistentObjects {
         match self.by_handle.entry(handle) {
             HashMapEntry::Occupied(entry) => {
                 {
-                    let state = &entry.get().lock();
+                    let state = entry.get().borrow();
                     if !state.flags.contains(HandleFlags::DATA_ACCESS_WRITE_META) {
                         return Err(Error::AccessDenied);
                     }
-                    let obj = state.object.lock();
+                    let obj = state.object.borrow();
                     debug_assert_eq!(obj.handles.len(), 1);
                     let removed = self.by_id.remove(&obj.id).is_some();
                     debug_assert!(removed);
@@ -443,7 +439,7 @@ impl PersistentObjects {
     fn rename(&mut self, handle: ObjectHandle, new_id: &[u8]) -> TeeResult {
         match self.by_handle.entry(handle) {
             HashMapEntry::Occupied(handle_entry) => {
-                let state = handle_entry.get().lock();
+                let state = handle_entry.get().borrow();
                 if !state.flags.contains(HandleFlags::DATA_ACCESS_WRITE_META) {
                     return Err(Error::AccessDenied);
                 }
@@ -454,7 +450,7 @@ impl PersistentObjects {
                         let _ = id_entry.insert(state.object.clone());
                     }
                 };
-                let mut obj = state.object.lock();
+                let mut obj = state.object.borrow_mut();
                 let removed = self.by_id.remove(&obj.id);
                 debug_assert!(removed.is_some());
                 obj.id = new_id;
@@ -464,18 +460,29 @@ impl PersistentObjects {
         }
     }
 
-    // Returns a read guard to the associated object view, if `handle` is
+    // Returns a shared reference to the associated object view, if `handle` is
     // valid; panics otherwise.
-    fn get(&self, handle: ObjectHandle) -> &'_ Mutex<PersistentObjectView> {
-        self.by_handle.get(&handle).unwrap_or_else(|| panic!("{handle:?} is not a valid handle"))
+    fn get(&self, handle: ObjectHandle) -> Ref<'_, PersistentObjectView> {
+        self.by_handle
+            .get(&handle)
+            .unwrap_or_else(|| panic!("{handle:?} is not a valid handle"))
+            .borrow()
     }
 
+    // Returns an exclusive reference to the associated object view, if `handle` is
+    // valid; panics otherwise.
+    fn get_mut(&self, handle: ObjectHandle) -> RefMut<'_, PersistentObjectView> {
+        self.by_handle
+            .get(&handle)
+            .unwrap_or_else(|| panic!("{handle:?} is not a valid handle"))
+            .borrow_mut()
+    }
     // See allocate_persistent_object_enumerator().
     fn allocate_enumerator(&mut self) -> ObjectEnumHandle {
         let enumerator = self.mint_enumerator_handle();
 
         let previous =
-            self.enum_handles.insert(enumerator.clone(), Mutex::new(EnumState { id: None }));
+            self.enum_handles.insert(enumerator.clone(), RefCell::new(EnumState { id: None }));
         debug_assert!(previous.is_none());
         enumerator
     }
@@ -494,7 +501,7 @@ impl PersistentObjects {
     fn reset_enumerator(&mut self, enumerator: ObjectEnumHandle) -> () {
         match self.enum_handles.get(&enumerator) {
             Some(state) => {
-                state.lock().id = None;
+                state.borrow_mut().id = None;
             }
             None => panic!("{enumerator:?} is not a valid enumerator handle"),
         }
@@ -508,7 +515,7 @@ impl PersistentObjects {
     ) -> TeeResult<(ObjectInfo, &'a [u8])> {
         match self.enum_handles.get(&enumerator) {
             Some(state) => {
-                let mut state = state.lock();
+                let mut state = state.borrow_mut();
                 let next = if state.id.is_none() {
                     self.by_id.first_key_value()
                 } else {
@@ -523,7 +530,7 @@ impl PersistentObjects {
                     let written = &mut id_buffer[..id.len()];
                     written.copy_from_slice(id);
                     state.id = Some(id.clone());
-                    Ok((obj.lock().get_info(/*data_size=*/ 0, /*data_position=*/ 0), written))
+                    Ok((obj.borrow().get_info(/*data_size=*/ 0, /*data_position=*/ 0), written))
                 } else {
                     Err(Error::ItemNotFound)
                 }
@@ -532,13 +539,17 @@ impl PersistentObjects {
         }
     }
 
-    fn mint_handle(&self) -> ObjectHandle {
+    fn mint_handle(&mut self) -> ObjectHandle {
         // Per the described convention above, always odd. (Initial value is 1.)
-        ObjectHandle::from_value(self.next_handle_value.fetch_add(2, Ordering::Relaxed))
+        let handle_value = self.next_handle_value;
+        self.next_handle_value += 2;
+        ObjectHandle::from_value(handle_value)
     }
 
-    fn mint_enumerator_handle(&self) -> ObjectEnumHandle {
-        ObjectEnumHandle::from_value(self.next_enum_handle_value.fetch_add(1, Ordering::Relaxed))
+    fn mint_enumerator_handle(&mut self) -> ObjectEnumHandle {
+        let handle_value = self.next_enum_handle_value;
+        self.next_enum_handle_value += 1;
+        ObjectEnumHandle::from_value(handle_value)
     }
 }
 
@@ -558,7 +569,7 @@ impl Storage {
         if is_transient_handle(object) {
             unimplemented!();
         }
-        self.persistent_objects.get(object).lock().get_info()
+        self.persistent_objects.get(object).get_info()
     }
 
     /// Restricts the usage of an open object handle.
@@ -569,7 +580,7 @@ impl Storage {
             unimplemented!();
         }
 
-        self.persistent_objects.get(object).lock().object.lock().restrict_usage(usage)
+        self.persistent_objects.get(object).object.borrow_mut().restrict_usage(usage)
     }
 }
 
@@ -624,7 +635,7 @@ impl Storage {
         if is_transient_handle(object) {
             unimplemented!();
         } else {
-            copy_from_key(&self.persistent_objects.get(object).lock().object.lock().key, buffer)
+            copy_from_key(&self.persistent_objects.get(object).object.as_ref().borrow().key, buffer)
         }
     }
 
@@ -653,7 +664,7 @@ impl Storage {
         if is_transient_handle(object) {
             unimplemented!();
         } else {
-            copy_from_key(&self.persistent_objects.get(object).lock().object.lock().key)
+            copy_from_key(&self.persistent_objects.get(object).object.borrow().key)
         }
     }
 
@@ -672,8 +683,8 @@ impl Storage {
     }
     pub fn allocate_transient_object(
         &self,
-        object_type: Type,
-        max_size: u32,
+        _object_type: Type,
+        _max_size: u32,
     ) -> TeeResult<ObjectHandle> {
         unimplemented!()
     }
@@ -691,7 +702,7 @@ impl Storage {
     pub fn populate_transient_object(
         &self,
         object: ObjectHandle,
-        attrs: &[Attribute],
+        _attrs: &[Attribute],
     ) -> TeeResult {
         assert!(is_transient_handle(object));
         unimplemented!()
@@ -708,12 +719,12 @@ pub fn init_value_attribute(id: AttributeId, value: ValueFields) -> Attribute {
     Attribute { id, content: BufferOrValue { value } }
 }
 
-pub fn copy_object_attributes(src: ObjectHandle, dest: ObjectHandle) -> TeeResult {
+pub fn copy_object_attributes(_src: ObjectHandle, dest: ObjectHandle) -> TeeResult {
     assert!(is_transient_handle(dest));
     unimplemented!()
 }
 
-pub fn generate_key(object: ObjectHandle, key_size: u32, params: &[Attribute]) -> TeeResult {
+pub fn generate_key(_object: ObjectHandle, _key_size: u32, _params: &[Attribute]) -> TeeResult {
     unimplemented!()
 }
 
@@ -772,9 +783,8 @@ impl Storage {
         let (key, usage, base_flags) = if attribute_src.is_null() {
             (Key::Data(NoKey {}), Usage::default(), HandleFlags::empty())
         } else if is_persistent_handle(attribute_src) {
-            let view_guard = self.persistent_objects.get(attribute_src);
-            let view = view_guard.lock();
-            let obj = view.object.lock();
+            let view = self.persistent_objects.get(attribute_src);
+            let obj = view.object.borrow();
             (obj.key.clone(), obj.usage, obj.base_flags)
         } else {
             unimplemented!();
@@ -867,7 +877,7 @@ impl Storage {
         buffer: &'a mut [u8],
     ) -> TeeResult<&'a [u8]> {
         assert!(is_persistent_handle(object));
-        self.persistent_objects.get(object).lock().read_data(buffer)
+        self.persistent_objects.get_mut(object).read_data(buffer)
     }
 
     /// Writes the provided data to the object's data stream at the handle's
@@ -882,7 +892,7 @@ impl Storage {
     /// Panics if `object` is invalid or does not have write access.
     pub fn write_object_data(&self, object: ObjectHandle, buffer: &[u8]) -> TeeResult {
         assert!(is_persistent_handle(object));
-        self.persistent_objects.get(object).lock().write_data(buffer)
+        self.persistent_objects.get_mut(object).write_data(buffer)
     }
 
     /// Truncates or zero-extends the object's data stream to provided size.
@@ -893,7 +903,7 @@ impl Storage {
     /// Panics if `object` is invalid or does not have write access.
     pub fn truncate_object_data(&self, object: ObjectHandle, size: usize) -> TeeResult {
         assert!(is_persistent_handle(object));
-        self.persistent_objects.get(object).lock().truncate_data(size)
+        self.persistent_objects.get(object).truncate_data(size)
     }
 
     /// Updates the handle's data positition, seeking at an offset from a
@@ -910,7 +920,7 @@ impl Storage {
         whence: Whence,
     ) -> TeeResult {
         assert!(is_persistent_handle(object));
-        self.persistent_objects.get(object).lock().seek_data(offset, whence)
+        self.persistent_objects.get_mut(object).seek_data(offset, whence)
     }
 }
 
