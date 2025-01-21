de7e227a4e34c432163827661f105e0d223ac7ae
Adam Perry <adamperry@google.com>
2025-01-21 12:47:13 -0800

    [examples] Add real test cases to rust integration test examples.
    Change-Id: I1bb4c16c1dd2e2fb566d66f465ac85c10fd4a29f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1190819
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- examples/rust/testing/tests/integration_test_two.rs
+++ examples/rust/testing/tests/integration_test_two.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-fn main() {
+#[test]
+fn two_times_two() {
     assert_eq!(4, testing::mult_two(2));
 }
