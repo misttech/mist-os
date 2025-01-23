a6fb4e7c5441e90d1d0b92f76591caa6646f1037
Justin Mattson <jmatt@google.com>
2025-01-23 12:23:39 -0800

    [drivers][power] Restore optimizations
    It is now possible to take a wake lease during a suspend
    callback, so restore optimizations where we avoid taking leases
    when the system is resumed and fulfill previously requested
    timeout values with leases taken during a suspend callback.
    
    Change-Id: Ia9065bdcf2585ad925f4c4d88f6bd0e5ff048fd2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1191994
    Commit-Queue: Justin Mattson <jmatt@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- sdk/lib/driver/power/cpp/wake-lease.cc
+++ sdk/lib/driver/power/cpp/wake-lease.cc
@@ -53,11 +53,17 @@ WakeLease::WakeLease(async_dispatcher_t* dispatcher, std::string_view lease_name
 
 bool WakeLease::HandleInterrupt(zx::duration timeout) {
   // Only acquire a wake lease if the system state is appropriate.
-  // TODO(https://fxbug.dev/389163722) Restore this optimization once we can
-  // acquire a wake lease in a suspend handler.
-  // if (!system_suspended_) {
-  //   return false;
-  // }
+  if (!system_suspended_) {
+    // If we don't acquire a wake lease, store the time we would have held it
+    // until. If we start suspension before this time, we'll acquire a wake
+    // lease then and hold it until the timeout.
+    prevent_sleep_before_ = zx::clock::get_monotonic().get() + timeout.to_nsecs();
+    return false;
+  }
+
+  // Since we're acquiring a wake lease, reset our sleep prevention time since
+  // we don't need to check upon suspension starting.
+  prevent_sleep_before_ = 0;
   return AcquireWakeLease(timeout);
 }
 
@@ -104,7 +110,7 @@ bool WakeLease::AcquireWakeLease(zx::duration timeout) {
 void WakeLease::DepositWakeLease(zx::eventpair wake_lease, zx::time timeout_deadline) {
   if (lease_) {
     if (timeout_deadline < lease_task_.last_deadline()) {
-      // If the current least out lives the new one, don't need to do anything.
+      // If the current lease out lives the new one, don't need to do anything.
       return;
     }
     // If already holding a lease, cancel the current timeout.
@@ -141,6 +147,16 @@ void WakeLease::OnResume(OnResumeCompleter::Sync& completer) {
 }
 
 void WakeLease::OnSuspendStarted(OnSuspendStartedCompleter::Sync& completer) {
+  // Check if we've moved past any previously set timeout for which we did
+  // NOT acquire a lease because the system wasn't suspended.
+  zx_time_t current_time = zx::clock::get_monotonic().get();
+  if (current_time < prevent_sleep_before_) {
+    if (log_) {
+      fdf::warn("Acquiring lease to honor previously set timeout.");
+    }
+    AcquireWakeLease(zx::nsec(prevent_sleep_before_ - current_time));
+  }
+
   system_suspended_ = true;
   completer.Reply();
 }
