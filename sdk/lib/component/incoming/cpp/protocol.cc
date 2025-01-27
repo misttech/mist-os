4bbe987cd6e3a6ff969358ef58439a4c8c53f755
Brandon Castellano <bcastell@google.com>
2025-01-27 10:15:30 -0800

    [component] Use r* for OpenServiceRoot
    As part of enforcing hierarchal rights with Open3, we need to ensure the
    set of readable directory rights is included whenever opening /svc. This
    ensures the availability of many expected rights on the resulting
    connection (e.g. enumerating services/protocols, or calling stat on the
    directory).
    
    This brings the implementation in line with //sdk/lib/sys which was
    updated in I1940c0a0f631347a35231a122555e56c07892fa7.
    
    Bug: 346585458
    Change-Id: I6696bc4c383f51427ea175b9a9fb5f40911b9368
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1190772
    Commit-Queue: Brandon Castellano <bcastell@google.com>
    Reviewed-by: Claire Gonyeo <cgonyeo@google.com>

--- sdk/lib/component/incoming/cpp/protocol.cc
+++ sdk/lib/component/incoming/cpp/protocol.cc
@@ -8,13 +8,16 @@
 namespace component {
 
 zx::result<fidl::ClientEnd<fuchsia_io::Directory>> OpenServiceRoot(std::string_view path) {
-  // NB: This can't be `return Connect<fuchsia_io::Directory>(path);` because some paths may be both
-  // services and directories.
+  // NOTE: Although connecting to a protocol does not require rights, many service directory clients
+  // assume a basic set of operations (e.g. enumerating service instances).
+  //
+  // The fuchsia.io/PERM_READABLE constant (r* in component manifests) capture these rights.
+  constexpr uint64_t kServiceRootFlags =
+      uint64_t{fuchsia_io::wire::kPermReadable | fuchsia_io::Flags::kProtocolDirectory};
   auto [client, server] = fidl::Endpoints<fuchsia_io::Directory>::Create();
-  return zx::make_result(fdio_open3(std::string(path).c_str(),
-                                    static_cast<uint64_t>(fuchsia_io::Flags::kProtocolDirectory),
-                                    server.TakeChannel().release()),
-                         std::move(client));
+  return zx::make_result(
+      fdio_open3(std::string(path).c_str(), kServiceRootFlags, server.TakeChannel().release()),
+      std::move(client));
 }
 
 }  // namespace component
