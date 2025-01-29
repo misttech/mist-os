a906b8627c3ea389371a9ddba0a0ad55b89e5d5b
Yilong Li <liyl@google.com>
2025-01-28 17:52:11 -0800

    [intel-display] Move ReadExtendedEdid() implementation out of GMBusI2c.
    The ReadExtendedEdid() implementation works for all types of DDC
    controllers, including both GMBus DDC controller and the DP AUX
    DDC controller. This change moves the ReadExtendedEdid()
    implementation to a global method so that it can be used by both
    HdmiDisplay and DpDisplay.
    
    Test: core.x64 booted on NUC11TNHv5 with MIMO HDMI Display.
    Test: core.x64 booted on NUC11TNHv5 with HDMI headless plug.
    Bug: 390552176
    Change-Id: I0ebca72236555755b32bc12bb1ebf9961f7ea099
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1191930
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Victor Costan <costan@google.com>

--- src/graphics/display/drivers/intel-display/i2c/gmbus-i2c.cc
+++ src/graphics/display/drivers/intel-display/i2c/gmbus-i2c.cc
@@ -9,6 +9,7 @@
 
 #include <fbl/auto_lock.h>
 
+#include "src/graphics/display/drivers/intel-display/edid-reader.h"
 #include "src/graphics/display/drivers/intel-display/i2c/gmbus-gpio.h"
 #include "src/graphics/display/drivers/intel-display/registers-gmbus.h"
 #include "src/graphics/display/lib/driver-utils/poll-until.h"
@@ -314,46 +315,7 @@ zx::result<> GMBusI2c::ReadEdidBlock(int index, std::span<uint8_t, edid::kBlockS
 }
 
 zx::result<fbl::Vector<uint8_t>> GMBusI2c::ReadExtendedEdid() {
-  std::array<uint8_t, edid::kBlockSize> base_edid;
-  zx::result<> base_edid_result = ReadEdidBlock(0, base_edid);
-  if (base_edid_result.is_error()) {
-    FDF_LOG(ERROR, "Failed to read EDID base block: %s", base_edid_result.status_string());
-    return base_edid_result.take_error();
-  }
-
-  // VESA Enhanced Extended Display Identification Data (E-EDID) Standard,
-  // Release A, Revision 2, dated September 25, 2006, revised December 31, 2020.
-  // Section 3.1 "EDID Format Overview", page 19.
-  static constexpr int kBaseEdidExtensionBlockCountOffset = 126;
-  const int extension_block_count = base_edid[kBaseEdidExtensionBlockCountOffset];
-
-  fbl::Vector<uint8_t> extended_edid;
-  fbl::AllocChecker alloc_checker;
-  const size_t extended_edid_size =
-      static_cast<size_t>(extension_block_count + 1) * edid::kBlockSize;
-  extended_edid.resize(extended_edid_size, 0, &alloc_checker);
-
-  if (!alloc_checker.check()) {
-    FDF_LOG(ERROR, "Failed to allocate %zu bytes for E-EDID", extended_edid_size);
-    return zx::error(ZX_ERR_NO_MEMORY);
-  }
-
-  std::ranges::copy(base_edid, extended_edid.begin());
-
-  for (int extension_block_index = 1; extension_block_index <= extension_block_count;
-       ++extension_block_index) {
-    int extension_block_offset = extension_block_index * static_cast<int>(edid::kBlockSize);
-    std::span<uint8_t, edid::kBlockSize> extension_block(
-        extended_edid.begin() + extension_block_offset, edid::kBlockSize);
-    zx::result<> extension_block_result = ReadEdidBlock(extension_block_index, extension_block);
-    if (extension_block_result.is_error()) {
-      FDF_LOG(ERROR, "Failed to read EDID extension block #%d: %s", extension_block_index,
-              extension_block_result.status_string());
-      return extension_block_result.take_error();
-    }
-  }
-
-  return zx::ok(std::move(extended_edid));
+  return intel_display::ReadExtendedEdid(fit::bind_member<&GMBusI2c::ReadEdidBlock>(this));
 }
 
 bool GMBusI2c::GMBusWrite(uint8_t addr, const uint8_t* buf, uint8_t size) {
