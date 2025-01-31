7fc7ab25f55a3acba91f72c894d7ba7017dd9809
Caslyn Tonelli <caslyn@google.com>
2025-01-30 19:17:46 -0800

    [dl] Add dl_iterate_phdr runtime test
    This CL adds a test that will check `dl_iterate_phdr` output after
    dlopen-ing and dlclosing a module.
    
    Bug: 338238626
    Cq-Include-Trybots: luci.turquoise.global.try:run-postsubmit-tryjobs
    Change-Id: I15458d7dc366bcf534d949151046fcd0bb0cae80
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1191633
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Caslyn Tonelli <caslyn@google.com>

--- sdk/lib/dl/test/dl-iterate-phdr-tests.cc
+++ sdk/lib/dl/test/dl-iterate-phdr-tests.cc
@@ -10,15 +10,32 @@
 
 namespace {
 
+using dl::testing::TestModule;
+using dl::testing::TestSym;
+
 using dl::testing::DlTests;
 TYPED_TEST_SUITE(DlTests, dl::testing::TestTypes);
 
 // This is a wrapper around `dl_phdr_info` that makes it easier to
 // perform comparisons with other `dl_phdr_info` instances.
-struct DlPhdrInfo {
+class DlPhdrInfo {
+ public:
+  using Elf = elfldltl::Elf<>;
+  using Phdr = Elf::Phdr;
+
   dl_phdr_info info;
 
   explicit DlPhdrInfo(dl_phdr_info info) : info(info) {}
+
+  std::string name() const {
+    auto path = std::filesystem::path(info.dlpi_name);
+    return path.filename();
+  }
+
+  // The number of modules that have been loaded at startup or by dlopen.
+  constexpr size_t loaded() const { return info.dlpi_adds; }
+  // The number of modules that have been unloaded with dlclose.
+  constexpr size_t unloaded() const { return info.dlpi_subs; }
 
   // TODO(https://fxbug.dev/331421403): Check tls_data, tls_modid fields.
   // This will compare module-specific `dl_phdr_info` fields for equality. This
@@ -28,6 +45,25 @@ struct DlPhdrInfo {
     return strcmp(info.dlpi_name, other.info.dlpi_name) == 0 &&
            info.dlpi_addr == other.info.dlpi_addr && info.dlpi_phdr == other.info.dlpi_phdr &&
            info.dlpi_phnum == other.info.dlpi_phnum;
+  }
+
+  // Check whether the given `addr` is within the address range of any one of
+  // this module's phdrs. This is used to test whether this module is
+  // responsible for a given symbol pointer.
+  bool contains_addr(uintptr_t addr) const {
+    std::span phdrs{reinterpret_cast<const Phdr*>(info.dlpi_phdr), info.dlpi_phnum};
+    auto load_bias = info.dlpi_addr;
+    for (const Phdr& phdr : phdrs) {
+      // Only consider PT_LOAD phdrs in this search.
+      if (phdr.type == elfldltl::ElfPhdrType::kLoad) {
+        size_t start = load_bias + phdr.vaddr;
+        size_t end = start + phdr.memsz;
+        if (addr >= start && addr <= end) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 };
 
@@ -70,6 +106,75 @@ TYPED_TEST(DlTests, DlIteratePhdrStartupModules) {
     EXPECT_EQ(gStartupPhdrInfo, startup_info_list);
   } else {
     EXPECT_THAT(gStartupPhdrInfo, ::testing::IsSubsetOf(startup_info_list));
+  }
+}
+
+// Test the following as it affects `dl_iterate_phdr` output:
+// dlopen module
+// `dl_iterate_phdr` output includes new module
+// dlclose module
+// `dl_iterate_phdr` output doesn't include the module
+TYPED_TEST(DlTests, DlIteratePhdrBasic) {
+  const std::string kRet17File = TestModule("ret17");
+
+  if constexpr (!TestFixture::kProvidesDlIteratePhdr) {
+    GTEST_SKIP() << "test requires dl_iterate_phdr";
+  }
+
+  // Record initial values to compare against during the test.
+  PhdrInfoList initial_info_list;
+  EXPECT_EQ(this->DlIteratePhdr(CollectPhdrInfo, &initial_info_list), 0);
+
+  DlPhdrInfo last_phdr_info = initial_info_list.back();
+  const size_t loaded = last_phdr_info.loaded();
+  const size_t unloaded = last_phdr_info.unloaded();
+
+  this->ExpectRootModule(kRet17File);
+
+  auto open_ret17 = this->DlOpen(kRet17File.c_str(), RTLD_NOW | RTLD_LOCAL);
+  ASSERT_TRUE(open_ret17.is_ok()) << open_ret17.error_value();
+  EXPECT_TRUE(open_ret17.value());
+
+  // Check that a struct `dl_pdhr_info` is produced for the dlopen-ed module.
+  PhdrInfoList open_info_list;
+  EXPECT_EQ(this->DlIteratePhdr(CollectPhdrInfo, &open_info_list), 0);
+  EXPECT_EQ(open_info_list.size(), initial_info_list.size() + 1);
+
+  DlPhdrInfo ret17_phdr_info = open_info_list.back();
+  EXPECT_EQ(ret17_phdr_info.name(), kRet17File);
+
+  // Check that the `.dlpi_adds` counter is adjusted.
+  EXPECT_EQ(ret17_phdr_info.loaded(), loaded + 1);
+
+  // Look up a symbol from the module and expect that its pointer value should
+  // be within the address range of the module's phdrs from its
+  // `dl_phdr_info`.
+  auto ret17_test_start = this->DlSym(open_ret17.value(), TestSym("TestStart").c_str());
+  ASSERT_TRUE(ret17_test_start.is_ok()) << ret17_test_start.error_value();
+  EXPECT_TRUE(ret17_test_start.value());
+
+  EXPECT_TRUE(ret17_phdr_info.contains_addr(reinterpret_cast<uintptr_t>(ret17_test_start.value())));
+
+  ASSERT_TRUE(this->DlClose(open_ret17.value()).is_ok());
+
+  // A final check that dl-closing the module will remove its entry and update
+  // the `.dlpi_subs` counter.
+  PhdrInfoList close_info_list;
+  EXPECT_EQ(this->DlIteratePhdr(CollectPhdrInfo, &close_info_list), 0);
+
+  // The last entry should be the same as at the beginning of the test.
+  DlPhdrInfo test_last_phdr_info = close_info_list.back();
+
+  if (TestFixture::kDlCloseUnloadsModules) {
+    EXPECT_EQ(test_last_phdr_info.unloaded(), unloaded + 1);
+    EXPECT_EQ(test_last_phdr_info, last_phdr_info);
+    EXPECT_EQ(close_info_list.size(), initial_info_list.size());
+  } else {
+    // Musl-Fuchsia's dlclose is a no-op and does not change dl_iterate_phdr
+    // output: the module entry is preserved.
+    EXPECT_EQ(test_last_phdr_info.unloaded(), unloaded);
+    EXPECT_EQ(test_last_phdr_info, ret17_phdr_info);
+    EXPECT_EQ(close_info_list.size(), open_info_list.size());
   }
 }
 
