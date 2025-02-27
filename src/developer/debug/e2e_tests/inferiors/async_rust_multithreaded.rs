5f403f655756285e256bf1f4bec19606bdc2187e
Jacob Rutherford <jruthe@google.com>
2025-02-27 09:21:30 -0800

    [debugger] Display Scope names in async-backtrace
    This CL fetches and displays the fuchsia-async Scope name added in
    fxrev.dev/1213426 instead of the pointer value for the Scope. The
    pointer can be displayed alongside the name by passing the verbose
    option to async-backtrace.
    
    Change-Id: I5eb9ed9dc05d0d4e07f841cf3b11ef1b42702917
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1215005
    Fuchsia-Auto-Submit: Jacob Rutherford <jruthe@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/developer/debug/e2e_tests/inferiors/async_rust_multithreaded.rs
+++ src/developer/debug/e2e_tests/inferiors/async_rust_multithreaded.rs
@@ -9,5 +9,15 @@ async fn main() {
     let _task_a = fasync::Task::spawn(async {});
     let _task_b = fasync::Task::spawn(async {});
     fasync::Timer::new(std::time::Duration::from_secs(1)).await;
+
+    let scope = fasync::Scope::new_with_name("scope");
+    let _scope_task = scope.spawn(foo(8));
+    let child_scope = scope.new_child_with_name("child");
+    let _child_task = child_scope.spawn(foo(21));
+
     panic!();
+}
+
+async fn foo(i: u64) {
+    fasync::Timer::new(std::time::Duration::from_secs(i)).await;
 }
