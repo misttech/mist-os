90c400dcc50acaca4dadba76a5fd21a28c1b72d7
Benjamin Lerman <qsr@chromium.org>
2025-02-28 06:01:52 -0800

    [starnix] Add rt_sigreturn syscall for arch32
    Bug: b/395946855
    Change-Id: I494cef48b37c262df1f7ddfd5a5bb0f87bdfe1a8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1217084
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/kernel/syscalls/table.rs
+++ src/starnix/kernel/syscalls/table.rs
@@ -145,6 +145,7 @@ pub fn dispatch_syscall(
         pub use crate::signals::syscalls::{
             sys_arch32_rt_sigaction, sys_arch32_sigaltstack, sys_kill as sys_arch32_kill,
             sys_rt_sigprocmask as sys_arch32_rt_sigprocmask,
+            sys_rt_sigreturn as sys_arch32_rt_sigreturn,
             sys_rt_tgsigqueueinfo as sys_arch32_rt_tgsigqueueinfo, sys_tgkill as sys_arch32_tgkill,
             sys_wait4 as sys_arch32_wait4,
         };
@@ -262,6 +263,7 @@ pub fn dispatch_syscall(
             rmdir[1],
             rt_sigaction[4],
             rt_sigprocmask[4],
+            rt_sigreturn[0],
             rt_tgsigqueueinfo[4],
             sched_getaffinity[3],
             sched_getscheduler[1],
