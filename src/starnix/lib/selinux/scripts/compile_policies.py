be62b7e2b35217fa66f00432b5607bd0e57669c4
Wez <wez@google.com>
2025-03-04 06:37:51 -0800

    [sestarnix] (Re)Compile legacy micro-policies with composite ones
    Rather than requiring the legacy monolithic "micro-policies" to be
    rebuilt manually by developers, integrate them with the policy
    compilation script, for consistency.
    
    Bug: 374489695
    Change-Id: I201fae5370fa24f954c7f80f747da75cd538bf1e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1218891
    Commit-Queue: Wez <wez@google.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Nelly Vouzoukidou <nellyv@google.com>

--- src/starnix/lib/selinux/scripts/compile_policies.py
+++ src/starnix/lib/selinux/scripts/compile_policies.py
@@ -29,94 +29,126 @@ _OUTPUT_POLICY_DIRECTORY = (
     f"{_SCRIPT_DIRECTORY}/../testdata/composite_policies/compiled"
 )
 
+_LEGACY_POLICY_DIRECTORY = f"{_SCRIPT_DIRECTORY}/../testdata/micro_policies"
+
 _INITIAL_SIDS_PATH = os.path.join(_SCRIPT_DIRECTORY, "initial_sids")
 
-_COMPOSITE_POLICY_PATHS = (
+_COMPOSITE_POLICY_PATHS = [
     (
-        (
+        [
             "base_policy.conf",
             "new_file/bounded_transition_policy.conf",
-        ),
+        ],
         "bounded_transition_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/minimal_policy.conf",
-        ),
+        ],
         "minimal_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/class_defaults_policy.conf",
-        ),
+        ],
         "class_defaults_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/exceptions_config_policy.conf",
-        ),
+        ],
         "exceptions_config_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/role_transition_policy.conf",
-        ),
+        ],
         "role_transition_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/role_transition_not_allowed_policy.conf",
-        ),
+        ],
         "role_transition_not_allowed_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/type_transition_policy.conf",
-        ),
+        ],
         "type_transition_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/range_transition_policy.conf",
-        ),
+        ],
         "range_transition_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/minimal_policy.conf",
             "new_file/allow_fork.conf",
-        ),
+        ],
         "allow_fork.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/minimal_policy.conf",
             "new_file/with_unlabeled_access_domain_policy.conf",
-        ),
+        ],
         "with_unlabeled_access_domain_policy.pp",
     ),
     (
-        (
+        [
             "base_policy.conf",
             "new_file/minimal_policy.conf",
             "new_file/with_unlabeled_access_domain_policy.conf",
             "new_file/with_additional_domain_policy.conf",
-        ),
+        ],
         "with_additional_domain_policy.pp",
     ),
-)
+]
 
-_HANDLE_UNKNOWN_POLICY_INPUTS = ("new_file/handle_unknown_policy.conf",)
+_HANDLE_UNKNOWN_POLICY_INPUTS = [
+    "new_file/handle_unknown_policy.conf",
+]
 _HANDLE_UNKNOWN_POLICY_OUTPUT = "handle_unknown_policy-%s.pp"
+
+_LEGACY_POLICIES = [
+    # keep-sorted start
+    "allow_a_attr_b_attr_class0_perm0_policy",
+    "allow_a_t_a1_attr_class0_perm0_a2_attr_class0_perm1_policy",
+    "allow_a_t_b_attr_class0_perm0_policy",
+    "allow_a_t_b_t_class0_perm0_policy",
+    "allow_with_constraints_policy",
+    "constraints_policy",
+    "file_no_defaults_policy",
+    "file_range_source_high_policy",
+    "file_range_source_low_high_policy",
+    "file_range_source_low_policy",
+    "file_range_target_high_policy",
+    "file_range_target_low_high_policy",
+    "file_range_target_low_policy",
+    "file_source_defaults_policy",
+    "file_target_defaults_policy",
+    "hooks_tests_policy",
+    "minimal_policy",
+    "multiple_levels_and_categories_policy",
+    "no_allow_a_attr_b_attr_class0_perm0_policy",
+    "no_allow_a_t_b_attr_class0_perm0_policy",
+    "no_allow_a_t_b_t_class0_perm0_policy",
+    "security_context_tests_policy",
+    "security_server_tests_policy",
+    # keep-sorted end
+]
 
 
 def _compile_composite_policy(
@@ -125,11 +157,12 @@ def _compile_composite_policy(
     output: str,
     handle_unknown: str,
 ) -> None:
+    """(Re)Compile "composite" test policy sources into a binary policy file."""
+    input_paths = list(
+        f"{_INPUT_POLICY_DIRECTORY}/{input_path}" for input_path in inputs
+    )
+    output_path = f"{_OUTPUT_POLICY_DIRECTORY}/{output}"
     with tempfile.TemporaryDirectory() as temporary_directory_name:
-        input_paths = tuple(
-            f"{_INPUT_POLICY_DIRECTORY}/{input_path}" for input_path in inputs
-        )
-        output_path = f"{_OUTPUT_POLICY_DIRECTORY}/{output}"
         merged_path = f"{temporary_directory_name}/policy.conf"
         merge_policies.merge_text_policies(
             _INITIAL_SIDS_PATH, input_paths, merged_path
@@ -142,10 +175,21 @@ def _compile_composite_policy(
         )
 
 
-def compile_composite_policies(checkpolicy_executable: str) -> None:
+def compile_policies(checkpolicy_executable: str) -> None:
+    """(Re)Compile all test policies with the specified `checkpolicy_executable`.
+    Both "composite" policies, built from a set of fragments, and legacy
+    all-in-one policies are rebuilt.
+    """
     for inputs, output in _COMPOSITE_POLICY_PATHS:
         _compile_composite_policy(
             checkpolicy_executable, inputs, output, "deny"
+        )
+    for name in _LEGACY_POLICIES:
+        merge_policies.compile_text_policy_to_binary_policy(
+            checkpolicy_executable,
+            f"{_LEGACY_POLICY_DIRECTORY}/{name}.conf",
+            f"{_LEGACY_POLICY_DIRECTORY}/{name}.pp",
+            "deny",
         )
     for handle_unknown in ("allow", "deny", "reject"):
         _compile_composite_policy(
@@ -166,4 +210,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    compile_composite_policies(args.checkpolicy_executable)
+    compile_policies(args.checkpolicy_executable)
