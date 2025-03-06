a37377e4e59e800093dfde87f3877e6b8cfaa96f
Braden Kell <bradenkell@google.com>
2025-03-06 12:47:19 -0800

    [sdk][devicetree] Add manager-test-helper and visitor-test-helper to SDK
    Add devicetree testing libraries to the SDK so that they can be used
    outside fuchsia.git.
    
    Bug: 400710881
    Test: CQ
    Change-Id: Ic8de3a7f624d766084a09dc9b2503125c7575ad4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1219464
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Fuchsia-Auto-Submit: Braden Kell <bradenkell@google.com>

--- sdk/lib/driver/devicetree/manager/manager-test-helper.cc
+++ sdk/lib/driver/devicetree/manager/manager-test-helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "manager-test-helper.h"
+#include "lib/driver/devicetree/manager/manager-test-helper.h"
 
 #include <fcntl.h>
 #include <lib/syslog/cpp/macros.h>
