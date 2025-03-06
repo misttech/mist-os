7a4ba8d71c6a633d36ec9fe43d422a0166d7b8a3
Chase Latta <chaselatta@google.com>
2025-03-05 18:11:17 -0800

    [bazel_sdk] remove example of dep selecting on api level
    This example adds an unnecessary constraint on an older API level. The
    use case is not needed at this time so we are removing it to avoid
    adding extra technical debt. We can always add it back when this use
    case is needed.
    
    Change-Id: Iab4414e76d5897298996bdfa411ece8ce0e6a7c6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1221250
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Commit-Queue: Chase Latta <chaselatta@google.com>
    Reviewed-by: David Fang <fangism@google.com>

--- build/bazel/examples/hello_cpp/hello_world.cc
+++ build/bazel/examples/hello_cpp/hello_world.cc
@@ -4,17 +4,7 @@
 
 #include <iostream>
 
-// In real code, use the macros in <zircon/availability.h> for such checks.
-// This is an exceptional case where a check for equality is needed.
-#if __Fuchsia_API_level__ == 17
-#include "lib/hello_printer_17.h"
-#endif
-
 int main() {
-#if __Fuchsia_API_level__ == 17
-  hello_printer_17::HelloPrinter printer;
-  printer.PrintHello();
-#endif
   std::cout << "Hello, my dear in-tree Bazel world!\n";
   return 0;
 }
