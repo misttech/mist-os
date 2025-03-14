9b7aac3cb4f13f7658125d8f3e9f0f9b45c9f939
Roland McGrath <mcgrathr@google.com>
2025-03-14 16:08:06 -0700

    [libc] Clean up printf_core build rules
    This completes the soft transition after the llvm-libc printf
    engine was turned into all-header templates.
    
    Fixed: 402907255
    Change-Id: I117acd9f1b41aed7a96224243b969cab2bc05481
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1229224
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Caslyn Tonelli <caslyn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/c/stdio/printf_core/wrapper.cc
+++ zircon/system/ulib/c/stdio/printf_core/wrapper.cc
@@ -4,35 +4,12 @@
 
 #include "wrapper.h"
 
-// TODO(https://fxbug.dev/42105189): These are defined as macros in
-// <zircon/compiler.h> and used by some other headers such as in libzx.  This
-// conflicts with their use as scoped identifiers in the llvm-libc code reached
-// from this header, when this header is included in someplace that also
-// includes <zircon/compiler.h> and those headers that rely on its macros.
-// <zircon/compiler.h> should not be defining macros in the public namespace
-// this way, but until that's fixed work around the issue by hiding the macros
-// during the evaluation of the llvm-libc headers.
-#pragma push_macro("add_overflow")
-#undef add_overflow
-#pragma push_macro("sub_overflow")
-#undef sub_overflow
-
 #include "src/stdio/printf_core/printf_main.h"
-
-// TODO(https://fxbug.dev/42105189): See comment above.
-#pragma pop_macro("add_overflow")
-#pragma pop_macro("sub_overflow")
 
 namespace LIBC_NAMESPACE::printf_core {
 
 constexpr cpp::string_view kNewline = "\n";
 constexpr cpp::string_view kEmpty = "";
-
-#ifdef LIBC_COPT_PRINTF_RUNTIME_DISPATCH
-using FlushToStreamWriteBuffer = WriteBuffer<WriteMode::FLUSH_TO_STREAM>;
-#else
-using FlushToStreamWriteBuffer = WriteBuffer;
-#endif
 
 int PrintfImpl(int (*write)(std::string_view str, void* hook), void* hook, std::span<char> buffer,
                PrintfNewline newline, const char* format, va_list args) {
@@ -41,7 +18,7 @@ int PrintfImpl(int (*write)(std::string_view str, void* hook), void* hook, std::
     void* hook;
   } write_buffer_hook = {.write = write, .hook = hook};
 
-  FlushToStreamWriteBuffer write_buffer{
+  WriteBuffer<WriteMode::FLUSH_TO_STREAM> write_buffer{
       buffer.data(),
       buffer.size(),
       [](cpp::string_view str, void* arg) -> int {
@@ -50,11 +27,7 @@ int PrintfImpl(int (*write)(std::string_view str, void* hook), void* hook, std::
       },
       &write_buffer_hook,
   };
-#ifdef LIBC_COPT_PRINTF_RUNTIME_DISPATCH
   Writer<WriteMode::FLUSH_TO_STREAM> writer{write_buffer};
-#else
-  Writer writer{&write_buffer};
-#endif
 
   internal::ArgList arg_list{args};
   int result = printf_main(&writer, format, arg_list);
