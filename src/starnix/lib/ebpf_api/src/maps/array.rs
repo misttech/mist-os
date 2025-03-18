87c01cc7cf594c0f13f7ecb29f8885e97a2d324e
Sergey Ulanov <sergeyu@google.com>
2025-03-18 11:56:30 -0700

    [eBPF] Align values in eBPF maps.
    Previously values in eBPF array maps were stored packed, i.e. they were
    not aligned properly.
    Fix it by adding padding when necessary.
    
    
    Bug: 378507648
    Change-Id: If9ef05e2147179d77f7e841dc6f1d1687add0d56
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1227589
    Reviewed-by: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@google.com>

--- src/starnix/lib/ebpf_api/src/maps/array.rs
+++ src/starnix/lib/ebpf_api/src/maps/array.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use super::{compute_map_storage_size, new_pinned_buffer, MapError, MapImpl, MapKey, PinnedBuffer};
+use super::{new_pinned_buffer, MapError, MapImpl, MapKey, PinnedBuffer};
 use ebpf::MapSchema;
 use fuchsia_sync::Mutex;
 use linux_uapi::BPF_NOEXIST;
@@ -14,6 +14,12 @@ fn array_key_to_index(key: &[u8]) -> u32 {
     u32::from_ne_bytes(key.try_into().expect("incorrect key length"))
 }
 
+fn get_value_storage_size(value_size: usize) -> Option<usize> {
+    // Ensure 8-byte alignment for all values stored in the map.
+    const ALIGNMENT: usize = 8;
+    Some(value_size.checked_add(ALIGNMENT - 1)? & !(ALIGNMENT - 1))
+}
+
 #[derive(Debug)]
 pub struct Array {
     // TODO(https://fxbug.dev/378507648): replace with a VMO.
@@ -21,9 +27,15 @@ pub struct Array {
 
     num_entries: usize,
     value_size: usize,
+
+    // Number of bytes per element. May be greater than `value_size` to ensure
+    // proper alignment for the elements.
+    bytes_per_element: usize,
 }
 
 impl Array {
+    const MAX_ARRAY_SIZE: usize = u32::MAX as usize;
+
     pub fn new(schema: &MapSchema) -> Result<Self, MapError> {
         // From <https://man7.org/linux/man-pages/man2/bpf.2.html>:
         //   The key is an array index, and must be exactly four
@@ -31,11 +43,21 @@ impl Array {
         if schema.key_size != 4 {
             return Err(MapError::InvalidParam);
         }
-        let size = compute_map_storage_size(schema)? as usize;
+
+        let bytes_per_element =
+            get_value_storage_size(schema.value_size as usize).ok_or(MapError::InvalidParam)?;
+        let size = bytes_per_element
+            .checked_mul(schema.max_entries as usize)
+            .ok_or(MapError::InvalidParam)?;
+        if size > Self::MAX_ARRAY_SIZE {
+            return Err(MapError::NoMemory);
+        }
+
         Ok(Array {
             buffer: Mutex::new(new_pinned_buffer(size)),
             num_entries: schema.max_entries as usize,
             value_size: schema.value_size as usize,
+            bytes_per_element,
         })
     }
 
@@ -44,7 +66,7 @@ impl Array {
         if index >= self.num_entries {
             return None;
         }
-        let base = index * self.value_size;
+        let base = index * self.bytes_per_element;
         let limit = base + self.value_size;
         Some((base as usize)..(limit as usize))
     }
@@ -92,5 +114,37 @@ impl MapImpl for Array {
     fn vmo(&self) -> Option<Arc<zx::Vmo>> {
         // TODO(https://fxbug.dev/378507648): Store the value in VMO and return it here.
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use linux_uapi::bpf_map_type_BPF_MAP_TYPE_ARRAY;
+
+    // Verifies that array elements are always 8-byte aligned.
+    #[fuchsia::test]
+    fn test_alignment() {
+        let schema = MapSchema {
+            map_type: bpf_map_type_BPF_MAP_TYPE_ARRAY,
+            key_size: 4,
+            value_size: 5,
+            max_entries: 10,
+        };
+
+        let array = Array::new(&schema, None).unwrap();
+        assert_eq!(array.get_raw(&[0, 0, 0, 0]).unwrap() as usize % 8, 0);
+        assert_eq!(array.get_raw(&[1, 0, 0, 0]).unwrap() as usize % 8, 0);
+
+        let schema = MapSchema {
+            map_type: bpf_map_type_BPF_MAP_TYPE_ARRAY,
+            key_size: 4,
+            value_size: 10,
+            max_entries: 10,
+        };
+
+        let array = Array::new(&schema, None).unwrap();
+        assert_eq!(array.get_raw(&[0, 0, 0, 0]).unwrap() as usize % 8, 0);
+        assert_eq!(array.get_raw(&[1, 0, 0, 0]).unwrap() as usize % 8, 0);
     }
 }
