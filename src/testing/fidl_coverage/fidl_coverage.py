019fea4ae1aee41626f9b5968f7cfbcf2c078cc2
Chris Phoenix <cphoenix@google.com>
2025-03-20 12:19:56 -0700

    [ctf] Bugfix the FIDL-coverage summary script
    Fix a bug (missing parameter in one codepath)
    Output more info about senders and receivers
    Be insensitive to missing "url" fields
    
    Run-All-Tests: True
    Bug: b/379766709
    Change-Id: I2749cac43ee099c7ad0774393e5e0bcafc6f45e6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1232391
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Chris Phoenix <cphoenix@google.com>

--- src/testing/fidl_coverage/fidl_coverage.py
+++ src/testing/fidl_coverage/fidl_coverage.py
@@ -10,7 +10,9 @@ import argparse
 import json
 import os
 import sys
+from io import TextIOWrapper
 from pathlib import Path
+from typing import Any, Dict, List
 
 # TODO(b/379766709) (comment 21): Rewrite this in Go.
 
@@ -18,28 +20,46 @@ from pathlib import Path
 # Output schema for a single FIDL method call: sender and receiver URL, and the path
 # to the test's output directory (from which the test name can be extracted)
 class Call:
-    def __init__(self, sender_url, receiver_url, test_path, direction):
-        self.sender_url = sender_url
-        self.receiver_url = receiver_url
+    def __init__(
+        self,
+        sender: Dict[str, Any],
+        receiver: Dict[str, Any],
+        test_path: str,
+        direction: str,
+    ):
+        self.sender = sender
+        self.receiver = receiver
         self.test_path = test_path
         self.direction = direction
+
+
+UNWANTED_CALL_KEYS = ["pid", "is_test"]
+
+
+def cleanup_call(call: Dict[str, Any]) -> Dict[str, Any]:
+    for key in UNWANTED_CALL_KEYS:
+        if key in call:
+            del call[key]
+    return call
 
 
 # Output schema for a single FIDL message. `name` is the name of the message, as
 # `fuchsia.library/Protocol.Message` - unless the name is not in the CTF ordinal<>message
 # map, in which case `name` is the same as `ordinal`. `calls` is a list of Call.
 class Message:
-    def __init__(self, name, ordinal):
+    def __init__(self, name: str, ordinal: str) -> None:
         self.name = name
         self.ordinal = ordinal
-        self.calls = []
+        self.calls: List[Call] = []
 
-    def add_calls(self, calls, test_path, direction):
+    def add_calls(
+        self, calls: List[Dict[str, Any]], test_path: Path, direction: str
+    ) -> None:
         for call in calls:
             self.calls.append(
                 Call(
-                    sender_url=call["sender"]["url"],
-                    receiver_url=call["receiver"]["url"],
+                    sender=cleanup_call(call["sender"]),
+                    receiver=cleanup_call(call["receiver"]),
                     test_path=str(test_path.parent),
                     direction=direction,
                 )
@@ -51,13 +71,13 @@ class Message:
 # SDK API name<>ordinal map file (whether or not it has calls) and for every event in
 # every scanned FIDL-snoop file (whether or not it's in the API).
 class Methods:
-    def __init__(self, message_file):
+    def __init__(self, message_file: TextIOWrapper) -> None:
         self.setup_data_structures(message_file)
 
-    def setup_data_structures(self, message_file):
+    def setup_data_structures(self, message_file: TextIOWrapper) -> None:
         self.raw_methods = json.load(message_file)
-        self.name_lookup = {}
-        self.ordinal_lookup = {}
+        self.name_lookup: Dict[str, Message] = {}
+        self.ordinal_lookup: Dict[str, Message] = {}
         # Schema is [{name: "fuchsia.library",
         #             methods: [{name: "fuchsia.library/Protocol.Message", ordinal: 1234}, ...]}, ...]
         for entry in self.raw_methods:
@@ -68,7 +88,7 @@ class Methods:
                 self.name_lookup[method["name"]] = message
                 self.ordinal_lookup[method["ordinal"]] = message
 
-    def process_calls(self, path, direction):
+    def process_calls(self, path: Path, direction: str) -> None:
         with open(path) as f:
             calls = json.load(f)
             # Schema is { message: [call, call...], message: [call, call...]...}
@@ -78,7 +98,7 @@ class Methods:
             for message, call_list in calls.items():
                 if message in self.ordinal_lookup:
                     self.ordinal_lookup[message].add_calls(
-                        calls=call_list, test_path=path
+                        calls=call_list, test_path=path, direction=direction
                     )
                 else:
                     if message not in self.name_lookup:
@@ -89,10 +109,10 @@ class Methods:
                         calls=call_list, test_path=path, direction=direction
                     )
 
-    def write_to_json(self, path):
+    def write_to_json(self, path: Path) -> None:
         """Writes the desired data to stdout in JSON format."""
 
-        def vars_or_obj(obj):
+        def vars_or_obj(obj: Any) -> Any:
             """Extracts fields from classes, and passes through built-in data types."""
             try:
                 return vars(obj)
@@ -110,7 +130,7 @@ class Methods:
 
 # The core of this script. Scans for intra_calls files, accumulates calls from them, and writes
 # the accumulated information to stdout in the correct schema.
-def check_coverage(args):
+def check_coverage(args: argparse.Namespace) -> None:
     api_path = Path(args.api_file)
     with open(api_path) as f:
         methods = Methods(f)
@@ -126,7 +146,7 @@ def check_coverage(args):
     methods.write_to_json(Path(args.json_output))
 
 
-def main(argv):
+def main(argv: List[str]) -> None:
     """This script takes an API file summarizing SDK FIDL methods (name <-> ordinal) and one or
     more directories containing test outputs, which should include FIDL-snoop files named
     `intra_calls.freeform.json`."""
