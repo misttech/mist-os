b2ccdc7d648538ca38969c58b9b0fd9c685b1580
Aidan Wolter <awolter@google.com>
2025-03-26 10:53:25 -0700

    [assembly] Remove config_schema/src/merge.rs
    This is unused now that AssemblyContainer manages overrides merging.
    
    Bug: 400719840
    Change-Id: Ic1d1c216d1ee609df565d60ec0a60b5915040e16
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1236926
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/config_schema/src/lib.rs
+++ src/lib/assembly/config_schema/src/lib.rs
@@ -11,7 +11,6 @@ mod board_input_bundle_set;
 pub mod common;
 pub mod developer_overrides;
 pub mod image_assembly_config;
-pub mod merge;
 pub mod platform_config;
 pub mod product_config;
 
@@ -22,7 +21,6 @@ pub use common::{
     DriverDetails, FeatureControl, PackageDetails, PackageSet, PackagedDriverDetails,
 };
 pub use image_assembly_config::{BoardDriverArguments, ImageAssemblyConfig};
-pub use merge::try_merge_into;
 pub use platform_config::example_config::ExampleConfig;
 pub use platform_config::icu_config::{ICUConfig, Revision};
 pub use platform_config::intl_config::IntlConfig;
