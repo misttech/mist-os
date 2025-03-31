30a2cc5f6ac958d0a03e329ba03ea19ef7ee0435
Jeff Martin <martinjeffrey@google.com>
2025-03-31 09:35:58 -0700

    [netstack3] Ignore NDP with an invalid hop-limit
    As per RFC 4861, any received RS, RA, NS, NA, or Redirect message should
    be silently ignored if its hop-limit != 255.
    
    Note: This is a reland of https://fxrev.dev/1236655, which was reverted
    because it only updated the receiving logic for RAs.
    
    Fixed: 42179534
    Change-Id: Ieb3b7b3c8b4cf3ce0e33a135fb798e5eec967d1a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1239565
    Commit-Queue: Jeff Martin <martinjeffrey@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>

--- src/connectivity/network/netstack3/core/ip/src/icmp.rs
+++ src/connectivity/network/netstack3/core/ip/src/icmp.rs
@@ -1224,15 +1224,36 @@ fn receive_ndp_packet<
         + NudIpHandler<Ipv6, BC>
         + IpLayerHandler<Ipv6, BC>
         + CounterContext<NdpCounters>,
+    H: IpHeaderInfo<Ipv6>,
 >(
     core_ctx: &mut CC,
     bindings_ctx: &mut BC,
     device_id: &CC::DeviceId,
     src_ip: Ipv6SourceAddr,
     packet: NdpPacket<B>,
+    header_info: &H,
 ) {
-    // TODO(https://fxbug.dev/42179534): Make sure IP's hop limit is set to 255 as
-    // per RFC 4861 section 6.1.2.
+    // All NDP messages should be dropped if the hop-limit != 255. See
+    //   Router Solicitations: RFC 4861 section 6.1.1,
+    //   Router Advertisements: RFC 4861 section 6.1.2,
+    //   Neighbor Solicitations: RFC 4861 section 7.1.1,
+    //   Neighbor Advertisements: RFC 4861 section 7.1.2, and
+    //   Redirect: RFC 4861 section 8.1:
+    //
+    //       A node MUST silently discard any received [NDP Message Type]
+    //       messages that do not satisfy all of the following validity
+    //       checks:
+    //
+    //          ...
+    //
+    //          - The IP Hop Limit field has a value of 255, i.e., the packet
+    //            could not possibly have been forwarded by a router.
+    //
+    //          ...
+    if header_info.hop_limit() != REQUIRED_NDP_IP_PACKET_HOP_LIMIT {
+        trace!("dropping NDP packet from {src_ip} with invalid hop limit");
+        return;
+    }
 
     match packet {
         NdpPacket::RouterSolicitation(_) | NdpPacket::Redirect(_) => {}
@@ -1749,7 +1770,7 @@ impl<
                 );
             }
             Icmpv6Packet::Ndp(packet) => {
-                receive_ndp_packet(core_ctx, bindings_ctx, device, src_ip, packet)
+                receive_ndp_packet(core_ctx, bindings_ctx, device, src_ip, packet, header_info)
             }
             Icmpv6Packet::PacketTooBig(packet_too_big) => {
                 CounterContext::<IcmpRxCounters<Ipv6>>::counters(core_ctx)
