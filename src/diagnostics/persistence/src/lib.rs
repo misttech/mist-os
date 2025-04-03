073e0b9565ecaf4bc9d312d230d3b14f340eef0f
Sam Balana <sbalana@google.com>
2025-04-03 12:09:36 -0700

    [persistence] Replace fasync TaskGroup with Scopes
    New code is encouraged to spawn directly on scopes instead the behavior
    of Task::spawn, which spawns on the root scope of the executor.
    
    Create an async scope containing one PersistServer per service.
    
    Change-Id: Id02e85c8acf765bc2215d8c8311159e24a43afa4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1243294
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Sam Balana <sbalana@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/diagnostics/persistence/src/lib.rs
+++ src/diagnostics/persistence/src/lib.rs
@@ -14,18 +14,15 @@ mod scheduler;
 use anyhow::{bail, Error};
 use argh::FromArgs;
 use fetcher::Fetcher;
-use fuchsia_async::TaskGroup;
+use fuchsia_async as fasync;
 use fuchsia_component::server::{ServiceFs, ServiceObj};
 use fuchsia_inspect::component;
 use fuchsia_inspect::health::Reporter;
-use fuchsia_sync::Mutex;
-use futures::future::join;
 use futures::StreamExt;
 use log::*;
 use persist_server::PersistServer;
 use persistence_config::Config;
 use scheduler::Scheduler;
-use std::sync::Arc;
 use zx::BootInstant;
 
 /// The name of the subcommand and the logs-tag.
@@ -53,6 +50,7 @@ macro_rules! on_error {
 
 pub async fn main(_args: CommandLine) -> Result<(), Error> {
     info!("Starting Diagnostics Persistence Service service");
+    let mut health = component::health();
     let config =
         on_error!(persistence_config::load_configuration_files(), "Error loading configs: {}")?;
     let inspector = component::inspector();
@@ -64,8 +62,6 @@ pub async fn main(_args: CommandLine) -> Result<(), Error> {
 
     let mut fs = ServiceFs::new();
 
-    component::health().set_starting_up();
-
     // Create the Inspect fetcher
     let (fetch_requester, _fetcher_task) =
         on_error!(Fetcher::new(&config), "Error initializing fetcher: {}")?;
@@ -73,14 +69,18 @@ pub async fn main(_args: CommandLine) -> Result<(), Error> {
     let scheduler = Scheduler::new(fetch_requester, &config);
 
     // Add a persistence fidl service for each service defined in the config files.
-    let _server_tasks = spawn_persist_services(config, &mut fs, scheduler);
+    let scope = fasync::Scope::new();
+    let service_scope = scope.new_child_with_name("services");
+    spawn_persist_services(&config, &mut fs, scheduler, &service_scope);
+
     fs.take_and_serve_directory_handle()?;
+    scope.spawn(fs.collect::<()>());
 
     // Before serving previous data, wait until the post-boot system update check has finished.
     // Note: We're already accepting persist requests. If we receive a request, store
     // some data, and then cache is cleared after data is persisted, that data will be lost. This
     // is correct behavior - we don't want to remember anything from before the cache was cleared.
-    let publish_fut = async move {
+    scope.spawn(async move {
         info!("Waiting for post-boot update check...");
         match fuchsia_component::client::connect_to_protocol::<fidl_fuchsia_update::ListenerMarker>(
         ) {
@@ -102,37 +102,44 @@ pub async fn main(_args: CommandLine) -> Result<(), Error> {
         info!("...Update check has completed; publishing previous boot data");
         inspector.root().record_child(PERSIST_NODE_NAME, |node| {
             inspect_server::serve_persisted_data(node);
-            component::health().set_ok();
+            health.set_ok();
             info!("Diagnostics Persistence Service ready");
         });
         inspector.root().record_int(PUBLISHED_TIME_KEY, BootInstant::get().into_nanos());
-    };
+    });
 
-    join(fs.collect::<()>(), publish_fut).await;
+    scope.await;
+
     Ok(())
 }
 
 // Takes a config and adds all the persist services defined in those configs to the servicefs of
 // the component.
-#[must_use]
 fn spawn_persist_services(
-    config: Config,
+    config: &Config,
     fs: &mut ServiceFs<ServiceObj<'static, ()>>,
     scheduler: Scheduler,
-) -> Arc<Mutex<TaskGroup>> {
+    scope: &fasync::Scope,
+) {
     let mut started_persist_services = 0;
-    // We want fault tolerance if only a subset of the service configs fail to initialize.
-    let task_holder = Arc::new(Mutex::new(TaskGroup::new()));
+
     for (service_name, tags) in config {
         info!("Launching persist service for {service_name}");
-        PersistServer::create(
+        let unique_service_name =
+            format!("{}-{}", constants::PERSIST_SERVICE_NAME_PREFIX, service_name);
+
+        let server = PersistServer::create(
             service_name.clone(),
             tags.keys().cloned().collect(),
             scheduler.clone(),
-        )
-        .launch_server(task_holder.clone(), fs);
+            // Fault tolerance if only a subset of the service configs fail to initialize.
+            scope.new_child_with_name(&service_name.clone()),
+        );
+        fs.dir("svc").add_fidl_service_at(unique_service_name, move |stream| {
+            server.spawn(stream);
+        });
         started_persist_services += 1;
     }
+
     info!("Started {} persist services", started_persist_services);
-    task_holder
 }
