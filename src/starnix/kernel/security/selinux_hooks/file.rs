e10ee533c1e7281d1b1efa95eb865da79d3f1eaa
Wez <wez@google.com>
2025-04-03 08:07:51 -0700

    [sestarnix] Validate BPF resource access in file_receive hook
    When a program receives BPF resources in a message, access to the
    underlying resources must be access-checked, rather than checking
    access to the FsNode associated with the file descriptors.
    
    Since BPF FsNode labels are seemingly unused, migrate BPF to use
    "private" anonymous nodes.
    
    Bug: 398654362, 406437558
    Cq-Include-Trybots: luci.turquoise.global.try:sdk-bazel-linux-android
    Change-Id: I1964e56ef3ecb28809ca366485cbdf72c768cde6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1243044
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Ambre Williams <ambre@google.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Commit-Queue: Wez <wez@google.com>

--- src/starnix/kernel/security/selinux_hooks/file.rs
+++ src/starnix/kernel/security/selinux_hooks/file.rs
@@ -5,10 +5,12 @@
 // TODO(https://github.com/rust-lang/rust/issues/39371): remove
 #![allow(non_upper_case_globals)]
 
+use super::bpf::{check_bpf_map_access, check_bpf_prog_access};
 use super::{
     fs_node_effective_sid_and_class, has_file_permissions, permissions_from_flags,
     todo_has_fs_node_permissions, FileObjectState, FsNodeSidAndClass, PermissionFlags,
 };
+use crate::bpf::fs::BpfHandle;
 use crate::mm::{MappingOptions, ProtectionFlags};
 use crate::security::selinux_hooks::{
     todo_check_permission, todo_has_file_permissions, CommonFilePermission, ProcessPermission,
@@ -73,6 +75,24 @@ pub(in crate::security) fn file_receive(
     let subject_sid = current_task.security_state.lock().current_sid;
     let fs_node_class = file.node().security_state.lock().class;
     let permission_flags = file.flags().into();
+
+    // BPF resources are wrapped into file descriptors for interaction with userspace,
+    // but have a distinct set of permissions associated with the underlying objects rather
+    // than on the `FsNode`.
+    if let Some(bpf_handle) = file.downcast_file::<BpfHandle>() {
+        has_file_permissions(&permission_check, subject_sid, file, &[], current_task.into())?;
+        match bpf_handle {
+            BpfHandle::Map(ref map) => {
+                check_bpf_map_access(security_server, current_task, map, permission_flags)?
+            }
+            BpfHandle::Program(ref prog) => {
+                check_bpf_prog_access(security_server, current_task, prog)?
+            }
+            _ => {}
+        }
+        return Ok(());
+    }
+
     todo_has_file_permissions(
         TODO_DENY!("https://fxbug.dev/399894966", "Check file receive permission."),
         &current_task.kernel(),
