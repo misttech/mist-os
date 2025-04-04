f9f4aba082056ca500dcd1c2acaae23590cec449
Laura Peskin <pesk@google.com>
2025-04-04 14:19:21 -0700

    [sestarnix] Add userspace tests for ioctl xperm check
    Includes a test that is currently expected to fail,
    but should pass when the ioctl xperm check is
    enforced.
    
    Bug: 374832936
    
    Change-Id: I1ddcda84e88d6a361f332aeb9717f19b9d0944b5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1242272
    Reviewed-by: Wez <wez@google.com>
    Commit-Queue: Laura Peskin <pesk@google.com>

--- src/starnix/tests/selinux/userspace/tests/ioctl.cc
+++ src/starnix/tests/selinux/userspace/tests/ioctl.cc
@@ -43,7 +43,43 @@ fit::result<int, int> GetBlockSize(int fd) {
   return fit::ok(result);
 }
 
-TEST(IoctlTest, BasicIoctlAllowed) {
+/// Check that the `ioctl` permission is granted when it is allowed by the policy,
+/// and ioctl extended permissions are not filtered.
+TEST(IoctlTest, BasicCommandAllowed) {
+  LoadPolicy("ioctl_policy.pp");
+
+  auto enforce = ScopedEnforcement::SetEnforcing();
+
+  auto test_fd = CreateTestFile();
+  ASSERT_TRUE(test_fd.is_ok());
+
+  constexpr char kTestSecurityContext[] = "test_u:test_r:test_ioctl_allowed_t:s0";
+  ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
+    size_t value = 0;
+    EXPECT_THAT(ioctl(test_fd.value().get(), 0xabcd, &value),
+                ::testing::Not(SyscallFailsWithErrno(EACCES)));
+  }));
+}
+
+/// Check that the `ioctl` permission is denied when it is disallowed by the policy.
+TEST(IoctlTest, BasicCommandDenied) {
+  LoadPolicy("ioctl_policy.pp");
+
+  auto enforce = ScopedEnforcement::SetEnforcing();
+
+  auto test_fd = CreateTestFile();
+  ASSERT_TRUE(test_fd.is_ok());
+
+  constexpr char kTestSecurityContext[] = "test_u:test_r:test_ioctl_denied_t:s0";
+  ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
+    size_t value = 0;
+    EXPECT_THAT(ioctl(test_fd.value().get(), 0xabcd, &value), SyscallFailsWithErrno(EACCES));
+  }));
+}
+
+/// Check that the `getattr` permission is granted when it is invoked by a special-cased
+/// ioctl command, and is allowed by the policy.
+TEST(IoctlTest, GetAttrCommandAllowed) {
   LoadPolicy("ioctl_policy.pp");
 
   auto enforce = ScopedEnforcement::SetEnforcing();
@@ -58,7 +94,9 @@ TEST(IoctlTest, BasicIoctlAllowed) {
   }));
 }
 
-TEST(IoctlTest, BasicIoctlDenied) {
+/// Check that the `getattr` permission is denied when it is invoked by a special-cased
+/// ioctl command, and is disallowed by the policy.
+TEST(IoctlTest, GetAttrCommandDenied) {
   LoadPolicy("ioctl_policy.pp");
 
   auto enforce = ScopedEnforcement::SetEnforcing();
@@ -70,6 +108,61 @@ TEST(IoctlTest, BasicIoctlDenied) {
   ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
     auto result = GetBlockSize(test_fd.value().get());
     EXPECT_TRUE(result.is_error());
+  }));
+}
+
+/// Check that the `ioctl` permission is granted when it is allowed by the policy,
+/// in the case where ioctl commands are filtered and the specified command is
+/// allowed by the filter.
+TEST(IoctlTest, FilteredCommandAllowed) {
+  LoadPolicy("ioctl_policy.pp");
+
+  auto enforce = ScopedEnforcement::SetEnforcing();
+
+  auto test_fd = CreateTestFile();
+  ASSERT_TRUE(test_fd.is_ok());
+
+  constexpr char kTestSecurityContext[] = "test_u:test_r:test_ioctl_xperms_filtered_t:s0";
+  ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
+    size_t value = 0;
+    EXPECT_THAT(ioctl(test_fd.value().get(), 0xabcd, &value),
+                ::testing::Not(SyscallFailsWithErrno(EACCES)));
+  }));
+}
+
+/// Check that the `ioctl` permission is denied when the `ioctl` permission is allowed
+/// by the policy but the specified ioctl command is disallowed by extended permission
+/// filtering.
+TEST(IoctlTest, FilteredCommandDenied) {
+  LoadPolicy("ioctl_policy.pp");
+
+  auto enforce = ScopedEnforcement::SetEnforcing();
+
+  auto test_fd = CreateTestFile();
+  ASSERT_TRUE(test_fd.is_ok());
+
+  constexpr char kTestSecurityContext[] = "test_u:test_r:test_ioctl_xperms_filtered_t:s0";
+  ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
+    size_t value = 0;
+    EXPECT_THAT(ioctl(test_fd.value().get(), 0xabcc, &value), SyscallFailsWithErrno(EACCES));
+  }));
+}
+
+/// Check that the `ioctl` permission is denied in the case where ioctl commands
+/// are filtered and the specified command is allowed by the filter, but the `ioctl`
+/// permission itself is disallowed by the policy.
+TEST(IoctlTest, FilteredCommandDeniedByPermission) {
+  LoadPolicy("ioctl_policy.pp");
+
+  auto enforce = ScopedEnforcement::SetEnforcing();
+
+  auto test_fd = CreateTestFile();
+  ASSERT_TRUE(test_fd.is_ok());
+
+  constexpr char kTestSecurityContext[] = "test_u:test_r:test_ioctl_xperms_only_t:s0";
+  ASSERT_TRUE(RunSubprocessAs(kTestSecurityContext, [&] {
+    size_t value = 0;
+    EXPECT_THAT(ioctl(test_fd.value().get(), 0xabcd, &value), SyscallFailsWithErrno(EACCES));
   }));
 }
 
