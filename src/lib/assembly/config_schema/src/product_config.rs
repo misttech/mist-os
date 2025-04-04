5cd854452c3256c45913a4fd1960a9d3c92e8b22
Jerry Belton <jcecil@google.com>
2025-04-03 18:49:00 -0700

    [assembly] Add release version numbers to Product configs.
    Product configs already include version information in the form
    of a file.
    
    For consistency, and to remove the need to read that file during
    bisection, we copy the contents of that file to the top level of the
    "product" section of the config json.
    
    Bug: b/399808455
    Change-Id: I7ec08187f925162cd7dc6c9c4c137564f1649ece
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1238666
    Commit-Queue: Jerry Belton <jcecil@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- src/lib/assembly/config_schema/src/product_config.rs
+++ src/lib/assembly/config_schema/src/product_config.rs
@@ -63,6 +63,13 @@ pub struct ProductConfig {
     #[schemars(schema_with = "crate::option_path_schema")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub bootfs_files_package: Option<Utf8PathBuf>,
+
+    /// Release version that this product config corresponds to.
+    /// TODO(https://fxbug.dev/397489730): Make this a mandatory field
+    /// once these changes have rolled into all downstream repositories.
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub release_version: Option<String>,
 }
 
 /// Packages provided by the product, to add to the assembled images.
