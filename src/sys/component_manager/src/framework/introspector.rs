d6634e74bfc636410af1f5ae30582347d319bdc8
Suraj Malhotra <surajmalhotra@google.com>
2025-04-11 09:50:22 -0700

    [component_manager] Removed a stray "cti" typo.
    Change-Id: I09bd3d1d62b39b606825a7f1768c91e7c8dd19a4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1250724
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Amanda Tait <atait@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/component_manager/src/framework/introspector.rs
+++ src/sys/component_manager/src/framework/introspector.rs
@@ -1,6 +1,6 @@
 // Copyright 2024 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.cti
+// found in the LICENSE file.
 
 use std::sync::Arc;
 
