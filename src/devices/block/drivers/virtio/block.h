3759d59de5797c67938401cee8d26584e2b08c05
Bruno Herrera <bruno.herrera@mist-os.com>
2025-04-15 12:24:33 -0300

    [mistos][block][virtio] Revert to fuchsia 274a5387d94631413230254b0bd768be45517b08
    Change-Id: I88eb804180478b4648f0f958a6414ef2011ef84e

--- src/devices/block/drivers/virtio/block.h
+++ src/devices/block/drivers/virtio/block.h
@@ -1,28 +1,28 @@
-// Copyright 2025 Mist Tecnologia Ltda. All rights reserved.
 // Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #ifndef SRC_DEVICES_BLOCK_DRIVERS_VIRTIO_BLOCK_H_
 #define SRC_DEVICES_BLOCK_DRIVERS_VIRTIO_BLOCK_H_
 
-#include <lib/bio.h>
+#include <fuchsia/hardware/block/driver/c/banjo.h>
+#include <fuchsia/hardware/block/driver/cpp/banjo.h>
+#include <lib/dma-buffer/buffer.h>
+#include <lib/driver/compat/cpp/compat.h>
+#include <lib/driver/component/cpp/driver_base.h>
+#include <lib/sync/completion.h>
 #include <lib/virtio/backends/backend.h>
 #include <lib/virtio/device.h>
 #include <lib/virtio/ring.h>
 #include <lib/zx/time.h>
-#include <mistos/hardware/block/driver/c/banjo.h>
 #include <stdlib.h>
 #include <zircon/compiler.h>
 
+#include <atomic>
 #include <memory>
 
-#include <kernel/event.h>
-#include <kernel/thread.h>
-#include <ktl/atomic.h>
 #include <virtio/block.h>
 
-#include "zircon/listnode.h"
+#include "src/lib/listnode/listnode.h"
 
 namespace virtio {
 
@@ -32,15 +32,16 @@ struct block_txn_t {
   void* cookie;
   struct vring_desc* desc;
   size_t req_index;
-  ktl::optional<size_t> discard_req_index;  // Only used if op is trim
+  std::optional<size_t> discard_req_index;  // Only used if op is trim
   list_node_t node;
+  zx_handle_t pmt;
 };
 
 class Ring;
-class BlockDevice : public virtio::Device, public bdev_t {
+class BlockDriver;
+class BlockDevice : public virtio::Device {
  public:
-  BlockDevice(fbl::RefPtr<BusTransactionInitiatorDispatcher> bti, ktl::unique_ptr<Backend> backend);
-  ~BlockDevice();
+  BlockDevice(zx::bti bti, std::unique_ptr<Backend> backend, fdf::Logger& logger);
 
   // virtio::Device overrides
   zx_status_t Init() override;
@@ -54,7 +55,7 @@ class BlockDevice : public virtio::Device, public bdev_t {
   const char* tag() const override { return "virtio-blk"; }
 
   // ddk::BlockImplProtocol functions invoked by BlockDriver.
-  // void BlockImplQuery(block_info_t* info, size_t* bopsz);
+  void BlockImplQuery(block_info_t* info, size_t* bopsz);
   void BlockImplQueue(block_op_t* bop, block_impl_queue_callback completion_cb, void* cookie);
 
  private:
@@ -76,31 +77,33 @@ class BlockDevice : public virtio::Device, public bdev_t {
 
   void txn_complete(block_txn_t* txn, zx_status_t status);
 
+  fdf::Logger& logger() { return logger_; }
+
   // The main virtio ring.
   Ring vring_{this};
 
   // Lock to be used around Ring::AllocDescChain and FreeDesc.
   // TODO: Move this into Ring class once it's certain that other users of the class are okay with
   // it.
-  DECLARE_MUTEX(BlockDevice) ring_lock_;
+  std::mutex ring_lock_;
 
   // Saved block device configuration out of the pci config BAR.
   virtio_blk_config_t config_ = {};
 
-  virtio::ContiguousBuffer blk_req_buf_;
+  std::unique_ptr<dma_buffer::ContiguousBuffer> blk_req_buf_;
   virtio_blk_req_t* blk_req_ = nullptr;
 
   zx_paddr_t blk_res_pa_ = 0;
   uint8_t* blk_res_ = nullptr;
 
   uint32_t blk_req_bitmap_ = 0;
-  static_assert(kBlkReqCount <= sizeof(blk_req_bitmap_) * CHAR_BIT);
+  static_assert(kBlkReqCount <= sizeof(blk_req_bitmap_) * CHAR_BIT, "");
 
   // When a transaction is enqueued, its start time (in the monotonic clock) is recorded, and the
   // timestamp is cleared when the transaction completes.  A watchdog task will fire after a
   // configured interval, and all timestamps will be checked against a deadline; if any exceed the
   // deadline an error is logged.
-  DECLARE_MUTEX(BlockDevice) watchdog_lock_;
+  std::mutex watchdog_lock_;
   zx::time blk_req_start_timestamps_[kBlkReqCount] __TA_GUARDED(watchdog_lock_);
 
   size_t alloc_blk_req() {
@@ -117,21 +120,52 @@ class BlockDevice : public virtio::Device, public bdev_t {
   void free_blk_req(size_t i) { blk_req_bitmap_ &= static_cast<uint32_t>(~(1 << i)); }
 
   // Pending txns and completion signal.
-  DECLARE_MUTEX(BlockDevice) txn_lock_;
+  std::mutex txn_lock_;
   list_node pending_txn_list_ = LIST_INITIAL_VALUE(pending_txn_list_);
-  Event txn_signal_;
+  sync_completion_t txn_signal_;
 
   // Worker state.
-  Thread* worker_thread_ = nullptr;
+  thrd_t worker_thread_;
   list_node worker_txn_list_ = LIST_INITIAL_VALUE(worker_txn_list_);
-  Event worker_signal_;
-  ktl::atomic<bool> worker_shutdown_ = false;
+  sync_completion_t worker_signal_;
+  std::atomic_bool worker_shutdown_ = false;
 
-  Thread* watchdog_thread_ = nullptr;
-  Event watchdog_signal_;
-  ktl::atomic<bool> watchdog_shutdown_ = false;
+  thrd_t watchdog_thread_;
+  sync_completion_t watchdog_signal_;
+  std::atomic_bool watchdog_shutdown_ = false;
 
   bool supports_discard_ = false;
+
+  fdf::Logger& logger_;
+};
+
+class BlockDriver : public fdf::DriverBase, public ddk::BlockImplProtocol<BlockDriver> {
+ public:
+  static constexpr char kDriverName[] = "virtio-block";
+
+  BlockDriver(fdf::DriverStartArgs start_args, fdf::UnownedSynchronizedDispatcher dispatcher)
+      : fdf::DriverBase(kDriverName, std::move(start_args), std::move(dispatcher)) {}
+
+  zx::result<> Start() override;
+
+  void PrepareStop(fdf::PrepareStopCompleter completer) override;
+
+  // ddk::BlockImplProtocol functions passed through to BlockDevice.
+  void BlockImplQuery(block_info_t* info, size_t* bopsz);
+  void BlockImplQueue(block_op_t* bop, block_impl_queue_callback completion_cb, void* cookie);
+
+ protected:
+  // Override to inject dependency for unit testing.
+  virtual zx::result<std::unique_ptr<BlockDevice>> CreateBlockDevice();
+
+ private:
+  std::unique_ptr<BlockDevice> block_device_;
+
+  fidl::WireSyncClient<fuchsia_driver_framework::Node> parent_node_;
+  fidl::WireSyncClient<fuchsia_driver_framework::NodeController> node_controller_;
+
+  compat::BanjoServer block_impl_server_{ZX_PROTOCOL_BLOCK_IMPL, this, &block_impl_protocol_ops_};
+  compat::SyncInitializedDeviceServer compat_server_;
 };
 
 }  // namespace virtio
