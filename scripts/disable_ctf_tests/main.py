5389caf5d169da5cac3ebae9b8a9999871df183e
Chris Phoenix <cphoenix@google.com>
2025-04-24 17:36:00 -0700

    [CTF] Skeleton of test-disable script
    //scripts/disable_ctf_tests/BUILD.gn (runs unit tests on .py sources)
    //scripts/disable_ctf_tests/main.py (almost empty)
    //scripts/disable_ctf_tests/tests/all_tests.py (almost empty)
    //sdk/ctf/disabled_tests.json (an empty list)
    
    Bug: b/412727988
    Change-Id: Ib4d2c7476542d605c6e219ddf13b990bc1035f79
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1258965
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Owners-Override: Alice Neels <neelsa@google.com>
    Commit-Queue: Chris Phoenix <cphoenix@google.com>
    Reviewed-by: Alice Neels <neelsa@google.com>

---
+++ scripts/disable_ctf_tests/main.py
@@ -0,0 +1,17 @@
+# Copyright 2025 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+
+def Main(args: list[str]) -> int:
+    return 0
+
+
+def run_unittests() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(Main(sys.argv[1:]))
