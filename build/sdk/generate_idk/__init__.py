1a73c2f8c4f06d21527b2a2ade900fa20cd9b1d7
David Dorwin <ddorwin@google.com>
2025-04-25 14:41:27 -0700

    [build][idk] Minor improvements to generate_idk
    Alphabetize types in `UnmergableMeta`, merge append calls, and other
    minor changes.
    
    Change-Id: Icdadfebc9dff174fa5beb280d39a38570534bd4f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1260845
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Commit-Queue: David Dorwin <ddorwin@google.com>

--- build/sdk/generate_idk/__init__.py
+++ build/sdk/generate_idk/__init__.py
@@ -28,7 +28,8 @@ from typing import (
 VERSION_HISTORY_PATH = pathlib.Path("version_history.json")
 
 
-STABLE_SUPPORTED_TYPES = ["cc_source_library", "fidl_library"]
+# These atom types include a "stable" field because they support unstable atoms.
+TYPES_SUPPORTING_UNSTABLE_ATOMS = ["cc_source_library", "fidl_library"]
 
 
 class BuildManifestJson(TypedDict):
@@ -89,16 +90,18 @@ class LoadableModuleMeta(TypedDict):
 class UnmergableMeta(TypedDict):
     name: str
     type: (
-        Literal["cc_source_library"]
-        | Literal["dart_library"]
-        | Literal["fidl_library"]
-        | Literal["documentation"]
-        | Literal["config"]
-        | Literal["license"]
+        # LINT.IfChange
+        Literal["bind_library"]
+        | Literal["cc_source_library"]
         | Literal["component_manifest"]
-        | Literal["bind_library"]
-        | Literal["version_history"]
+        | Literal["config"]
+        | Literal["dart_library"]
+        | Literal["documentation"]
         | Literal["experimental_python_e2e_test"]
+        | Literal["fidl_library"]
+        | Literal["license"]
+        | Literal["version_history"]
+        # LINT.ThenChange(//build/sdk/generate_idk/generate_idk_unittest.py)
     )
     stable: bool
 
@@ -264,6 +267,8 @@ class MergedIDK:
         for meta_path, atom in self.atoms.items():
             # Some atoms are given different "types" in the overall manifest...
             if meta_path == VERSION_HISTORY_PATH:
+                # version_history.json doesn't have a 'type' field.
+                assert "type" not in atom.keys()
                 type = "version_history"
             # LINT.IfChange
             elif atom["type"] in ["component_manifest", "config"]:
@@ -272,16 +277,19 @@ class MergedIDK:
             else:
                 type = atom["type"]
 
-            if type in STABLE_SUPPORTED_TYPES:
-                index.append(
-                    dict(
-                        meta=str(meta_path),
-                        type=type,
-                        stable=atom.get("stable", False),
-                    )
-                )
+            if type in TYPES_SUPPORTING_UNSTABLE_ATOMS:
+                is_stable = atom["stable"]
             else:
-                index.append(dict(meta=str(meta_path), type=type, stable=True))
+                assert "stable" not in atom.keys()
+                is_stable = True
+
+            index.append(
+                dict(
+                    meta=str(meta_path),
+                    type=type,
+                    stable=is_stable,
+                )
+            )
 
         index.sort(key=lambda a: (a["meta"], a["type"]))
 
@@ -428,16 +436,18 @@ def _merge_disjoint_dicts(
 def _merge_atom_meta(a: AtomMeta, b: AtomMeta) -> AtomMeta:
     """Merge two atoms, according to type-specific rules."""
     if a["type"] in (
-        "cc_source_library",
-        "dart_library",
-        "fidl_library",
-        "documentation",
-        "config",
-        "license",
-        "component_manifest",
+        # LINT.IfChange
         "bind_library",
-        "version_history",
+        "cc_source_library",
+        "component_manifest",
+        "config",
+        "dart_library",
+        "documentation",
         "experimental_python_e2e_test",
+        "fidl_library",
+        "license",
+        "version_history",
+        # LINT.ThenChange(//build/sdk/generate_idk/generate_idk_unittest.py)
     ):
         _assert_dicts_equal(a, b, [])
         return a
@@ -490,4 +500,5 @@ def _merge_atom_meta(a: AtomMeta, b: AtomMeta) -> AtomMeta:
             a.get("variants"), b.get("variants"), lambda v: v["constraints"]
         )
         return a
+
     raise AssertionError("Unknown atom type: " + a["type"])
