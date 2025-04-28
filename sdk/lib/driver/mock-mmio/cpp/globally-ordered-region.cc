536671404203cdb273d43502b15d71bb845b5252
Victor Costan <costan@google.com>
2025-04-28 13:46:20 -0700

    [driver][sdk] Improve mock-mmio/globally-ordered-region diagnostics.
    This CL
    
    1. Adds messages to the ZX_ASSERT() checks. The messages include details
       about the expected memory access and the received memory access, to
       help driver developers debug their code and tests.
    
    2. Adds two missing checks: operation size for memory reads, operation
       type for memory writes.
    
    Test: Tested against in-development driver.
    Change-Id: I770fa74399598544f6411f74bd893f3206d50903
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/711448
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>
    Commit-Queue: Victor Costan <costan@google.com>

--- sdk/lib/driver/mock-mmio/cpp/globally-ordered-region.cc
+++ sdk/lib/driver/mock-mmio/cpp/globally-ordered-region.cc
@@ -3,10 +3,15 @@
 // found in the LICENSE file.
 
 #include <lib/driver/mock-mmio/cpp/globally-ordered-region.h>
+#include <zircon/assert.h>
+
+#include <cinttypes>
+#include <cstdint>
 
 namespace mock_mmio {
 
 namespace {
+
 fdf::MmioBuffer CreateMmioBuffer(size_t size, uint32_t cache_policy, const fdf::MmioBufferOps* ops,
                                  void* ctx) {
   zx::vmo vmo;
@@ -15,6 +20,9 @@ fdf::MmioBuffer CreateMmioBuffer(size_t size, uint32_t cache_policy, const fdf::
   ZX_ASSERT(mmio_buffer_init(&mmio, 0, size, vmo.release(), cache_policy) == ZX_OK);
   return fdf::MmioBuffer(mmio, ops, ctx);
 }
+
+int SizeInt(GloballyOrderedRegion::Size access_size) { return static_cast<int>(access_size); }
+
 }  // namespace
 
 void GloballyOrderedRegion::Expect(cpp20::span<const GloballyOrderedRegion::Access> accesses) {
@@ -32,7 +40,9 @@ void GloballyOrderedRegion::Expect(cpp20::span<const GloballyOrderedRegion::Acce
 
 void GloballyOrderedRegion::CheckAllAccessesReplayed() {
   const std::lock_guard<std::mutex> lock(mutex_);
-  ZX_ASSERT(access_list_.size() == access_index_);
+  ZX_ASSERT_MSG(access_list_.size() == access_index_,
+                "Expected %zu MMIO accesses, only received %zu", access_list_.size(),
+                access_index_);
 }
 
 fdf::MmioBuffer GloballyOrderedRegion::GetMmioBuffer() {
@@ -59,8 +69,26 @@ uint64_t GloballyOrderedRegion::Read(zx_off_t address, GloballyOrderedRegion::Si
   const GloballyOrderedRegion::Access& expected_access = access_list_[access_index_];
   ++access_index_;
 
-  ZX_ASSERT(expected_access.address == address);
-  ZX_ASSERT(expected_access.write == false);
+  ZX_ASSERT_MSG(expected_access.address == address,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte read at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), uint64_t{address});
+
+  ZX_ASSERT_MSG(expected_access.write == false,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte read at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), uint64_t{address});
+
+  ZX_ASSERT_MSG(expected_access.size == size,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte read at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), uint64_t{address});
   return expected_access.value;
 }
 
@@ -72,9 +100,33 @@ void GloballyOrderedRegion::Write(zx_off_t address, uint64_t value,
   const GloballyOrderedRegion::Access& expected_access = access_list_[access_index_];
   ++access_index_;
 
-  ZX_ASSERT(expected_access.address == address);
-  ZX_ASSERT(expected_access.value == value);
-  ZX_ASSERT(expected_access.size == size);
+  ZX_ASSERT_MSG(expected_access.address == address,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte write of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), value, value, uint64_t{address});
+
+  ZX_ASSERT_MSG(expected_access.write == true,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte write of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), value, value, uint64_t{address});
+
+  ZX_ASSERT_MSG(expected_access.size == size,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte write of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), value, value, uint64_t{address});
+
+  ZX_ASSERT_MSG(expected_access.value == value,
+                "Expected %d-byte %s of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64
+                ", got %d-byte write of %" PRIu64 " (0x%" PRIx64 ") at address 0x%" PRIx64,
+                SizeInt(expected_access.size), expected_access.write ? "write" : "read",
+                expected_access.value, expected_access.value, uint64_t{expected_access.address},
+                SizeInt(size), value, value, uint64_t{address});
 }
 
 }  // namespace mock_mmio
