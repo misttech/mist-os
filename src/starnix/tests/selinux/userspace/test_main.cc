545bd97eb00a46e396c935149bcc8fdeba8e2eb7
Wez <wez@google.com>
2025-05-01 13:23:02 -0700

    [sestarnix] Fix userspace tests to detect environment setup failure
    Bug: 414880689
    Change-Id: I4867e1b0a822b22a19b486ba02514c92bcd84721
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1266124
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Reviewed-by: Laura Peskin <pesk@google.com>

--- src/starnix/tests/selinux/userspace/test_main.cc
+++ src/starnix/tests/selinux/userspace/test_main.cc
@@ -8,23 +8,46 @@
 
 #include "src/starnix/tests/selinux/userspace/util.h"
 
+namespace {
+
+// Perform one-time initialization of the test system.
 void PrepareTestEnvironment() {
-  EXPECT_THAT(mkdir("/proc", 0755), SyscallSucceeds());
-  EXPECT_THAT(mkdir("/sys", 0755), SyscallSucceeds());
-  EXPECT_THAT(mkdir("/tmp", 0755), SyscallSucceeds());
-  EXPECT_THAT(mount("proc", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, 0),
+  ASSERT_THAT(mkdir("/proc", 0755), SyscallSucceeds());
+  ASSERT_THAT(mkdir("/sys", 0755), SyscallSucceeds());
+  ASSERT_THAT(mkdir("/tmp", 0755), SyscallSucceeds());
+  ASSERT_THAT(mount("proc", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, 0),
               SyscallSucceeds());
-  EXPECT_THAT(mount("sysfs", "/sys", "sysfs", MS_NOEXEC | MS_NOSUID | MS_NODEV, 0),
+  ASSERT_THAT(mount("sysfs", "/sys", "sysfs", MS_NOEXEC | MS_NOSUID | MS_NODEV, 0),
               SyscallSucceeds());
-  EXPECT_THAT(mount("selinuxfs", "/sys/fs/selinux", "selinuxfs", MS_NOEXEC | MS_NOSUID, nullptr),
+  ASSERT_THAT(mount("selinuxfs", "/sys/fs/selinux", "selinuxfs", MS_NOEXEC | MS_NOSUID, nullptr),
               SyscallSucceeds());
-  EXPECT_THAT(mount("tmpfs", "/tmp", "tmpfs", MS_RELATIME, nullptr), SyscallSucceeds());
+  ASSERT_THAT(mount("tmpfs", "/tmp", "tmpfs", MS_RELATIME, nullptr), SyscallSucceeds());
 }
+
+class UserspaceTestEnvironment : public ::testing::Environment {
+ public:
+  void SetUp() override {
+    PrepareTestEnvironment();
+
+    // gTest is documented as treating `Environment::SetUp` fatal failures as fatal, but does not
+    // appear to actually do so, so we manually terminate the attempt on setup failures.
+    if (::testing::Test::HasFailure()) {
+      fprintf(stderr, "Test environment setup failed => failing all tests.\n");
+      fflush(stdout);
+      fflush(stderr);
+      _exit(1);
+    }
+  }
+};
+
+}  // namespace
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  PrepareTestEnvironment();
+  // Set up gTest to perform test environment setup at-most-once.
+  GTEST_FLAG_SET(recreate_environments_when_repeating, false);
+  ::testing::AddGlobalTestEnvironment(new UserspaceTestEnvironment);
 
   return RUN_ALL_TESTS();
 }
