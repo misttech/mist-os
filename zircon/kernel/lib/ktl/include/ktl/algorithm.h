7adedb1acdd81d437274a95b27f120fc2d22d071
Adrian Danis <adanis@google.com>
2025-05-04 15:55:03 -0700

    [kernel][ktl] Add ranges to <ktl/algorithm.h>
    Adds ranges from c++20
    
    Change-Id: I292d0aa4be31f7b68042b3cd724c9279680a9af9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1266404
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Adrian Danis <adanis@google.com>

--- zircon/kernel/lib/ktl/include/ktl/algorithm.h
+++ zircon/kernel/lib/ktl/include/ktl/algorithm.h
@@ -28,6 +28,24 @@ using std::none_of;
 using std::search;
 using std::search_n;
 
+namespace ranges {
+using std::ranges::adjacent_find;
+using std::ranges::all_of;
+using std::ranges::any_of;
+using std::ranges::count;
+using std::ranges::count_if;
+using std::ranges::find;
+using std::ranges::find_end;
+using std::ranges::find_first_of;
+using std::ranges::find_if;
+using std::ranges::find_if_not;
+using std::ranges::for_each;
+using std::ranges::for_each_n;
+using std::ranges::none_of;
+using std::ranges::search;
+using std::ranges::search_n;
+}  // namespace ranges
+
 // "Modifying sequence operations" (subset)
 using std::copy;
 using std::copy_if;
@@ -36,17 +54,40 @@ using std::fill_n;
 using std::reverse;
 using std::transform;
 
+namespace ranges {
+using std::ranges::copy;
+using std::ranges::copy_if;
+using std::ranges::fill;
+using std::ranges::fill_n;
+using std::ranges::reverse;
+using std::ranges::transform;
+}  // namespace ranges
+
 // "Sorting operations" (subset)
 using std::is_sorted;
 using std::is_sorted_until;
 using std::sort;
 using std::stable_sort;
 
+namespace ranges {
+using std::ranges::is_sorted;
+using std::ranges::is_sorted_until;
+using std::ranges::sort;
+using std::ranges::stable_sort;
+}  // namespace ranges
+
 // "Binary search operations (on sorted ranges)"
 using std::binary_search;
 using std::equal_range;
 using std::lower_bound;
 using std::upper_bound;
+
+namespace ranges {
+using std::ranges::binary_search;
+using std::ranges::equal_range;
+using std::ranges::lower_bound;
+using std::ranges::upper_bound;
+}  // namespace ranges
 
 // "Minimum/maximum operations"
 using std::clamp;
@@ -56,6 +97,16 @@ using std::min;
 using std::min_element;
 using std::minmax;
 using std::minmax_element;
+
+namespace ranges {
+using std::ranges::clamp;
+using std::ranges::max;
+using std::ranges::max_element;
+using std::ranges::min;
+using std::ranges::min_element;
+using std::ranges::minmax;
+using std::ranges::minmax_element;
+}
 
 }  // namespace ktl
 
