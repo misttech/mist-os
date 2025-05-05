eadf71572e4af86e5a98433bff96cee586a3f5e2
awdavies <awdavies@google.com>
2025-05-05 14:58:07 -0700

    [fuchsia-controller] Enable mypy for e2e.py tests
    Bug: 346628306
    Change-Id: Ie2094cd31a23ca73bac69da940a3f4c16a6a636c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1265870
    Reviewed-by: Marc Khouri <mnck@google.com>
    Commit-Queue: Andrew Davies <awdavies@google.com>

--- src/developer/ffx/lib/fuchsia-controller/tests/e2e.py
+++ src/developer/ffx/lib/fuchsia-controller/tests/e2e.py
@@ -44,29 +44,39 @@ class TestSocketServer(fc_test.TestingServer):
 
     def return_socket_or_error(
         self,
-    ) -> fc_test.TestingReturnSocketOrErrorResponse:
+    ) -> fc_test.TestingServer.ReturnSocketOrErrorResponse:
         if self.server.as_int() == 0:
             return DomainError(ZxStatus.ZX_ERR_NOT_SUPPORTED)
         return fc_test.TestingReturnSocketOrErrorResponse(
             reader_thing=self.server.take()
         )
 
-    def return_union(self):
+    def return_union(self) -> fc_test.TestingServer.ReturnUnionResponse:
         res = fc_test.TestingReturnUnionResponse(x=10)
         return res
 
-    def return_union_with_table(self):
+    def return_union_with_table(
+        self,
+    ) -> fc_test.TestingServer.ReturnUnionWithTableResponse:
         x = fc_test.NoopUnion(union_str="foobar")
         res = fc_test.TestingReturnUnionWithTableResponse(x=x)
         return res
 
-    def return_other_composed_protocol(self):
+    def return_other_composed_protocol(
+        self,
+    ) -> fc_test.TestingServer.ReturnOtherComposedProtocolResponse:
+        return fc_test.TestingReturnOtherComposedProtocolResponse(
+            client_thing=0
+        )
+
+    def return_possible_error(
+        self,
+    ) -> fc_test.TestingServer.ReturnPossibleErrorResponse:
         return DomainError("not implemented")
 
-    def return_possible_error(self):
-        return DomainError("not implemented")
-
-    def return_possible_error2(self):
+    def return_possible_error2(
+        self,
+    ) -> fc_test.TestingServer.ReturnPossibleError2Response:
         return DomainError("not implemented")
 
 
@@ -84,17 +94,17 @@ class EndToEnd(unittest.IsolatedAsyncioTestCase):
             isolation_path = os.path.join(tmp_path, "isolate")
         return IsolateDir(dir=isolation_path)
 
-    def _make_ctx(self):
+    def _make_ctx(self) -> Context:
         return Context(
             config=self._get_default_config(),
             isolate_dir=self._get_isolate_dir(),
         )
 
-    def test_config_get_nonexistent(self):
+    def test_config_get_nonexistent(self) -> None:
         ctx = self._make_ctx()
         self.assertEqual(ctx.config_get_string("foobarzzzzzzo==?"), None)
 
-    def test_config_get_exists(self):
+    def test_config_get_exists(self) -> None:
         config = self._get_default_config()
         key = "foobar"
         expect = "bazmumble"
@@ -102,7 +112,7 @@ class EndToEnd(unittest.IsolatedAsyncioTestCase):
         ctx = Context(config=config, isolate_dir=self._get_isolate_dir())
         self.assertEqual(ctx.config_get_string(key), expect)
 
-    def test_config_get_too_long(self):
+    def test_config_get_too_long(self) -> None:
         config = self._get_default_config()
         key = "foobarzington"
         expect = "b" * 50000
@@ -111,7 +121,7 @@ class EndToEnd(unittest.IsolatedAsyncioTestCase):
         with self.assertRaises(BufferError):
             ctx.config_get_string(key)
 
-    async def test_client_sends_message_before_coro_await(self):
+    async def test_client_sends_message_before_coro_await(self) -> None:
         (ch0, ch1) = Channel.create()
         echo_proxy = ffx_fidl.EchoClient(ch0)
         coro = echo_proxy.echo_string(value="foo")
@@ -140,14 +150,14 @@ class EndToEnd(unittest.IsolatedAsyncioTestCase):
         result = await coro
         self.assertEqual(result.response, "otherthing")
 
-    def test_context_creation_duplicate_target_raises_exception(self):
+    def test_context_creation_duplicate_target_raises_exception(self) -> None:
         with self.assertRaises(RuntimeError):
             _ctx = Context(target="foo", config={"target.default": "bar"})
 
-    def test_context_creation_no_args(self):
+    def test_context_creation_no_args(self) -> None:
         Context()
 
-    async def test_sending_fidl_protocol(self):
+    async def test_sending_fidl_protocol(self) -> None:
         tc_server, tc_client = Channel.create()
         list_server, list_client = Channel.create()
         tc_proxy = ffx_fidl.TargetCollectionClient(tc_client)
@@ -161,20 +171,23 @@ class EndToEnd(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(list_hdls), 0)
         self.assertEqual(list_buf, bytearray([5, 6, 7]))
 
-    async def test_sending_socket_as_result(self):
+    async def test_sending_socket_as_result(self) -> None:
         t_server, t_client = Channel.create()
         server = TestSocketServer(t_server)
         client = fc_test.TestingClient(t_client)
         server_task = asyncio.get_running_loop().create_task(server.serve())
-        res = await client.return_union()
-        self.assertEqual(res.x, 10)
-        res = await client.return_union_with_table()
-        self.assertEqual(res.x.union_str, "foobar")
-        res = await client.return_socket_or_error()
+        res1 = await client.return_union()
+        self.assertEqual(res1.x, 10)
+        res2 = await client.return_union_with_table()
+        # This is to placate mypy
+        assert res2.x is not None
+        self.assertEqual(res2.x.union_str, "foobar")
+        res3 = await client.return_socket_or_error()
         # TODO(b/415379365): Given this is intended to be a socket, it should return
         # the appropriate type here.
-        self.assertNotEqual(res.response.reader_thing, 0)
-        res = await client.return_socket_or_error()
-        self.assertEqual(res.err, ZxStatus.ZX_ERR_NOT_SUPPORTED)
+        assert res3.response is not None
+        self.assertNotEqual(res3.response.reader_thing, 0)
+        res4 = await client.return_socket_or_error()
+        self.assertEqual(res4.err, ZxStatus.ZX_ERR_NOT_SUPPORTED)
         server_task.cancel()
         server.close()
