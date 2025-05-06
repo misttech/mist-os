062a8b64e596bc69ddd2417a01876931d920e485
Adam barth <abarth@google.com>
2025-05-06 15:18:01 -0700

    [starnix][vfs] Move file_system.rs up a level
    This file was in its own subdirectory, but I don't see any reason for
    that. Looks like it might have been some automated refactoring of the
    codebase. This CL moves the file back to the normal place.
    
    Change-Id: I7b5f7e922b988c66d69c3928dd3d9994aa313690
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1270404
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Guocheng Wei <guochengwei@google.com>

--- src/starnix/kernel/vfs/file_system.rs
+++ src/starnix/kernel/vfs/file_system.rs
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Copyright 2021 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 use crate::security;
 use crate::task::{CurrentTask, Kernel};
 use crate::vfs::fs_args::MountParams;
