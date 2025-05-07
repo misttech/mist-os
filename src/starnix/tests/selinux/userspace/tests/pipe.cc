e897a968c21c1a2fabbe1c6982248446ffa9a29c
Wez <wez@google.com>
2025-05-06 18:46:35 -0700

    [sestarnix] Add basic userspace test for mkfifo() file labels
    Bug: 416040663
    Change-Id: Ie7fb7b21879218299365b49710bd038e481e0976
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1269884
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>

--- src/starnix/tests/selinux/userspace/tests/pipe.cc
+++ src/starnix/tests/selinux/userspace/tests/pipe.cc
@@ -2,15 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <string>
 
+#include <fbl/unique_fd.h>
 #include <gtest/gtest.h>
 
 #include "src/starnix/tests/selinux/userspace/util.h"
 
 namespace {
+
+TEST(FifoTest, LabeledFromFilesystem) {
+  auto scoped_current_task =
+      ScopedTaskAttrResetter::SetTaskAttr("current", "test_u:test_r:pipe_test_t:s0");
+
+  constexpr char kFifoPath[] = "/tmp/fifo_label_test";
+  ASSERT_THAT(mkfifo(kFifoPath, 0600), SyscallSucceeds());
+
+  EXPECT_THAT(GetLabel(kFifoPath), IsOk("test_u:object_r:test_fifo_file_t:s0"));
+
+  fbl::unique_fd fifo(open(kFifoPath, O_RDWR));
+  ASSERT_TRUE(fifo.is_valid());
+
+  EXPECT_THAT(GetLabel(fifo.get()), IsOk("test_u:object_r:test_fifo_file_t:s0"));
+}
 
 // Pipes receive the creating task's context, with no transitions applied.
 TEST(PipeTest, LabeledFromTask) {
