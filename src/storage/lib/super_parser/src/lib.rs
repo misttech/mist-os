a7902945a1abc8519622893b1acc025f39215b6d
Rui Qi Sim <rqsim@google.com>
2025-05-11 20:20:34 -0700

    [super_parser] Rename struct SuperParser -> Metadata
    Now that the struct is named `Metadata`, drop the `metadata_` prefix in
    names of its field. I.e.:
      * metadata_geometry -> geometry
      * meatadata_header -> header
    
    Also renamed function `SuperParser::open_device(..)` to
    `Metadata::load_from_device(..)` which better suits the new struct name.
    
    Bug: 404952286
    Test: No functional change
    Change-Id: I85fe725c6ac2498bf61fb5f359ffa2217539334b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1272609
    Commit-Queue: Rui Qi Sim <rqsim@google.com>
    Reviewed-by: Aaron Drew <ripper@google.com>

--- src/storage/lib/super_parser/src/lib.rs
+++ src/storage/lib/super_parser/src/lib.rs
@@ -29,48 +29,46 @@ fn round_up_to_alignment(x: u32, alignment: u32) -> Result<u32, Error> {
 // from the device and test for this.
 // TODO(https://fxbug.dev/404952286): Add fuzzer to check for arithmetic overflows.
 
-pub struct SuperParser {
-    metadata_geometry: MetadataGeometry,
-    metadata_header: MetadataHeader,
+/// Struct to help interpret the deserialized "super" metadata.
+pub struct Metadata {
+    geometry: MetadataGeometry,
+    header: MetadataHeader,
     partitions: Vec<MetadataPartition>,
     extents: Vec<MetadataExtent>,
     partition_groups: Vec<MetadataPartitionGroup>,
     block_devices: Vec<MetadataBlockDevice>,
 }
 
-impl SuperParser {
-    pub async fn open_device(device: FakeDevice) -> Result<Self, Error> {
-        let metadata_geometry = Self::parse_metadata_geometry(&device).await.unwrap();
+impl Metadata {
+    pub async fn load_from_device(device: FakeDevice) -> Result<Self, Error> {
+        let geometry = Self::parse_metadata_geometry(&device).await.unwrap();
         // TODO(https://fxbug.dev/404952286): read backup geometry if needed.
 
-        let metadata_header = Self::parse_metadata_header(&device).await.unwrap();
+        let header = Self::parse_metadata_header(&device).await.unwrap();
 
         // Now that we have more information on the tables, validate table-specific fields.
-        ensure!(
-            metadata_header.tables_size <= metadata_geometry.metadata_max_size,
-            "Invalid tables size."
-        );
+        ensure!(header.tables_size <= geometry.metadata_max_size, "Invalid tables size.");
 
         // TODO(https://fxbug.dev/404952286): read backup metadata if we fail any of the validation
         // the first time.
 
         // Get tables bytes
-        let metadata_header_offset = PARTITION_RESERVED_BYTES + 2 * METADATA_GEOMETRY_RESERVED_SIZE;
-        ensure!(metadata_header_offset % device.block_size() == 0, "Reads must be block aligned.");
-        let header_and_tables_size = metadata_header
+        let header_offset = PARTITION_RESERVED_BYTES + 2 * METADATA_GEOMETRY_RESERVED_SIZE;
+        ensure!(header_offset % device.block_size() == 0, "Reads must be block aligned.");
+        let header_and_tables_size = header
             .header_size
-            .checked_add(metadata_header.tables_size)
+            .checked_add(header.tables_size)
             .ok_or_else(|| anyhow!("arithmetic overflow: cannot calculate header and tables size to read from device"))?;
         let buffer_len = round_up_to_alignment(header_and_tables_size, device.block_size())?;
         let mut buffer = device.allocate_buffer(buffer_len as usize).await;
-        device.read(metadata_header_offset as u64, buffer.as_mut()).await?;
-        let tables_bytes = &buffer.as_slice()
-            [metadata_header.header_size as usize..header_and_tables_size as usize];
+        device.read(header_offset as u64, buffer.as_mut()).await?;
+        let tables_bytes =
+            &buffer.as_slice()[header.header_size as usize..header_and_tables_size as usize];
 
         // Read tables to verify `table_checksum` in metadata_header.
         let computed_tables_checksum: [u8; 32] = sha2::Sha256::digest(tables_bytes).into();
         ensure!(
-            computed_tables_checksum == metadata_header.tables_checksum,
+            computed_tables_checksum == header.tables_checksum,
             "Invalid metadata tables checksum"
         );
 
@@ -79,47 +77,47 @@ impl SuperParser {
         let partitions = Self::parse_table::<MetadataPartition>(
             tables_bytes,
             tables_offset,
-            &metadata_header,
-            &metadata_header.partitions,
+            &header,
+            &header.partitions,
         )
         .await
         .unwrap();
 
         // Parse extent table entries.
         let tables_offset = tables_offset
-            .checked_add(metadata_header.partitions.get_table_size()?)
+            .checked_add(header.partitions.get_table_size()?)
             .ok_or_else(|| anyhow!("Adding offset + num_entries * entry_size overflowed."))?;
         let extents = Self::parse_table::<MetadataExtent>(
             tables_bytes,
             tables_offset,
-            &metadata_header,
-            &metadata_header.extents,
+            &header,
+            &header.extents,
         )
         .await
         .unwrap();
 
         // Parse partition group table entries.
         let tables_offset = tables_offset
-            .checked_add(metadata_header.extents.get_table_size()?)
+            .checked_add(header.extents.get_table_size()?)
             .ok_or_else(|| anyhow!("Adding offset + num_entries * entry_size overflowed."))?;
         let partition_groups = Self::parse_table::<MetadataPartitionGroup>(
             tables_bytes,
             tables_offset,
-            &metadata_header,
-            &metadata_header.groups,
+            &header,
+            &header.groups,
         )
         .await
         .unwrap();
 
         // Parse block device table entries.
         let tables_offset = tables_offset
-            .checked_add(metadata_header.groups.get_table_size()?)
+            .checked_add(header.groups.get_table_size()?)
             .ok_or_else(|| anyhow!("Adding offset + num_entries * entry_size overflowed."))?;
         let block_devices = Self::parse_table::<MetadataBlockDevice>(
             tables_bytes,
             tables_offset,
-            &metadata_header,
-            &metadata_header.block_devices,
+            &header,
+            &header.block_devices,
         )
         .await
         .unwrap();
@@ -127,24 +125,14 @@ impl SuperParser {
         // Expect there to be at least be one block device: "super".
         ensure!(block_devices.len() > 0, "Metadata did not specify a super device.");
         let super_device = block_devices[0];
-        // The block device would have passed validation before. We don't expect this to return an
-        // error.
         let logical_partition_offset = super_device.get_first_logical_sector_in_bytes()?;
-        // `metadata_geometry` passed validation before. We don't expect this to return an error.
-        let metadata_region = metadata_geometry.get_total_metadata_size()?;
+        let metadata_region = geometry.get_total_metadata_size()?;
         ensure!(
             metadata_region <= logical_partition_offset,
             "Logical partition metadata overlaps with logical partition contents."
         );
 
-        Ok(Self {
-            metadata_geometry,
-            metadata_header,
-            partitions,
-            extents,
-            partition_groups,
-            block_devices,
-        })
+        Ok(Self { geometry, header, partitions, extents, partition_groups, block_devices })
     }
 
     async fn parse_metadata_geometry(device: &FakeDevice) -> Result<MetadataGeometry, Error> {
@@ -229,17 +217,17 @@ mod tests {
         let golden = std::path::Path::new("/pkg/data/simple_super.img.zstd");
         let device = open_image(golden);
 
-        let super_partition = SuperParser::open_device(device).await.expect("open device failed");
+        let super_partition = Metadata::load_from_device(device).await.expect("open device failed");
 
         // Check geometry.
-        let max_size = super_partition.metadata_geometry.metadata_max_size;
-        let metadata_slot_count = super_partition.metadata_geometry.metadata_slot_count;
+        let max_size = super_partition.geometry.metadata_max_size;
+        let metadata_slot_count = super_partition.geometry.metadata_slot_count;
         assert_eq!(max_size, 65536);
         assert_eq!(metadata_slot_count, 1);
 
         // Check header.
-        let num_partition_entries = super_partition.metadata_header.partitions.num_entries;
-        let num_extent_entries = super_partition.metadata_header.extents.num_entries;
+        let num_partition_entries = super_partition.header.partitions.num_entries;
+        let num_extent_entries = super_partition.header.extents.num_entries;
         assert_eq!(num_partition_entries, 2);
         assert_eq!(num_extent_entries, 1);
 
