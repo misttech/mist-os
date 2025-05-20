d52edd8a35cdd7ba024c3f5edcb6fe7d90f75015
Gianfranco Valentino <gevalentino@google.com>
2025-05-20 11:56:05 -0700

    [devicetree-boot-shim] Introduce BootShimHelper
    This object drives the entirety of PhysMain, giving a couple of spots to
    inject custom code when forking the shim for new boards.
    
    After the constructore and before initializing items is where one would
    add custom items and initialize them. After Init but before Boot one
    could inspect existing items and generate additional information, such
    as programmatically generate data for another item.
    
    Boot is a noreturn function that will append the contents of the items.
    
    Change-Id: I89540f2f001e59046180b23865f55150f19b0243
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1259504
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/arch/riscv64/phys/boot-shim/linux-riscv64-boot-shim.cc
+++ zircon/kernel/arch/riscv64/phys/boot-shim/linux-riscv64-boot-shim.cc
@@ -5,32 +5,12 @@
 // https://opensource.org/licenses/MIT
 
 #include <lib/boot-shim/devicetree-boot-shim.h>
-#include <lib/zbi-format/board.h>
-#include <lib/zbitl/view.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <zircon/assert.h>
-#include <zircon/compiler.h>
 
-#include <fbl/alloc_checker.h>
-#include <ktl/align.h>
-#include <ktl/span.h>
-#include <ktl/string_view.h>
-#include <phys/address-space.h>
-#include <phys/allocation.h>
-#include <phys/arch/arch-phys-info.h>
 #include <phys/boot-shim/devicetree.h>
-#include <phys/boot-zbi.h>
-#include <phys/main.h>
-#include <phys/new.h>
-#include <phys/stdio.h>
-#include <phys/symbolize.h>
-#include <phys/uart.h>
 
 #include <ktl/enforce.h>
 
 namespace {
-
 using Shim = Riscv64StandardBootShimItems::Shim<boot_shim::DevicetreeBootShim>;
 
 constexpr const char* kShimName = "linux-riscv64-boot-shim";
@@ -42,48 +22,10 @@ uint64_t BootHartIdGetter::Get() {
   return gArchPhysInfo->boot_hart_id;
 }
 
-void PhysMain(void* fdt, arch::EarlyTicks ticks) {
-  InitStdout();
-  ApplyRelocations();
-
-  // Set up gArchPhysInfo as early as possible, as the boot hart ID needs to
-  // be supplied to the CPU topology devicetree matcher.
-  ArchSetUp({});
-
-  // Explicitly provide `nullptr` address space, so the MMU is not set up.
-  // We do not know yet the supported features/extension in riscv, avoid possibly setting
-  // invalid bits in the page tables by not setting up the MMU at all.
-  InitMemory(fdt, {}, nullptr);
-
-  MainSymbolize symbolize(kShimName);
-
-  // Memory has been initialized, we can finish up parsing the rest of the items from the boot shim.
-  Shim shim(kShimName, gDevicetreeBoot.fdt);
-  shim.set_allocator([](size_t size, size_t align, fbl::AllocChecker& ac) -> void* {
-    return new (ktl::align_val_t{align}, gPhysNew<memalloc::Type::kPhysScratch>, ac) uint8_t[size];
-  });
-  shim.set_cmdline(gDevicetreeBoot.cmdline);
-  shim.Get<boot_shim::UartItem<>>().Init(GetUartDriver().config());
-  shim.Get<boot_shim::DevicetreeDtbItem>().set_payload(ktl::as_bytes(gDevicetreeBoot.fdt.fdt()));
-  shim.Get<boot_shim::PoolMemConfigItem>().Init(Allocation::GetPool());
-  if (gDevicetreeBoot.nvram) {
-    shim.Get<boot_shim::NvramItem>().set_payload(*gDevicetreeBoot.nvram);
-  }
-
-  // Fill DevicetreeItems.
-  ZX_ASSERT(shim.Init());
-
-  // Finally we can boot into the kernel image.
-  BootZbi::InputZbi zbi_view(gDevicetreeBoot.ramdisk);
-  BootZbi boot;
-
-  if (shim.Check("Not a bootable ZBI", boot.Init(zbi_view)) &&
-      shim.Check("Failed to load ZBI", boot.Load(static_cast<uint32_t>(shim.size_bytes()))) &&
-      shim.Check("Failed to append boot loader items to data ZBI",
-                 shim.AppendItems(boot.DataZbi()))) {
-    boot.Log();
-    boot.Boot();
-  }
-
-  abort();
+void PhysMain(void* flat_devicetree_blob, arch::EarlyTicks ticks) {
+  BootShimHelper<Shim> shim_helper(kShimName, flat_devicetree_blob);
+  // Items will be initialized, and the matching process will begin.
+  ZX_ASSERT(shim_helper.InitItems());
+  // Items will be appended to the zbi, and we will boot into the kernel in that zbi.
+  shim_helper.Boot();
 }
