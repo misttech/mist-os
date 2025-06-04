3af4edb868c293794c7715a3e8333698125d95df
Christopher R. Johnson <crjohns@google.com>
2025-06-04 16:50:47 -0700

    [audio_recording] Reland: Refactor service
    This is a reland of commit e10a1774594e3316b47e9186cb9db9b577a38015
    
    This fixes the underlying issue by lazily initializing input and output
    devices again, rather than starting them at startup.
    
    Bug: 422488990
    Bug: 422429879
    
    Original change's description:
    > [audio_recording] Refactor service
    >
    > This refactor removes asynchronous channels in favor of just directly
    > executing operations on workers behind a mutex.
    >
    > Change-Id: I8711709d5f9d81dcda14d243ca3c3c649b241c76
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1289472
    > Reviewed-by: Henry-Michel Cantave <hmcantave@google.com>
    > Fuchsia-Auto-Submit: Christopher Johnson <crjohns@google.com>
    > Commit-Queue: Christopher Johnson <crjohns@google.com>
    
    Change-Id: I7bc65c6a4e220e238b44ab84c79717f91767038f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1292548
    Reviewed-by: Henry-Michel Cantave <hmcantave@google.com>
    Commit-Queue: Christopher Johnson <crjohns@google.com>

--- src/testing/sl4f/audio_recording/src/main.rs
+++ src/testing/sl4f/audio_recording/src/main.rs
@@ -2,26 +2,33 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::sync::Arc;
+
 use anyhow::Error;
 use fuchsia_async as fasync;
 use fuchsia_component::server::ServiceFs;
 use futures::stream::StreamExt;
-use log::error;
-use std::sync::Arc;
+use log::{error, info};
 
 mod audio_facade;
+mod input_worker;
+mod output_worker;
 mod server;
+mod util;
 
 use crate::audio_facade::AudioFacade;
 
 #[fuchsia::main]
 async fn main() -> Result<(), Error> {
+    info!("Starting audio recording service");
     let mut fs = ServiceFs::new_local();
-    let audio_facade = Arc::new(AudioFacade::new()?);
 
-    let facade_one = audio_facade.clone();
+    let audio_facade: Arc<AudioFacade> = Arc::new(AudioFacade::new().await?);
+
+    let audio_facade_clone = audio_facade.clone();
     fs.dir("svc").add_fidl_service(move |stream| {
-        let audio_facade_clone = facade_one.clone();
+        info!("Starting injection stream");
+        let audio_facade_clone = audio_facade_clone.clone();
         fasync::Task::spawn(async move {
             server::handle_injection_request(audio_facade_clone, stream)
                 .await
@@ -31,6 +38,7 @@ async fn main() -> Result<(), Error> {
     });
 
     fs.dir("svc").add_fidl_service(move |stream| {
+        info!("Starting capture stream");
         let audio_facade_clone = audio_facade.clone();
         fasync::Task::spawn(async move {
             server::handle_capture_request(audio_facade_clone, stream)
