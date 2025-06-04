88247c797faf9e866f71c122c750c9889a46842d
Adam barth <abarth@google.com>
2025-06-03 17:48:16 -0700

    [starnix][sysfs] Revert "[starnix][pseudo] Create PseudoDirectory"
    This reverts commit 0510dc902b1a927ad2b03c19bf8779ad7f1f7abe.
    
    That commit broke the ability to chown and chmod some files in sysfs.
    
    Fixed: b/421261074
    Change-Id: Icb327693a7bd9ce80df3c9c73f83db9aa3c8eff4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1291905
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Reviewed-by: Onath Dillinger <claridge@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Adam Barth <abarth@google.com>

--- src/starnix/kernel/fs/sysfs/fs.rs
+++ src/starnix/kernel/fs/sysfs/fs.rs
@@ -8,8 +8,8 @@ use crate::fs::sysfs::{
     VulnerabilitiesClassDirectory,
 };
 use crate::task::CurrentTask;
-use crate::vfs::pseudo::pseudo_directory::PseudoDirectoryBuilder;
 use crate::vfs::pseudo::simple_file::BytesFile;
+use crate::vfs::pseudo::static_directory::StaticDirectoryBuilder;
 use crate::vfs::pseudo::stub_empty_file::StubEmptyFile;
 use crate::vfs::{
     CacheConfig, CacheMode, FileSystem, FileSystemHandle, FileSystemOps, FileSystemOptions,
@@ -21,7 +21,7 @@ use starnix_sync::{FileOpsCore, Locked, Unlocked};
 use starnix_types::vfs::default_statfs;
 use starnix_uapi::auth::FsCred;
 use starnix_uapi::errors::Errno;
-use starnix_uapi::file_mode::{mode, FileMode};
+use starnix_uapi::file_mode::mode;
 use starnix_uapi::{statfs, SYSFS_MAGIC};
 
 pub const SYSFS_DEVICES: &str = "devices";
@@ -47,68 +47,71 @@ impl FileSystemOps for SysFs {
 
 impl SysFs {
     fn new_fs(current_task: &CurrentTask, options: FileSystemOptions) -> FileSystemHandle {
-        const DIR_MODE: FileMode = mode!(IFDIR, 0o755);
-        const REG_MODE: FileMode = mode!(IFREG, 0o444);
-        const CREDS: FsCred = FsCred::root();
-
-        let mut dir = PseudoDirectoryBuilder::default();
-        {
-            let dir = dir.subdir(b"fs");
-            dir.subdir(b"selinux");
-            dir.subdir(b"bpf");
-            dir.subdir(b"cgroup");
-            {
-                let dir = dir.subdir(b"fuse");
-                dir.subdir(b"connections");
-                {
-                    let dir = dir.subdir(b"features");
-                    dir.node(b"fuse_bpf", REG_MODE, CREDS, || {
-                        BytesFile::new_node(b"supported\n".to_vec())
-                    });
-                }
-                dir.node(b"bpf_prog_type_fuse", REG_MODE, CREDS, || {
-                    BytesFile::new_node(format!("{}\n", BPF_PROG_TYPE_FUSE).into_bytes())
-                });
-            }
-            dir.subdir(b"pstore");
-        }
-
         let kernel = current_task.kernel();
         let registry = &kernel.device_registry;
 
-        let devices = registry.objects.devices.clone();
-        dir.node_ops(SYSFS_DEVICES, DIR_MODE, CREDS, move || devices.ops());
+        let fs = FileSystem::new_with_node_cache(
+            kernel,
+            CacheMode::Cached(CacheConfig::default()),
+            SysFs,
+            options,
+            registry.objects.node_cache.clone(),
+        )
+        .expect("sysfs constructed with valid options");
+        let mut dir = StaticDirectoryBuilder::new(&fs);
+        let dir_mode = mode!(IFDIR, 0o755);
+        dir.subdir(current_task, "fs", 0o755, |dir| {
+            dir.subdir(current_task, "selinux", 0o755, |_| ());
+            dir.subdir(current_task, "bpf", 0o755, |_| ());
+            dir.subdir(current_task, "cgroup", 0o755, |_| ());
+            dir.subdir(current_task, "fuse", 0o755, |dir| {
+                dir.subdir(current_task, "connections", 0o755, |_| ());
+                dir.subdir(current_task, "features", 0o755, |dir| {
+                    dir.node(
+                        "fuse_bpf",
+                        fs.create_node_and_allocate_node_id(
+                            current_task,
+                            BytesFile::new_node(b"supported\n".to_vec()),
+                            FsNodeInfo::new(mode!(IFREG, 0o444), FsCred::root()),
+                        ),
+                    );
+                });
+                dir.node(
+                    "bpf_prog_type_fuse",
+                    fs.create_node_and_allocate_node_id(
+                        current_task,
+                        BytesFile::new_node(format!("{}\n", BPF_PROG_TYPE_FUSE).into_bytes()),
+                        FsNodeInfo::new(mode!(IFREG, 0o444), FsCred::root()),
+                    ),
+                );
+            });
+            dir.subdir(current_task, "pstore", 0o755, |_| ());
+        });
 
-        let bus = registry.objects.bus.clone();
-        dir.node_ops(SYSFS_BUS, DIR_MODE, CREDS, move || bus.ops());
+        dir.entry(current_task, SYSFS_DEVICES, registry.objects.devices.ops(), dir_mode);
+        dir.entry(current_task, SYSFS_BUS, registry.objects.bus.ops(), dir_mode);
+        dir.entry(current_task, SYSFS_BLOCK, registry.objects.block.ops(), dir_mode);
+        dir.entry(current_task, SYSFS_CLASS, registry.objects.class.ops(), dir_mode);
+        dir.entry(current_task, SYSFS_DEV, registry.objects.dev.ops(), dir_mode);
 
-        let block = registry.objects.block.clone();
-        dir.node_ops(SYSFS_BLOCK, DIR_MODE, CREDS, move || block.ops());
+        sysfs_kernel_directory(current_task, &mut dir);
+        sysfs_power_directory(current_task, &mut dir);
 
-        let class = registry.objects.class.clone();
-        dir.node_ops(SYSFS_CLASS, DIR_MODE, CREDS, move || class.ops());
-
-        let dev = registry.objects.dev.clone();
-        dir.node_ops(SYSFS_DEV, DIR_MODE, CREDS, move || dev.ops());
-
-        sysfs_kernel_directory(kernel, &mut dir.subdir(b"kernel"));
-        sysfs_power_directory(kernel, &mut dir.subdir(b"power"));
-
-        {
-            let dir = dir.subdir(b"module");
-            {
-                let dir = dir.subdir(b"dm_verity");
-                {
-                    let dir = dir.subdir(b"parameters");
-                    dir.node(b"prefetch_cluster", mode!(IFREG, 0o644), CREDS, || {
+        dir.subdir(current_task, "module", 0o755, |dir| {
+            dir.subdir(current_task, "dm_verity", 0o755, |dir| {
+                dir.subdir(current_task, "parameters", 0o755, |dir| {
+                    dir.entry(
+                        current_task,
+                        "prefetch_cluster",
                         StubEmptyFile::new_node(
                             "/sys/module/dm_verity/paramters/prefetch_cluster",
                             bug_ref!("https://fxbug.dev/322893670"),
-                        )
-                    });
-                }
-            }
-        }
+                        ),
+                        mode!(IFREG, 0o644),
+                    );
+                });
+            });
+        });
 
         // TODO(https://fxbug.dev/42072346): Temporary fix of flakeness in tcp_socket_test.
         // Remove after registry.rs refactor is in place.
@@ -119,15 +122,7 @@ impl SysFs {
             .get_or_create_child("cpu".into(), CpuClassDirectory::new)
             .get_or_create_child("vulnerabilities".into(), VulnerabilitiesClassDirectory::new);
 
-        let fs = FileSystem::new_with_node_cache(
-            kernel,
-            CacheMode::Cached(CacheConfig::default()),
-            SysFs,
-            options,
-            registry.objects.node_cache.clone(),
-        )
-        .expect("sysfs constructed with valid options");
-        dir.build_root(&fs, mode!(IFDIR, 0o777), CREDS);
+        dir.build_root();
         fs
     }
 }
--- src/starnix/kernel/fs/sysfs/kernel_directory.rs
+++ src/starnix/kernel/fs/sysfs/kernel_directory.rs
@@ -2,52 +2,62 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use crate::task::Kernel;
-use crate::vfs::pseudo::pseudo_directory::PseudoDirectoryBuilder;
+use crate::task::CurrentTask;
 use crate::vfs::pseudo::simple_file::create_bytes_file_with_handler;
+use crate::vfs::pseudo::static_directory::StaticDirectoryBuilder;
 use crate::vfs::pseudo::stub_empty_file::StubEmptyFile;
 use starnix_logging::bug_ref;
-use starnix_uapi::auth::FsCred;
 use starnix_uapi::file_mode::mode;
 use std::sync::Arc;
 
 /// This directory contains various files and subdirectories that provide information about
 /// the running kernel.
-pub fn sysfs_kernel_directory(kernel: &Arc<Kernel>, dir: &mut PseudoDirectoryBuilder) {
-    let weak_kernel = Arc::downgrade(kernel);
-    dir.subdir(b"tracing");
-    {
-        let dir = dir.subdir(b"mm");
-        {
-            let dir = dir.subdir(b"transparent_hugepage");
-            dir.node(b"enabled", mode!(IFREG, 0o644), FsCred::root(), || {
-                StubEmptyFile::new_node(
-                    "/sys/kernel/mm/transparent_hugepage/enabled",
-                    bug_ref!("https://fxbug.dev/322894184"),
-                )
+pub fn sysfs_kernel_directory(current_task: &CurrentTask, dir: &mut StaticDirectoryBuilder<'_>) {
+    let kernel = current_task.kernel();
+    dir.subdir(current_task, "kernel", 0o755, |dir| {
+        dir.subdir(current_task, "tracing", 0o755, |_| ());
+        dir.subdir(current_task, "mm", 0o755, |dir| {
+            dir.subdir(current_task, "transparent_hugepage", 0o755, |dir| {
+                dir.entry(
+                    current_task,
+                    "enabled",
+                    StubEmptyFile::new_node(
+                        "/sys/kernel/mm/transparent_hugepage/enabled",
+                        bug_ref!("https://fxbug.dev/322894184"),
+                    ),
+                    mode!(IFREG, 0o644),
+                );
             });
-        }
-    }
-    {
-        let dir = dir.subdir(b"wakeup_reasons");
-        let k = weak_kernel.clone();
-        dir.node(b"last_resume_reason", mode!(IFREG, 0o444), FsCred::root(), move || {
-            create_bytes_file_with_handler(k.clone(), |kernel| {
-                kernel.suspend_resume_manager.suspend_stats().last_resume_reason.unwrap_or_default()
-            })
         });
-        let k = weak_kernel;
-        dir.node(b"last_suspend_time", mode!(IFREG, 0o444), FsCred::root(), move || {
-            create_bytes_file_with_handler(k.clone(), |kernel| {
-                let suspend_stats = kernel.suspend_resume_manager.suspend_stats();
-                // First number is the time spent in suspend and resume processes.
-                // Second number is the time spent in sleep state.
-                format!(
-                    "{} {}",
-                    suspend_stats.last_time_in_suspend_operations.into_seconds_f64(),
-                    suspend_stats.last_time_in_sleep.into_seconds_f64()
-                )
-            })
+        dir.subdir(current_task, "wakeup_reasons", 0o755, |dir| {
+            let read_only_file_mode = mode!(IFREG, 0o444);
+            dir.entry(
+                current_task,
+                "last_resume_reason",
+                create_bytes_file_with_handler(Arc::downgrade(kernel), |kernel| {
+                    kernel
+                        .suspend_resume_manager
+                        .suspend_stats()
+                        .last_resume_reason
+                        .unwrap_or_default()
+                }),
+                read_only_file_mode,
+            );
+            dir.entry(
+                current_task,
+                "last_suspend_time",
+                create_bytes_file_with_handler(Arc::downgrade(kernel), |kernel| {
+                    let suspend_stats = kernel.suspend_resume_manager.suspend_stats();
+                    // First number is the time spent in suspend and resume processes.
+                    // Second number is the time spent in sleep state.
+                    format!(
+                        "{} {}",
+                        suspend_stats.last_time_in_suspend_operations.into_seconds_f64(),
+                        suspend_stats.last_time_in_sleep.into_seconds_f64()
+                    )
+                }),
+                read_only_file_mode,
+            );
         });
-    }
+    });
 }
