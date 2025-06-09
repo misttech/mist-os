56a22e0356e66c5647ba18d5f45641ae1c7b846b
Guocheng Wei <guochengwei@google.com>
2025-06-09 13:22:51 -0700

    [starnix] Add timerslack to zx timer usage
    Starnix task already stored the timerslack setting from the userspace.
    It should be applied to all zx::Timer.
    
    For other zircon syscalls, e.g. futex wait, wait_one, etc, timerslack
    cannot be applied yet.
    
    Bug: 331970600, 42082730
    Change-Id: Ia8384f2d9bb9662bb601ce8317eef444be60745f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1295807
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Guocheng Wei <guochengwei@google.com>

--- src/starnix/kernel/fs/fuchsia/timer.rs
+++ src/starnix/kernel/fs/fuchsia/timer.rs
@@ -24,15 +24,16 @@ impl MonotonicZxTimer {
 impl TimerOps for MonotonicZxTimer {
     fn start(
         &self,
-        _currnet_task: &CurrentTask,
+        current_task: &CurrentTask,
         _source: Option<Weak<dyn OnWakeOps>>,
         deadline: TargetTime,
     ) -> Result<(), Errno> {
+        let timerslack =
+            zx::MonotonicDuration::from_nanos(current_task.read().get_timerslack_ns() as i64);
         match deadline {
-            TargetTime::Monotonic(t) => self
-                .timer
-                .set(t, zx::MonotonicDuration::default())
-                .map_err(|status| from_status_like_fdio!(status))?,
+            TargetTime::Monotonic(t) => {
+                self.timer.set(t, timerslack).map_err(|status| from_status_like_fdio!(status))?
+            }
             TargetTime::BootInstant(_) | TargetTime::RealTime(_) => return error!(EINVAL),
         };
 
@@ -65,18 +66,18 @@ impl BootZxTimer {
 impl TimerOps for BootZxTimer {
     fn start(
         &self,
-        _currnet_task: &CurrentTask,
+        current_task: &CurrentTask,
         _source: Option<Weak<dyn OnWakeOps>>,
         deadline: TargetTime,
     ) -> Result<(), Errno> {
+        let timerslack = zx::Duration::from_nanos(current_task.read().get_timerslack_ns() as i64);
         match deadline {
-            TargetTime::BootInstant(t) => self
-                .timer
-                .set(t, zx::Duration::default())
-                .map_err(|status| from_status_like_fdio!(status))?,
+            TargetTime::BootInstant(t) => {
+                self.timer.set(t, timerslack).map_err(|status| from_status_like_fdio!(status))?
+            }
             TargetTime::RealTime(t) => self
                 .timer
-                .set(estimate_boot_deadline_from_utc(t), zx::Duration::default())
+                .set(estimate_boot_deadline_from_utc(t), timerslack)
                 .map_err(|status| from_status_like_fdio!(status))?,
             TargetTime::Monotonic(_) => return error!(EINVAL),
         }
