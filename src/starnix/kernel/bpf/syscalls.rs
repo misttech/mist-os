455d3cf9fef2deea40fa6b10082e0824e557610a
David Iserovich <diserovich@google.com>
2025-06-10 23:17:46 -0700

    [starnix][bpf] return enoent instead of einval
    Return enoent instead of einval on BPF_PROG_GET_NEXT_ID and
    BPF_MAP_GET_NEXT_ID.
    
    Bug: 423897349
    Change-Id: Id1849fc9a0e279bb2e0f5f1522a03f55631ed3ce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1297104
    Commit-Queue: David Iserovich <diserovich@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@google.com>
    Fuchsia-Auto-Submit: David Iserovich <diserovich@google.com>

--- src/starnix/kernel/bpf/syscalls.rs
+++ src/starnix/kernel/bpf/syscalls.rs
@@ -454,12 +454,12 @@ pub fn sys_bpf(
             error!(EINVAL)
         }
         bpf_cmd_BPF_PROG_GET_NEXT_ID => {
-            track_stub!(TODO("https://fxbug.dev/322874055"), "BPF_PROG_GET_NEXT_ID");
-            error!(EINVAL)
+            track_stub!(TODO("https://fxbug.dev/423965951"), "BPF_PROG_GET_NEXT_ID");
+            error!(ENOENT)
         }
         bpf_cmd_BPF_MAP_GET_NEXT_ID => {
-            track_stub!(TODO("https://fxbug.dev/322874055"), "BPF_MAP_GET_NEXT_ID");
-            error!(EINVAL)
+            track_stub!(TODO("https://fxbug.dev/423965951"), "BPF_MAP_GET_NEXT_ID");
+            error!(ENOENT)
         }
         bpf_cmd_BPF_PROG_GET_FD_BY_ID => {
             track_stub!(TODO("https://fxbug.dev/322874055"), "BPF_PROG_GET_FD_BY_ID");
