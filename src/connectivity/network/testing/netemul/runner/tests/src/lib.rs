90b27ceac9268f2301f41f3fca4dd59398fcfd20
Dale Sather <dalesat@google.com>
2025-06-11 13:18:16 -0700

    [debug_agent] use new test_manager api
    This change updates the debug_agent and netemul/runner to use the new
    test_manager API, removing that last of the references to the old API.
    Several .cml files are also updated to no longer route the old API.
    
    Bug: 309995479
    
    Change-Id: I8c2d6d90295f635e6c00bb0d004b9123bee90177
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1282669
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Steven Grady <slgrady@google.com>
    Commit-Queue: Dale Sather <dalesat@google.com>
    Mega-CQ: Dale Sather <dalesat@google.com>
    Reviewed-by: Jacob Rutherford <jruthe@google.com>

--- src/connectivity/network/testing/netemul/runner/tests/src/lib.rs
+++ src/connectivity/network/testing/netemul/runner/tests/src/lib.rs
@@ -2,29 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use assert_matches::assert_matches;
 use fidl_fuchsia_test_manager as ftest_manager;
 use test_manager_test_lib::RunEvent;
 
 pub async fn run_test(test_url: &str) -> Result<(Vec<RunEvent>, Vec<String>), anyhow::Error> {
-    let run_builder =
-        fuchsia_component::client::connect_to_protocol::<ftest_manager::RunBuilderMarker>()
+    let suite_runner =
+        fuchsia_component::client::connect_to_protocol::<ftest_manager::SuiteRunnerMarker>()
             .expect("connect to test manager protocol");
-    let builder = test_manager_test_lib::TestBuilder::new(run_builder).filter_debug_data();
-    let suite_instance = builder
-        .add_suite(test_url, ftest_manager::RunOptions::default())
-        .await
-        .expect("create suite instance");
+    let runner = test_manager_test_lib::SuiteRunner::new(suite_runner);
 
-    let (test_run_result, test_suite_result) = futures_util::future::join(
-        builder.run(),
-        test_runners_test_lib::process_events(suite_instance, true),
-    )
-    .await;
-    // `TestBuilder::run` returns a `Vec` of `TestRunEvent`s, which only ever
-    // include debug data currently, so expect this to be empty.
-    let test_run_events = test_run_result.expect("builder execution failed");
-    assert_matches!(&test_run_events[..], []);
+    let suite_instance =
+        runner.start_suite_run(test_url, ftest_manager::RunSuiteOptions::default())?;
 
-    test_suite_result
+    test_runners_test_lib::process_events(suite_instance, true).await
 }
