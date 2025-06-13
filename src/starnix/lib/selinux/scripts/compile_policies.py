bc2ae87abe2fd7540d725715767f2b9c269df67c
Jean-François <jif@google.com>
2025-06-13 04:48:17 -0700

    [sestarnix][binder] Add tests opening binder
    These tests highlights a discrepancy between starnix and
    linux when opening binder.
    
    Bug: 422968534
    
    Change-Id: Iec23d352d5ce3bace35c38b8ee840f8655eb91f6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1294704
    Commit-Queue: Jean-François Geyelin <jif@google.com>
    Reviewed-by: Ambre Williams <ambre@google.com>

--- src/starnix/lib/selinux/scripts/compile_policies.py
+++ src/starnix/lib/selinux/scripts/compile_policies.py
@@ -45,6 +45,14 @@ _COMPOSITE_POLICY_PATHS = [
     (
         [
             "base_policy.conf",
+            "new_file/test_policy.conf",
+            "new_file/binder.conf",
+        ],
+        "binder.pp",
+    ),
+    (
+        [
+            "base_policy.conf",
             "new_file/bounded_transition_policy.conf",
         ],
         "bounded_transition_policy.pp",
