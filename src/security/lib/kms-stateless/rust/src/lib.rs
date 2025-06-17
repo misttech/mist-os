21d12908d8acdf826785896d300e22203dde4afd
Francois Rousseau <frousseau@google.com>
2025-06-17 10:30:49 -0700

    [kms_stateless] expose sealing keys methods
    * the integration test is added next to the FIDL server in a separate CL
    
    Bug: 401349208
    Change-Id: Ia6b9686cd5410c58385c48b4e95abdb863709760
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1301804
    Reviewed-by: Ali Zhang <alizhang@google.com>
    Commit-Queue: Francois Rousseau <frousseau@google.com>

---
+++ src/security/lib/kms-stateless/rust/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+mod legacy_hardware_derive_keys;
+mod sealing_keys;
+
+pub use self::legacy_hardware_derive_keys::*;
+pub use self::sealing_keys::*;
