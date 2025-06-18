1da04b0af2d8f99bfa00c8e2a83d8659b8961331
Jia Li <lizhijia@google.com>
2025-06-18 16:53:44 -0700

    [profiler] Add unit test for symbolized text to pprof
    Bug: 401034854
    
    Change-Id: I8d57c0b179246a618f000abdade25f6467c65d2a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1304283
    Commit-Queue: Jia Li <lizhijia@google.com>
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>

--- src/developer/ffx/lib/profiler/src/pprof.rs
+++ src/developer/ffx/lib/profiler/src/pprof.rs
@@ -192,3 +192,187 @@ pub fn samples_to_pprof(input: SymbolizedRecords, output: PathBuf) -> Result<()>
     std::fs::write(output, profile.encode_to_vec()).map_err(|e| anyhow::anyhow!(e))?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parse::{Pid, Tid};
+    use crate::symbolize::SymbolizedRecord;
+    use assert_matches::assert_matches;
+    // Helper to create a ResolvedAddress for testing
+    fn make_resolved_address(addr: u64, function: &str, file: &str, line: u64) -> ResolvedAddress {
+        ResolvedAddress {
+            addr,
+            locations: vec![ResolvedLocation {
+                function: function.to_string(),
+                file_and_line: Some((file.to_string(), line as u32)),
+                library: Some("libtest.so".to_string()),
+                library_offset: 0x1234,
+            }],
+        }
+    }
+
+    #[test]
+    fn test_build_location_multiple_inlined() {
+        let mut builder = ProfileBuilder::new();
+        let addr = ResolvedAddress {
+            addr: 0xABCD,
+            locations: vec![
+                ResolvedLocation {
+                    function: "inlined_func".to_string(),
+                    file_and_line: Some(("header.h".to_string(), 25)),
+                    library: Some("libtest.so".to_string()),
+                    library_offset: 0x1234,
+                },
+                ResolvedLocation {
+                    function: "caller_func".to_string(),
+                    file_and_line: Some(("caller.c".to_string(), 100)),
+                    library: Some("libtest.so".to_string()),
+                    library_offset: 0x1234,
+                },
+            ],
+        };
+
+        let loc_id = builder.build_location(addr);
+        assert_eq!(loc_id, 1);
+        assert_eq!(builder.profile.location.len(), 1);
+        // Two functions should have been created
+        assert_eq!(builder.profile.function.len(), 2);
+
+        let location = &builder.profile.location[0];
+        assert_eq!(location.id, 1);
+        assert_eq!(location.address, 0xABCD);
+        // There should be two lines, one for each inlined function
+        assert_eq!(location.line.len(), 2);
+
+        let inlined_func = &builder.profile.function[0];
+        assert_eq!(builder.st.table[inlined_func.name as usize], "inlined_func");
+        let caller_func = &builder.profile.function[1];
+        assert_eq!(builder.st.table[caller_func.name as usize], "caller_func");
+
+        assert_matches!(
+            &location.line[..],
+            [Line { function_id: 1, line: 25 }, Line { function_id: 2, line: 100 },]
+        );
+    }
+
+    #[test]
+    fn test_build_profile() {
+        let builder = ProfileBuilder::new();
+        let pid_val = 123u64;
+        let tid_val = 456u64;
+
+        let pid = Pid(pid_val);
+        let tid = Tid(tid_val);
+
+        let call_stack_trace1 = vec![
+            make_resolved_address(0x1000, "main", "main.rs", 10),
+            make_resolved_address(0x2000, "helper", "helper.rs", 5),
+        ];
+
+        let call_stack_trace2 = vec![
+            make_resolved_address(0x1000, "main", "main.rs", 10),
+            make_resolved_address(0x3000, "another", "another.rs", 20),
+        ];
+
+        let symbolized_record_for_tid =
+            SymbolizedRecord { tid, call_stacks: vec![call_stack_trace1, call_stack_trace2] };
+
+        let input = SymbolizedRecords { records: vec![(pid, vec![symbolized_record_for_tid])] };
+
+        let profile = builder.build_profile(input);
+
+        // Expected strings: "", "pid", "tid", "123", "pid: 123", "456", "tid: 456",
+        // "main", "main.rs", "helper", "helper.rs", "another", "another.rs", "samples", "count",
+        assert_eq!(profile.string_table.len(), 15);
+        assert_eq!(profile.sample_type.len(), 1);
+        assert_eq!(profile.sample_type[0].r#type, 13); // "samples"
+        assert_eq!(profile.sample_type[0].unit, 14); // "count"
+
+        assert_eq!(profile.sample.len(), 2);
+        assert_eq!(profile.function.len(), 5); // main, helper, another, pid, tid
+        assert_eq!(profile.location.len(), 5); // 0x1000, 0x2000, 0x3000, pid_loc, tid_loc
+
+        // --- Check Sample 1 ---
+        let sample1 = &profile.sample[0];
+        assert_eq!(sample1.value, vec![1]);
+        assert_eq!(sample1.label.len(), 2); // pid, tid
+        assert_eq!(sample1.label[0].key, PID_INTERN);
+        assert_eq!(sample1.label[0].num, 123);
+        assert_eq!(profile.string_table[sample1.label[0].str as usize], "123");
+
+        assert_eq!(sample1.label[1].key, TID_INTERN);
+        assert_eq!(sample1.label[1].num, 456);
+        assert_eq!(profile.string_table[sample1.label[1].str as usize], "456");
+
+        // Location IDs:
+        // main_addr (0x1000) -> loc_id 1
+        // helper_addr (0x2000) -> loc_id 2
+        // tid_addr (func "tid:456") -> loc_id 3
+        // pid_addr (func "pid:123") -> loc_id 4
+        // another_addr (0x3000) -> loc_id 5
+        // Sample 1 stack: [main_addr, helper_addr], tid_addr, pid_addr
+        // Expected location_id: [loc_id(main), loc_id(helper), loc_id(tid), loc_id(pid)]
+        assert_eq!(sample1.location_id, vec![1, 2, 3, 4]);
+
+        // --- Check Sample 2 ---
+        let sample2 = &profile.sample[1];
+        // Sample 2 stack: [main_addr, another_addr], tid_addr, pid_addr
+        // Expected location_id: [loc_id(main), loc_id(another), loc_id(tid), loc_id(pid)]
+        assert_eq!(sample2.location_id, vec![1, 5, 3, 4]);
+
+        // --- Check Locations ---
+        let loc1 = profile.location.iter().find(|l| l.address == 0x1000).unwrap();
+        assert_eq!(loc1.id, 1);
+        let loc2 = profile.location.iter().find(|l| l.address == 0x2000).unwrap();
+        assert_eq!(loc2.id, 2);
+        let loc3 = profile.location.iter().find(|l| l.address == 0x3000).unwrap();
+        assert_eq!(loc3.id, 5);
+        let tid_loc = profile.location.iter().find(|l| l.id == 3).unwrap();
+        assert_eq!(tid_loc.address, 0xDEADBEEF); // Pseudo address
+        assert_eq!(
+            profile.string_table
+                [profile.function[tid_loc.line[0].function_id as usize - 1].name as usize],
+            "tid: 456"
+        );
+
+        let pid_loc = profile.location.iter().find(|l| l.id == 4).unwrap();
+        assert_eq!(pid_loc.address, 0xDEADBEEF); // Pseudo address
+        assert_eq!(
+            profile.string_table
+                [profile.function[pid_loc.line[0].function_id as usize - 1].name as usize],
+            "pid: 123"
+        );
+
+        // --- Check Functions ---
+        let main_fn = profile
+            .function
+            .iter()
+            .find(|f| profile.string_table[f.name as usize] == "main")
+            .unwrap();
+        assert_eq!(main_fn.id, 1);
+        assert_eq!(profile.string_table[main_fn.filename as usize], "main.rs");
+        assert_eq!(main_fn.start_line, 10);
+
+        let helper_fn = profile
+            .function
+            .iter()
+            .find(|f| profile.string_table[f.name as usize] == "helper")
+            .unwrap();
+        assert_eq!(helper_fn.id, 2);
+
+        let tid_fn = profile
+            .function
+            .iter()
+            .find(|f| profile.string_table[f.name as usize] == "tid: 456")
+            .unwrap();
+        assert_eq!(tid_fn.id, 3);
+
+        let pid_fn = profile
+            .function
+            .iter()
+            .find(|f| profile.string_table[f.name as usize] == "pid: 123")
+            .unwrap();
+        assert_eq!(pid_fn.id, 4);
+    }
+}
