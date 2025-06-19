dae1ef56eb68854003735186c5923ab9aedd76d4
Bruno Dal Bo <brunodalbo@google.com>
2025-06-18 17:36:03 -0700

    [netstack] Rename fakestd => teststd
    Because this is simpler than wrangling GN rules, repurpose fakestd to
    teststd (it's marked as testonly already) as a re-export place to access
    std things from tests and testutil builds of netstack3 crates.
    
    This will allow us to add more std dependencies for tests, like
    spawning threads.
    
    Bug: 42111399
    Change-Id: Ia16868bfb4dd4617c1d9d91086a1cc83cb208a49
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1303952
    Commit-Queue: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>

---
+++ src/connectivity/network/netstack3/core/teststd/src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Re-export specific items from `std` for use in tests by netstack3.
+
+pub use ::std::println;
