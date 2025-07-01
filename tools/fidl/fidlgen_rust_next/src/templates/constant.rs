18bb3c330065748b9c51d9724193976d86d5e036
David Koloski <dkoloski@google.com>
2025-07-01 14:49:13 -0700

    [fidl][rust] Manually implement constant template
    Change-Id: I0917f9895686305a8a47db4d30ff203fc56526e2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1308944
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Commit-Queue: David Koloski <dkoloski@google.com>
    Fuchsia-Auto-Submit: David Koloski <dkoloski@google.com>

--- tools/fidl/fidlgen_rust_next/src/templates/constant.rs
+++ tools/fidl/fidlgen_rust_next/src/templates/constant.rs
@@ -2,14 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use askama::Template;
+use core::fmt;
 
 use super::{Context, Contextual};
 use crate::id::IdExt as _;
 use crate::ir::{Constant, ConstantKind, DeclType, LiteralKind, Type, TypeKind};
 
-#[derive(Template)]
-#[template(path = "constant.askama", whitespace = "suppress")]
 pub struct ConstantTemplate<'a> {
     constant: &'a Constant,
     ty: &'a Type,
@@ -25,5 +23,64 @@ impl<'a> ConstantTemplate<'a> {
 impl<'a> Contextual<'a> for ConstantTemplate<'a> {
     fn context(&self) -> Context<'a> {
         self.context
+    }
+}
+
+impl fmt::Display for ConstantTemplate<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.constant.kind {
+            ConstantKind::Identifier { identifier } => {
+                let (comp_id, member) = identifier.split();
+                let item = self.natural_id(comp_id);
+
+                if let Some(member) = member {
+                    let decl_type = self.schema().get_decl_type(comp_id).unwrap();
+                    let member_name = match decl_type {
+                        DeclType::Bits => member.screaming_snake(),
+                        DeclType::Enum => {
+                            if comp_id.library() == "zx"
+                                && comp_id.decl_name().non_canonical() == "ObjType"
+                            {
+                                member.screaming_snake()
+                            } else {
+                                member.camel()
+                            }
+                        }
+                        _ => panic!("expected member to be bits or enum"),
+                    };
+                    write!(f, "{item}::{member_name}")?;
+                } else {
+                    write!(f, "{item}")?;
+                }
+            }
+            ConstantKind::Literal { literal } => match literal.kind {
+                LiteralKind::String => write!(f, "\"{}\"", literal.value.escape_default())?,
+                LiteralKind::Bool => write!(f, "{}", literal.value)?,
+                LiteralKind::Numeric => match &self.ty.kind {
+                    TypeKind::Identifier { identifier, .. } => write!(
+                        f,
+                        "{}::from_bits_retain({})",
+                        self.natural_id(identifier),
+                        self.constant.value
+                    )?,
+                    TypeKind::Primitive { .. } => write!(f, "{}", literal.value)?,
+                    _ => panic!("invalid constant type"),
+                },
+            },
+            ConstantKind::BinaryOperator => {
+                if let TypeKind::Identifier { identifier, .. } = &self.ty.kind {
+                    write!(
+                        f,
+                        "{}::from_bits_retain({})",
+                        self.natural_id(identifier),
+                        self.constant.value
+                    )?;
+                } else {
+                    panic!("invalid constant type");
+                }
+            }
+        }
+
+        Ok(())
     }
 }
