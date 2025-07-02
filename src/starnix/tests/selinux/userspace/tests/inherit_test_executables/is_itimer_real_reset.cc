ff251075591fe21796c38463222b5b97adc27aa2
Laura Peskin <pesk@google.com>
2025-07-01 19:16:41 -0700

    [sestarnix] Add tests for itimer inheritance conditional on siginh
    The ITIMER_REAL timer should be preserved across exec if and
    only if the siginh permission is allowed.
    
    Currently Starnix/SEStarnix always preserves ITIMER_REAL, so
    expect failure for the test case where siginh is denied.
    
    Bug: 378655436
    Change-Id: I82714c9b4a06220f5f3b14d018369a83c20a2fa0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1311172
    Fuchsia-Auto-Submit: Laura Peskin <pesk@google.com>
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>
    Commit-Queue: Laura Peskin <pesk@google.com>

---
+++ src/starnix/tests/selinux/userspace/tests/inherit_test_executables/is_itimer_real_reset.cc
@@ -0,0 +1,27 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <sys/time.h>
+
+#include <gtest/gtest.h>
+
+#include "src/starnix/tests/syscalls/cpp/syscall_matchers.h"
+
+void get_itimer(itimerval* val) { ASSERT_THAT(getitimer(ITIMER_REAL, val), SyscallSucceeds()); }
+
+int main(int argc, char** argv) {
+  bool expect_itimer_real_reset = atoi(argv[1]);
+
+  struct itimerval val;
+  get_itimer(&val);
+
+  if (expect_itimer_real_reset) {
+    EXPECT_EQ(val.it_value.tv_sec, 0);
+    EXPECT_EQ(val.it_value.tv_usec, 0);
+  } else {
+    EXPECT_TRUE(val.it_value.tv_sec != 0 || val.it_value.tv_usec != 0);
+  }
+
+  return ::testing::Test::HasFailure() ? 1 : 0;
+}
