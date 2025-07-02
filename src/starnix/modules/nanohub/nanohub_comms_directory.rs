fdce0167cb2a87e77070994a597cb1c9b17f9396
Adam barth <abarth@google.com>
2025-07-02 00:47:37 -0700

    [starnix][nanohub] Move build_nanohub_comms_directory off entry_etc
    We're calling entry_etc with the default arguments. We can just use the
    entry method.
    
    Change-Id: I1fa5070e543db977e573f513b0d3f0af9b237b72
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1312965
    Reviewed-by: Jeremiah Griffin <jzgriffin@google.com>
    Fuchsia-Auto-Submit: Adam Barth <abarth@google.com>
    Commit-Queue: Adam Barth <abarth@google.com>

--- src/starnix/modules/nanohub/nanohub_comms_directory.rs
+++ src/starnix/modules/nanohub/nanohub_comms_directory.rs
@@ -10,41 +10,30 @@ use crate::socket_tunnel_file::{FirmwareFile, SocketTunnelSysfsFile};
 use starnix_core::device::kobject::Device;
 use starnix_core::fs::sysfs::build_device_directory;
 use starnix_core::vfs::pseudo::simple_directory::SimpleDirectoryMutator;
-use starnix_uapi::auth::FsCred;
-use starnix_uapi::device_type::DeviceType;
 use starnix_uapi::file_mode::mode;
 
 pub fn build_nanohub_comms_directory(device: &Device, dir: &SimpleDirectoryMutator) {
     build_device_directory(device, dir);
-    // TODO(https://fxbug.dev/419041879): These are currently set to "system", but they
-    // should be set to FsCred::root().
-    let system_creds = FsCred::root();
-    dir.entry_etc(
+    dir.entry(
         "display_panel_name".into(),
         SocketTunnelSysfsFile::new(
             b"/sys/devices/virtual/nanohub/nanohub_comms/display_panel_name".into(),
         ),
         mode!(IFREG, 0o440),
-        DeviceType::NONE,
-        system_creds,
     );
-    dir.entry_etc(
+    dir.entry(
         "display_select".into(),
         SocketTunnelSysfsFile::new(
             b"/sys/devices/virtual/nanohub/nanohub_comms/display_select".into(),
         ),
         mode!(IFREG, 0o660),
-        DeviceType::NONE,
-        system_creds,
     );
-    dir.entry_etc(
+    dir.entry(
         "display_state".into(),
         SocketTunnelSysfsFile::new(
             b"/sys/devices/virtual/nanohub/nanohub_comms/display_state".into(),
         ),
         mode!(IFREG, 0o440),
-        DeviceType::NONE,
-        system_creds,
     );
     dir.entry("download_firmware", FirmwareFile::new(), mode!(IFREG, 0o220));
     dir.entry(
@@ -62,13 +51,7 @@ pub fn build_nanohub_comms_directory(device: &Device, dir: &SimpleDirectoryMutat
         SocketTunnelSysfsFile::new(b"/sys/devices/virtual/nanohub/nanohub_comms/hw_reset".into()),
         mode!(IFREG, 0o220),
     );
-    dir.entry_etc(
-        "time_sync".into(),
-        NanohubSysFsNode::<TimeSyncSysFsOps>::new(),
-        mode!(IFREG, 0o440),
-        DeviceType::NONE,
-        system_creds,
-    );
+    dir.entry("time_sync".into(), NanohubSysFsNode::<TimeSyncSysFsOps>::new(), mode!(IFREG, 0o440));
     dir.entry(
         "wakeup_event_msec",
         NanohubSysFsNode::<WakeUpEventDuration>::new(),
