7211d263300cee2863818896749af1a492ea53d6
Gwen Mittertreiner <gmtr@google.com>
2025-07-02 17:35:28 -0700

    [fxtdump] Added Detailed Breakdown
    Added an additional --detailed option which also prints by-name
    statistics for trace stats.
    
    Added a --limit option to only print the top n items
    
    Example output:
    ```
    fx fxtdump category_stats big_trace.fxt --limit 10 --detailed
    WARNING couldn't parse string as utf-8
    +------------------------------------+--------------------------+-----------------------+
    | Name (Category: magma)             | Size  (Total: 10618 KiB) | Count (Total: 387009) |
    +------------------------------------+--------------------------+-----------------------+
    | atom                               | 3192 KiB                 | 136221                |
    +------------------------------------+--------------------------+-----------------------+
    | AtomCompleted                      | 1915 KiB                 | 40865                 |
    +------------------------------------+--------------------------+-----------------------+
    | Connection::ExecuteAtom            | 957 KiB                  | 40864                 |
    +------------------------------------+--------------------------+-----------------------+
    | ScheduleRunnableAtoms              | 679 KiB                  | 29002                 |
    +------------------------------------+--------------------------+-----------------------+
    | ExecuteAtomOnDevice                | 638 KiB                  | 13623                 |
    +------------------------------------+--------------------------+-----------------------+
    | MapCpu                             | 411 KiB                  | 10528                 |
    +------------------------------------+--------------------------+-----------------------+
    | Atom Slot 1                        | 386 KiB                  | 16482                 |
    +------------------------------------+--------------------------+-----------------------+
    | MsdArmDevice::ProcessScheduleAtoms | 342 KiB                  | 14632                 |
    +------------------------------------+--------------------------+-----------------------+
    | JobCompleted                       | 319 KiB                  | 13623                 |
    +------------------------------------+--------------------------+-----------------------+
    | MsdArmDevice::ProcessJobInterrupt  | 285 KiB                  | 12179                 |
    +------------------------------------+--------------------------+-----------------------+
    ```
    
    Change-Id: I895a9e5e60844a524cd22358584c2fa9703f9c73
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1314084
    Reviewed-by: Jia Li <lizhijia@google.com>
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Fuchsia-Auto-Submit: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Gwen Mittertreiner <gmtr@google.com>

--- src/performance/fxtdump/src/main.rs
+++ src/performance/fxtdump/src/main.rs
@@ -5,6 +5,7 @@
 use anyhow::Result;
 use argh::{ArgsInfo, FromArgs};
 use fxt::session::SessionParser;
+use prettytable::{cell, row, Table};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
@@ -49,6 +50,21 @@ pub struct Dump {
 pub struct CategoryStats {
     #[argh(positional)]
     pub input: String,
+    #[argh(switch)]
+    /// if set, include a breakdown of the stats by name as well
+    pub detailed: bool,
+
+    /// if set, only include the top `limit` entries
+    #[argh(option)]
+    pub limit: Option<usize>,
+}
+
+fn format_size(size: usize) -> String {
+    if size > 1024 {
+        format!("{} KiB", size / 1024)
+    } else {
+        format!("{} bytes", size)
+    }
 }
 
 fn dump<R: std::io::Read>(fxt: &mut SessionParser<R>, strict: bool) {
@@ -71,72 +87,112 @@ fn dump<R: std::io::Read>(fxt: &mut SessionParser<R>, strict: bool) {
     }
 }
 
-fn category_stats<R: std::io::Read>(fxt: &mut SessionParser<R>) {
-    let mut category_counts = HashMap::<String, usize>::new();
-    let mut category_sizes = HashMap::<String, usize>::new();
+#[derive(Default)]
+struct Stats {
+    count: usize,
+    size_bytes: usize,
+}
+
+fn category_stats<R: std::io::Read>(
+    fxt: &mut SessionParser<R>,
+    detailed: bool,
+    limit: Option<usize>,
+) {
+    let mut category_name_counts = HashMap::<String, HashMap<String, Stats>>::new();
 
     while let Some(record) = fxt.next() {
         match record {
-            Ok(trace_record) => match trace_record {
-                fxt::TraceRecord::Event(event_record) => {
-                    *category_counts.entry(event_record.category.as_str().into()).or_insert(0) += 1;
-                    *category_sizes.entry(event_record.category.as_str().into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::Scheduling(_scheduling_record) => {
-                    *category_counts.entry("kernel:sched".into()).or_insert(0) += 1;
-                    *category_sizes.entry("kernel:sched".into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::Blob(blob_record) => {
-                    *category_counts.entry(blob_record.name.as_str().into()).or_insert(0) += 1;
-                    *category_sizes.entry(blob_record.name.as_str().into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::LargeBlob(large_blob_record) => {
-                    *category_counts
-                        .entry(large_blob_record.category.as_str().into())
-                        .or_insert(0) += 1;
-                    *category_sizes
-                        .entry(large_blob_record.category.as_str().into())
-                        .or_insert(0) += fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::KernelObj(_kernel_obj_record) => {
-                    *category_counts.entry("kernel:meta".into()).or_insert(0) += 1;
-                    *category_sizes.entry("kernel:meta".into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::UserspaceObj(_user_obj_record) => {
-                    *category_counts.entry("user:meta".into()).or_insert(0) += 1;
-                    *category_sizes.entry("user:meta".into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::Log(_log_record) => {
-                    *category_counts.entry("log".into()).or_insert(0) += 1;
-                    *category_sizes.entry("log".into()).or_insert(0) += fxt.parsed_bytes().len();
-                }
-                fxt::TraceRecord::ProviderEvent { .. } => {
-                    *category_counts.entry("metadata".into()).or_insert(0) += 1;
-                    *category_sizes.entry("metadata".into()).or_insert(0) +=
-                        fxt.parsed_bytes().len();
-                }
-            },
+            Ok(trace_record) => {
+                let (category, name) = match trace_record {
+                    fxt::TraceRecord::Event(event_record) => {
+                        (event_record.category.as_str().into(), event_record.name.as_str().into())
+                    }
+                    fxt::TraceRecord::Scheduling(_scheduling_record) => {
+                        ("kernel_sched".into(), "kernel:sched".into())
+                    }
+                    fxt::TraceRecord::Blob(blob_record) => {
+                        (blob_record.name.as_str().into(), blob_record.name.as_str().into())
+                    }
+                    fxt::TraceRecord::LargeBlob(large_blob_record) => (
+                        large_blob_record.category.as_str().into(),
+                        large_blob_record.name.as_str().into(),
+                    ),
+                    fxt::TraceRecord::KernelObj(_kernel_obj_record) => {
+                        ("kernel:meta".into(), "kernel:meta".into())
+                    }
+                    fxt::TraceRecord::UserspaceObj(_user_obj_record) => {
+                        ("user:meta".into(), "user:meta".into())
+                    }
+                    fxt::TraceRecord::Log(_log_record) => ("log".into(), "log".into()),
+                    fxt::TraceRecord::ProviderEvent { .. } => {
+                        ("metadata".into(), "metadata".into())
+                    }
+                };
+                let name_map = category_name_counts.entry(category).or_insert(HashMap::new());
+                let stats = name_map.entry(name).or_insert(Stats::default());
+                stats.count += 1;
+                stats.size_bytes += fxt.parsed_bytes().len();
+            }
             Err(parse_err) => eprintln!("WARNING {:#}", parse_err),
         }
     }
+    if detailed {
+        let mut category_stats: Vec<_> = category_name_counts
+            .into_iter()
+            .map(|(category, name_map)| {
+                let total_stats = name_map.values().fold(Stats::default(), |s1, s2| Stats {
+                    count: s1.count + s2.count,
+                    size_bytes: s1.size_bytes + s2.size_bytes,
+                });
+                let mut name_stats: Vec<_> = name_map.into_iter().collect();
+                name_stats.sort_by(|a, b| b.1.size_bytes.cmp(&a.1.size_bytes));
+                (category, total_stats, name_stats)
+            })
+            .collect();
 
-    let mut sorted_counts: Vec<_> = category_counts.into_iter().collect();
-    sorted_counts.sort_by(|a, b| b.1.cmp(&a.1));
+        category_stats.sort_by(|a, b| b.1.size_bytes.cmp(&a.1.size_bytes));
 
-    println!("{:26}{:16}    {:10}        ", "Category", "Size", "Count");
-    println!();
-    for (category, count) in sorted_counts {
-        println!(
-            "{:20}  {:10} KiB  {:10} records",
-            category,
-            category_sizes[&*category] / 1024,
-            count
-        );
+        let num_categories = limit.unwrap_or(category_stats.len());
+        for (category, total_stats, name_stats) in category_stats.into_iter().take(num_categories) {
+            let mut table = Table::new();
+            table.add_row(row![
+                format!("Name (Category: {})", category),
+                format!("Size  (Total: {})", format_size(total_stats.size_bytes)),
+                format!("Count (Total: {})", total_stats.count)
+            ]);
+            let mut sorted_name_stats: Vec<_> = name_stats.into_iter().collect();
+            sorted_name_stats.sort_by(|a, b| b.1.size_bytes.cmp(&a.1.size_bytes));
+            let num_names = limit.unwrap_or(sorted_name_stats.len());
+            for (name, stats) in sorted_name_stats.into_iter().take(num_names) {
+                table.add_row(row![name, format_size(stats.size_bytes), stats.count]);
+            }
+            table.printstd();
+        }
+    } else {
+        let mut flattened_counts: Vec<_> = category_name_counts
+            .into_iter()
+            .map(|(name, stats)| {
+                (
+                    name,
+                    stats.into_iter().fold(Stats::default(), |s1, (_, s2)| Stats {
+                        count: s1.count + s2.count,
+                        size_bytes: s1.size_bytes + s2.size_bytes,
+                    }),
+                )
+            })
+            .collect();
+        flattened_counts.sort_by(|a, b| b.1.size_bytes.cmp(&a.1.size_bytes));
+
+        let mut table = Table::new();
+        table.add_row(row!["Category", "Size", "Count"]);
+
+        let num_categories = limit.unwrap_or(flattened_counts.len());
+        for (category, Stats { count, size_bytes }) in
+            flattened_counts.into_iter().take(num_categories)
+        {
+            table.add_row(row![category, format_size(size_bytes), count]);
+        }
+        table.printstd();
     }
 }
 
@@ -144,11 +200,11 @@ fn main() -> Result<()> {
     let args: Args = argh::from_env();
 
     match args.command {
-        FxtDump::CategoryStats(CategoryStats { input }) => {
+        FxtDump::CategoryStats(CategoryStats { input, detailed, limit }) => {
             let file = File::open(input)?;
             let mut reader = BufReader::new(file);
             let mut session = SessionParser::new(&mut reader);
-            category_stats(&mut session)
+            category_stats(&mut session, detailed, limit)
         }
         FxtDump::Dump(Dump { input, strict }) => {
             let file = File::open(input)?;
