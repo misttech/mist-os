e0481bd3091ae3c52ffda835b6cad857b21c5cc7
Suraj Malhotra <surajmalhotra@google.com>
2025-07-07 15:55:58 -0700

    [driver][component] Implement Suspendable trait
    This trait allows us to add additional hooks from which we may trigger
    optional suspend/resume callbacks.
    
    Test: fx test power-rust-driver-realm-test
    Change-Id: I48a04e7b776a29bc88848a58370e6a14c8713f62
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1307009
    Commit-Queue: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Megan Batty <mgnb@google.com>
    Reviewed-by: Justin Mattson <jmatt@google.com>

---
+++ examples/drivers/power/rust/realm_test.rs
@@ -0,0 +1,77 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::Result;
+use fidl::endpoints::ClientEnd;
+use fidl_fuchsia_driver_test::RealmArgs;
+use fuchsia_component::server::ServiceFs;
+use fuchsia_component_test::{ChildOptions, LocalComponentHandles, RealmBuilder};
+use fuchsia_driver_test::{DriverTestRealmBuilder, DriverTestRealmInstance};
+use futures::channel::mpsc;
+use futures::{StreamExt, TryStreamExt};
+use {fidl_fuchsia_power_system as fps, fuchsia_async as fasync};
+
+async fn sag_serve(
+    mut stream: fps::ActivityGovernorRequestStream,
+    mut sender: mpsc::Sender<ClientEnd<fps::SuspendBlockerMarker>>,
+) {
+    while let Some(fps::ActivityGovernorRequest::RegisterSuspendBlocker { payload, responder }) =
+        stream.try_next().await.expect("Stream failed")
+    {
+        sender.try_send(payload.suspend_blocker.unwrap()).expect("Sender failed");
+        let (fake_lease, _) = zx::EventPair::create();
+        let _ = responder.send(Ok(fake_lease));
+    }
+}
+
+async fn sag_component(
+    handles: LocalComponentHandles,
+    sender: mpsc::Sender<ClientEnd<fps::SuspendBlockerMarker>>,
+) -> Result<()> {
+    let mut fs = ServiceFs::new();
+    fs.dir("svc").add_fidl_service(move |stream: fps::ActivityGovernorRequestStream| {
+        fasync::Task::spawn(sag_serve(stream, sender.clone())).detach()
+    });
+    fs.serve_connection(handles.outgoing_dir)?;
+    Ok(fs.collect::<()>().await)
+}
+
+#[fuchsia::test]
+async fn test_power_driver() -> Result<()> {
+    let (sender, mut receiver) = mpsc::channel(1);
+
+    // Create the RealmBuilder.
+    let builder = RealmBuilder::new().await?;
+    builder.driver_test_realm_setup().await?;
+    let waiter = builder
+        .add_local_child(
+            "fake-sag",
+            move |handles: LocalComponentHandles| Box::pin(sag_component(handles, sender.clone())),
+            ChildOptions::new(),
+        )
+        .await?;
+    let offer =
+        fuchsia_component_test::Capability::protocol::<fps::ActivityGovernorMarker>().into();
+    let dtr_offers = vec![offer];
+
+    builder.driver_test_realm_add_dtr_offers(&dtr_offers, (&waiter).into()).await?;
+    // Build the Realm.
+    let instance = builder.build().await?;
+    // Start DriverTestRealm
+    instance
+        .driver_test_realm_start(RealmArgs {
+            root_driver: Some("#meta/power_rust_driver.cm".to_owned()),
+            dtr_offers: Some(dtr_offers),
+            ..Default::default()
+        })
+        .await?;
+
+    let proxy = receiver.try_next()?.ok_or(anyhow::anyhow!("missing proxy"))?.into_proxy();
+    // Invoke suspend
+    proxy.before_suspend().await?;
+    // Invoke resume
+    proxy.after_resume().await?;
+
+    Ok(())
+}
