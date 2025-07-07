07bc089ed815bc3d2f90b545d1a176daa94f0ae6
Thomas Zander <thomasalpinus@google.com>
2025-07-07 06:31:15 -0700

    [rust-3p] Update predicates-core to 1.0.9
    Change-Id: Ie8aa7775d8da137f7a8339b6bd6bcc6fc6e4099f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1314804
    Reviewed-by: David Koloski <dkoloski@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>
    Fuchsia-Auto-Submit: Thomas Zander <thomasalpinus@google.com>

--- third_party/rust_crates/vendor/predicates-core-1.0.9/src/reflection.rs
+++ third_party/rust_crates/vendor/predicates-core-1.0.9/src/reflection.rs
@@ -40,7 +40,7 @@ pub struct Parameter<'a>(&'a str, &'a dyn fmt::Display);
 impl<'a> Parameter<'a> {
     /// Create a new `Parameter`.
     pub fn new(key: &'a str, value: &'a dyn fmt::Display) -> Self {
-        Self { 0: key, 1: value }
+        Self(key, value)
     }
 
     /// Access the `Parameter` name.
@@ -54,13 +54,13 @@ impl<'a> Parameter<'a> {
     }
 }
 
-impl<'a> fmt::Display for Parameter<'a> {
+impl fmt::Display for Parameter<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}: {}", self.0, self.1)
     }
 }
 
-impl<'a> fmt::Debug for Parameter<'a> {
+impl fmt::Debug for Parameter<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?}, {})", self.0, self.1)
     }
@@ -72,7 +72,7 @@ pub struct Child<'a>(&'a str, &'a dyn PredicateReflection);
 impl<'a> Child<'a> {
     /// Create a new `Predicate` child.
     pub fn new(key: &'a str, value: &'a dyn PredicateReflection) -> Self {
-        Self { 0: key, 1: value }
+        Self(key, value)
     }
 
     /// Access the `Child`'s name.
@@ -86,13 +86,13 @@ impl<'a> Child<'a> {
     }
 }
 
-impl<'a> fmt::Display for Child<'a> {
+impl fmt::Display for Child<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}: {}", self.0, self.1)
     }
 }
 
-impl<'a> fmt::Debug for Child<'a> {
+impl fmt::Debug for Child<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?}, {})", self.0, self.1)
     }
@@ -141,20 +141,16 @@ impl<'a> Case<'a> {
 
     /// Access the by-products from determining this case.
     pub fn products(&self) -> CaseProducts<'_> {
-        CaseProducts {
-            0: self.products.iter(),
-        }
+        CaseProducts(self.products.iter())
     }
 
     /// Access the sub-cases.
     pub fn children(&self) -> CaseChildren<'_> {
-        CaseChildren {
-            0: self.children.iter(),
-        }
+        CaseChildren(self.children.iter())
     }
 }
 
-impl<'a> fmt::Debug for Case<'a> {
+impl fmt::Debug for Case<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let predicate = if let Some(ref predicate) = self.predicate {
             format!("Some({})", predicate)
@@ -229,10 +225,7 @@ impl Product {
         S: Into<borrow::Cow<'static, str>>,
         D: fmt::Display + 'static,
     {
-        Self {
-            0: key.into(),
-            1: Box::new(value),
-        }
+        Self(key.into(), Box::new(value))
     }
 
     /// Access the `Product` name.
@@ -246,13 +239,13 @@ impl Product {
     }
 }
 
-impl<'a> fmt::Display for Product {
+impl fmt::Display for Product {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}: {}", self.0, self.1)
     }
 }
 
-impl<'a> fmt::Debug for Product {
+impl fmt::Debug for Product {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?}, {})", self.0, self.1)
     }
