f6b28bc01b536147742a1ea70acaa0dd694ddde4
Thomas Zander <thomasalpinus@google.com>
2025-07-07 06:32:42 -0700

    [rust-3p] Update predicates-tree to 1.0.12
    Change-Id: Ic55ea86870d6b152f9d4bf7d0cc2d8fa2a53bfae
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1315724
    Fuchsia-Auto-Submit: Thomas Zander <thomasalpinus@google.com>
    Reviewed-by: David Koloski <dkoloski@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>

--- third_party/rust_crates/vendor/predicates-tree-1.0.12/src/lib.rs
+++ third_party/rust_crates/vendor/predicates-tree-1.0.12/src/lib.rs
@@ -8,6 +8,8 @@
 
 //! Render `Case` as a tree.
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
 use std::fmt;
 
 use predicates_core::reflection;
@@ -18,33 +20,36 @@ pub trait CaseTreeExt {
     fn tree(&self) -> CaseTree;
 }
 
-impl<'a> CaseTreeExt for reflection::Case<'a> {
+impl CaseTreeExt for reflection::Case<'_> {
     fn tree(&self) -> CaseTree {
         CaseTree(convert(self))
     }
 }
 
-type CaseTreeInner = treeline::Tree<Box<dyn fmt::Display>>;
+type CaseTreeInner = termtree::Tree<Displayable>;
 
 fn convert(case: &reflection::Case<'_>) -> CaseTreeInner {
     let mut leaves: Vec<CaseTreeInner> = vec![];
 
     leaves.extend(case.predicate().iter().flat_map(|pred| {
         pred.parameters().map(|item| {
-            let root: Box<dyn fmt::Display> = Box::new(item.to_string());
-            treeline::Tree::new(root, vec![])
+            let root = Displayable::new(&item);
+            termtree::Tree::new(root).with_multiline(true)
         })
     }));
 
     leaves.extend(case.products().map(|item| {
-        let root: Box<dyn fmt::Display> = Box::new(item.to_string());
-        treeline::Tree::new(root, vec![])
+        let root = Displayable::new(item);
+        termtree::Tree::new(root).with_multiline(true)
     }));
 
-    leaves.extend(case.children().map(|item| convert(item)));
+    leaves.extend(case.children().map(convert));
 
-    let root = Box::new(case.predicate().map(|p| p.to_string()).unwrap_or_default());
-    CaseTreeInner::new(root, leaves)
+    let root = case
+        .predicate()
+        .map(|p| Displayable::new(&p))
+        .unwrap_or_default();
+    CaseTreeInner::new(root).with_leaves(leaves)
 }
 
 /// A `Case` rendered as a tree for display.
@@ -54,5 +59,29 @@ pub struct CaseTree(CaseTreeInner);
 impl fmt::Display for CaseTree {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
+    }
+}
+
+#[derive(Default)]
+struct Displayable {
+    primary: String,
+    alternate: String,
+}
+
+impl Displayable {
+    fn new(display: &dyn std::fmt::Display) -> Self {
+        let primary = format!("{}", display);
+        let alternate = format!("{:#}", display);
+        Self { primary, alternate }
+    }
+}
+
+impl fmt::Display for Displayable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            self.alternate.fmt(f)
+        } else {
+            self.primary.fmt(f)
+        }
     }
 }
