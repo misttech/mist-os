d47dbeee4a580dbb3f922b33881f0391819550a5
Wez <wez@google.com>
2025-07-11 02:12:52 -0700

    [sestarnix] Enforce binder { call } permission checks
    Bug: 427888888
    Depends-On: turquoise-internal:I4ee57bfa097d79bbe7fb12bc4be8fd2eafb691f8
    Change-Id: I90d19d3e7d0dca231857d202079c3be55eb38cf0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1319444
    Reviewed-by: Ambre Williams <ambre@google.com>
    Commit-Queue: Wez <wez@google.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>

--- src/starnix/kernel/security/selinux_hooks/binder.rs
+++ src/starnix/kernel/security/selinux_hooks/binder.rs
@@ -2,11 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use crate::security::selinux_hooks::{
-    check_self_permission, task_effective_sid, todo_check_permission,
-};
+use crate::security::selinux_hooks::{check_permission, check_self_permission, task_effective_sid};
 use crate::task::CurrentTask;
-use crate::TODO_DENY;
 use selinux::{BinderPermission, SecurityServer};
 use starnix_core::task::Task;
 use starnix_uapi::errors::Errno;
@@ -37,8 +34,7 @@ pub fn binder_transaction(
     let audit_context = current_task.into();
     let source_sid = task_effective_sid(current_task);
     let target_sid = target_task.security_state.lock().current_sid;
-    todo_check_permission(
-        TODO_DENY!("https://fxbug.dev/427888888", "Enforce call check."),
+    check_permission(
         &security_server.as_permission_check(),
         target_task.kernel(),
         source_sid,
