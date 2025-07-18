729dfdfc4fd260a4cc6acbd9eedcf540c125699f
Etienne J. Membrives <etiennej@google.com>
2025-07-18 02:37:10 -0700

    [starnix] Implement some missing meminfo fields
    Implement some of the missing /proc/meminfo fields. In particular, the
    Active/Inactive file values are computed in the same way as is done by
    /proc/vmstats.
    
    BUG=431958764
    
    Change-Id: I1e66cbaa1ec87cd782f73b8731e21b985e8dbe42
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1324324
    Commit-Queue: Étienne J. Membrives <etiennej@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/modules/procfs/meminfo.rs
+++ src/starnix/modules/procfs/meminfo.rs
@@ -42,17 +42,32 @@ impl DynamicFileSource for MeminfoFile {
             + memory_stats.vmo_discardable_unlocked_bytes.unwrap_or_default())
             / 1024;
 
+        let userpager_total = memory_stats.vmo_pager_total_bytes.unwrap_or_default();
+        let userpager_active = memory_stats.vmo_pager_newest_bytes.unwrap_or_default();
+        let userpager_inactive = userpager_total.saturating_sub(userpager_active);
+
+        let anonymous_total = memory_stats
+            .vmo_bytes
+            .unwrap_or_default()
+            .saturating_sub(memory_stats.vmo_pager_total_bytes.unwrap_or_default());
+
         let swap_used = compression_stats.uncompressed_storage_bytes.unwrap_or_default() / 1024;
         // Fuchsia doesn't have a limit on the size of its swap file, so we just pretend that
-        // we're willing to grow the swap by half the amount of free memory.
-        let swap_free = mem_free / 2;
+        // we're willing to grow the swap by half the amount of available memory.
+        let swap_free = mem_available / 2;
         let swap_total = swap_used + swap_free;
 
         writeln!(sink, "MemTotal:       {:8} kB", mem_total)?;
         writeln!(sink, "MemFree:        {:8} kB", mem_free)?;
         writeln!(sink, "MemAvailable:   {:8} kB", mem_available)?;
+        writeln!(sink, "Cached:         {:8} kB", userpager_total)?;
         writeln!(sink, "SwapTotal:      {:8} kB", swap_total)?;
         writeln!(sink, "SwapFree:       {:8} kB", swap_free)?;
+        writeln!(sink, "Active(anon):   {:8} kB", anonymous_total)?;
+        writeln!(sink, "Inactive(anon): {:8} kB", 0)?;
+        writeln!(sink, "Active(file):   {:8} kB", userpager_active)?;
+        writeln!(sink, "Inactive(file): {:8} kB", userpager_inactive)?;
+
         Ok(())
     }
 }
