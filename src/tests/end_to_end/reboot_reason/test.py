176d0622fd5da1c1fde3f241fcd33014cf446789
Francois Rousseau <frousseau@google.com>
2025-07-21 14:37:24 -0700

    [e2e] add helper message on failure
    * the test has already revealed bugs in driver shutdown paths
      so adding a clarifying message for each scenario
    
    Bug: 432864757
    Change-Id: I425d04ba4014fd0deefdb70f4fd171a31e8ff8e8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1327066
    Fuchsia-Auto-Submit: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Prashanth Swaminathan <prashanthsw@google.com>
    Commit-Queue: Francois Rousseau <frousseau@google.com>

--- src/tests/end_to_end/reboot_reason/test.py
+++ src/tests/end_to_end/reboot_reason/test.py
@@ -23,7 +23,32 @@ class RebootReasonTest(fuchsia_base_test.FuchsiaBaseTest):
         # USER_REQUEST.
         self.dut.reboot()
         _LOGGER.info("[test_reboot_reason] Device has rebooted successfully")
-        asserts.assert_equal(self.dut.last_reboot_reason, "USER_REQUEST")
+
+        # We always expect "USER_REQUEST", but now that we have set up the test, we are finding
+        # instances of true bugs where something goes wrong during shutdown. To help with
+        # https://fxbug.dev/432864757, we want a different message on failure depending on what
+        # the reason is to better inform whoemever is looking at the test failure.
+        if self.dut.last_reboot_reason == "ROOT_JOB_TERMINATION":
+            asserts.assert_equal(
+                self.dut.last_reboot_reason,
+                "USER_REQUEST",
+                msg=(
+                    "There was likely a driver hang during userspace shutdown. See"
+                    " serial_log.txt and https://fxbug.dev/432968401"
+                ),
+            )
+        elif self.dut.last_reboot_reason in ["COLD", "BRIEF_POWER_LOSS"]:
+            asserts.assert_equal(
+                self.dut.last_reboot_reason,
+                "USER_REQUEST",
+                msg=(
+                    "There was likely a hardware reboot due to a driver action during"
+                    " userspace shutdown. See serial_log.txt and"
+                    " https://fxbug.dev/433253369"
+                ),
+            )
+        else:
+            asserts.assert_equal(self.dut.last_reboot_reason, "USER_REQUEST")
 
 
 if __name__ == "__main__":
