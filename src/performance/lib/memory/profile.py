862fbfbdbdce1907aca21b8193b2c749b942a2d7
Chris Masone <cmasone@google.com>
2025-07-29 10:04:09 -0700

    [memory] Clean up some testing vestiges of mm1
    No longer need metrics generating code that parses memory monitor 1
    output, or the unit tests that validate that code.
    
    Bug: 381032254
    Test: fx test profile_test
    
    Depends-on: turquoise-internal:I38bd50a60bd39b8e193c5c05dff6493f197efcce
    Change-Id: I58a3f4ac566a764a8fec5a30909681c0995af15b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1331409
    Dependencies-Satisfied: CL Deps Checker <cl-deps-checker@fuchsia-internal-service-accts.iam.gserviceaccount.com>
    Reviewed-by: Mike McCreavy <mccreavy@google.com>
    Fuchsia-Auto-Submit: Chris Masone <cmasone@google.com>
    Commit-Queue: Chris Masone <cmasone@google.com>

--- src/performance/lib/memory/profile.py
+++ src/performance/lib/memory/profile.py
@@ -8,7 +8,6 @@ import json
 from typing import Any, Mapping, MutableSequence, cast
 
 from honeydew.fuchsia_device.fuchsia_device import FuchsiaDevice
-from honeydew.transports.ffx import errors as ffx_errors
 from trace_processing import trace_metrics, trace_model
 from trace_processing.trace_metrics import JSON
 
@@ -33,57 +32,29 @@ class _MemoryProfileMetrics(trace_metrics.MetricsProcessor):
     def __init__(
         self,
         principal_groups: Mapping[str, str],
-        process_groups: Mapping[str, str],
         component_profile: Any,
-        process_profile: Any,
     ):
         self._principal_groups = principal_groups
-        self._process_groups = process_groups
         self._component_profile = component_profile
-        self._process_profile = process_profile
 
     def process_metrics(
         self, model: trace_model.Model
     ) -> MutableSequence[trace_metrics.TestCaseResult]:
-        metrics = process_component_profile(
+        return process_component_profile(
             self._principal_groups, self._component_profile
         )
-
-        if self._process_profile:
-            for (
-                process_group_name,
-                process_name_pattern,
-            ) in self._process_groups.items():
-                private_populated = sum(
-                    proc["memory"]["private_populated"]
-                    for proc in self._process_profile["CompleteDigest"][
-                        "processes"
-                    ]
-                    if fnmatch.fnmatch(proc["name"], process_name_pattern)
-                )
-                metrics.append(
-                    trace_metrics.TestCaseResult(
-                        label=f"Memory/Process/{process_group_name}/PrivatePopulated",
-                        unit=trace_metrics.Unit.bytes,
-                        values=[private_populated],
-                        doc=(f"{self.DESCRIPTION_BASE}: {process_group_name}"),
-                    )
-                )
-        return metrics
 
     def process_freeform_metrics(
         self, model: trace_model.Model
     ) -> tuple[str, JSON]:
         return (
             "memory_profile",
-            _simplify_digest(self._process_profile, self._component_profile),
+            _simplify_digest(self._component_profile),
         )
 
 
 def capture(
-    dut: FuchsiaDevice,
-    principal_groups: Mapping[str, str] | None = None,
-    process_groups: Mapping[str, str] | None = None,
+    dut: FuchsiaDevice, principal_groups: Mapping[str, str]
 ) -> trace_metrics.MetricsProcessor:
     """Captures kernel and user space memory metrics using `ffx profile memory`.
 
@@ -103,7 +74,6 @@ def capture(
             groups. This is private uncompressed memory.
         - A whole-device memory digest.
     """
-    process_groups = process_groups or {}
     principal_groups = principal_groups or {}
 
     component_profile = json.loads(
@@ -119,27 +89,7 @@ def capture(
         )
     )
 
-    # If memory_monitor1 is available.
-    try:
-        process_profile = json.loads(
-            dut.ffx.run(
-                [
-                    "--machine",
-                    "json",
-                    "profile",
-                    "memory",
-                    "--buckets",
-                    "--backend",
-                    "memory_monitor_1",
-                ],
-            )
-        )
-    except ffx_errors.FfxCommandError:
-        process_profile = None
-
-    return _MemoryProfileMetrics(
-        principal_groups, process_groups, component_profile, process_profile
-    )
+    return _MemoryProfileMetrics(principal_groups, component_profile)
 
 
 def process_component_profile(
@@ -162,37 +112,6 @@ def process_component_profile(
             )
         )
     return metrics
-
-
-def _simplify_bucket(bucket: JSON) -> dict[str, JSON]:
-    """Prepares `ffx profile memory` JSON data for BigQuery.
-
-    Input sample:
-        {
-            "name": "ZBI Buffer",
-            "size": 0,
-            "vmos": []
-        }
-
-    Output sample:
-        {
-            "name": "ZBI Buffer",
-            "size_in_bytes": 0
-        }
-    """
-    if not isinstance(bucket, dict):
-        raise ValueError(f"Expects a dict but found {bucket!r}")
-    return dict(name=bucket["name"], size_in_bytes=bucket["size"])
-
-
-def _simplify_buckets(buckets: JSON) -> list[JSON]:
-    """Prepares `ffx profile memory` JSON data for BigQuery.
-
-    Turns a list of buckets into a list of simplified buckets.
-    """
-    if not isinstance(buckets, list):
-        raise ValueError(f"Expects a list but found {buckets!r}")
-    return [_simplify_bucket(b) for b in buckets]
 
 
 def _simplify_name_to_vmo_memory(name_to_vmo_memory: JSON) -> list[JSON]:
@@ -233,58 +152,6 @@ def _simplify_name_to_vmo_memory(name_to_vmo_memory: JSON) -> list[JSON]:
     ]
 
 
-def _simplify_process(process: JSON) -> dict[str, JSON]:
-    """Prepares `ffx profile memory` JSON data for BigQuery.
-
-    Input sample:
-        {
-            "koid": 37989,
-            "name": "starnix_kernel.cm",
-            "memory": {
-                "private": 124809216,
-                "private_populated": 301281280,
-                "scaled": 463242577,
-                "scaled_populated": 640658601,
-                "total": 1138892800,
-                "total_populated": 1318207488,
-                "vmos": []
-            },
-            "name_to_vmo_memory": { ... }
-        }
-
-    Output sample:
-        {
-            "name": "starnix_kernel.cm",
-            "memory": {
-                "private": 124809216,
-                "private_populated": 301281280,
-                "scaled": 463242577,
-                "scaled_populated": 640658601,
-                "total": 1138892800,
-                "total_populated": 1318207488,
-            },
-            "name_to_vmo_memory": { ... }
-        }
-    """
-    if not isinstance(process, dict):
-        raise ValueError
-    return dict(
-        name=process["name"],
-        memory=_with_vmos_removed(process["memory"]),
-        vmo_groups=_simplify_name_to_vmo_memory(process["name_to_vmo_memory"]),
-    )
-
-
-def _simplify_processes(processes: JSON) -> list[JSON]:
-    """Prepares `ffx profile memory` JSON data for BigQuery.
-
-    Turns a list of processes into a list of simplified processes.
-    """
-    if not isinstance(processes, list):
-        raise ValueError
-    return [_simplify_process(b) for b in processes]
-
-
 def _with_vmos_removed(metrics_dict: JSON) -> dict[str, JSON]:
     """Returns a copy of the specified directory without the "vmos" key."""
     if not isinstance(metrics_dict, dict):
@@ -309,15 +176,8 @@ def _simplify_principals(principals: JSON) -> list[JSON]:
     return [_simplify_principal(b) for b in principals]
 
 
-def _simplify_digest(process_profile: JSON, component_profile: JSON) -> JSON:
+def _simplify_digest(component_profile: JSON) -> JSON:
     result = {}
-
-    if isinstance(process_profile, dict):
-        digest = process_profile["CompleteDigest"]
-        if not isinstance(digest, dict):
-            raise ValueError
-        result["buckets"] = _simplify_buckets(digest["buckets"])
-        result["processes"] = _simplify_processes(digest["processes"])
 
     if isinstance(component_profile, dict):
         digest = component_profile["ComponentDigest"]
