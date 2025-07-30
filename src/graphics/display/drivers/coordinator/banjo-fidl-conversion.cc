4cc66596385ae78986c51ffcb20080c275ed3c80
Yilong Li <liyl@google.com>
2025-07-30 14:25:45 -0700

    [display][coordinator] Add Banjo to FIDL conversion for DisplayConfig.
    This change introduces helpers to convert the Banjo display_config_t to
    the FIDL fuchsia.hardware.display.engine.DisplayConfig wire type.
    
    This will be used in the transition from the Banjo-based Display
    Controller protocol to the new FIDL-based Display Engine protocol.
    
    This change is created by gemini-cli and reviewed manually.
    --PROMPT--
    Implement a function in banjo-fidl-conversion.h and
    banjo-fidl-conversion.cc in src/graphics/display/drivers/coordinator:
    
    fuchsia_hardware_display_engine::DisplayConfig ToFidlDisplayConfig(const
    display_config_t& banjo_config);
    
    The function converts banjo types defined in
    sdk/banjo/fuchsia.hardware.display.controller/display-controller.fidl to
    FIDL types defined in
    sdk/fidl/fuchsia.hardware.display.engine/engine.fidl.
    
    Implement tests to make sure fields are forwarded correctly.
    
    Run the test to make sure all tests pass before going to the next step.
    --PROMPT--
    
    Bug: 314126494
    Test: fx test //src/graphics/display
    
    Change-Id: I80b1a460d95eb5570ecf3a7e627a696bfbe17c06
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1332268
    Reviewed-by: Victor Costan <costan@google.com>
    Commit-Queue: Victor Costan <costan@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>

---
+++ src/graphics/display/drivers/coordinator/banjo-fidl-conversion.cc
@@ -0,0 +1,70 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/graphics/display/drivers/coordinator/banjo-fidl-conversion.h"
+
+#include <fidl/fuchsia.hardware.display.engine/cpp/wire.h>
+#include <fuchsia/hardware/display/controller/cpp/banjo.h>
+#include <lib/fdf/cpp/arena.h>
+
+#include <algorithm>
+#include <span>
+
+#include "src/graphics/display/lib/api-types/cpp/display-id.h"
+#include "src/graphics/display/lib/api-types/cpp/display-timing.h"
+#include "src/graphics/display/lib/api-types/cpp/driver-layer.h"
+
+namespace display_coordinator {
+
+namespace {
+
+// `banjo_preoffsets` must point to an array of size 3.
+fidl::Array<float, 3> ToFidlPreoffsets(const float* banjo_preoffsets) {
+  fidl::Array<float, 3> fidl_preoffsets;
+  std::ranges::copy(std::span(banjo_preoffsets, 3), fidl_preoffsets.begin());
+  return fidl_preoffsets;
+}
+
+// `banjo_coefficients` must point to an array of size 3, and each element
+// of `banjo_coefficients` must point to an array of size 3.
+fidl::Array<fidl::Array<float, 3>, 3> ToFidlCoefficients(const float (*banjo_coefficients)[3]) {
+  fidl::Array<fidl::Array<float, 3>, 3> fidl_coefficients;
+  for (size_t i = 0; i < 3; ++i) {
+    std::ranges::copy(std::span(banjo_coefficients[i], 3), fidl_coefficients[i].begin());
+  }
+  return fidl_coefficients;
+}
+
+// `banjo_postoffsets` must point to an array of size 3.
+fidl::Array<float, 3> ToFidlPostoffsets(const float* banjo_postoffsets) {
+  fidl::Array<float, 3> fidl_postoffsets;
+  std::ranges::copy(std::span(banjo_postoffsets, 3), fidl_postoffsets.begin());
+  return fidl_postoffsets;
+}
+
+}  // namespace
+
+fuchsia_hardware_display_engine::wire::DisplayConfig ToFidlDisplayConfig(
+    const display_config_t& banjo_config, fdf::Arena& arena) {
+  fidl::VectorView<fuchsia_hardware_display_engine::wire::Layer> layers(arena,
+                                                                        banjo_config.layers_count);
+  for (size_t i = 0; i < banjo_config.layers_count; ++i) {
+    layers[i] = display::DriverLayer(banjo_config.layers_list[i]).ToFidl();
+  }
+
+  const fuchsia_hardware_display_engine::wire::DisplayTiming timing =
+      display::ToFidlDisplayTiming(display::ToDisplayTiming(banjo_config.timing));
+
+  return fuchsia_hardware_display_engine::wire::DisplayConfig{
+      .display_id = display::DisplayId(banjo_config.display_id).ToFidl(),
+      .timing = timing,
+      .cc_flags = banjo_config.cc_flags,
+      .cc_preoffsets = ToFidlPreoffsets(banjo_config.cc_preoffsets),
+      .cc_coefficients = ToFidlCoefficients(banjo_config.cc_coefficients),
+      .cc_postoffsets = ToFidlPostoffsets(banjo_config.cc_postoffsets),
+      .layers = layers,
+  };
+}
+
+}  // namespace display_coordinator
