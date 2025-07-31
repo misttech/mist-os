496d39e09cfa7ef9bed8e95f565ad1a17fcc544e
Benjamin Lerman <qsr@google.com>
2025-07-31 03:45:37 -0700

    [starnix] Stub IORING_SETUP_COOP_TASKRUN for io_uring
    This change implements the  flag for the
     syscall. This flag is currently a no-op, but it is
    required by some applications that use io_uring.
    
    The implementation adds the flag to the list of ignored flags, so that
    it doesn't return an error when used.
    
    This is a reland of
    https://fuchsia-review.googlesource.com/c/fuchsia/+/1334364 after
    io_uring syscalls had been added for aarch32.
    
    Bug: 297431387
    Change-Id: Icb6c9cc03399aed2f4f09156d8ca577bdbdb585a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1334786
    Commit-Queue: Benjamin Lerman <qsr@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>

---
+++ src/starnix/tests/syscalls/cpp/io_uring_test.cc
@@ -0,0 +1,34 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <fbl/unique_fd.h>
+#include <gtest/gtest.h>
+#include <linux/io_uring.h>
+
+#include "src/starnix/tests/syscalls/cpp/test_helper.h"
+
+#ifndef IORING_SETUP_COOP_TASKRUN
+#define IORING_SETUP_COOP_TASKRUN (1U << 8)
+#endif
+
+namespace {
+
+int io_uring_setup(uint32_t entries, io_uring_params* params) {
+  return static_cast<int>(syscall(__NR_io_uring_setup, entries, params));
+}
+
+TEST(IoUringTest, IoUringSetupCoopTaskrun) {
+  if (!test_helper::IsStarnix() && !test_helper::IsKernelVersionAtLeast(5, 19)) {
+    GTEST_SKIP() << "Skip test for unsupported feature";
+  }
+  struct io_uring_params params = {};
+  params.flags = IORING_SETUP_COOP_TASKRUN;
+  fbl::unique_fd fd(io_uring_setup(1, &params));
+  ASSERT_TRUE(fd.is_valid()) << strerror(errno);
+}
+
+}  // namespace
