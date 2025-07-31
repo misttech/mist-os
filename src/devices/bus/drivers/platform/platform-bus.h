e5c5a28224fb30b24e0d43c346e40de4b17b46d9
Francois Rousseau <frousseau@google.com>
2025-07-31 16:48:40 -0700

    [platform-bus] temporarily add more shutdown logs
    * I am not sure how much this will help, but given that I can't
      repro the hard reset locally anymore but it still happens 1-2
      times a day on CI, I figure these extra shutdown logs can't hurt
      to see if the hard reset happens during or later
    * not polluting the code with a TODO given that the blame history
      will point to this CL that will be reverted once the investigation
      has concluded
    
    Bug: 433586825
    Change-Id: Ie4c23fdb33cadd9e44b5adf9295ce524701dd839
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1335851
    Commit-Queue: Francois Rousseau <frousseau@google.com>
    Fuchsia-Auto-Submit: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/bus/drivers/platform/platform-bus.h
+++ src/devices/bus/drivers/platform/platform-bus.h
@@ -40,6 +40,9 @@ class PlatformBus : public fdf::DriverBase,
   PlatformBus(fdf::DriverStartArgs start_args,
               fdf::UnownedSynchronizedDispatcher driver_dispatcher);
 
+  ~PlatformBus() { FDF_LOG(INFO, "~PlatformBus()"); }
+  void Stop() override { FDF_LOG(INFO, "Stop()"); }
+
   using fdf::DriverBase::dispatcher;
   using fdf::DriverBase::driver_dispatcher;
   using fdf::DriverBase::outgoing;
