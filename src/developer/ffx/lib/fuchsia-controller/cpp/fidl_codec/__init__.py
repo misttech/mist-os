f67a9cc65f4e4e9ee4a788004ec3040823c3603a
Marc Khouri <mnck@google.com>
2025-08-04 10:07:44 -0700

    [fuchsia-controller] Add type hints for CPP files to IDE integration
    We already had type stubs for these CPP files, however pyright/pylance
    in the IDE were not able to find them. This CL moves them to the top-
    level directory of the module and adds the module path to the
    pyrightconfig.json, which makes them work in the IDE.
    
    Change-Id: Id18f08635d4be31c276d864e27b6acc994a4c871
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1336445
    Reviewed-by: Charles Celerier <chcl@google.com>
    Reviewed-by: Pavan Kumar Juturu <jpavankumar@google.com>
    Fuchsia-Auto-Submit: Marc Khouri <mnck@google.com>
    Commit-Queue: Marc Khouri <mnck@google.com>

---
+++ src/developer/ffx/lib/fuchsia-controller/cpp/fidl_codec/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2025 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This file is unused during compilation. It merely exists to help the IDE integration realize
+# there's a python package with this name, and to look for the __init__.pyi file adjacent to it.
